37ee786aae961af568ab89b949e45807c3476b06
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-10-12 01:21:02 +0330

    attach QR pages added

--- packages/extension-polkagate/src/popup/attachQR/index.tsx
+++ packages/extension-polkagate/src/popup/attachQR/index.tsx
@@ -1,27 +1,18 @@
 // Copyright 2019-2022 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { ResponseJsonGetAccountInfo } from '@polkadot/extension-base/background/types';
-import type { KeyringPair$Json } from '@polkadot/keyring/types';
-import type { KeyringPairs$Json } from '@polkadot/ui-keyring/types';
-import { QrScanAddress } from '@polkadot/react-qr';
-
-import { Box, Grid, Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 
-import { u8aToString } from '@polkadot/util';
+import Name from '@polkadot/extension-ui/partials/Name';
+import { QrScanAddress } from '@polkadot/react-qr';
 
-import { AccountContext, ActionContext, InputFileWithLabel, InputWithLabel, Warning } from '../../../../extension-ui/src/components';
-import { DEFAULT_TYPE } from '../../../../extension-ui/src/util/defaultType';
-import { isKeyringPairs$Json } from '../../../../extension-ui/src/util/typeGuards';
+import { AccountContext, AccountNamePasswordCreation, ActionContext } from '../../../../extension-ui/src/components';
 import Address from '../../components/Address'
 import PButton from '../../components/PButton';
 import { useTranslation } from '../../hooks';
-import { batchRestore, jsonGetAccountInfo, jsonRestore } from '../../messaging';
-import HeaderBrand from '../../partials/HeaderBrand';
 import { createAccountExternal, createAccountSuri, createSeed } from '../../messaging';
-
-const acceptedFormats = ['application/json', 'text/plain'].join(', ');
+import HeaderBrand from '../../partials/HeaderBrand';
 
 interface Props {
   className?: string;
@@ -42,25 +33,37 @@ export default function AttachQR({ className }: Props): React.ReactElement {
   const [address, setAddress] = useState<string | null>(null);
   const [name, setName] = useState<string | null>(null);
   const [password, setPassword] = useState<string | null>(null);
+  const [genesisHash, setGenesisHash] = useState<string | null>(null);
 
   const [stepOne, setStep] = useState(true);
-  const [isPasswordError, setIsPasswordError] = useState(false);
 
   useEffect((): void => {
     !accounts.length && onAction();
   }, [accounts, onAction]);
 
-  const _onChangePass = useCallback(
-    (pass: string): void => {
-      setPassword(pass);
-      setIsPasswordError(false);
-    }, []
+  const _onCreate = useCallback(
+    (): void => {
+      if (account && name) {
+        if (account.isAddress) {
+          createAccountExternal(name, account.content, account.genesisHash)
+            .then(() => onAction('/'))
+            .catch((error: Error) => console.error(error));
+        } else if (password) {
+          createAccountSuri(name, password, account.content, 'sr25519', account.genesisHash)
+            .then(() => onAction('/'))
+            .catch((error: Error) => console.error(error));
+        }
+      }
+    },
+    [account, name, onAction, password]
   );
 
   const _setAccount = useCallback(
     (qrAccount: QrAccount) => {
       setAccount(qrAccount);
       setName(qrAccount?.name || null);
+      setGenesisHash(qrAccount.genesisHash);
+      setStep(false);
 
       if (qrAccount.isAddress) {
         setAddress(qrAccount.content);
@@ -78,6 +81,7 @@ export default function AttachQR({ className }: Props): React.ReactElement {
       onAction('/');
     } else {
       setStep(true);
+      setAccount(null);
     }
   }, [onAction, stepOne]);
 
@@ -88,63 +92,83 @@ export default function AttachQR({ className }: Props): React.ReactElement {
         showBackArrow
         text={t<string>(`Attach QR-signer (${stepOne ? 1 : 2}/2)`)}
       />
-      {isPasswordError && !stepOne &&
-        <Grid
-          color='red'
-          height='30px'
-          m='auto'
-          pt='5px'
-          width='92%'
-        >
-          <Warning
-            isBelowInput
-            isDanger
+      {stepOne &&
+        <div>
+          <Typography
+            fontSize='14px'
+            fontWeight={300}
+            m='auto'
+            pt='20px'
+            textAlign='center'
+            width='92%'
           >
-            {t<string>('You’ve used an incorrect password. Try again.')}
-          </Warning>
-        </Grid>
+            {t('Scan address QR code')}
+          </Typography>
+          <Grid
+            alignItems='center'
+            border='1px dashed'
+            borderColor='secondary.light'
+            borderRadius='5px'
+            boxSizing='border-box'
+            container
+            fontSize='16px'
+            height='328px'
+            justifyContent='center'
+            m='10px 15px'
+            sx={{ backgroundColor: 'background.paper' }}
+            width='328px'
+          >
+            {!account &&
+              <QrScanAddress
+                onScan={_setAccount}
+                size={272}
+              />
+            }
+          </Grid>
+          <Typography
+            fontSize='14px'
+            fontWeight={300}
+            m='auto'
+            pt='20px'
+            textAlign='center'
+            width='92%'
+          >
+            {t('Hold the QR code in front of the device’s camera')}
+          </Typography>
+        </div>
       }
-      <Typography
-        fontSize='14px'
-        fontWeight={300}
-        m='auto'
-        pt='20px'
-        textAlign='center'
-        width='92%'
-      >
-        {t('Scan address QR code')}
-      </Typography>
-      <Grid
-        alignItems='center'
-        border='1px dashed'
-        borderColor='secondary.light'
-        borderRadius='5px'
-        boxSizing='border-box'
-        container
-        fontSize='16px'
-        height='328px'
-        justifyContent='center'
-        m='10px 15px'
-        sx={{ backgroundColor: 'background.paper' }}
-        width='328px'
-      >
-        {!account &&
-          <QrScanAddress
-            onScan={_setAccount}
-            size={272}
-          />
-        }
-      </Grid>
-      <Typography
-        fontSize='14px'
-        fontWeight={300}
-        m='auto'
-        pt='20px'
-        textAlign='center'
-        width='92%'
-      >
-        {t('Hold the QR code in front of the device’s camera')}
-      </Typography>
+      {!stepOne && account &&
+        <Address
+          address={address}
+          name={name}
+        />
+      }
+      {!stepOne && account &&
+        (
+          account?.isAddress
+            ? (
+              <div>
+                <Name
+                  isFocused
+                  onChange={setName}
+                  value={name || ''}
+                />
+                <PButton
+                  _onClick={_onCreate}
+                  _variant='contained'
+                  disabled={!name}
+                  text={t<string>('Add account')}
+                />
+              </div>
+            )
+            : (
+              <AccountNamePasswordCreation
+                buttonLabel={t<string>('Add account')}
+                onCreate={_onCreate}
+                onNameChange={setName}
+                onPasswordChange={setPassword}
+              />)
+        )}
     </>
   );
 }
