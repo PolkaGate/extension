cbca0f82d73f9009534e782dfef003dc9ceb4eb4
Kami <ekbatanifard@gmail.com>
2022-10-15 02:41:18 +0330

    Update getParentNameSuri.ts

--- packages/extension-polkagate/src/util/getParentNameSuri.ts
+++ packages/extension-polkagate/src/util/getParentNameSuri.ts
@@ -1,6 +1,6 @@
-// Copyright 2019-2022 @polkadot/extension-ui authors & contributors
+// Copyright 2019-2022 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 export default function (parentName?: string | null, suri?: string): string {
-  return `${parentName || ''}  ${suri || ''}`;
+  return `${parentName || ''}${suri || ''}`;
 }
