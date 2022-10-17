5dffc2445ba21659afd8678e544368ad02875195
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-10-17 15:55:44 +0330

    Create InputWithLabelAndIdenticon.tsx

---
+++ packages/extension-polkagate/src/components/InputWithLabelAndIdenticon.tsx
@@ -0,0 +1,132 @@
+// Copyright 2019-2022 @polkadot/extension-ui authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { faPaste } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Grid, IconButton, SxProps, Theme, useTheme } from '@mui/material';
+import React, { useCallback, useState } from 'react';
+
+import { Chain } from '@polkadot/extension-chains/types';
+
+import isValidAddress from '../util/validateAddress';
+import Identicon from './Identicon';
+import Label from './Label';
+import { Input } from './TextInputs';
+
+interface Props {
+  label: string;
+  onChange?: (value: string) => void;
+  style?: SxProps<Theme>;
+  chain?: Chain;
+}
+
+export default function InputWithLabelAndIdenticon({ chain = undefined, onChange, label, style }: Props): React.ReactElement<Props> {
+  const [offFocus, setOffFocus] = useState(false);
+  const [address, setAddress] = useState<string | null | undefined>();
+  const [pasteValue, setPasteValue] = useState<string | undefined>();
+  const theme = useTheme();
+
+  const handleAddress = useCallback(({ target: { value } }: React.ChangeEvent<HTMLInputElement>): void => {
+    if (!value) {
+      setAddress(undefined);
+
+      return;
+    }
+
+    if (isValidAddress(value)) {
+      setAddress(value);
+    } else {
+      setAddress(null);
+    }
+  }, [setAddress]);
+
+  const _setOffFocus = useCallback(() => {
+    setOffFocus(true);
+  }, []);
+
+  const pasteAddress = useCallback(async () => {
+    const val = await navigator.clipboard.readText();
+
+    setPasteValue(val);
+    // handleAddress({ target: { value: val } });
+  }, []);
+
+  return (
+    <Grid
+      alignItems='flex-end'
+      container
+      justifyContent='space-between'
+      sx={{ ...style }}
+    >
+      <Grid
+        item
+        xs={10.5}
+      >
+        <Label
+          label={label}
+          style={{ position: 'relative' }}
+        >
+          <Input
+            autoCapitalize='off'
+            autoCorrect='off'
+            onBlur={_setOffFocus}
+            onChange={handleAddress}
+            style={{
+              borderColor: address !== undefined && !isValidAddress(address) ? theme.palette.warning.main : theme.palette.secondary.light,
+              borderWidth: address !== undefined && !isValidAddress(address) ? '3px' : '1px',
+              fontSize: '18px',
+              fontWeight: 300,
+              padding: 0,
+              paddingLeft: '10px'
+            }}
+            theme={theme}
+            type='text'
+            property={pasteValue}
+            withError={offFocus && address !== undefined && !isValidAddress(address)}
+          />
+          <IconButton
+            onClick={pasteAddress}
+            sx={{
+              bottom: '0',
+              position: 'absolute',
+              right: '0'
+            }}
+          >
+            <FontAwesomeIcon
+              color={theme.palette.secondary.light}
+              fontSize='15px'
+              icon={faPaste}
+            />
+          </IconButton>
+        </Label>
+      </Grid>
+      <Grid
+        item
+        xs={1.2}
+      >
+        {isValidAddress(address)
+          ? (
+            <Identicon
+              prefix={chain?.ss58Format ?? 42}
+              size={31}
+              theme={chain?.icon || 'polkadot'}
+              value={address}
+            />
+          )
+          : (
+            <Grid
+              sx={{
+                bgcolor: 'action.disabledBackground',
+                border: '1px solid',
+                borderColor: 'secondary.light',
+                borderRadius: '50%',
+                height: '31px',
+                width: '31px'
+              }}
+            >
+            </Grid>)
+        }
+      </Grid>
+    </Grid>
+  );
+}
