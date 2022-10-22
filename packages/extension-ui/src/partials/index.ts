91c9026674e56dfa0ce2b2f6fd1e5cecf9830d64
Kami <ekbatanifard@gmail.com>
2022-10-22 14:07:32 +0330

    clean -ui

--- packages/extension-ui/src/partials/index.ts
+++ packages/extension-ui/src/partials/index.ts
@@ -1,9 +1,6 @@
 // Copyright 2019-2022 @polkadot/extension-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export { default as Header } from './Header';
-export { default as PHeader } from './PHeader';
 export { default as MenuSettings } from './MenuSettings';
 export { default as Name } from '../../../extension-polkagate/src/partials/Name';
 export { default as Password } from '../../../extension-polkagate/src/partials/Password';
-export { default as HeaderWithSteps } from './HeaderWithSteps';
