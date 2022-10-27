4df19a69ae66c7ba352aa42e87daff7871b50778
Kami <ekbatanifard@gmail.com>
2022-10-28 01:58:31 +0330

    Create useRedirectOnRefresh.ts

---
+++ packages/extension-polkagate/src/hooks/useRedirectOnRefresh.ts
@@ -0,0 +1,19 @@
+// Copyright 2019-2022 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function useRedirectOnRefresh(path: string | undefined): void {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (window.performance) {
+      if (performance.navigation.type === 1) {
+        history.push({
+          pathname: path
+        });
+      }
+    }
+  }, [path, history]);
+}
