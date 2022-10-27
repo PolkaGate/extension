bcc45b839506c90da89f227096f61c17ca6ae19c
Kami <ekbatanifard@gmail.com>
2022-10-27 20:30:24 +0330

    Update useProxies.ts

--- packages/extension-polkagate/src/hooks/useProxies.ts
+++ packages/extension-polkagate/src/hooks/useProxies.ts
@@ -1,19 +1,44 @@
 // Copyright 2019-2022 @polkadot/extension-plus authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useEffect, useState } from 'react';
+/** 
+ * @description
+ * this hook returns a proxied proxies in non formatted style 
+ * */
+
+import React, { useContext, useEffect, useState } from 'react';
 
 import { ApiPromise } from '@polkadot/api';
 
+import { AccountContext } from '../components';
 import { Proxy } from '../util/types';
+import { getSubstrateAddress } from '../util/utils';
 
-export default function useProxies(api: ApiPromise | undefined, proxiedAddress: string | undefined | null): Proxy[] | undefined {
+export default function useProxies(api: ApiPromise | undefined, proxiedAddress: string | undefined | null, onlyAvailable = false): Proxy[] | undefined {
   const [proxies, setProxies] = useState<Proxy[] | undefined>();
+  const [proxiesWithAvailability, setProxiesWithAvailability] = useState<Proxy[] | undefined>();
+  const { accounts } = useContext(AccountContext);
 
   useEffect(() => {
     proxiedAddress && api && api.query.proxy?.proxies(proxiedAddress)
       .then((p) => setProxies(JSON.parse(JSON.stringify(p[0]))) as unknown as Proxy[]);
   }, [api, proxiedAddress]);
 
-  return proxies;
+  useEffect(() => {
+    if (proxies && accounts && onlyAvailable) {
+      const temp = proxies.filter((proxy) => {
+        const found = accounts.find((acc) => acc.address === getSubstrateAddress(proxy.delegate));
+
+        return !!found;
+      });
+
+      setProxiesWithAvailability(temp);
+    }
+  }, [accounts, onlyAvailable, proxies]);
+
+  if (!onlyAvailable) {
+    return proxies;
+  } else {
+    return proxiesWithAvailability;
+  }
 }
