39830a690b740ba5a06898ac3e69461a334b4fbd
Kami <ekbatanifard@gmail.com>
2022-10-28 19:01:57 +0330

    Update dark.ts

--- packages/extension-polkagate/src/themes/dark.ts
+++ packages/extension-polkagate/src/themes/dark.ts
@@ -13,7 +13,7 @@ export const darkTheme: ThemeOptions = {
   palette: {
     mode: 'dark',
     primary: { main: '#99004F' },
-    secondary: { main: '#99004F', light: '#BA2882' },
+    secondary: { main: '#BA2882', light: '#BA2682' },
     background: { default: '#180710', paper: '#000000' },
     text: { primary: '#FFFFFF', secondary: '#000000', disabled: '#4B4B4B' },
     action: { disabled: '#fff', disabledBackground: '#4B4B4B', focus: '#BA82A5' },
