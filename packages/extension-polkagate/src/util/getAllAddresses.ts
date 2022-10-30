60789323e175c668e5fd0202d6306071becbc642
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-10-30 12:52:07 +0330

    Update getAllAddresses.ts

--- packages/extension-polkagate/src/util/getAllAddresses.ts
+++ packages/extension-polkagate/src/util/getAllAddresses.ts
@@ -4,18 +4,18 @@
 import { AccountWithChildren } from '@polkadot/extension-base/background/types';
 import { canDerive } from '@polkadot/extension-base/utils';
 
-import getFormatted from './getFormatted';
+import { getFormattedAddress } from './utils';
 
-export default function getAllAddressess(hierarchy: AccountWithChildren[], formatted = false, formatType: number | undefined, ignore: string | undefined): [string, string | null, string | undefined][] {
+export default function getAllAddressess(hierarchy: AccountWithChildren[], filterExternal = true, formatted = false, formatType: number | undefined, ignoredAddress: string | undefined): [string, string | null, string | undefined][] {
   const allAddresses = hierarchy
-    .filter(({ isExternal }) => !isExternal)
+    .filter(({ isExternal }) => filterExternal ? !isExternal : isExternal || !isExternal)
     .filter(({ type }) => canDerive(type))
     .map(({ address, genesisHash, name }): [string, string | null, string | undefined] => {
-      return ([formatted ? getFormatted(address, formatType) : address, genesisHash || null, name]);
+      return ([formatted ? getFormattedAddress(address, undefined, formatType) : address, genesisHash || null, name]);
     });
 
-  if (ignore) {
-    return allAddresses.filter((item) => item[0] !== ignore);
+  if (ignoredAddress) {
+    return allAddresses.filter((item) => item[0] !== ignoredAddress);
   }
 
   return allAddresses;
