51dd6c23ade2def34c779dfedb2e24e96bdb00ec
Kami <ekbatanifard@gmail.com>
2022-11-10 02:35:34 +0330

    Update BalanceFee.tsx

--- packages/extension-polkagate/src/popup/send/partial/BalanceFee.tsx
+++ packages/extension-polkagate/src/popup/send/partial/BalanceFee.tsx
@@ -15,6 +15,8 @@ import type { Balance } from '@polkadot/types/interfaces';
 import { Grid } from '@mui/material';
 import React from 'react';
 
+import { BN } from '@polkadot/util';
+
 import { ShowBalance } from '../../../components';
 import { getValue } from '../../account/util';
 
@@ -23,10 +25,12 @@ interface Props {
   balances: DeriveBalancesAll | null | undefined;
   fee: Balance | undefined;
   api: ApiPromise | undefined;
+  balance?: BN | undefined
+
 }
 
-export default function BalanceFee({ api, balances, fee, type }: Props): React.ReactElement<Props> {
-  const value = getValue(type, balances);
+export default function BalanceFee({ api, balance, balances, fee, type }: Props): React.ReactElement<Props> {
+  const value = balance ?? getValue(type, balances);
 
   return (
     <Grid
