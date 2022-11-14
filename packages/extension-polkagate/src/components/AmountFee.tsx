ac9b01f6d3723896c4e67be798894a869dcadf8a
Kami <ekbatanifard@gmail.com>
2022-11-14 13:02:28 +0330

    Update AmountFee.tsx

--- packages/extension-polkagate/src/components/AmountFee.tsx
+++ packages/extension-polkagate/src/components/AmountFee.tsx
@@ -3,20 +3,20 @@
 
 /* eslint-disable react/jsx-max-props-per-line */
 
+import type { Balance } from '@polkadot/types/interfaces';
+
 import { Divider, Grid, SxProps, Theme } from '@mui/material';
 import React from 'react';
 
-import { BN } from '@polkadot/util';
-
-import { useApi, useChain, useTranslation } from '../hooks';
-import { ChainLogo, FormatBalance } from '.';
+import { useChain, useTranslation } from '../hooks';
+import { ChainLogo } from '.';
 
 interface Props {
   address: string;
   amount: string | Element;
   children?: React.ReactNode;
   label: string;
-  fee?: BN | undefined;
+  fee?: Balance | undefined;
   style?: SxProps<Theme> | undefined;
   token?: string;
   showDivider?: boolean;
@@ -25,14 +25,13 @@ interface Props {
 function AmountFee({ address, amount, children, fee, label, style = {}, showDivider = false, token }: Props): React.ReactElement {
   const { t } = useTranslation();
   const chain = useChain(address);
-  const api = useApi(address);
 
   return (
     <Grid alignItems='center' container direction='column' justifyContent='center' sx={{ fontWeight: 300, letterSpacing: '-0.015em', ...style }}>
       <Grid item sx={{ fontSize: '16px' }}>
         {label}
       </Grid>
-      <Grid alignItems='center' container item justifyContent='center' sx={{ lineHeight: '28px', pt: '5px' }}>
+      <Grid alignItems='center' container item justifyContent='center' sx={{ lineHeight: '28px' }}>
         <Grid alignItems='center' container item justifyContent='center' sx={{ fontSize: '28px', fontWeight: 400 }}>
           <Grid item>
             <ChainLogo genesisHash={chain?.genesisHash} size={31} />
@@ -42,12 +41,12 @@ function AmountFee({ address, amount, children, fee, label, style = {}, showDivi
           </Grid>
         </Grid>
         {fee &&
-          <Grid alignItems='center' container item justifyContent='center' >
+          <Grid alignItems='center' container item justifyContent='center'>
             <Grid item>
               {t('Fee')}:
             </Grid>
             <Grid item sx={{ pl: '5px' }}>
-              {api && <FormatBalance api={api} value={fee} />}
+              {fee?.toHuman()}
             </Grid>
           </Grid>
         }
