cae2c148b299d394b2c82ff868c5d2243083737a
Kami <ekbatanifard@gmail.com>
2022-11-15 16:06:53 +0330

    Update Stake.tsx

--- packages/extension-polkagate/src/popup/stake/pool/rewards/Stake.tsx
+++ packages/extension-polkagate/src/popup/stake/pool/rewards/Stake.tsx
@@ -125,9 +125,7 @@ export default function RewardsStakeReview({ address, api, chain, claimable, for
       history.push(info);
       setTxInfo({ ...info, api, chain });
 
-
-      // eslint-disable-next-line no-void
-      void saveHistory(chain, hierarchy, formatted, history);
+      saveHistory(chain, hierarchy, formatted, history);
 
       setShowWaitScreen(false);
       setShowConfirmation(true);
