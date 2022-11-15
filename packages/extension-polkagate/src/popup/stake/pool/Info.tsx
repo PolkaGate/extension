f13bd1c1f2d3c462e2d7b40cafd856194a97bec5
Kami <ekbatanifard@gmail.com>
2022-11-16 00:35:15 +0330

    Update Info.tsx

--- packages/extension-polkagate/src/popup/stake/pool/Info.tsx
+++ packages/extension-polkagate/src/popup/stake/pool/Info.tsx
@@ -1,32 +1,21 @@
 // Copyright 2019-2022 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable react/jsx-max-props-per-line */
+
 import '@vaadin/icons';
 
 import type { ApiPromise } from '@polkadot/api';
-import type { DeriveAccountRegistration, DeriveBalancesAll } from '@polkadot/api-derive/types';
-import type { Option, StorageKey } from '@polkadot/types';
-import type { AccountId32 } from '@polkadot/types/interfaces';
-import type { AccountsBalanceType, MembersMapEntry, MyPoolInfo, NominatorInfo, PoolInfo, PoolStakingConsts, SavedMetaData, StakingConsts, Validators } from '../../../util/types';
+import type { PoolStakingConsts } from '../../../util/types';
 
-import { faHistory, faMinusCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { ArrowForwardIos as ArrowForwardIosIcon } from '@mui/icons-material';
-import { Container, Divider, Grid, IconButton, MenuItem, Typography, useTheme } from '@mui/material';
-import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
-import { useParams } from 'react-router';
-import { useLocation } from 'react-router-dom';
+import { Container, Divider, Grid } from '@mui/material';
+import React, { useCallback, useContext } from 'react';
 
-import { DeriveAccountInfo, DeriveStakingQuery } from '@polkadot/api-derive/types';
-import { Chain } from '@polkadot/extension-chains/types';
-import { BN, BN_ZERO, bnMax } from '@polkadot/util';
+import { BN } from '@polkadot/util';
 
-import { ActionContext, FormatBalance, PButton, Popup, ShowBalance, ShowValue } from '../../../components';
-import { useApi, useEndpoint, useMapEntries, useMetadata, useTranslation } from '../../../hooks';
-import { updateMeta } from '../../../messaging';
+import { ActionContext, Popup, ShowBalance, ShowValue } from '../../../components';
+import { useTranslation } from '../../../hooks';
 import { HeaderBrand } from '../../../partials';
-import { getSubstrateAddress, prepareMetaData } from '../../../util/utils';
-import { getValue } from '../../account/util';
 
 interface Props {
   api: ApiPromise | undefined;
@@ -60,7 +49,7 @@ export default function Info({ api, info, setShowInfo, showInfo }: Props): React
   const Row = ({ label, showDivider = true, value }: { label: string, value: BN | undefined, showDivider?: boolean }) => {
     return (
       <>
-        <Grid alignItems='center' p='5px 15px' container justifyContent='space-between'>
+        <Grid alignItems='center' container justifyContent='space-between' p='5px 15px'>
           <Grid item sx={{ fontSize: '16px', fontWeight: 300, letterSpacing: '-0.015em' }}>
             {label}
           </Grid>
@@ -72,7 +61,7 @@ export default function Info({ api, info, setShowInfo, showInfo }: Props): React
           </Grid>
         </Grid>
         {showDivider &&
-          <Grid item container justifyContent='center' xs={12}>
+          <Grid container item justifyContent='center' xs={12}>
             <Divider sx={{ bgcolor: 'secondary.main', m: '1px auto', width: '90%' }} />
           </Grid>
         }
@@ -83,7 +72,7 @@ export default function Info({ api, info, setShowInfo, showInfo }: Props): React
   const TextRow = ({ label, showDivider = true, text }: { label: string, text: string, showDivider?: boolean }) => {
     return (
       <>
-        <Grid p='5px 15px' container sx={{ fontSize: '16px', letterSpacing: '-0.015em' }}>
+        <Grid container p='5px 15px' sx={{ fontSize: '16px', letterSpacing: '-0.015em' }}>
           <Grid item sx={{ fontWeight: 300 }}>
             {label}
           </Grid>
@@ -92,7 +81,7 @@ export default function Info({ api, info, setShowInfo, showInfo }: Props): React
           </Grid>
         </Grid>
         {showDivider &&
-          <Grid item container justifyContent='center' xs={12}>
+          <Grid container item justifyContent='center' xs={12}>
             <Divider sx={{ bgcolor: 'secondary.main', m: '1px auto', width: '90%' }} />
           </Grid>
         }
@@ -112,7 +101,7 @@ export default function Info({ api, info, setShowInfo, showInfo }: Props): React
       <SubTitle title={t('Information')} />
       <Container
         disableGutters
-        sx={{ pt: '5px' }}
+        sx={{ pt: '15px' }}
       >
         <Row label={t('Min {{token}} to join a pool', { replace: { token } })} value={info?.minJoinBond} />
         <Row label={t('Min {{token}} to create a pool', { replace: { token } })} value={info?.minCreationBond} />
