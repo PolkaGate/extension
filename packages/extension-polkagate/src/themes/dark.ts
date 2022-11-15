8c501d399c400ab880424c5fe2ac6cca27f7d6b3
Kami <ekbatanifard@gmail.com>
2022-11-16 00:35:23 +0330

    update green

--- packages/extension-polkagate/src/themes/dark.ts
+++ packages/extension-polkagate/src/themes/dark.ts
@@ -17,7 +17,7 @@ export const darkTheme: ThemeOptions = {
     background: { default: '#180710', paper: '#000000' },
     text: { primary: '#FFFFFF', secondary: '#000000', disabled: '#4B4B4B' },
     action: { disabled: '#fff', disabledBackground: '#4B4B4B', focus: '#BA82A5' },
-    success: { main: '#1F7720' },
+    success: { main: '#46890C' },
     warning: { main: '#FF002B' }
   },
   components: {
