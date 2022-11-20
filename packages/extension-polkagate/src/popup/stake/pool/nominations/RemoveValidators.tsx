ccc6f402fc938fec6ec82d2d0a128924d053169f
Kami <ekbatanifard@gmail.com>
2022-11-20 15:13:45 +0330

    add remove validators

---
+++ packages/extension-polkagate/src/popup/stake/pool/nominations/RemoveValidators.tsx
@@ -0,0 +1,237 @@
+// Copyright 2019-2022 @polkadot/extension-polkadot authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/* eslint-disable react/jsx-max-props-per-line */
+
+/**
+ * @description
+ * this component opens unstake review page
+ * */
+
+import { Divider, Grid, Typography, useTheme } from '@mui/material';
+import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+
+import { ApiPromise } from '@polkadot/api';
+import { AccountWithChildren } from '@polkadot/extension-base/background/types';
+import { Chain } from '@polkadot/extension-chains/types';
+import { Balance } from '@polkadot/types/interfaces';
+import keyring from '@polkadot/ui-keyring';
+import { BN } from '@polkadot/util';
+
+import { AccountContext, ActionContext, Motion, PasswordWithUseProxy, PButton, Popup, ShowValue, Warning } from '../../../../components';
+import { useAccountName, useProxies, useTranslation } from '../../../../hooks';
+import { updateMeta } from '../../../../messaging';
+import { HeaderBrand, SubTitle, WaitScreen } from '../../../../partials';
+import Confirmation from '../../../../partials/Confirmation';
+import broadcast from '../../../../util/api/broadcast';
+import { Proxy, ProxyItem, TransactionDetail, TxInfo } from '../../../../util/types';
+import { getSubstrateAddress, getTransactionHistoryFromLocalStorage, prepareMetaData } from '../../../../util/utils';
+import TxDetail from './partials/TxDetail';
+
+interface Props {
+  address: string;
+  api: ApiPromise;
+  chain: Chain | null;
+  formatted: string;
+  title: string;
+  poolId: BN | undefined;
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+  show: boolean;
+}
+
+export default function RemoveValidators({ address, api, chain, formatted, poolId, setShow, show, title }: Props): React.ReactElement {
+  const { t } = useTranslation();
+  const proxies = useProxies(api, formatted);
+  const name = useAccountName(address);
+  const theme = useTheme();
+  const onAction = useContext(ActionContext);
+  const { accounts, hierarchy } = useContext(AccountContext);
+  const [password, setPassword] = useState<string | undefined>();
+  const [isPasswordError, setIsPasswordError] = useState(false);
+  const [selectedProxy, setSelectedProxy] = useState<Proxy | undefined>();
+  const [proxyItems, setProxyItems] = useState<ProxyItem[]>();
+  const [txInfo, setTxInfo] = useState<TxInfo | undefined>();
+  const [showWaitScreen, setShowWaitScreen] = useState<boolean>(false);
+  const [showConfirmation, setShowConfirmation] = useState<boolean>(false);
+  const [estimatedFee, setEstimatedFee] = useState<Balance>();
+
+  const chilled = api.tx.nominationPools.chill;
+  const params = useMemo(() => [poolId], [poolId]);
+
+  const selectedProxyAddress = selectedProxy?.delegate as unknown as string;
+  const selectedProxyName = useMemo(() => accounts?.find((a) => a.address === getSubstrateAddress(selectedProxyAddress))?.name, [accounts, selectedProxyAddress]);
+
+  function saveHistory(chain: Chain, hierarchy: AccountWithChildren[], address: string, history: TransactionDetail[]) {
+    if (!history.length) {
+      return;
+    }
+
+    const accountSubstrateAddress = getSubstrateAddress(address);
+
+    if (!accountSubstrateAddress) {
+      return; // should not happen !
+    }
+
+    const savedHistory: TransactionDetail[] = getTransactionHistoryFromLocalStorage(chain, hierarchy, accountSubstrateAddress);
+
+    savedHistory.push(...history);
+
+    updateMeta(accountSubstrateAddress, prepareMetaData(chain, 'history', savedHistory)).catch(console.error);
+  }
+
+  const goToStakingHome = useCallback(() => {
+    setShow(false);
+
+    onAction(`/pool/${address}`);
+  }, [address, onAction, setShow]);
+
+  const goToMyAccounts = useCallback(() => {
+    setShow(false);
+
+    onAction('/');
+  }, [onAction, setShow]);
+
+  useEffect((): void => {
+    const fetchedProxyItems = proxies?.map((p: Proxy) => ({ proxy: p, status: 'current' })) as ProxyItem[];
+
+    setProxyItems(fetchedProxyItems);
+  }, [proxies]);
+
+  useEffect((): void => {
+    chilled(...params).paymentInfo(formatted).then((i) => setEstimatedFee(i?.partialFee)).catch(console.error);
+  }, [chilled, formatted, params, poolId]);
+
+  const remove = useCallback(async () => {
+    const history: TransactionDetail[] = []; /** collects all records to save in the local history at the end */
+
+    try {
+      if (!formatted || !chilled) {
+        return;
+      }
+
+      const signer = keyring.getPair(selectedProxyAddress ?? formatted);
+
+      signer.unlock(password);
+      setShowWaitScreen(true);
+
+      const { block, failureText, fee, status, txHash } = await broadcast(api, chilled, params, signer, formatted, selectedProxy);
+
+      const info = {
+        action: 'pool_remove_validators',
+        // amount,
+        block,
+        date: Date.now(),
+        failureText,
+        fee: fee || String(estimatedFee),
+        from: { address: formatted, name },
+        status,
+        throughProxy: selectedProxyAddress ? { address: selectedProxyAddress, name: selectedProxyName } : undefined,
+        txHash
+      };
+
+      history.push(info);
+      setTxInfo({ ...info, api, chain });
+
+      // eslint-disable-next-line no-void
+      void saveHistory(chain, hierarchy, formatted, history);
+
+      setShowWaitScreen(false);
+      setShowConfirmation(true);
+    } catch (e) {
+      console.log('error:', e);
+      setIsPasswordError(true);
+    }
+  }, [api, chain, chilled, estimatedFee, formatted, hierarchy, name, params, password, selectedProxy, selectedProxyAddress, selectedProxyName]);
+
+  const _onBackClick = useCallback(() => {
+    setShow(false);
+  }, [setShow]);
+
+  return (
+    <Motion>
+      <Popup show={show}>
+        <HeaderBrand
+          onBackClick={_onBackClick}
+          shortBorder
+          showBackArrow
+          showClose
+          text={title}
+        />
+        {isPasswordError &&
+          <Grid
+            color='red'
+            height='30px'
+            m='auto'
+            mt='-10px'
+            width='92%'
+          >
+            <Warning
+              fontWeight={400}
+              isBelowInput
+              isDanger
+              theme={theme}
+            >
+              {t<string>('You’ve used an incorrect password. Try again.')}
+            </Warning>
+          </Grid>
+        }
+        <SubTitle label={t('Review')} />
+        <Grid container sx={{ p: '50px 35px', justifyContent: 'center' }}>
+          <Typography fontSize='18px' fontWeight={400} pb='33px' textAlign='center'>
+            {t('There will be no selected validators and you will not get any rewards after.')}
+          </Typography>
+          <Divider sx={{ bgcolor: 'secondary.main', height: '2px', width: '240px' }} />
+          <Grid alignItems='center' container item justifyContent='center' pt='10px'>
+            <Grid item>
+              {t('Fee')}:
+            </Grid>
+            <Grid item sx={{ pl: '5px' }}>
+              <ShowValue value={estimatedFee?.toHuman()} height={16} />
+            </Grid>
+          </Grid>
+        </Grid>
+        <PasswordWithUseProxy
+          api={api}
+          genesisHash={chain?.genesisHash}
+          isPasswordError={isPasswordError}
+          label={`${t<string>('Password')} for ${selectedProxyName || name}`}
+          onChange={setPassword}
+          proxiedAddress={formatted}
+          proxies={proxyItems}
+          proxyTypeFilter={['Any']}
+          selectedProxy={selectedProxy}
+          setIsPasswordError={setIsPasswordError}
+          setSelectedProxy={setSelectedProxy}
+          style={{
+            bottom: '80px',
+            left: '4%',
+            position: 'absolute',
+            width: '92%'
+          }}
+        />
+        <PButton
+          _onClick={remove}
+          disabled={!password}
+          text={t<string>('Confirm')}
+        />
+        <WaitScreen
+          show={showWaitScreen}
+          title={title}
+        />
+        {txInfo && (
+          <Confirmation
+            headerTitle={title}
+            onPrimaryBtnClick={goToStakingHome}
+            onSecondaryBtnClick={goToMyAccounts}
+            primaryBtnText={t('Staking Home')}
+            secondaryBtnText={t('My Accounts')}
+            showConfirmation={showConfirmation}
+            txInfo={txInfo}
+          >
+            <TxDetail txInfo={txInfo} />
+          </Confirmation>)
+        }
+      </Popup>
+    </Motion>
+  );
+}
--- packages/extension-polkagate/src/popup/stake/pool/nominations/partials/ValidatorsTable.tsx
+++ packages/extension-polkagate/src/popup/stake/pool/nominations/partials/ValidatorsTable.tsx
@@ -9,15 +9,16 @@ import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { DirectionsRun as DirectionsRunIcon, MoreVert as MoreVertIcon } from '@mui/icons-material/';
 import { Divider, Grid, SxProps, Theme, Tooltip } from '@mui/material';
-import React, { useMemo, useRef } from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 
 import { ApiPromise } from '@polkadot/api';
 import { DeriveStakingQuery } from '@polkadot/api-derive/types';
 import { Chain } from '@polkadot/extension-chains/types';
+import { BN } from '@polkadot/util';
 
 import { Identity, ShowBalance } from '../../../../../components';
 import { useTranslation } from '../../../../../hooks';
-import { AllValidators, ValidatorInfo } from '../../../../../util/types';
+import { AllValidators, StakingConsts, ValidatorInfo } from '../../../../../util/types';
 
 interface Props {
   api?: ApiPromise;
@@ -26,9 +27,11 @@ interface Props {
   stashId: string | undefined;
   selectedValidatorsId: AccountId[] | null | undefined
   style?: SxProps<Theme> | undefined;
+  staked: BN | undefined
+  stakingConsts: StakingConsts | null | undefined
 }
 
-export default function ValidatorsTable({ allValidatorsInfo, api, chain, selectedValidatorsId, stashId, style }: Props): React.ReactElement {
+export default function ValidatorsTable({ allValidatorsInfo, api, chain, selectedValidatorsId, staked, stakingConsts, stashId, style }: Props): React.ReactElement {
   const { t } = useTranslation();
   const ref = useRef();
 
@@ -39,6 +42,21 @@ export default function ValidatorsTable({ allValidatorsInfo, api, chain, selecte
     , [allValidatorsInfo, selectedValidatorsId]);
 
   const activeValidators = useMemo(() => selectedValidatorsInfo?.filter((sv) => sv.exposure.others.find(({ who }) => who.toString() === stashId)), [stashId, selectedValidatorsInfo]);
+
+  const overSubscribed = useCallback((v: ValidatorInfo): { notSafe: boolean, safe: boolean } | undefined => {
+    if (!stakingConsts) {
+      return;
+    }
+
+    const threshold = stakingConsts.maxNominatorRewardedPerValidator;
+    const sortedNominators = v.exposure.others.sort((a, b) => b.value - a.value);
+    const maybeMyIndex = staked ? sortedNominators.findIndex((n) => n.value < staked.toNumber()) : -1;
+
+    return {
+      notSafe: v.exposure.others.length > threshold && (maybeMyIndex > threshold || maybeMyIndex === -1),
+      safe: v.exposure.others.length > threshold && (maybeMyIndex < threshold || maybeMyIndex === -1)
+    };
+  }, [staked, stakingConsts]);
 
   /** put active validators at the top of the list **/
   React.useMemo(() => {
@@ -86,6 +104,7 @@ export default function ValidatorsTable({ allValidatorsInfo, api, chain, selecte
       <Grid container direction='column' sx={{ scrollBehavior: 'smooth', '&::-webkit-scrollbar': { display: 'none', width: 0 }, '> div:not(:last-child))': { borderBottom: '1px solid', borderBottomColor: 'secondary.light' }, bgcolor: 'background.paper', border: '1px solid', borderColor: 'secondary.light', borderRadius: '5px', display: 'block', maxHeight: window.innerHeight - 190, minHeight: '59px', overflowY: 'scroll', scrollbarWidth: 'none', textAlign: 'center' }}>
         {selectedValidatorsInfo?.map((v: ValidatorInfo, index: number) => {
           const isActive = activeValidators?.find((av) => v.accountId === av?.accountId);
+          const isOversubscribed = overSubscribed(v);
 
           return (
             <Grid container item key={index} sx={{
@@ -95,12 +114,12 @@ export default function ValidatorsTable({ allValidatorsInfo, api, chain, selecte
               // opacity: unableToJoinPools(pool) ? 0.7 : 1
             }}
             >
-              <Grid container direction='column' item p='3px 8px' sx={{ borderRight: '1px solid', borderRightColor: 'secondary.main' }} width='92%'>
+              <Grid container direction='column' item p='3px 5px' sx={{ borderRight: '1px solid', borderRightColor: 'secondary.main' }} width='94%'>
                 <Grid container item lineHeight='30px'>
                   {/* <Grid item width='22px'> */}
                   {/* <Select index={index} pool={pool} /> */}
                   {/* </Grid>  */}
-                  <Grid fontSize='12px' item overflow='hidden' textAlign='left' textOverflow='ellipsis' whiteSpace='nowrap' xs pr='20px'>
+                  <Grid container fontSize='12px' item overflow='hidden' textAlign='left' textOverflow='ellipsis' whiteSpace='nowrap' >
                     <Identity
                       api={api}
                       chain={chain}
@@ -112,14 +131,14 @@ export default function ValidatorsTable({ allValidatorsInfo, api, chain, selecte
                   </Grid>
                 </Grid>
                 <Grid alignItems='center' container item>
-                  <Grid alignItems='center' container item maxWidth='50%' width='fit-content' sx={{ fontSize: '12px', fontWeight: 300, lineHeight: '23px' }}>
+                  <Grid alignItems='center' container item maxWidth='50%' sx={{ fontSize: '12px', fontWeight: 300, lineHeight: '23px' }} width='fit-content'>
                     {t<string>('Staked:')}
-                    <Grid fontSize='12px' fontWeight={400} item lineHeight='22px' pl='5px'>
+                    <Grid fontSize='12px' fontWeight={400} item lineHeight='22px' pl='3px'>
                       {v.exposure.total
                         ? <ShowBalance
                           api={api}
                           balance={v.exposure.total}
-                          decimalPoint={0}
+                          decimalPoint={1}
                           height={22}
                           skeletonWidth={50}
                         />
@@ -128,34 +147,36 @@ export default function ValidatorsTable({ allValidatorsInfo, api, chain, selecte
                     </Grid>
                   </Grid>
                   <Div />
-                  <Grid alignItems='center' container item width='fit-content' sx={{ fontSize: '12px', fontWeight: 300, lineHeight: '23px' }}>
+                  <Grid alignItems='center' container item sx={{ fontSize: '12px', fontWeight: 300, lineHeight: '23px' }} width='fit-content'>
                     {t<string>('Com.')}
-                    <Grid fontSize='12px' fontWeight={400} item lineHeight='22px' pl='5px'>
+                    <Grid fontSize='12px' fontWeight={400} item lineHeight='22px' pl='3px'>
                       {Number(v.validatorPrefs.commission) / (10 ** 7) < 1 ? 0 : Number(v.validatorPrefs.commission) / (10 ** 7)}%
                     </Grid>
                   </Grid>
                   <Div />
-                  <Grid alignItems='end' container item width='fit-content' sx={{ fontSize: '12px', fontWeight: 300, lineHeight: '23px' }}>
+                  <Grid alignItems='end' container item sx={{ fontSize: '12px', fontWeight: 300, lineHeight: '23px' }} width='fit-content'>
                     {t<string>('Nominators:')}
-                    <Grid fontSize='12px' fontWeight={400} item lineHeight='22px' pl='5px'>
+                    <Grid fontSize='12px' fontWeight={400} item lineHeight='22px' pl='3px'>
                       {v.exposure.others.length || t('N/A')}
                     </Grid>
                   </Grid>
-                  <Grid alignItems='center' justifyContent='flex-end' container item width='fit-content' sx={{ lineHeight: '23px', pl: '4px' }}>
+                  <Grid alignItems='center' container item justifyContent='flex-end' sx={{ lineHeight: '23px', pl: '4px' }} width='fit-content'>
                     {isActive &&
                       <Tooltip placement='left' title={t('Active')}>
                         <DirectionsRunIcon sx={{ color: '#1F7720', fontSize: '15px' }} />
                       </Tooltip>
                     }
-                    <FontAwesomeIcon
-                      color='#FFB800'
-                      fontSize='12px'
-                      icon={faExclamationTriangle}
-                    />
+                    {(isOversubscribed?.safe || isOversubscribed?.notSafe) &&
+                      <FontAwesomeIcon
+                        color={isOversubscribed?.safe ? '#FFB800' : '#FF002B'}
+                        fontSize='12px'
+                        icon={faExclamationTriangle}
+                      />
+                    }
                   </Grid>
                 </Grid>
               </Grid>
-              <Grid alignItems='center' container item justifyContent='center' sx={{ cursor: 'pointer' }} width='8%'>
+              <Grid alignItems='center' container item justifyContent='center' sx={{ cursor: 'pointer' }} width='6%'>
                 <MoreVertIcon sx={{ color: 'secondary.light', fontSize: '33px' }} />
               </Grid>
             </Grid>
