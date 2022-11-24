70534de8126f3d4b0543cde1375f4b7e26e2ace5
Kami <ekbatanifard@gmail.com>
2022-11-24 19:31:36 +0330

    Create Checkbox2.tsx

---
+++ packages/extension-polkagate/src/components/Checkbox2.tsx
@@ -0,0 +1,32 @@
+// Copyright 2019-2022 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { CheckBoxOutlineBlankRounded as CheckBoxOutlineBlankRoundedIcon, CheckBoxOutlined as CheckBoxOutlinedIcon } from '@mui/icons-material';
+import { Checkbox, FormControlLabel, SxProps, Theme } from '@mui/material';
+import React from 'react';
+
+interface Props {
+  checked?: boolean;
+  label?: string;
+  onChange?: (checked: boolean) => void;
+  style?: SxProps<Theme> | undefined
+  labelStyle?: React.CSSProperties | undefined
+}
+
+export default function Checkbox2({ checked = false, label, labelStyle = { fontSize: '14px', fontWeight: 300 }, onChange, style }: Props): React.ReactElement<Props> {
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          checkedIcon={<CheckBoxOutlinedIcon sx={{ color: 'primary.light' }} />}
+          icon={<CheckBoxOutlineBlankRoundedIcon sx={{ color: 'primary.light' }} />}
+          onChange={onChange}
+          sx={{ p: 0, pr: label && '5px' }}
+        />
+      }
+      label={<span style={{ ...labelStyle }}>{label}</span>}
+      sx={{ m: 0, p: 0, ...style }}
+    />
+  );
+}
