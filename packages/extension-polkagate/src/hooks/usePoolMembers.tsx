cc4915f87b5a5d9261bfc05abdc4121d20c18cce
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-11-26 11:28:33 +0330

    Create usePoolMembers.tsx

---
+++ packages/extension-polkagate/src/hooks/usePoolMembers.tsx
@@ -0,0 +1,32 @@
+// Copyright 2017-2022 @polkadot/react-hooks authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { ApiPromise } from '@polkadot/api';
+import { useState } from 'react';
+
+import { MembersMapEntry } from '../util/types';
+
+export function usePoolMembers(api: ApiPromise, poolID: string): MembersMapEntry[] | undefined {
+  const [poolMembers, setPoolMembers] = useState();
+
+  // eslint-disable-next-line no-void
+  void api.query.nominationPools.poolMembers.entries().then((entries) => {
+    const members = entries.reduce((all, [{ args: [accountId] }, optMember]) => {
+      if (optMember.isSome) {
+        const member = optMember.unwrap();
+        const poolId = member.poolId.toString();
+
+        if (!all[poolId]) {
+          all[poolId] = [];
+        }
+
+        all[poolId].push({ accountId: accountId.toString(), member });
+      }
+
+      return all;
+    }, {});
+    setPoolMembers(members[poolID]);
+  });
+
+  return poolMembers;
+}
