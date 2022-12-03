8075dffe4958b30343dfa9e36ba75a773843e00d
Kami <ekbatanifard@gmail.com>
2022-12-03 23:54:26 +0330

    Create del-getPrice.ts

---
+++ packages/extension-polkagate/src/util/api/del-getPrice.ts
@@ -0,0 +1,25 @@
+// Copyright 2019-2022 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import request from 'umi-request';
+
+import { Chain } from '@polkadot/extension-chains/types';
+
+export async function getPrice(chain: Chain, currency = 'usd'): Promise<number> {
+  const chainName = chain.name.replace(' Relay Chain', '')?.replace(' Network', '');
+
+  if (chainName === 'Westend') {
+    return 0;
+  }
+
+  const price = await getReq(`https://api.coingecko.com/api/v3/simple/price?ids=${chainName}&vs_currencies=${currency}`, {});
+
+  return price[chainName]?.usd ?? 0;
+}
+
+function getReq(api: string, data: Record<string, any> = {}, option?: Record<string, any>): Promise<Record<string, any>> {
+  return request.get(api, {
+    data,
+    ...option
+  });
+}
