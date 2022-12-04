ec3e2cd9dcd9d5c265998b88f16407832412c720
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-12-04 13:43:53 +0330

    Update index.tsx

--- packages/extension-polkagate/src/popup/staking/solo/rewards/index.tsx
+++ packages/extension-polkagate/src/popup/staking/solo/rewards/index.tsx
@@ -9,14 +9,14 @@
 import { ExpandMore as ExpandMoreIcon, KeyboardDoubleArrowLeft as KeyboardDoubleArrowLeftIcon, KeyboardDoubleArrowRight as KeyboardDoubleArrowRightIcon } from '@mui/icons-material';
 import { Accordion, AccordionDetails, AccordionSummary, Divider, Grid, IconButton, Typography, useTheme } from '@mui/material';
 import { BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip } from 'chart.js';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import { useParams } from 'react-router';
 import { useHistory, useLocation } from 'react-router-dom';
 
 import { ApiPromise } from '@polkadot/api';
 import { Chain } from '@polkadot/extension-chains/types';
-import { BN } from '@polkadot/util';
+import { BN, BN_ZERO } from '@polkadot/util';
 
 import { ChainLogo, Identity, PButton, Progress } from '../../../../components';
 import { useApi, useChain, useFormatted, useTranslation } from '../../../../hooks';
@@ -37,14 +37,17 @@ ChartJS.register(
 );
 
 const MAX_REWARDS_INFO_TO_SHOW = 20;
+const MAX_CHUNK_LENGTH = 4;
 
-function sliceIntoChunks (arr: string[]) {
+function sliceIntoChunks(arr: string[]) {
   const res = [];
 
-  for (let i = 0; i < arr.length; i += 7) {
-    const chunk = arr.slice(i, i + 7);
+  const reveresedArr = [...arr].reverse();
 
-    res.push(chunk);
+  for (let i = 0; i < reveresedArr.length; i += MAX_CHUNK_LENGTH) {
+    const chunk = reveresedArr.slice(i, i + MAX_CHUNK_LENGTH);
+
+    res.push(chunk.reverse());
   }
 
   return res;
@@ -71,15 +74,82 @@ export default function RewardDetails(): React.ReactElement {
   const theme = useTheme();
 
   const [rewardsInfo, setRewardsInfo] = useState<RewardInfo[]>();
-  const [dataLabelsToShow, setDataLabelsToShow] = useState<string[][]>();
-  const [dataToShow, setDataToShow] = useState<string[][]>();
-  const [descSortedRewards, setDescSortedRewards] = useState<RewardInfo[]>();
   const [pageIndex, setPageIndex] = useState<number>(0);
 
   const chainName = chain?.name?.replace(' Relay Chain', '')?.replace(' Network', '');
   const decimal = api && api.registry.chainDecimals[0];
   const token = api && api.registry.chainTokens[0];
   const [expanded, setExpanded] = useState<number>(-1);
+
+  const formateDate = (date: number) => {
+    const options = { day: 'numeric', month: 'short' };
+
+    return new Date(date * 1000).toLocaleDateString('en-GB', options);
+  };
+
+  const descSortedRewards = useMemo(() => {
+    const sorted = rewardsInfo && [...rewardsInfo];
+
+    sorted?.sort((a, b) => b.era - a.era);
+
+    return sorted;
+  }, [rewardsInfo]);
+
+  const ascSortedRewards = useMemo(() => {
+    const sorted = rewardsInfo && [...rewardsInfo];
+
+    sorted?.sort((a, b) => a.era - b.era);
+
+    const newSorted = sorted?.map((reward) => {
+      reward['date'] = formateDate(reward.timeStamp);
+
+      return reward;
+    });
+
+    return newSorted;
+  }, [rewardsInfo]);
+
+  const ascSortedLabels = useMemo(() => {
+    const uDate = new Set();
+
+    ascSortedRewards?.forEach((item) => {
+      uDate.add(item.date);
+    });
+
+    return Array.from(uDate) as string[];
+  }, [ascSortedRewards]);
+
+  const labelsToShow = useMemo(() => {
+    if (!ascSortedLabels?.length) {
+      return;
+    }
+
+    return sliceIntoChunks(ascSortedLabels);
+  }, [ascSortedLabels]);
+
+  const aggregatedRewards = useMemo(() => {
+    if (!ascSortedRewards?.length || !ascSortedLabels?.length) {
+      return;
+    }
+
+    const tempToHuman: string[] = [];
+    const temp = new Array(ascSortedLabels.length).fill(BN_ZERO) as BN[];
+    let index = 0;
+
+    ascSortedRewards.forEach((item) => {
+      if (item.date === ascSortedLabels[index]) {
+        temp[index] = temp[index].add(item.amount);
+        tempToHuman[index] = amountToHuman(temp[index], decimal);
+      } else {
+        index++;
+        temp[index] = temp[index].add(item.amount);
+        tempToHuman[index] = amountToHuman(temp[index], decimal);
+      }
+    });
+
+    return sliceIntoChunks(tempToHuman);
+    // return temp;
+  }, [ascSortedLabels, ascSortedRewards, decimal]);
 
   const handleAccordionChange = useCallback((panel: number) => (event: React.SyntheticEvent, isExpanded: boolean) => {
     setExpanded(isExpanded ? panel : -1);
@@ -93,12 +163,16 @@ export default function RewardDetails(): React.ReactElement {
   }, [address, history, state]);
 
   const onPrevious = useCallback(() => {
-    dataToShow && pageIndex && setPageIndex(pageIndex - 1);
-  }, [dataToShow, pageIndex]);
+    pageIndex && setPageIndex(pageIndex - 1);
+  }, [pageIndex]);
 
   const onNext = useCallback(() => {
-    dataToShow && pageIndex !== (dataToShow?.length - 1) && setPageIndex(pageIndex + 1);
-  }, [dataToShow, pageIndex]);
+    aggregatedRewards && pageIndex !== (aggregatedRewards.length - 1) && setPageIndex(pageIndex + 1);
+  }, [aggregatedRewards, pageIndex]);
+
+  const dateRatio = (next?: boolean): string | undefined => {
+    return labelsToShow && labelsToShow[(next ? pageIndex + 1 : pageIndex - 1)] && `${labelsToShow[(next ? pageIndex + 1 : pageIndex - 1)][0]} - ${labelsToShow[(next ? pageIndex + 1 : pageIndex - 1)].at(-1)}`;
+  };
 
   useEffect((): void => {
     // TODO: to get rewrads info from subquery
@@ -144,63 +218,29 @@ export default function RewardDetails(): React.ReactElement {
     });
   }, [chainName, formatted]);
 
-  const Arrows = ({ onNext, onPrevious }: ArrowsProps) => (
-    <Grid container m='auto' width='92%'>
-      <Grid alignItems='center' container item justifyContent='flex-start' xs={6}>
-        <IconButton onClick={onPrevious} size='medium' sx={{ p: 0 }}>
-          <KeyboardDoubleArrowLeftIcon sx={{ color: 'secondary.light', fontSize: '25px' }} />
-        </IconButton>
-        <Divider orientation='vertical' sx={{ bgcolor: 'text.primary', height: '28px', ml: '3px', mr: '7px', my: 'auto', width: '1px' }} />
-        <Grid container direction='column' item xs={7}>
-          <Typography color='secondary.light' fontSize='14px' fontWeight={400}>{t<string>('Previous')}</Typography>
-          <Typography fontSize='12px' fontWeight={300}>{dataLabelsToShow[pageIndex - 1] && dataLabelsToShow[pageIndex - 1][6]}</Typography>
-        </Grid>
-      </Grid>
-      <Grid alignItems='center' container item justifyContent='flex-end' xs={6}>
-        <Grid container direction='column' item textAlign='right' xs={7}>
-          <Typography color='secondary.light' fontSize='14px' fontWeight={400}>{t<string>('Next')}</Typography>
-          <Typography fontSize='12px' fontWeight={300}>{dataLabelsToShow[pageIndex + 1] && dataLabelsToShow[pageIndex + 1][0]}</Typography>
-        </Grid>
-        <Divider orientation='vertical' sx={{ bgcolor: 'text.primary', height: '28px', ml: '7px', mr: '3px', my: 'auto', width: '1px' }} />
-        <IconButton onClick={onNext} size='medium' sx={{ p: 0 }}>
-          <KeyboardDoubleArrowRightIcon sx={{ color: 'secondary.light', fontSize: '25px' }} />
-        </IconButton>
-      </Grid>
-    </Grid>
-  );
-
-  const formateDate = (date: number) => {
-    const options = { day: 'numeric', month: 'short' };
-
-    return new Date(date * 1000).toLocaleDateString('en-GB', options);
-  };
-
-  useEffect(() => {
-    if (!rewardsInfo?.length) {
-      return;
-    }
-
-    // const sortedRewards = [...rewardsInfo];
-
-    // sortedRewards?.sort((a, b) => a.era - b.era);
-
-    const DescSortedRewards = rewardsInfo && [...rewardsInfo];
-
-    DescSortedRewards?.sort((a, b) => b.era - a.era);
-
-    const labels = DescSortedRewards?.map((d) => formateDate(d.timeStamp));
-    const y = DescSortedRewards?.map((d) => amountToHuman(d.amount, decimal));
-
-    setDescSortedRewards(DescSortedRewards);
-    setDataToShow(sliceIntoChunks(y));
-    setDataLabelsToShow(sliceIntoChunks(labels));
-  }, [decimal, rewardsInfo]);
-
   const options = {
     aspectRatio: 1.9,
     plugins: {
       legend: {
         display: false
+      },
+      tooltip: {
+        backgroundColor: theme.palette.mode === 'dark' ? '#fff' : '#000',
+        bodyColor: theme.palette.mode === 'dark' ? '#000' : '#fff',
+        bodyFont: {
+          displayColors: false,
+          family: 'Roboto',
+          size: 14,
+          weight: 'bold'
+        },
+        displayColors: false,
+        titleColor: theme.palette.mode === 'dark' ? '#000' : '#fff',
+        titleFont: {
+          displayColors: false,
+          family: 'Roboto',
+          size: 14,
+          weight: 'bold'
+        }
       }
     },
     responsive: true,
@@ -210,13 +250,15 @@ export default function RewardDetails(): React.ReactElement {
           borderColor: theme.palette.secondary.light,
           color: '',
           tickColor: ''
-        }
+        },
+        ticks: { color: theme.palette.text.primary }
       },
       y: {
         grid: {
           color: theme.palette.secondary.light,
           tickColor: ''
-        }
+        },
+        ticks: { color: theme.palette.text.primary }
       }
     }
   };
@@ -229,12 +271,37 @@ export default function RewardDetails(): React.ReactElement {
         borderColor: '#3A0B63',
         borderRadius: 3,
         borderWidth: 1,
-        data: dataToShow && dataToShow[pageIndex],
+        data: aggregatedRewards && aggregatedRewards[pageIndex],
         label: token
       }
     ],
-    labels: dataLabelsToShow && dataLabelsToShow[pageIndex]
+    labels: labelsToShow && labelsToShow[pageIndex]
   };
+
+  const Arrows = ({ onNext, onPrevious }: ArrowsProps) => (
+    <Grid container m='auto' width='92%'>
+      <Grid alignItems='center' container item justifyContent='flex-start' xs={6}>
+        <IconButton onClick={onNext} size='medium' sx={{ p: 0 }}>
+          <KeyboardDoubleArrowLeftIcon sx={{ color: 'secondary.light', fontSize: '25px' }} />
+        </IconButton>
+        <Divider orientation='vertical' sx={{ bgcolor: 'text.primary', height: '28px', ml: '3px', mr: '7px', my: 'auto', width: '1px' }} />
+        <Grid container direction='column' item xs={7}>
+          <Typography color='secondary.light' fontSize='14px' fontWeight={400}>{t<string>('Previous')}</Typography>
+          <Typography fontSize='12px' fontWeight={300}>{dateRatio(true)}</Typography>
+        </Grid>
+      </Grid>
+      <Grid alignItems='center' container item justifyContent='flex-end' xs={6}>
+        <Grid container direction='column' item textAlign='right' xs={7}>
+          <Typography color='secondary.light' fontSize='14px' fontWeight={400}>{t<string>('Next')}</Typography>
+          <Typography fontSize='12px' fontWeight={300}>{dateRatio(false)}</Typography>
+        </Grid>
+        <Divider orientation='vertical' sx={{ bgcolor: 'text.primary', height: '28px', ml: '7px', mr: '3px', my: 'auto', width: '1px' }} />
+        <IconButton onClick={onPrevious} size='medium' sx={{ p: 0 }}>
+          <KeyboardDoubleArrowRightIcon sx={{ color: 'secondary.light', fontSize: '25px' }} />
+        </IconButton>
+      </Grid>
+    </Grid>
+  );
 
   return (
     <>
@@ -245,7 +312,7 @@ export default function RewardDetails(): React.ReactElement {
         showClose
         text={t<string>('Rewards')}
       />
-      {descSortedRewards && dataLabelsToShow && dataToShow && decimal
+      {descSortedRewards && decimal
         ? (
           <>
             <Grid container justifyContent='center'>
@@ -271,11 +338,11 @@ export default function RewardDetails(): React.ReactElement {
                 {t('Reward')}
               </Typography>
             </Grid>
-            <Grid container sx={{ '&::-webkit-scrollbar': { display: 'none', width: 0 }, '> .MuiPaper-root::before': { bgcolor: 'transparent' }, maxHeight: parent.innerHeight * 1 / 4, overflowX: 'hidden', overflowY: 'scroll', scrollbarWidth: 'none' }}>
+            <Grid container sx={{ '&::-webkit-scrollbar': { display: 'none', width: 0 }, '> .MuiPaper-root': { boxShadow: 'none', backgroundImage: 'none' }, '> .MuiPaper-root::before': { bgcolor: 'transparent' }, maxHeight: parent.innerHeight - 450, overflowX: 'hidden', overflowY: 'scroll', scrollbarWidth: 'none' }}>
               {descSortedRewards?.slice(0, MAX_REWARDS_INFO_TO_SHOW).map((d, index: number) =>
                 <>
-                  <Accordion disableGutters expanded={expanded === index} key={index} onChange={handleAccordionChange(index)} sx={{ flexGrow: 1, fontSize: 12 }}>
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ height: '35px', minHeight: '35px' }}>
+                  <Accordion disableGutters expanded={expanded === index} key={index} onChange={handleAccordionChange(index)} sx={{ bgcolor: 'transparent', flexGrow: 1, fontSize: 12 }}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ bgcolor: 'background.paper', height: '35px', m: 'auto', minHeight: '35px', width: '92%' }}>
                       <Grid container item key={index} sx={{ textAlign: 'left' }}>
                         <Grid item width='40%'>
                           {d.timeStamp ? new Date(d.timeStamp * 1000).toDateString() : d.era}
