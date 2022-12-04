32574a7b959c9f0e5716b9998458ef1d3b5c490d
Kami <ekbatanifard@gmail.com>
2022-12-04 12:12:24 +0330

    use extension name injected

--- packages/extension/src/page.ts
+++ packages/extension/src/page.ts
@@ -12,7 +12,7 @@ import { packageInfo } from './packageInfo';
 
 function inject () {
   injectExtension(enable, {
-    name: 'polkadot-js',
+    name: 'polkagate', // added by polkagate
     version: packageInfo.version
   });
 }
