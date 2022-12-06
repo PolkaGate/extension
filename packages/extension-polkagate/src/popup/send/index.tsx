0d290c07118f133d5976f4e9eb6dad40a2c36e33
Kami <ekbatanifard@gmail.com>
2022-12-06 22:45:29 +0330

    Update index.tsx

--- packages/extension-polkagate/src/popup/send/index.tsx
+++ packages/extension-polkagate/src/popup/send/index.tsx
@@ -22,7 +22,7 @@ import keyring from '@polkadot/ui-keyring';
 import { BN, BN_ZERO } from '@polkadot/util';
 import { cryptoWaitReady } from '@polkadot/util-crypto';
 
-import { AccountContext, AmountWithOptions, ButtonWithCancel, Identicon, Motion, PButton, SettingsContext, ShortAddress, To } from '../../components';
+import { AccountContext, AmountWithOptions, Identicon, Motion, PButton, SettingsContext, ShortAddress, To } from '../../components';
 import { useApi, useEndpoint, useMetadata, useTranslation } from '../../hooks';
 import { HeaderBrand } from '../../partials';
 import { DEFAULT_TOKEN_DECIMALS, FLOATING_POINT_DIGIT, MAX_AMOUNT_LENGTH } from '../../util/constants';
