3f2bf99a3e98d487531850c65b38c0507ea38e2e
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-12-08 15:48:29 +0330

    Fix Expand icon size and color

--- packages/extension-polkagate/src/popup/staking/solo/rewards/index.tsx
+++ packages/extension-polkagate/src/popup/staking/solo/rewards/index.tsx
@@ -445,7 +445,7 @@ export default function RewardDetails(): React.ReactElement {
                 ? descSortedRewards.slice(0, MAX_REWARDS_INFO_TO_SHOW).map((d, index: number) =>
                   <>
                     <Accordion disableGutters expanded={expanded === index} key={index} onChange={handleAccordionChange(index)} sx={{ bgcolor: 'transparent', flexGrow: 1, fontSize: 12 }}>
-                      <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{ height: '35px', m: 'auto', minHeight: '35px', width: '92%' }}>
+                      <AccordionSummary expandIcon={<ExpandMoreIcon sx={{ color: 'secondary.light', fontSize: '35px' }} />} sx={{ height: '35px', m: 'auto', minHeight: '35px', width: '92%' }}>
                         <Grid container item key={index} sx={{ textAlign: 'left' }}>
                           <Grid item width='40%'>
                             {d.timeStamp ? new Date(d.timeStamp * 1000).toDateString() : d.era}
