9758ebe9d9416b4746725d72874c25d25ae25e0e
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-12-20 12:27:04 +0330

    Update TransactionIndex.tsx

--- packages/extension-polkagate/src/popup/signing/TransactionIndex.tsx
+++ packages/extension-polkagate/src/popup/signing/TransactionIndex.tsx
@@ -1,22 +1,21 @@
 // Copyright 2019-2022 @polkadot/extension-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { ThemeProps } from '../../types';
-
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { KeyboardDoubleArrowLeft as KeyboardDoubleArrowLeftIcon, KeyboardDoubleArrowRight as KeyboardDoubleArrowRightIcon } from '@mui/icons-material';
+import { Divider, Grid, Typography } from '@mui/material';
 import React, { useCallback } from 'react';
-import styled from 'styled-components';
+
+import { useTranslation } from '../../components/translate';
 
 interface Props {
-  className?: string;
   index: number;
   totalItems: number;
   onNextClick: () => void;
   onPreviousClick: () => void;
 }
 
-function TransactionIndex ({ className, index, onNextClick, onPreviousClick, totalItems }: Props): React.ReactElement<Props> {
+export default function TransactionIndex({ index, onNextClick, onPreviousClick, totalItems }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
   const previousClickActive = index !== 0;
   const nextClickActive = index < totalItems - 1;
 
@@ -35,60 +34,24 @@ function TransactionIndex ({ className, index, onNextClick, onPreviousClick, tot
   );
 
   return (
-    <div className={className}>
-      <div>
-        <span className='current'>{index + 1}</span>
-        <span className='total'>/{totalItems}</span>
-      </div>
-      <div>
-        <FontAwesomeIcon
-          className={`arrowLeft ${previousClickActive ? 'active' : ''}`}
-          icon={faArrowLeft}
-          onClick={prevClick}
-          size='sm'
-        />
-        <FontAwesomeIcon
-          className={`arrowRight ${nextClickActive ? 'active' : ''}`}
-          icon={faArrowRight}
-          onClick={nextClick}
-          size='sm'
-        />
-      </div>
-    </div>
+    <Grid container justifyContent='space-between' m='15px auto 10px' width='92%'>
+      <Grid alignItems='center' container item justifyContent='flex-start' maxWidth='35%' onClick={prevClick} sx={{ cursor: index === 0 ? 'default' : 'pointer' }} width='fit_content'>
+        <KeyboardDoubleArrowLeftIcon sx={{ color: index === 0 ? 'secondary.contrastText' : 'secondary.light', fontSize: '25px' }} />
+        <Divider orientation='vertical' sx={{ bgcolor: index === 0 ? 'secondary.contrastText' : 'text.primary', height: '22px', ml: '3px', mr: '7px', my: 'auto', width: '1px' }} />
+        <Grid container item xs={7}>
+          <Typography color={index === 0 ? 'secondary.contrastText' : 'secondary.light'} fontSize='14px' fontWeight={400}>{t<string>('Previous')}</Typography>
+        </Grid>
+      </Grid>
+      <Grid alignItems='center' container item justifyContent='center' width='30%'>
+        <Typography fontSize='16px' fontWeight={400}>{`${index + 1} of ${totalItems}`}</Typography>
+      </Grid>
+      <Grid alignItems='center' container item justifyContent='flex-end' maxWidth='35%' onClick={nextClick} sx={{ cursor: index + 1 === totalItems ? 'default' : 'pointer' }} width='fit_content'>
+        <Grid container item justifyContent='right' xs={7}>
+          <Typography color={index + 1 === totalItems ? 'secondary.contrastText' : 'secondary.light'} fontSize='14px' fontWeight={400} textAlign='left'>{t<string>('Next')}</Typography>
+        </Grid>
+        <Divider orientation='vertical' sx={{ bgcolor: index + 1 === totalItems ? 'secondary.contrastText' : 'text.primary', height: '22px', ml: '7px', mr: '3px', my: 'auto', width: '1px' }} />
+        <KeyboardDoubleArrowRightIcon sx={{ color: index + 1 === totalItems ? 'secondary.contrastText' : 'secondary.light', fontSize: '25px' }} />
+      </Grid>
+    </Grid>
   );
 }
-
-export default styled(TransactionIndex)(({ theme }: ThemeProps) => `
-  align-items: center;
-  display: flex;
-  justify-content: space-between;
-  flex-grow: 1;
-  padding-right: 24px;
-
-  .arrowLeft, .arrowRight {
-    display: inline-block;
-    color: ${theme.iconNeutralColor};
-
-    &.active {
-      color: ${theme.primaryColor};
-      cursor: pointer;
-    }
-  }
-
-  .arrowRight {
-    margin-left: 0.5rem;
-  }
-
-  .current {
-    color: ${theme.primaryColor};
-    font-size: ${theme.labelFontSize};
-    line-height: ${theme.labelLineHeight};
-    margin-left: 10px;
-  }
-
-  .total {
-    font-size: ${theme.labelFontSize};
-    line-height: ${theme.labelLineHeight};
-    color: ${theme.textColor};
-  }
-`);
