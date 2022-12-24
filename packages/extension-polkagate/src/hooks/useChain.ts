dce40ed6d7242197fdcd807475d49274bbd7a99f
Kami <ekbatanifard@gmail.com>
2022-12-25 00:13:02 +0330

    Update useChain.ts

--- packages/extension-polkagate/src/hooks/useChain.ts
+++ packages/extension-polkagate/src/hooks/useChain.ts
@@ -1,21 +1,31 @@
 // Copyright 2019-2022 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { useEffect, useState } from 'react';
+
 import { Chain } from '@polkadot/extension-chains/types';
 import { AccountId } from '@polkadot/types/interfaces/runtime';
 
 import { getSubstrateAddress } from '../util/utils';
 import { useAccount, useMetadata } from './';
 
-export default function useChain(address: AccountId | string | undefined, chain?: Chain): Chain | null {
+export default function useChain(address: AccountId | string | undefined, chain?: Chain): Chain | null | undefined {
   /** address can be a formatted address hence needs to find its substrate format first */
   const sAddr = getSubstrateAddress(address);
   const account = useAccount(sAddr);
-  const localChain = useMetadata(account?.genesisHash, true);
+  const metaDataChain = useMetadata(account?.genesisHash, true);
 
-  if (chain) {
-    return chain;
-  }
+  const [newChain, setNewChain] = useState<Chain | null | undefined>();
 
-  return localChain;
+  useEffect(() => {
+    if (chain) {
+      setNewChain(chain);
+
+      return;
+    }
+
+    setNewChain(metaDataChain);
+  }, [chain, metaDataChain]);
+
+  return newChain;
 }
