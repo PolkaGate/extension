d89859b4cf320cf3d83718c6e3c5e20b99f98482
Kami <ekbatanifard@gmail.com>
2022-12-25 00:06:37 +0330

    Update constants.tsx

--- packages/extension-polkagate/src/util/constants.tsx
+++ packages/extension-polkagate/src/util/constants.tsx
@@ -127,3 +127,4 @@ export const DEFAULT_FILTERS = {
 };
 
 export const TOTAL_STAKE_HELPER_TEXT = 'Your total amount of stake after completing this transaction.';
+export const SYSTEM_SUGGESTION_TEXT = 'Our system suggests trusted, high return, low commission validators. Polkagate assumes no responsibility or liability for any misconduct resulting from the future actions of the validators.'// which not slashed before.' //TODO: add a disclaimer to the text too
