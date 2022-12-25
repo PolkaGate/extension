89d150e356e210b618cdb7c81a092d5647030d66
Kami <ekbatanifard@gmail.com>
2022-12-25 13:01:05 +0330

    Update useToken.ts

--- packages/extension-polkagate/src/hooks/useToken.ts
+++ packages/extension-polkagate/src/hooks/useToken.ts
@@ -5,14 +5,14 @@ import { selectableNetworks } from '@polkadot/networks';
 import { AccountId } from '@polkadot/types/interfaces/runtime';
 
 import { getSubstrateAddress } from '../util/utils';
-import useChain from './useChain';
+import useAccount from './useAccount';
 
-export default function useDecimal(address: AccountId | string | undefined): string | undefined {
+export default function useToken(address: AccountId | string | undefined): string | undefined {
   /** address can be a formatted address hence needs to find its substrate format first */
   const sAddr = getSubstrateAddress(address);
-  const localChain = useChain(sAddr);
+  const account = useAccount(sAddr);
 
-  const network = selectableNetworks.find((network) => network.genesisHash[0] === localChain?.genesisHash);
+  const network = selectableNetworks.find((network) => network.genesisHash[0] === account?.genesisHash);
 
   return network?.symbols?.length ? network.symbols[0] : undefined;
 }
