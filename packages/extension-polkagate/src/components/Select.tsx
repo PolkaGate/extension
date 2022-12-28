d7b670244ac1cf19a3f9258f9fbed05a722b5fd4
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-12-28 12:04:24 +0330

    Add disabled color in input components

--- packages/extension-polkagate/src/components/Select.tsx
+++ packages/extension-polkagate/src/components/Select.tsx
@@ -7,29 +7,6 @@ import React, { useCallback } from 'react';
 
 import Label from './Label';
 
-const BootstrapInput = styled(InputBase)(({ theme }) => ({
-  '& .MuiInputBase-input': {
-    '&:focus': {
-      // borderRadius: 4,
-      borderColor: theme.palette.secondary.main,
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
-    },
-    backgroundColor: theme.palette.background.paper,
-    border: `1px solid ${theme.palette.primary.main}`,
-    borderRadius: 0,
-    fontSize: '14px',
-    fontWeight: '300',
-    letterSpacing: '-0.015em',
-    padding: '5px 10px 0px',
-    transition: theme.transitions.create(['border-color', 'box-shadow'])
-  },
-  'label + &': {
-    fontSize: '10px',
-    fontWeight: '300',
-    letterSpacing: '-0.015em'
-  }
-}));
-
 interface DropdownOption {
   text: string;
   value: string;
@@ -48,6 +25,29 @@ interface Props {
 }
 
 export default function CustomizedSelect({ _mt = 0, defaultValue, disabledItems, helperText, isDisabled = false, label, onChange, options, value }: Props) {
+  const BootstrapInput = styled(InputBase)(({ theme }) => ({
+    '& .MuiInputBase-input': {
+      '&:focus': {
+        // borderRadius: 4,
+        borderColor: theme.palette.secondary.main,
+        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
+      },
+      backgroundColor: isDisabled ? theme.palette.primary.contrastText : theme.palette.background.paper,
+      border: `1px solid ${theme.palette.primary.main}`,
+      borderRadius: 0,
+      fontSize: '14px',
+      fontWeight: '300',
+      letterSpacing: '-0.015em',
+      padding: '5px 10px 0px',
+      transition: theme.transitions.create(['border-color', 'box-shadow'])
+    },
+    'label + &': {
+      fontSize: '10px',
+      fontWeight: '300',
+      letterSpacing: '-0.015em'
+    }
+  }));
+
   const _onChange = useCallback(
     ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) =>
       onChange && onChange(typeof value === 'string' ? value.trim() : value),
@@ -89,7 +89,8 @@ export default function CustomizedSelect({ _mt = 0, defaultValue, disabledItems,
               color: 'action.disabledBackground',
               fontSize: '30px'
             },
-            width: '100%'
+            width: '100%',
+            bgcolor: isDisabled && 'primary.contrastText'
           }}
           value={value}
         >
