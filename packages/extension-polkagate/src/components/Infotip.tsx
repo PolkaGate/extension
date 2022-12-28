f62ac64543c65ce6724f305c997dd3f64509d3fd
Kami <ekbatanifard@gmail.com>
2022-12-28 13:28:02 +0330

    Update Infotip.tsx

--- packages/extension-polkagate/src/components/Infotip.tsx
+++ packages/extension-polkagate/src/components/Infotip.tsx
@@ -1,6 +1,8 @@
 // Copyright 2019-2022 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable react/jsx-max-props-per-line */
+
 import { QuestionMarkRounded as QuestionMarkRoundedIcon } from '@mui/icons-material';
 import { Grid, Tooltip } from '@mui/material';
 import React, { useEffect, useRef, useState } from 'react';
@@ -17,67 +19,71 @@ interface Props {
 
 function Infotip({ children, fontSize = '14px', iconLeft = 10, iconTop = 4, placement = 'top', showQuestionMark = false, text }: Props): React.ReactElement<Props> {
   const ref = useRef(null);
-  const [tpLocation, setTpLocation] = useState<string | undefined>();
+  const [tpLocation, setTpLocation] = useState<number | undefined>();
 
   useEffect(() => {
     if (ref) {
-      setTpLocation(`${ref.current?.offsetWidth + iconLeft}px`);
+      setTpLocation(ref.current?.offsetWidth + iconLeft);
     }
   }, [iconLeft, ref.current?.offsetWidth, tpLocation]);
 
   return (
-    <Tooltip
-      arrow
-      componentsProps={{
-        popper: {
-          sx: {
-            '.MuiTooltip-tooltip.MuiTooltip-tooltipPlacementTop.css-18kejt8': {
-              mb: '3px',
-              p: '3px 15px'
-            },
-            '.MuiTooltip-tooltip.MuiTooltip-tooltipPlacementTop.css-1yuxi3g': {
-              mb: '3px',
-              p: '3px 15px'
+    <Grid item ref={ref} style={{ position: 'relative' }}>
+      <div>
+        {children}
+      </div>
+      <Tooltip
+        arrow
+        componentsProps={{
+          popper: {
+            sx: {
+              '.MuiTooltip-tooltip.MuiTooltip-tooltipPlacementTop.css-18kejt8': {
+                mb: '3px',
+                p: '3px 15px'
+              },
+              '.MuiTooltip-tooltip.MuiTooltip-tooltipPlacementTop.css-1yuxi3g': {
+                mb: '3px',
+                p: '3px 15px'
+              }
+            }
+          },
+          tooltip: {
+            sx: {
+              '& .MuiTooltip-arrow': {
+                color: 'text.primary',
+                height: '10px'
+              },
+              backgroundColor: 'text.primary',
+              color: 'text.secondary',
+              fontSize,
+              fontWeight: 400
             }
           }
-        },
-        tooltip: {
-          sx: {
-            '& .MuiTooltip-arrow': {
-              color: 'text.primary',
-              height: '10px'
-            },
-            backgroundColor: 'text.primary',
-            color: 'text.secondary',
-            fontSize,
-            fontWeight: 400
+        }}
+        enterDelay={500}
+        // enterNextDelay={7000}
+        leaveDelay={500}
+        placement={placement}
+        title={text || ''}
+      >
+        <div>
+          {showQuestionMark &&
+            <QuestionMarkRoundedIcon
+              sx={{
+                bgcolor: 'secondary.light',
+                borderRadius: '50%',
+                color: 'background.default',
+                height: '15px',
+                left: `${tpLocation}px`,
+                position: 'absolute',
+                top: `${iconTop}px`,
+                width: '15px'
+              }}
+            />
           }
-        }
-      }}
-      enterDelay={500}
-      enterNextDelay={7000}
-      leaveDelay={500}
-      placement={placement}
-      title={text || ''}
-    >
-      <div ref={ref} style={{ position: 'relative' }}>
-        {children}
-        {showQuestionMark &&
-          <QuestionMarkRoundedIcon
-            sx={{
-              bgcolor: 'secondary.light',
-              borderRadius: '50%',
-              color: 'background.default',
-              height: '15px',
-              left: tpLocation,
-              position: 'absolute',
-              top: `${iconTop}px`,
-              width: '15px'
-            }}
-          />
-        }
-      </div>
-    </Tooltip>
+        </div>
+      </Tooltip>
+    </Grid>
   );
 }
 
