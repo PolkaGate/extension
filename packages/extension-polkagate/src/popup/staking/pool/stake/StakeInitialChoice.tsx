2e91e06265431fd9eddb7e14d598d7e6af3c9041
Kami <ekbatanifard@gmail.com>
2022-12-30 18:03:18 +0330

    add join/create pool logo

--- packages/extension-polkagate/src/popup/staking/pool/stake/StakeInitialChoice.tsx
+++ packages/extension-polkagate/src/popup/staking/pool/stake/StakeInitialChoice.tsx
@@ -3,6 +3,9 @@
 
 import type { DeriveBalancesAll } from '@polkadot/api-derive/types';
 
+import { faRightToBracket, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useTheme } from '@mui/material';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -10,7 +13,7 @@ import { ApiPromise } from '@polkadot/api';
 import { Balance } from '@polkadot/types/interfaces';
 
 import { useTranslation } from '../../../../hooks';
-import { HeaderBrand } from '../../../../partials';
+import { HeaderBrand, SubTitle } from '../../../../partials';
 import { PoolStakingConsts } from '../../../../util/types';
 import Option from '../../partial/StakingOption';
 
@@ -24,6 +27,7 @@ interface Props {
 export default function StakeInitialChoice({ address, api, balances, consts }: Props): React.ReactElement {
   const { t } = useTranslation();
   const history = useHistory();
+  const theme = useTheme();
 
   const [availableBalance, setAvailableBalance] = useState<Balance | undefined>();
   const [joinDisabled, setJoinDisabled] = useState<boolean>(true);
@@ -96,12 +100,19 @@ export default function StakeInitialChoice({ address, api, balances, consts }: P
         showClose
         text={t<string>('Pool Staking')}
       />
+      <SubTitle label={t('Stake')} />
       <Option
         api={api}
         balance={consts?.minJoinBond}
         balanceText={t<string>('Minimum to join')}
         buttonText={t<string>('Join')}
         isDisabled={joinDisabled}
+        logo={<FontAwesomeIcon
+          color={theme.palette.mode === 'dark' ? 'white' : 'black'}
+          fontSize='18px'
+          icon={faRightToBracket}
+        />}
+        noToolTip
         onClick={joinPool}
         style={{
           m: '20px auto',
@@ -109,7 +120,6 @@ export default function StakeInitialChoice({ address, api, balances, consts }: P
         }}
         title={t<string>('Join Pool')}
         warningText={joinWarningText}
-        noToolTip
       />
       <Option
         api={api}
@@ -117,6 +127,12 @@ export default function StakeInitialChoice({ address, api, balances, consts }: P
         balanceText={t<string>('Minimum to create')}
         buttonText={t<string>('Create')}
         isDisabled={createDisabled}
+        logo={<FontAwesomeIcon
+          color={theme.palette.mode === 'dark' ? 'white' : 'black'}
+          fontSize='18px'
+          icon={faSquarePlus}
+        />}
+        noToolTip
         onClick={createPool}
         style={{
           m: 'auto',
@@ -124,7 +140,6 @@ export default function StakeInitialChoice({ address, api, balances, consts }: P
         }}
         title={t<string>('Create Pool')}
         warningText={createWarningText}
-        noToolTip
       />
     </>
   );
