6d89f45a8932c8ab3e35c04f019708a7bb0ddc54
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2023-01-01 13:15:05 +0330

    Update blockToDate.tsx

--- packages/extension-polkagate/src/popup/crowdloans/partials/blockToDate.tsx
+++ packages/extension-polkagate/src/popup/crowdloans/partials/blockToDate.tsx
@@ -1,7 +1,7 @@
 // Copyright 2019-2023 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export default function blockToDate(blockNumber?: number, currentBlock?: number) {
+export default function blockToDate(blockNumber?: number, currentBlock?: number, option?: Intl.DateTimeFormatOptions) {
   if (!blockNumber || !currentBlock) {
     return 'N/A';
   }
@@ -10,11 +10,11 @@ export default function blockToDate(blockNumber?: number, currentBlock?: number)
     const time = (blockNumber - currentBlock) * 6000;
     const now = Date.now();
 
-    return new Date(now + time).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
+    return new Date(now + time).toLocaleDateString('en-US', option ?? { day: 'numeric', month: 'short', year: 'numeric' });
   }
 
   const diff = (currentBlock - blockNumber) * 6000;
   const now = Date.now();
 
-  return new Date(now - diff).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
+  return new Date(now - diff).toLocaleDateString('en-US', option ?? { day: 'numeric', month: 'short', year: 'numeric' });
 }
