60aca2633e5ce7129cdc4cfa11c62b58807bb658
Kami <ekbatanifard@gmail.com>
2023-01-05 19:26:25 +0330

    add

---
+++ packages/extension-polkagate/src/popup/createAccount/Mnemonic.tsx
@@ -0,0 +1,63 @@
+// Copyright 2019-2023 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { useTheme } from '@mui/material';
+import React, { useCallback, useState } from 'react';
+
+import { Checkbox, MnemonicSeed, PButton, Warning } from '../../components';
+import { useToast, useTranslation } from '../../hooks';
+
+interface Props {
+  onNextStep: () => void;
+  seed: string;
+}
+
+const onCopy = (): void => {
+  const mnemonicSeedTextElement = document.querySelector('textarea');
+
+  if (!mnemonicSeedTextElement) {
+    return;
+  }
+
+  mnemonicSeedTextElement.select();
+  document.execCommand('copy');
+};
+
+function Mnemonic({ onNextStep, seed }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+  const theme = useTheme();
+  const [isMnemonicSaved, setIsMnemonicSaved] = useState(false);
+  const { show } = useToast();
+
+  const _onCopy = useCallback((): void => {
+    onCopy();
+    show(t('Copied'));
+  }, [show, t]);
+
+  return (
+    <>
+      <MnemonicSeed
+        onCopy={_onCopy}
+        seed={seed}
+      />
+      <Warning theme={theme}>
+        {t<string>('Please write down your wallet’s mnemonic seed and keep it in a safe place. The mnemonic can be used to restore your wallet. Keep it carefully to not lose your assets.')}
+      </Warning>
+      <Checkbox
+        checked={isMnemonicSaved}
+        label={t<string>('I have saved my mnemonic seed safely.')}
+        onChange={setIsMnemonicSaved}
+        style={{ fontSize: '16px', marginTop: '55px', mx: 'auto', width: '92%' }}
+        theme={theme}
+      />
+      <PButton
+        _onClick={onNextStep}
+        _variant='contained'
+        disabled={!isMnemonicSaved}
+        text={t<string>('Next')}
+      />
+    </>
+  );
+}
+
+export default React.memo(Mnemonic);
---
+++ packages/extension-polkagate/src/popup/createAccount/index.tsx
@@ -0,0 +1,158 @@
+// Copyright 2019-2023 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+import { Chain } from '@polkadot/extension-chains/types';
+
+import { AccountNamePasswordCreation, ActionContext, Address, DropdownWithIcon, Loading } from '../../components';
+import { useGenesisHashOptions, useMetadata, useTranslation } from '../../hooks';
+import { createAccountSuri, createSeed, getMetadata, validateSeed } from '../../messaging';
+import HeaderBrand from '../../partials/HeaderBrand';
+import { DEFAULT_TYPE } from '../../util/defaultType';
+import getLogo from '../../util/getLogo';
+import Mnemonic from './Mnemonic';
+
+interface Props {
+  className?: string;
+}
+
+function CreateAccount({ className }: Props): React.ReactElement {
+  const { t } = useTranslation();
+  const onAction = useContext(ActionContext);
+  const options = useGenesisHashOptions();
+  const [isBusy, setIsBusy] = useState(false);
+  const [newChain, setNewChain] = useState<Chain | null>(null);
+  const [step, setStep] = useState(1);
+  const [address, setAddress] = useState<null | string>(null);
+  const [seed, setSeed] = useState<null | string>(null);
+  const [type, setType] = useState(DEFAULT_TYPE);
+  const [name, setName] = useState('');
+  const [genesisHash, setGenesis] = useState<string | undefined>('');
+  const chain = useMetadata(genesisHash, true);
+
+  useEffect((): void => {
+    createSeed(undefined)
+      .then(({ address, seed }): void => {
+        setAddress(address);
+        setSeed(seed);
+      })
+      .catch(console.error);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect((): void => {
+    if (seed) {
+      const type = chain && chain.definition.chainType === 'ethereum'
+        ? 'ethereum'
+        : DEFAULT_TYPE;
+
+      setType(type);
+      validateSeed(seed, type)
+        .then(({ address }) => setAddress(address))
+        .catch(console.error);
+    }
+  }, [seed, chain]);
+
+  const _onCreate = useCallback(
+    (name: string, password: string): void => {
+      // this should always be the case
+      if (name && password && seed) {
+        setIsBusy(true);
+
+        createAccountSuri(name, password, seed, type, genesisHash)
+          .then(() => onAction('/'))
+          .catch((error: Error): void => {
+            setIsBusy(false);
+            console.error(error);
+          });
+      }
+    },
+    [genesisHash, onAction, seed, type]
+  );
+
+  const _onNextStep = useCallback(
+    () => setStep((step) => step + 1),
+    []
+  );
+
+  const _onPreviousStep = useCallback(
+    () => setStep((step) => step - 1),
+    []
+  );
+
+  const _onChangeNetwork = useCallback(
+    (newGenesisHash: string) => setGenesis(newGenesisHash),
+    []
+  );
+
+  const _onBackClick = useCallback(() => {
+    step === 1 ? onAction('/') : _onPreviousStep();
+  }, [_onPreviousStep, onAction, step]);
+
+  useEffect(() => {
+    genesisHash && getMetadata(genesisHash, true).then(setNewChain).catch((error): void => {
+      console.error(error);
+      setNewChain(null);
+    });
+  }, [genesisHash]);
+
+  return (
+    <>
+      <HeaderBrand
+        onBackClick={_onBackClick}
+        showBackArrow
+        text={t<string>('Create an Account')}
+        withSteps={{
+          current: `${step}`,
+          total: 2
+        }}
+      />
+      <Loading>
+        <div>
+          <Address
+            address={address}
+            genesisHash={genesisHash}
+            name={name}
+          />
+        </div>
+        {seed && (
+          step === 1
+            ? (
+              <Mnemonic
+                onNextStep={_onNextStep}
+                seed={seed}
+              />
+            )
+            : (
+              <>
+                <DropdownWithIcon
+                  defaultValue={newChain?.genesisHash || options[0].text}
+                  icon={getLogo(newChain ?? undefined)}
+                  label={t<string>('Select the chain')}
+                  onChange={_onChangeNetwork}
+                  options={options}
+                  style={{ margin: 'auto', p: 0, width: '92%' }}
+                />
+                <AccountNamePasswordCreation
+                  buttonLabel={t<string>('Create account')}
+                  isBusy={isBusy}
+                  onCreate={_onCreate}
+                  onNameChange={setName}
+                />
+              </>
+            )
+        )}
+      </Loading>
+    </>
+  );
+}
+
+export default styled(CreateAccount)`
+  margin-bottom: 16px;
+
+  label::after {
+    right: 36px;
+  }
+`;
