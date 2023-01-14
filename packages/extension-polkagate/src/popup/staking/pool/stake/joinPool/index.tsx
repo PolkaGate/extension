eed7d3cfd722db5937c072a3daf9559bf21a4a46
Kami <ekbatanifard@gmail.com>
2023-01-14 22:28:24 +0330

    Update index.tsx

--- packages/extension-polkagate/src/popup/staking/pool/stake/joinPool/index.tsx
+++ packages/extension-polkagate/src/popup/staking/pool/stake/joinPool/index.tsx
@@ -159,7 +159,7 @@ export default function JoinPool(): React.ReactElement {
         primaryBtnText={t<string>('Min amount')}
         secondaryBtnText={t<string>('Max amount')}
         style={{
-          m: '20px auto 10px',
+          m: '20px auto 7px',
           width: '92%'
         }}
         value={stakeAmount}
@@ -168,7 +168,7 @@ export default function JoinPool(): React.ReactElement {
         <Typography fontSize='14px' fontWeight={300} lineHeight='23px'>
           {t<string>('Fee:')}
         </Typography>
-        <Grid item lineHeight='22px' pl='5px'>
+        <Grid fontSize='14px' fontWeight={400} item lineHeight='22px' pl='5px'>
           <ShowBalance
             api={api}
             balance={estimatedFee}
