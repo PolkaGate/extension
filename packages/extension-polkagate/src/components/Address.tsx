69b34e1d7f89431fe205607a7fb84c25d63cc2de
Amir Ekbatani Fard <amiref007@gmail.com>
2023-02-08 14:21:50 +0330

    Update Address.tsx
    Update Address.tsx

--- packages/extension-polkagate/src/components/Address.tsx
+++ packages/extension-polkagate/src/components/Address.tsx
@@ -1,35 +1,29 @@
 // Copyright 2019-2023 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { AccountJson, AccountWithChildren } from '@polkadot/extension-base/background/types';
+/* eslint-disable react/jsx-max-props-per-line */
+
+import type { AccountJson } from '@polkadot/extension-base/background/types';
 import type { Chain } from '@polkadot/extension-chains/types';
 import type { IconTheme } from '@polkadot/react-identicon/types';
 import type { SettingsStruct } from '@polkadot/ui-settings/types';
 import type { KeypairType } from '@polkadot/util-crypto/types';
 
 import { Grid, SxProps, Theme, Typography } from '@mui/material';
-import React, { useContext, useEffect, useMemo, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import { decodeAddress, encodeAddress } from '@polkadot/util-crypto';
 
-import { useAccountName, useTranslation } from '../hooks';
+import { useAccount, useAccountName, useTranslation } from '../hooks';
 import useMetadata from '../hooks/useMetadata';
 import { DEFAULT_TYPE } from '../util/defaultType';
-import { AccountContext, Identicon, SettingsContext, ShortAddress } from './';
+import { Identicon, SettingsContext, ShortAddress } from '.';
 
-export interface Props {
-  actions?: React.ReactNode;
+interface Props {
   address?: string | null;
-  children?: React.ReactNode;
   className?: string;
   genesisHash?: string | null;
-  isExternal?: boolean | null;
-  isHardware?: boolean | null;
-  isHidden?: boolean;
   name?: string | null;
-  parentName?: string | null;
-  suri?: string;
-  toggleActions?: number;
   type?: KeypairType;
   style?: SxProps<Theme> | undefined;
   showCopy?: boolean;
@@ -38,36 +32,18 @@ export interface Props {
 }
 
 interface Recoded {
-  account: AccountJson | null;
+  account: AccountJson | undefined | null;
   formatted: string | null;
   genesisHash?: string | null;
   prefix?: number;
   type: KeypairType;
 }
 
-// find an account in our list
-function findSubstrateAccount(accounts: AccountJson[], publicKey: Uint8Array): AccountJson | null {
-  const pkStr = publicKey.toString();
-
-  return accounts.find(({ address }): boolean =>
-    decodeAddress(address).toString() === pkStr
-  ) || null;
-}
-
-// find an account in our list
-function findAccountByAddress(accounts: AccountJson[], _address: string): AccountJson | null {
-  return accounts.find(({ address }): boolean =>
-    address === _address
-  ) || null;
-}
-
 // recodes an supplied address using the prefix/genesisHash, include the actual saved account & chain
-function recodeAddress(address: string, accounts: AccountWithChildren[], chain: Chain | null, settings: SettingsStruct): Recoded {
+function recodeAddress(address: string, account: AccountJson | null | undefined, chain: Chain | null, settings: SettingsStruct): Recoded {
   // decode and create a shortcut for the encoded address
   const publicKey = decodeAddress(address);
 
-  // find our account using the actual publicKey, and then find the associated chain
-  const account = findSubstrateAccount(accounts, publicKey);
   const prefix = chain ? chain.ss58Format : (settings.prefix === -1 ? 42 : settings.prefix);
 
   // always allow the actual settings to override the display
@@ -84,9 +60,10 @@ function recodeAddress(address: string, accounts: AccountWithChildren[], chain: 
 
 const defaultRecoded = { account: null, formatted: null, prefix: 42, type: DEFAULT_TYPE };
 
-function Address({ address, className, genesisHash, isExternal, isHardware, margin = '20px auto', name, width = '92%', showCopy = true, style, type: givenType }: Props): React.ReactElement<Props> {
+export default function Address ({ address, className, genesisHash, margin = '20px auto', name, showCopy = true, style, type: givenType, width = '92%' }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
-  const { accounts } = useContext(AccountContext);
+  const account = useAccount(address);
+
   const accountName = useAccountName(address);
   const settings = useContext(SettingsContext);
   const [{ formatted, genesisHash: recodedGenesis, prefix, type }, setRecoded] = useState<Recoded>(defaultRecoded);
@@ -97,100 +74,45 @@ function Address({ address, className, genesisHash, isExternal, isHardware, marg
       return setRecoded(defaultRecoded);
     }
 
-    const account = findAccountByAddress(accounts, address);
+    setRecoded((chain?.definition.chainType === 'ethereum' || account?.type === 'ethereum' || (!account && givenType === 'ethereum'))
+      ? { account, formatted: address, type: 'ethereum' }
+      : recodeAddress(address, account, chain, settings));
+  }, [account, address, chain, givenType, settings]);
 
-    setRecoded(
-      (
-        chain?.definition.chainType === 'ethereum' ||
-        account?.type === 'ethereum' ||
-        (!account && givenType === 'ethereum')
-      )
-        ? { account, formatted: address, type: 'ethereum' }
-        : recodeAddress(address, accounts, chain, settings)
-    );
-  }, [accounts, address, chain, givenType, settings]);
-
-  const theme = (
-    type === 'ethereum'
-      ? 'ethereum'
-      : (chain?.icon || 'polkadot')
-  ) as IconTheme;
+  const theme = (type === 'ethereum' ? 'ethereum' : (chain?.icon || 'polkadot')) as IconTheme;
 
   return (
     <>
-      <Grid
-        className={className}
-        container
-        direction={'row'}
-        justifyContent={'space-between'}
-        sx={{
-          backgroundColor: 'background.paper',
-          border: '0.5px solid',
-          borderColor: 'secondary.light',
-          borderRadius: '5px',
-          height: '70px',
-          m: { margin },
-          p: '14px 8px',
-          width: { width },
-          ...style
-        }}
-      >
-        <Grid
-          item
-          width='40px'
-        >
+      <Grid className={className} container direction='row' justifyContent='space-between' sx={{ backgroundColor: 'background.paper', border: '0.5px solid', borderColor: 'secondary.light', borderRadius: '5px', height: '70px', m: { margin }, p: '14px 8px', width: { width }, ...style }}>
+        <Grid item width='40px'>
           <Identicon
             className='identityIcon'
             iconTheme={theme}
-            isExternal={isExternal}
-            // onCopy={_onCopy}
             prefix={prefix}
             size={40}
             value={formatted || address}
           />
         </Grid>
-        <Grid
-          container
-          direction={'column'}
-          item
-          width='calc(95% - 40px)'
-        >
-          <Typography
-            fontSize={'16px'}
-            fontWeight={400}
-            maxWidth='95%'
-            overflow='hidden'
-            variant='h3'
-            whiteSpace='nowrap'
-          >
+        <Grid container direction='column' item width='calc(95% - 40px)'>
+          <Typography fontSize={'16px'} fontWeight={400} maxWidth='95%' overflow='hidden' variant='h3' whiteSpace='nowrap'>
             {name || accountName || t('<unknown>')}
           </Typography>
-          <Grid
-            container
-            direction={'row'}
-            item
-            justifyContent={'space-between'}
-          >
+          <Grid container direction={'row'} item justifyContent={'space-between'}>
             {(formatted || address)
               ? (
                 <ShortAddress
                   address={formatted || address}
+                  clipped
+                  showCopy={showCopy}
                   style={{
                     fontSize: '10px',
                     fontWeight: 300,
                     justifyContent: 'space-between',
                     lineHeight: '23px'
                   }}
-                  clipped
-                  showCopy={showCopy}
                 />)
               : (
-                <Typography
-                  fontSize={'10px'}
-                  fontWeight={300}
-                  variant='h3'
-                  whiteSpace='nowrap'
-                >
+                <Typography fontSize={'10px'} fontWeight={300} variant='h3' whiteSpace='nowrap'>
                   {t('<unknown>')}
                 </Typography>)
             }
@@ -200,5 +122,3 @@ function Address({ address, className, genesisHash, isExternal, isHardware, marg
     </>
   );
 }
-
-export default (Address);
