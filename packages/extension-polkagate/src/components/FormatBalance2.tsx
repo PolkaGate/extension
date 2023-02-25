890b85e2c6390ce98da7a5e6fe073b3c911ce7b4
Amir Ekbatani Fard <amiref007@gmail.com>
2023-02-25 16:51:45 +0330

    Update FormatBalance2.tsx

--- packages/extension-polkagate/src/components/FormatBalance2.tsx
+++ packages/extension-polkagate/src/components/FormatBalance2.tsx
@@ -2,103 +2,44 @@
 // SPDX-License-Identifier: Apache-2.0
 /* eslint-disable header/header */
 
-import type BN from 'bn.js';
 import type { Compact } from '@polkadot/types';
 
-import React, { useMemo } from 'react';
+import React from 'react';
 
-import { formatBalance, isString } from '@polkadot/util';
+import { BN, formatBalance } from '@polkadot/util';
 
-import { useTranslation } from '../hooks';
 import { FLOATING_POINT_DIGIT } from '../util/constants';
 
 interface Props {
   decimals: number[],
   tokens: string[],
-  children?: React.ReactNode;
-  className?: string;
-  format?: [number, string];
-  formatIndex?: number;
-  isShort?: boolean;
   label?: React.ReactNode;
-  labelPost?: LabelPost;
-  value?: Compact<any> | BN | string | null | 'all';
-  valueFormatted?: string;
-  withCurrency?: boolean;
-  withSi?: boolean;
+  value?: Compact<any> | BN | string | null;
   decimalPoint?: number;
 }
 
-// for million, 2 * 3-grouping + comma
-const M_LENGTH = 6 + 1;
-const K_LENGTH = 3 + 1;
-
-type LabelPost = string | React.ReactNode
-
-function getFormat(decimals: number[], tokens: string[], formatIndex = 0): [number, string] {
-  return [
-    formatIndex < decimals.length
-      ? decimals[formatIndex]
-      : decimals[0],
-    formatIndex < tokens.length
-      ? tokens[formatIndex]
-      : tokens[1]
-  ];
+function createElement(prefix: string, postfix: string, unit: string, decimalPoint: number): React.ReactNode {
+  return <>{`${prefix}.`}{<span className='ui--FormatBalance-postfix'>{`00${postfix?.slice(0, decimalPoint) || ''}`.slice(-decimalPoint)}</span>}<span className='ui--FormatBalance-unit'> {unit}</span></>;
 }
 
-function createElement(prefix: string, postfix: string, unit: string, label: LabelPost = '', isShort = false, decimalPoint: number): React.ReactNode {
-  return <>{`${prefix}${isShort ? '' : '.'}`}{!isShort && <span className='ui--FormatBalance-postfix'>{`00${postfix?.slice(0, decimalPoint) || ''}`.slice(-decimalPoint)}</span>}<span className='ui--FormatBalance-unit'> {unit}</span>{label}</>;
-}
-
-function splitFormat(value: string, decimalPoint: number, label?: LabelPost, isShort?: boolean): React.ReactNode {
-  const [prefix, postfixFull] = value.split('.');
-  const [postfix, unit] = postfixFull.split(' ');
-
-  return createElement(prefix, postfix, unit, label, isShort, decimalPoint);
-}
-
-function applyFormat(decimalPoint: number, value: Compact<any> | BN | string, [decimals, token]: [number, string], withCurrency = true, withSi?: boolean, _isShort?: boolean, labelPost?: LabelPost): React.ReactNode {
+function applyFormat(decimalPoint: number, value: Compact<any> | BN | string, [decimals, token]: [number, string]): React.ReactNode {
   const [prefix, postfix] = formatBalance(value, { decimals, forceUnit: '-', withSi: false }).split('.');
-  const isShort = _isShort || (withSi && prefix.length >= K_LENGTH);
-  const unitPost = withCurrency ? token : '';
 
-  if (prefix.length > K_LENGTH) {
-    const [major, rest] = formatBalance(value, { decimals, withUnit: false }).split('.');
-    const minor = rest.substr(0, decimalPoint);
-    const unit = rest.substr(4);
-
-    return <>{major}.<span className='ui--FormatBalance-postfix'>{minor}</span><span className='ui--FormatBalance-unit'>{unit}{unit ? unitPost : ` ${unitPost}`}</span>{labelPost || ''}</>;
-  }
-
-  return createElement(prefix, postfix, unitPost, labelPost, isShort, decimalPoint);
+  return createElement(prefix, postfix, token, decimalPoint);
 }
 
-function FormatBalance({ children, className = '', decimalPoint = FLOATING_POINT_DIGIT, decimals, format, formatIndex, isShort, label, labelPost, tokens, value, valueFormatted, withCurrency, withSi }: Props): React.ReactElement<Props> {
-  const { t } = useTranslation();
-
-  const formatInfo = useMemo(
-    () => format || getFormat(decimals, tokens, formatIndex),
-    [decimals, tokens, format, formatIndex]
-  );
-
-  // labelPost here looks messy, however we ensure we have one less text node
+function FormatBalance({ decimalPoint = FLOATING_POINT_DIGIT, decimals, label, tokens, value }: Props): React.ReactElement<Props> {
   return (
-    <div className={`ui--FormatBalance ${className}`}>
+    <div className='ui--FormatBalance'>
       {label ? <>{label}&nbsp;</> : ''}
       <span
         className='ui--FormatBalance-value'
         data-testid='balance-summary'
       >{
-          valueFormatted
-            ? splitFormat(valueFormatted, decimalPoint, labelPost, isShort)
-            : value
-              ? value === 'all'
-                ? <>{t<string>('everything')}{labelPost || ''}</>
-                : applyFormat(decimalPoint, value, formatInfo, withCurrency, withSi, isShort, labelPost)
-              : isString(labelPost)
-                ? `-${labelPost}`
-                : labelPost
-        }</span>{children}
+          value && decimals?.length && tokens?.length
+            ? applyFormat(decimalPoint, value, [decimals[0], tokens[0]])
+            : ''
+        }</span>
     </div>
   );
 }
