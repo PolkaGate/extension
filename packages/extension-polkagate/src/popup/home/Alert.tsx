0fab7b78020ff4f29a769baad3bdb8941965e744
Kami <ekbatanifard@gmail.com>
2023-04-15 12:27:28 +0430

    Update Alert.tsx

--- packages/extension-polkagate/src/popup/home/Alert.tsx
+++ packages/extension-polkagate/src/popup/home/Alert.tsx
@@ -1,8 +1,8 @@
 // Copyright 2019-2023 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { Grid, useTheme } from '@mui/material';
 import React, { useCallback, useContext } from 'react';
+import { Grid, useTheme } from '@mui/material';
 
 import { ActionContext, Header, PButton, Popup, Warning } from '../../components';
 import useTranslation from '../../hooks/useTranslation';
@@ -22,41 +22,32 @@ export default function Alert({ setShowAlert, show }: Props): React.ReactElement
     onAction('/');
   }, [onAction, setShowAlert]);
 
-  const _goToExportAll = useCallback(
-    () => {
-      window.localStorage.setItem('export_account_open', 'ok');
-      onAction('/account/export-all');
-    }, [onAction]
-  );
+  const goToExportAll = useCallback(() => {
+    window.localStorage.setItem('export_account_open', 'ok');
+    onAction('/account/export-all');
+  }, [onAction]);
 
   return (
     <Popup show={show}>
       <Header onClose={goHome} text={t<string>('Attention!')} />
-      <Grid height='120px' m='auto' pt='40px' width='90%'>
-        <Warning
-          fontWeight={400}
-          isBelowInput
-          isDanger
-          theme={theme}
-        >
-          <Grid container item xs={12}>
-            <Grid item xs={12} sx={{ pb: '20px', fontSize: 20 }}>
-              Export Your Account Data!
-            </Grid>
-
+      <Grid container justifyContent="center" alignItems="center" height="120px" width="90%">
+        <Warning fontWeight={400} isBelowInput isDanger theme={theme}>
+          <Grid item xs={12} sx={{ pb: '20px', fontSize: 20 }}>
+            Export Your Account Data!
           </Grid>
         </Warning>
       </Grid>
-      <Grid container sx={{ px: '15px' }}>
-        Protect your accounts - Export all your data and keep it in a safe place. Safeguard against potential loss due to browser crashes or inconsistent updates.
+      <Grid container justifyContent="center" sx={{ px: '15px', mt: '20px' }}>
+        <Grid item xs={12} sx={{ textAlign: 'center' }}>
+          Protect your accounts - Export all your data and keep it in a safe place. Safeguard against potential loss due to browser crashes or inconsistent updates.
+        </Grid>
       </Grid>
-      <Grid container sx={{ px: '15px', pt: '20px' }}>
-        Take action now to keep your accounts safe!
+      <Grid container justifyContent="center" sx={{ px: '15px', mt: '20px' }}>
+        <Grid item xs={12} sx={{ textAlign: 'center' }}>
+          Take action now to keep your accounts safe!
+        </Grid>
       </Grid>
-      <PButton
-        _onClick={_goToExportAll}
-        text={t<string>('Export All accounts')}
-      />
+      <PButton _onClick={goToExportAll} text={t<string>('Export All Accounts')} />
     </Popup>
   );
 }
