1819ec5f303eaeeae644e151608f6dbdd51b0c75
Kami <ekbatanifard@gmail.com>
2023-04-15 12:43:38 +0430

    Update Alert.tsx

--- packages/extension-polkagate/src/popup/home/Alert.tsx
+++ packages/extension-polkagate/src/popup/home/Alert.tsx
@@ -30,21 +30,21 @@ export default function Alert({ setShowAlert, show }: Props): React.ReactElement
   return (
     <Popup show={show}>
       <Header onClose={goHome} text={t<string>('Attention!')} />
-      <Grid container justifyContent="center" alignItems="center" height="120px" width="90%">
+      <Grid container justifyContent='center' alignItems='center' height='120px'>
         <Warning fontWeight={400} isBelowInput isDanger theme={theme}>
-          <Grid item xs={12} sx={{ pb: '20px', fontSize: 20 }}>
-            Export Your Account Data!
+          <Grid item xs={12} sx={{ pb: '20px', fontSize: 19 }}>
+            Export Your Account Data Now!
           </Grid>
         </Warning>
       </Grid>
-      <Grid container justifyContent="center" sx={{ px: '15px', mt: '20px' }}>
-        <Grid item xs={12} sx={{ textAlign: 'center' }}>
-          Protect your accounts - Export all your data and keep it in a safe place. Safeguard against potential loss due to browser crashes or inconsistent updates.
+      <Grid container justifyContent='center' sx={{ px: '15px', mt: '20px' }}>
+        <Grid item xs={12} sx={{ textAlign: 'left' }}>
+          Protect Your Accounts - Export all your data and securely store it in a safe place. Safeguard against potential loss due to browser crashes, hardware failures, or inconsistent updates.
         </Grid>
       </Grid>
-      <Grid container justifyContent="center" sx={{ px: '15px', mt: '20px' }}>
-        <Grid item xs={12} sx={{ textAlign: 'center' }}>
-          Take action now to keep your accounts safe!
+      <Grid container justifyContent='center' sx={{ px: '15px', mt: '20px' }}>
+        <Grid item xs={12} sx={{ textAlign: 'left', pt: '15px' }}>
+          Take Action Now to Ensure the Safety of Your Accounts!
         </Grid>
       </Grid>
       <PButton _onClick={goToExportAll} text={t<string>('Export All Accounts')} />
