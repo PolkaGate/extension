7b76686bf1e75fa01d99f0cd9d72d51587363b60
Amir Ekbatani Fard <amiref007@gmail.com>
2023-05-13 13:09:09 +0330

    Update Alert.tsx

--- packages/extension-polkagate/src/popup/home/Alert.tsx
+++ packages/extension-polkagate/src/popup/home/Alert.tsx
@@ -4,7 +4,7 @@
 /* eslint-disable react/jsx-max-props-per-line */
 
 import CircleIcon from '@mui/icons-material/Circle';
-import { Grid, useTheme } from '@mui/material';
+import { Grid, useTheme, Typography } from '@mui/material';
 import React, { useCallback, useContext } from 'react';
 
 import { ActionContext, Header, PButton, Popup, Warning } from '../../components';
@@ -27,21 +27,44 @@ export default function Alert({ setShowAlert, show }: Props): React.ReactElement
     onAction('/');
   }, [onAction, setShowAlert]);
 
-  const Item = ({ text }: { text: string }) => (
-    <Grid container justifyContent='center' sx={{ mt: '10px' }}>
-      <Grid item sx={{ textAlign: 'left' }} xs={1}>
-        <CircleIcon sx={{ fontSize: '16px', mr: '10px', mt: '5px' }} />
+  // const Item = ({ text }: { text: string }) => (
+  //   <Grid container justifyContent='center' sx={{ mt: '10px' }}>
+  //     <Grid item sx={{ textAlign: 'left' }} xs={1}>
+  //       <CircleIcon sx={{ fontSize: '16px', mr: '10px', mt: '5px' }} />
+  //     </Grid>
+  //     <Grid item sx={{ textAlign: 'left' }} xs={11}>
+  //       {text}
+  //     </Grid>
+  //   </Grid>
+  // );
+
+  const UL = ({ notes, title }: { title: string, notes: string[] }) => {
+    return (
+      <Grid container direction='column' pt='15px'>
+        <Grid container item>
+          <Typography fontSize='16px' fontWeight={500}>
+            {title}
+          </Typography>
+        </Grid>
+        <Grid container item>
+          <ul style={{ margin: 0, paddingLeft: '25px' }}>
+            {notes.map((note, index) => (
+              <li key={index} style={{ paddingTop: '10px' }}>
+                <Typography fontSize='14px' fontWeight={400} textAlign='left'>
+                  {note}
+                </Typography>
+              </li>
+            ))}
+          </ul>
+        </Grid>
       </Grid>
-      <Grid item sx={{ textAlign: 'left' }} xs={11}>
-        {text}
-      </Grid>
-    </Grid>
-  );
+    );
+  };
 
   return (
     <Popup show={show}>
-      <Header onClose={goHome} text={t<string>('Polkagate Updated!')} />
-      <Grid alignItems='center' container height='120px' justifyContent='center'>
+      <Header onClose={goHome} text={t<string>('Polkagate')} />
+      {/* <Grid alignItems='center' container height='120px' justifyContent='center'>
         <Warning fontWeight={400} isBelowInput theme={theme}>
           <Grid item sx={{ fontSize: 19, pb: '20px' }} xs={12}>
             The latest in this release:
@@ -58,8 +81,25 @@ export default function Alert({ setShowAlert, show }: Props): React.ReactElement
         <Item
           text={'Known issues have been fixed.'}
         />
+      </Grid> */}
+      <Grid container direction='column' px='15px'>
+        <Grid container item justifyContent='center' py='50px'>
+          <Typography fontSize='22px' fontWeight={700} pt='45px'>
+            {t<string>('PolkaGate Updated!')}
+          </Typography>
+        </Grid>
+        <Grid container item>
+          <UL
+            notes={[
+              t<string>('The testnet is disabled by default. However, It can enabled through the main menu/settings by selecting "Enable testnet chain".'),
+              t<string>('"Lucky Friday" has been added to both the Kusama and Polkadot networks as an endpoint.'),
+              t<string>('Known issues have been fixed.'),
+            ]}
+            title={t<string>('The latest in this release:')}
+          />
+        </Grid>
       </Grid>
       <PButton _onClick={goHome} text={t<string>('Ok')} />
-    </Popup >
+    </Popup>
   );
 }
