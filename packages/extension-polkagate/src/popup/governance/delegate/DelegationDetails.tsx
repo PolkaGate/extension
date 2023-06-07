dbee4797e7868f36182e652e98d31b8701550743
Amir Ekbatani Fard <amiref007@gmail.com>
2023-06-07 15:05:55 +0330

    Create DelegationDetails.tsx

---
+++ packages/extension-polkagate/src/popup/governance/delegate/DelegationDetails.tsx
@@ -0,0 +1,235 @@
+// Copyright 2019-2023 @polkadot/extension-polkadot authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/* eslint-disable react/jsx-max-props-per-line */
+
+/**
+ * @description
+ * this component opens send review page
+ * */
+
+import { KeyboardDoubleArrowLeft as KeyboardDoubleArrowLeftIcon, KeyboardDoubleArrowRight as KeyboardDoubleArrowRightIcon } from '@mui/icons-material';
+import { Divider, Grid, Typography } from '@mui/material';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+
+import { BN } from '@polkadot/util';
+
+import { Identity, Motion, TwoButtons } from '../../../components';
+import { useApi, useChain, useDecimal, useToken, useTracks, useTranslation } from '../../../hooks';
+import { TxInfo } from '../../../util/types';
+import { amountToHuman } from '../../../util/utils';
+import DisplayValue from '../post/castVote/partial/DisplayValue';
+import ReferendaTable from './partial/ReferendaTable';
+import RemoveDelegate from './RemoveDelegate';
+import { DelegateInformation, DiffDelegation, STEPS } from '.';
+
+interface Props {
+  address: string | undefined;
+  formatted: string | undefined;
+  filteredDelegation: DiffDelegation[] | null | undefined;
+  setStep: React.Dispatch<React.SetStateAction<number>>;
+  step: number;
+  setTxInfo: React.Dispatch<React.SetStateAction<TxInfo | undefined>>;
+  setStatus: React.Dispatch<React.SetStateAction<'Delegate' | 'Remove' | 'Modify'>>
+}
+
+interface ArrowsProps {
+  onNext: () => void;
+  onPrevious: () => void;
+}
+
+export default function DelegationDetails({ address, filteredDelegation, formatted, setStep, setStatus, setTxInfo, step }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+  const api = useApi(address);
+  const chain = useChain(address);
+  const token = useToken(address);
+  const decimal = useDecimal(address);
+  const { tracks } = useTracks(address);
+  const ref = useRef(null);
+
+  const [delegateeIndex, setDelegateeIndex] = useState<number>(0);
+  const [modalHeight, setModalHeight] = useState<number | undefined>();
+
+  useEffect(() => {
+    if (ref) {
+      setModalHeight(ref.current?.offsetHeight as number);
+      console.log('ref.current?.offsetHeight:', ref.current?.offsetHeight)
+    }
+  }, []);
+
+  const variousDelegation = useMemo(() => {
+    if (!filteredDelegation) {
+      return;
+    }
+
+    const compareArray = filteredDelegation[delegateeIndex].info;
+
+    for (let i = 0; i < compareArray.length - 1; i++) {
+      if (compareArray[i].conviction !== compareArray[i + 1].conviction || !compareArray[i].delegatedBalance.eq(compareArray[i + 1].delegatedBalance)) {
+        return true;
+      }
+    }
+
+    return false;
+  }, [delegateeIndex, filteredDelegation]);
+
+  const delegateAmountInHuman = useMemo(() => filteredDelegation && !variousDelegation ? amountToHuman(filteredDelegation[delegateeIndex].info[0].delegatedBalance, decimal) : undefined, [decimal, delegateeIndex, variousDelegation, filteredDelegation]);
+
+  const delegatePower = useCallback((conviction: number, delegateAmountBN: BN) => {
+    if (conviction === undefined || delegateAmountBN.isZero()) {
+      return 0;
+    }
+
+    const bn = conviction !== 0.1 ? delegateAmountBN.muln(conviction) : delegateAmountBN.divn(10);
+
+    return Number(amountToHuman(bn, decimal));
+  }, [decimal]);
+
+  const classicDelegation: DelegateInformation | undefined = useMemo(() =>
+    filteredDelegation && filteredDelegation.length === 1 && !variousDelegation
+      ? {
+        delegateAmount: delegateAmountInHuman,
+        delegateAmountBN: filteredDelegation[0].info[0].delegatedBalance,
+        delegateConviction: filteredDelegation[0].info[0].conviction,
+        delegatePower: delegatePower(filteredDelegation[0].info[0].conviction, filteredDelegation[0].info[0].delegatedBalance),
+        delegatedTracks: filteredDelegation[0].info.map((value) => value.track),
+        delegateeAddress: filteredDelegation[0].delegatee
+      }
+      : undefined
+    , [delegateAmountInHuman, delegatePower, filteredDelegation, variousDelegation]);
+
+  const Arrows = ({ onNext, onPrevious }: ArrowsProps) => (
+    <Grid container justifyContent='space-between' m='10px auto 0'>
+      <Grid alignItems='center' container item onClick={onPrevious} sx={{ cursor: 'pointer' }} xs={1}>
+        <KeyboardDoubleArrowLeftIcon sx={{ color: 'secondary.light', fontSize: '25px' }} />
+      </Grid>
+      <Grid alignItems='center' container item justifyContent='center' xs={10}>
+        <Grid alignItems='center' container direction='column' justifyContent='center' width='90%'>
+          <Typography fontSize='16px' fontWeight={400} lineHeight='23px'>
+            {t<string>('Delegate To')}:
+          </Typography>
+          <Identity
+            formatted={filteredDelegation[delegateeIndex].delegatee}
+            api={api}
+            chain={chain}
+            identiconSize={31}
+            showSocial={false}
+            style={{ maxWidth: '100%', width: 'fit-content' }}
+          />
+        </Grid>
+      </Grid>
+      <Grid alignItems='center' container onClick={onNext} sx={{ cursor: 'pointer' }} xs={1}>
+        <KeyboardDoubleArrowRightIcon sx={{ color: 'secondary.light', fontSize: '25px' }} />
+      </Grid>
+    </Grid>
+  );
+
+  const nextDelegatee = useCallback(() => {
+    filteredDelegation && (delegateeIndex === filteredDelegation.length - 1 ? setDelegateeIndex(0) : setDelegateeIndex(delegateeIndex + 1));
+  }, [delegateeIndex, filteredDelegation]);
+
+  const previousDelegatee = useCallback(() => {
+    filteredDelegation && (delegateeIndex === 0 ? setDelegateeIndex(filteredDelegation.length - 1) : setDelegateeIndex(delegateeIndex - 1));
+  }, [delegateeIndex, filteredDelegation]);
+
+  const goModify = useCallback(() => {
+    setStep(STEPS.MODIFY);
+    setStatus('Modify');
+  }, [setStatus, setStep]);
+
+  const goRemove = useCallback(() => {
+    setStep(STEPS.REMOVE);
+    setStatus('Remove');
+  }, [setStatus, setStep]);
+
+  return (
+    <Motion style={{ height: '100%' }}>
+      {step === STEPS.PREVIEW &&
+        <Grid container ref={ref}>
+          <Grid alignItems='center' container direction='column' justifyContent='center' sx={{ m: 'auto', pt: '30px', width: '90%' }}>
+            <Typography fontSize='16px' fontWeight={400} lineHeight='23px'>
+              {t<string>('Delegate from')}:
+            </Typography>
+            <Identity
+              address={address}
+              api={api}
+              chain={chain}
+              identiconSize={31}
+              showSocial={false}
+              style={{ maxWidth: '100%', width: 'fit-content' }}
+            />
+          </Grid>
+          <Divider sx={{ bgcolor: 'secondary.main', height: '2px', mx: 'auto', my: '5px', width: '170px' }} />
+          {filteredDelegation &&
+            (filteredDelegation.length > 1
+              ? <Arrows onNext={nextDelegatee} onPrevious={previousDelegatee} />
+              : <Grid alignItems='center' container direction='column' justifyContent='center' sx={{ m: 'auto', pt: '10px', width: '90%' }}>
+                <Typography fontSize='16px' fontWeight={400} lineHeight='23px'>
+                  {t<string>('Delegate to')}:
+                </Typography>
+                <Identity
+                  api={api}
+                  chain={chain}
+                  formatted={filteredDelegation[0].delegatee}
+                  identiconSize={31}
+                  showSocial={false}
+                  style={{ maxWidth: '100%', width: 'fit-content' }}
+                />
+              </Grid>)
+          }
+          <Divider sx={{ bgcolor: 'secondary.main', height: '2px', mx: 'auto', my: '5px', width: '170px' }} />
+          {filteredDelegation && variousDelegation &&
+            <Grid container item mb='70px' mt='15px'>
+              <ReferendaTable
+                decimal={decimal}
+                delegatedInfo={filteredDelegation[delegateeIndex].info}
+                token={token}
+                tracks={tracks}
+              />
+            </Grid>
+          }
+          {filteredDelegation && !variousDelegation &&
+            <>
+              <DisplayValue title={t<string>('Delegated Value ({{token}})', { replace: { token } })} topDivider={false}>
+                <Typography fontSize='28px' fontWeight={400}>
+                  {delegateAmountInHuman}
+                </Typography>
+              </DisplayValue>
+              <DisplayValue title={t<string>('Vote Multiplier')}>
+                <Typography fontSize='28px' fontWeight={400}>
+                  {`${filteredDelegation[delegateeIndex].info[0].conviction ? filteredDelegation[delegateeIndex].info[0].conviction : 0.1}x`}
+                </Typography>
+              </DisplayValue>
+              <DisplayValue title={t<string>('Number of Referenda Categories')}>
+                <Typography fontSize='28px' fontWeight={400}>
+                  {`${filteredDelegation[delegateeIndex].info.length} of ${tracks?.length ?? 15}`}
+                </Typography>
+              </DisplayValue>
+            </>
+          }
+          <Grid container item sx={{ '> div': { marginLeft: 0 }, bottom: '35px', position: 'absolute' }}>
+            <TwoButtons
+              mt='1px'
+              onPrimaryClick={goModify}
+              onSecondaryClick={goRemove}
+              primaryBtnText={t<string>('Modify')}
+              secondaryBtnText={t<string>('Remove')}
+            />
+          </Grid>
+        </Grid>
+      }
+      {(step === STEPS.REMOVE || step === STEPS.PROXY) && filteredDelegation &&
+        <RemoveDelegate
+          address={address}
+          delegateInformation={filteredDelegation[delegateeIndex]}
+          formatted={formatted}
+          modalHeight={modalHeight}
+          mode={classicDelegation ? 'classic' : 'variousDelegation'}
+          setStep={setStep}
+          setTxInfo={setTxInfo}
+          step={step}
+        />
+      }
+    </Motion>
+  );
+}
