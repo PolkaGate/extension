818c6b27bd3e63f962a876da7dc9187d6d87077a
Kami <ekbatanifard@gmail.com>
2023-06-11 15:38:37 +0330

    Update Standards.tsx

--- packages/extension-polkagate/src/popup/governance/post/allVote/Standards.tsx
+++ packages/extension-polkagate/src/popup/governance/post/allVote/Standards.tsx
@@ -143,7 +143,7 @@ export default function Standards({ address, allVotes, filteredVotes, numberOfFe
     minWidth: '108px',
     textTransform: 'capitalize',
     width: '33%'
-  }
+  };
 
   return (
     <DraggableModal onClose={handleClose} open={open} width={762}>
@@ -166,7 +166,7 @@ export default function Standards({ address, allVotes, filteredVotes, numberOfFe
                 theme={theme}
               // value={searchKeyword ?? ''}
               />
-              : <SearchIcon sx={{ color: 'rgba(0,0,0,0.2)', display: 'block', fontSize: '30px', width: 'fit-content' }} />
+              : <SearchIcon sx={{ color: 'secondary.contrastText', display: 'block', fontSize: '30px', width: 'fit-content' }} />
             }
           </Grid>
           <Grid item>
