832855d13e05f7535850d0c5b51087bb0d62cdbd
Amir Ekbatani Fard <amiref007@gmail.com>
2023-06-28 12:40:43 +0330

    Update PROXY_TYPE_POLKADOT

--- packages/extension-polkagate/src/util/constants.tsx
+++ packages/extension-polkagate/src/util/constants.tsx
@@ -97,7 +97,7 @@ export const STAKING_ACTIONS = ['Solo Staking', 'Pool Staking'];
 export const STATES_NEEDS_MESSAGE = ['withdrawUnbound', 'unstake', 'stopNominating', 'tuneUp'];
 export const CONFIRMING_STATE = ['fail', 'success', 'confirming'];
 
-const PROXY_TYPE_POLKADOT = ['Any', 'NonTransfer', 'Staking', 'Governance', 'IdentityJudgement', 'CancelProxy', 'Auction'];
+const PROXY_TYPE_POLKADOT = ['Any', 'NonTransfer', 'Staking', 'Governance', 'IdentityJudgement', 'CancelProxy', 'Auction', 'NominationPools'];
 const PROXY_TYPE_KUSAMA = ['Any', 'NonTransfer', 'Staking', 'Society', 'Governance', 'IdentityJudgement', 'CancelProxy', 'Auction', 'NominationPools'];
 const PROXY_TYPE_WESTEND = ['Any', 'NonTransfer', 'Staking', 'SudoBalances', 'IdentityJudgement', 'CancelProxy', 'Auction', 'NominationPools'];
 
