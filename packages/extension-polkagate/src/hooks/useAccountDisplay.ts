5dac942afa51484579cf9dcb4198b5074c1930e1
Amir Ekbatani Fard <amiref007@gmail.com>
2023-07-03 14:22:05 +0330

    Update useAccountDisplay.ts

--- packages/extension-polkagate/src/hooks/useAccountDisplay.ts
+++ packages/extension-polkagate/src/hooks/useAccountDisplay.ts
@@ -6,19 +6,25 @@ import { useEffect, useState } from 'react';
 import useAccountName from './useAccountName';
 import useMyAccountIdentity from './useMyAccountIdentity';
 
-export default function useAccountDisplay (address: string | undefined): string | undefined {
+export default function useAccountDisplay(address: string | undefined): string | undefined {
   const [name, setName] = useState<string | undefined>();
 
   const accountIdentityName = useMyAccountIdentity(address)?.display;
   const accountName = useAccountName(address);
 
   useEffect(() => {
+    if (!address) {
+      setName(undefined);
+
+      return;
+    }
+
     if (!accountIdentityName && !accountName) {
       return;
     }
 
     setName(accountIdentityName ?? accountName);
-  }, [accountIdentityName, accountName]);
+  }, [accountIdentityName, accountName, address]);
 
   return name;
 }
