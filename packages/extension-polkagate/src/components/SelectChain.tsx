dbf06dd279bbbd3b92fa9414f437e666de5ce9c2
Nick <ekbatanifard@gmail.com>
2023-07-10 21:00:56 +0330

    Update SelectChain.tsx

--- packages/extension-polkagate/src/components/SelectChain.tsx
+++ packages/extension-polkagate/src/components/SelectChain.tsx
@@ -84,7 +84,7 @@ function SelectChain({ address, defaultValue, disabledItems, icon = undefined, l
     } catch (error) {
       console.error(error);
     }
-  }, [address, onChange, previousGenesisHash]);
+  }, [address, onChange]);
 
   return (
     <Grid alignItems='flex-end' container justifyContent='space-between' pt={1} sx={{ ...style }}>
