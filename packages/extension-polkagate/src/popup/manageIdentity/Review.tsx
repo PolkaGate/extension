3f713856ba8e8db6d4689e895dfd5f667d55ea46
Amir Ekbatani Fard <amiref007@gmail.com>
2023-07-12 14:51:16 +0330

    Update Review.tsx

--- packages/extension-polkagate/src/popup/manageIdentity/Review.tsx
+++ packages/extension-polkagate/src/popup/manageIdentity/Review.tsx
@@ -6,7 +6,8 @@
 import type { Balance } from '@polkadot/types/interfaces';
 import type { PalletIdentityIdentityInfo } from '@polkadot/types/lookup';
 
-import { Divider, Grid, Typography } from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
+import { Divider, Grid, Typography, useTheme } from '@mui/material';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { ApiPromise } from '@polkadot/api';
@@ -15,10 +16,10 @@ import { Chain } from '@polkadot/extension-chains/types';
 import keyring from '@polkadot/ui-keyring';
 import { BN, BN_ONE } from '@polkadot/util';
 
-import { AccountHolderWithProxy, Motion, ShowBalance, WrongPasswordAlert } from '../../components';
+import { Identity, Motion, ShowBalance, WrongPasswordAlert } from '../../components';
 import { useAccountDisplay, useFormatted, useProxies } from '../../hooks';
 import useTranslation from '../../hooks/useTranslation';
-import { SubTitle } from '../../partials';
+import { ThroughProxy } from '../../partials';
 import { signAndSend } from '../../util/api';
 import { Proxy, ProxyItem, TxInfo } from '../../util/types';
 import { getSubstrateAddress, saveAsHistory } from '../../util/utils';
@@ -26,9 +27,10 @@ import { DraggableModal } from '../governance/components/DraggableModal';
 import PasswordWithTwoButtonsAndUseProxy from '../governance/components/PasswordWithTwoButtonsAndUseProxy';
 import SelectProxyModal from '../governance/components/SelectProxyModal';
 import WaitScreen from '../governance/partials/WaitScreen';
+import DisplayValue from '../governance/post/castVote/partial/DisplayValue';
 import Confirmation from './partial/Confirmation';
 import IdentityTable from './partial/IdentityTable';
-import { STEPS } from '.';
+import { Mode, STEPS } from '.';
 
 interface Props {
   address: string;
@@ -39,7 +41,7 @@ interface Props {
   infoParams: PalletIdentityIdentityInfo | null;
   setStep: React.Dispatch<React.SetStateAction<number>>;
   step: number;
-  mode: 'Set' | 'Remove' | 'Modify' | undefined;
+  mode: Mode;
   setRefresh: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
@@ -48,13 +50,12 @@ export default function Review({ address, api, chain, depositValue, identityToSe
   const name = useAccountDisplay(address);
   const formatted = useFormatted(address);
   const proxies = useProxies(api, formatted);
+  const theme = useTheme();
 
   const [estimatedFee, setEstimatedFee] = useState<Balance | undefined>();
   const [txInfo, setTxInfo] = useState<TxInfo | undefined>();
   const [password, setPassword] = useState<string>();
   const [isPasswordError, setIsPasswordError] = useState<boolean>(false);
-  const [showWaitScreen, setShowWaitScreen] = useState<boolean>(false);
-  const [showConfirmation, setShowConfirmation] = useState<boolean>(false);
   const [selectedProxy, setSelectedProxy] = useState<Proxy | undefined>();
   const [proxyItems, setProxyItems] = useState<ProxyItem[]>();
 
@@ -102,7 +103,7 @@ export default function Review({ address, api, chain, depositValue, identityToSe
       const signer = keyring.getPair(from);
 
       signer.unlock(password);
-      setShowWaitScreen(true);
+      setStep(STEPS.WAIT_SCREEN);
 
       const decidedTx = selectedProxy ? api.tx.proxy.proxy(formatted, selectedProxy.proxyType, tx) : tx;
 
@@ -116,7 +117,7 @@ export default function Review({ address, api, chain, depositValue, identityToSe
         failureText,
         fee: fee || String(estimatedFee || 0),
         from: { address: formatted, name },
-        subAction: 'Add/Remove/Modify Identity',
+        subAction: `${mode === 'Remove' ? 'Clear' : mode} Identity`,
         success,
         throughProxy: selectedProxyAddress ? { address: selectedProxyAddress, name: selectedProxyName } : undefined,
         txHash: txHash || ''
@@ -124,119 +125,150 @@ export default function Review({ address, api, chain, depositValue, identityToSe
 
       setTxInfo({ ...info, api, chain });
       saveAsHistory(from, info);
-      setShowWaitScreen(false);
-      setShowConfirmation(true);
+      setStep(STEPS.CONFIRM);
     } catch (e) {
       console.log('error:', e);
       setIsPasswordError(true);
     }
-  }, [api, chain, estimatedFee, formatted, name, password, selectedProxy, selectedProxyAddress, selectedProxyName, tx]);
+  }, [api, chain, estimatedFee, formatted, mode, name, password, selectedProxy, selectedProxyAddress, selectedProxyName, setStep, tx]);
 
   const handleClose = useCallback(() => setStep(mode === 'Set' || mode === 'Modify' ? STEPS.INDEX : STEPS.PREVIEW), [mode, setStep]);
+  const closeProxy = useCallback(() => setStep(STEPS.REVIEW), [setStep]);
   const closeConfirmation = useCallback(() => {
-    setShowConfirmation(false);
     setRefresh(true);
     setStep(0);
   }, [setRefresh, setStep]);
 
   return (
-    <DraggableModal onClose={handleClose} open={step === STEPS.REVIEW || step === STEPS.PROXY}>
+    <DraggableModal onClose={handleClose} open={step === STEPS.REVIEW || step === STEPS.PROXY || step === STEPS.WAIT_SCREEN || step === STEPS.CONFIRM}>
       <Motion style={{ height: '100%' }}>
-        {step === STEPS.REVIEW && !showWaitScreen && !showConfirmation &&
-          <>
-            {isPasswordError &&
-              <WrongPasswordAlert />
-            }
-            <Grid container my='20px'>
-              <SubTitle label={t<string>('Review')} />
+        <>
+          <Grid alignItems='center' container justifyContent='space-between' pt='5px'>
+            <Grid item>
+              <Typography fontSize='22px' fontWeight={700}>
+                {step === STEPS.REVIEW && (
+                  <>
+                    {mode === 'Set' && t('Review Identity')}
+                    {mode === 'Remove' && t('Remove Identity')}
+                    {mode === 'Modify' && t('Modify Identity')}
+                  </>
+                )}
+                {step === STEPS.WAIT_SCREEN && (
+                  <>
+                    {mode === 'Set' && t('Setting Identity')}
+                    {mode === 'Remove' && t('Removing Identity')}
+                    {mode === 'Modify' && t('Modifying Identity')}
+                  </>
+                )}
+                {step === STEPS.CONFIRM && mode === 'Set' && (
+                  txInfo?.success ? t('Identity Set') : t('Identity Setup Failed')
+                )}
+                {step === STEPS.CONFIRM && mode === 'Modify' && (
+                  txInfo?.success ? t('Identity Modified') : t('Identity Modification Failed')
+                )}
+                {step === STEPS.CONFIRM && mode === 'Remove' && (
+                  txInfo?.success ? t('Identity Removed') : t('Identity Removal Failed')
+                )}
+                {step === STEPS.PROXY && t('Select Proxy')}
+              </Typography>
             </Grid>
-            <AccountHolderWithProxy
-              address={address}
-              chain={chain}
-              selectedProxyAddress={selectedProxyAddress}
-              showDivider
-              style={{ mt: '-5px' }}
-              title={t('Account holder')}
-            />
-            {identityToSet &&
-              <>
-                <Typography sx={{ m: '10px auto' }}>
-                  {t<string>('Identity')}
+            <Grid item>
+              {step !== STEPS.WAIT_SCREEN && <CloseIcon onClick={step === STEPS.PROXY ? closeProxy : handleClose} sx={{ color: 'primary.main', cursor: 'pointer', stroke: theme.palette.primary.main, strokeWidth: 1.5 }} />}
+            </Grid>
+          </Grid>
+          {step === STEPS.REVIEW &&
+            <>
+              {isPasswordError &&
+                <WrongPasswordAlert />
+              }
+              <Grid alignItems='center' container direction='column' justifyContent='center' sx={{ m: 'auto', pt: isPasswordError ? 0 : '10px', width: '90%' }}>
+                <Typography fontSize='16px' fontWeight={400} lineHeight='23px'>
+                  {t<string>('Account holder')}
                 </Typography>
-                <IdentityTable
-                  identity={identityToSet}
+                <Identity
+                  address={address}
+                  api={api}
+                  chain={chain}
+                  direction='row'
+                  identiconSize={31}
+                  showSocial={false}
+                  style={{ maxWidth: '100%', width: 'fit-content' }}
+                  withShortAddress
                 />
-              </>
-            }
-            <Grid alignItems='center' container justifyContent='center' m='20px auto 5px' width='92%'>
-              <Grid display='inline-flex' item>
-                <Typography fontSize='16px' fontWeight={400} lineHeight='23px'>
-                  {t<string>('Deposit:')}
-                </Typography>
-                <Grid item lineHeight='22px' pl='5px'>
-                  <ShowBalance
-                    api={api}
-                    balance={depositValue}
-                    decimalPoint={4}
-                    height={22}
-                  />
-                </Grid>
               </Grid>
-              <Divider orientation='vertical' sx={{ backgroundColor: 'secondary.main', height: '30px', mx: '5px', my: 'auto' }} />
-              <Grid display='inline-flex' item>
-                <Typography fontSize='16px' fontWeight={400} lineHeight='23px'>
-                  {t<string>('Fee:')}
-                </Typography>
-                <Grid item lineHeight='22px' pl='5px'>
-                  <ShowBalance
-                    api={api}
-                    balance={estimatedFee}
-                    decimalPoint={4}
-                    height={22}
-                  />
+              {selectedProxyAddress &&
+                <Grid container m='auto' maxWidth='92%'>
+                  <ThroughProxy address={selectedProxyAddress} chain={chain} />
                 </Grid>
-              </Grid>
-            </Grid>
-            <PasswordWithTwoButtonsAndUseProxy
-              chain={chain}
-              isPasswordError={isPasswordError}
-              label={`${t<string>('Password')} for ${selectedProxyName || name}`}
-              onChange={setPassword}
-              onPrimaryClick={onNext}
-              onSecondaryClick={handleClose}
-              primaryBtnText={t<string>('Confirm')}
-              proxiedAddress={formatted}
+              }
+              <Divider sx={{ bgcolor: 'secondary.main', height: '2px', mx: 'auto', my: '5px', width: '170px' }} />
+              {identityToSet &&
+                <>
+                  <Typography sx={{ m: '10px auto' }}>
+                    {t<string>('Identity')}
+                  </Typography>
+                  <IdentityTable
+                    identity={identityToSet}
+                  />
+                </>
+              }
+              <DisplayValue title={t<string>('Total Deposit')}>
+                <ShowBalance
+                  api={api}
+                  balance={depositValue}
+                  decimalPoint={4}
+                  height={22}
+                />
+              </DisplayValue>
+              <DisplayValue title={t<string>('Fee')}>
+                <ShowBalance
+                  api={api}
+                  balance={estimatedFee}
+                  decimalPoint={4}
+                  height={22}
+                />
+              </DisplayValue>
+              <PasswordWithTwoButtonsAndUseProxy
+                chain={chain}
+                isPasswordError={isPasswordError}
+                label={`${t<string>('Password')} for ${selectedProxyName || name}`}
+                onChange={setPassword}
+                onPrimaryClick={onNext}
+                onSecondaryClick={handleClose}
+                primaryBtnText={t<string>('Confirm')}
+                proxiedAddress={formatted}
+                proxies={proxyItems}
+                proxyTypeFilter={['Any', 'NonTransfer']}
+                selectedProxy={selectedProxy}
+                setIsPasswordError={setIsPasswordError}
+                setStep={setStep}
+              />
+            </>
+          }
+          {step === STEPS.PROXY &&
+            <SelectProxyModal
+              address={address}
+              height={500}
+              nextStep={STEPS.REVIEW}
               proxies={proxyItems}
               proxyTypeFilter={['Any', 'NonTransfer']}
               selectedProxy={selectedProxy}
-              setIsPasswordError={setIsPasswordError}
+              setSelectedProxy={setSelectedProxy}
               setStep={setStep}
             />
-          </>
-        }
-        {step === STEPS.PROXY &&
-          <SelectProxyModal
-            address={address}
-            height={500}
-            nextStep={STEPS.REVIEW}
-            proxies={proxyItems}
-            proxyTypeFilter={['Any', 'NonTransfer']}
-            selectedProxy={selectedProxy}
-            setSelectedProxy={setSelectedProxy}
-            setStep={setStep}
-          />
-        }
-        {showWaitScreen &&
-          <WaitScreen />
-        }
-        {txInfo && showConfirmation &&
-          <Confirmation
-            handleClose={closeConfirmation}
-            identity={identityToSet}
-            status={'set'}
-            txInfo={txInfo}
-          />
-        }
+          }
+          {step === STEPS.WAIT_SCREEN &&
+            <WaitScreen />
+          }
+          {txInfo && step === STEPS.CONFIRM &&
+            <Confirmation
+              handleClose={closeConfirmation}
+              identity={identityToSet}
+              status={'set'}
+              txInfo={txInfo}
+            />
+          }
+        </>
       </Motion>
     </DraggableModal>
   );
