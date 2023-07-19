3d6dfb04153fcff6bd1a7af76b5055217a60306f
Kami <ekbatanifard@gmail.com>
2023-07-19 13:20:50 +0330

    show stateScan logo white in dark mode

--- packages/extension-polkagate/src/popup/history/Explorer.tsx
+++ packages/extension-polkagate/src/popup/history/Explorer.tsx
@@ -3,10 +3,10 @@
 
 /* eslint-disable react/jsx-max-props-per-line */
 
-import { Grid, Link } from '@mui/material';
-import React from 'react';
+import { Avatar, Grid, Link, useTheme } from '@mui/material';
+import React, { useState, useEffect } from 'react';
 
-import { CHAINS_ON_POLKAHOLIC } from '../../util/constants';
+import { CHAINS_ON_POLKAHOLIC, CHAINS_WITH_BLACK_LOGO } from '../../util/constants';
 import getLogo from '../../util/getLogo';
 
 interface Props {
@@ -16,26 +16,32 @@ interface Props {
 }
 
 export default function Explorer({ chainName, formatted, txHash }: Props): React.ReactElement {
+  const theme = useTheme();
+  const [explorer, setExplorer] = useState<{ name: string, link: string }>();
+
   const mayBeTheFirstPartOfChain = chainName?.split(' ')?.[0];
   const isWestmint = chainName === 'Westend Asset Hub';
-  const subscanLink = () => 'https://' + mayBeTheFirstPartOfChain + '.subscan.io/extrinsic/' + String(txHash);
-  const polkaholicLink = () => 'https://' + mayBeTheFirstPartOfChain + '.polkaholic.io/tx/' + String(txHash);
-  const statescanLink = () => 'https://westmint.statescan.io/#/accounts/' + String(formatted);
+
+  const subscanLink = 'https://' + mayBeTheFirstPartOfChain + '.subscan.io/extrinsic/' + String(txHash);
+  const polkaholicLink = 'https://' + mayBeTheFirstPartOfChain + '.polkaholic.io/tx/' + String(txHash);
+  const statescanLink = 'https://westmint.statescan.io/#/accounts/' + String(formatted);
+
+  useEffect(() => {
+    if (CHAINS_ON_POLKAHOLIC.includes(chainName)) {
+      return setExplorer({ link: polkaholicLink, name: 'polkaholic' });
+    }
+
+    if (isWestmint) {
+      return setExplorer({ link: statescanLink, name: 'statescan' });
+    }
+
+    setExplorer({ link: subscanLink, name: 'subscan' });
+  }, [chainName, isWestmint, polkaholicLink, statescanLink, subscanLink]);
 
   return (
-    <>
-      {CHAINS_ON_POLKAHOLIC.includes(chainName)
-        ? <Link href={`${polkaholicLink()}`} rel='noreferrer' target='_blank' underline='none'>
-          <Grid alt={'polkaholic'} component='img' src={getLogo('polkaholic')} sx={{ height: 40, width: 40 }} />
-        </Link>
-        : isWestmint
-          ? <Link href={`${statescanLink()}`} rel='noreferrer' target='_blank' underline='none'>
-            <Grid alt={'statescan'} component='img' src={getLogo('statescan')} sx={{ height: 40, width: 40 }} />
-          </Link>
-          : <Link href={`${subscanLink()}`} rel='noreferrer' target='_blank' underline='none'>
-            <Grid alt={'subscan'} component='img' src={getLogo('subscan')} sx={{ height: 40, width: 40 }} />
-          </Link>
-      }
-    </>
+    <Link href={`${explorer?.link}`} rel='noreferrer' target='_blank' underline='none'>
+      <Avatar src={getLogo(explorer?.name)} sx={{ filter: (CHAINS_WITH_BLACK_LOGO.includes(explorer?.name) && theme.palette.mode === 'dark') ? 'invert(1)' : '', borderRadius: '50%', height: 40, width: 40 }} variant='square' />
+    </Link>
+
   );
 }
