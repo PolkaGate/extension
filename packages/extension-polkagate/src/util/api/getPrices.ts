4d55c23d3da2529a519800a55449545cc8e8ddff
Kami <ekbatanifard@gmail.com>
2023-07-19 12:59:57 +0330

    Update getPrices.ts

--- packages/extension-polkagate/src/util/api/getPrices.ts
+++ packages/extension-polkagate/src/util/api/getPrices.ts
@@ -6,23 +6,33 @@ import request from 'umi-request';
 import { Prices } from '../types';
 
 export default async function getPrices(chainNames: string[], currency = 'usd'): Promise<Prices> {
-  const replaceAssetHubs = chainNames.map((item) => item.replace('westendassethub', 'westend').replace('kusamaassethub', 'kusama').replace('polkadotassethub', 'polkadot'));
-  const nonDuplicateChainNames = [...new Set(replaceAssetHubs)];
+  try {
+    const replaceAssetHubs = chainNames.map((item) =>
+      item.replace('westendassethub', 'westend').replace('kusamaassethub', 'kusama').replace('polkadotassethub', 'polkadot')
+        .replace('westend asset hub', 'westend').replace('kusama asset hub', 'kusama').replace('polkadot asset hub', 'polkadot')
+        .replace('westmint', 'westend').replace('statemine', 'kusama').replace('statemint', 'polkadot')
+    );
+    const nonDuplicateChainNames = [...new Set(replaceAssetHubs)];
 
-  const prices = await getReq(`https://api.coingecko.com/api/v3/simple/price?ids=${nonDuplicateChainNames}&vs_currencies=${currency}`, {});
+    console.log(' getting prices for:', nonDuplicateChainNames);
 
-  if (chainNames.includes('pendulum')) {
-    const pendulumPrice = await getReq(`https://min-api.cryptocompare.com/data/price?fsym=PEN&tsyms=USD`, {});
+    const prices = await getReq(`https://api.coingecko.com/api/v3/simple/price?ids=${nonDuplicateChainNames}&vs_currencies=${currency}`, {});
 
-    if (pendulumPrice?.USD) {
-      prices.pendulum = { usd: pendulumPrice.USD };
+    if (chainNames.includes('pendulum')) {
+      const pendulumPrice = await getReq(`https://min-api.cryptocompare.com/data/price?fsym=PEN&tsyms=USD`, {});
+
+      if (pendulumPrice?.USD) {
+        prices.pendulum = { usd: pendulumPrice.USD };
+      }
     }
+
+    prices.westend = { usd: 0 };
+    console.log('Prices:', prices);
+
+    return { date: Date.now(), prices };
+  } catch (e) {
+    console.log('error while fetching prices:', e)
   }
-
-  prices.westend = { usd: 0 };
-  console.log('Prices:', prices);
-
-  return { date: Date.now(), prices };
 }
 
 function getReq(api: string, data: Record<string, any> = {}, option?: Record<string, any>): Promise<Record<string, any>> {
