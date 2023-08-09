9b25a009117a05b4d4f63e59a29a6dd5c7e1eb22
Amir Ekbatanifard <amiref007@gmail.com>
2023-08-09 10:15:22 +0330

    Update the design

--- packages/extension-polkagate/src/partials/NodeSwitch.tsx
+++ packages/extension-polkagate/src/partials/NodeSwitch.tsx
@@ -3,8 +3,8 @@
 
 /* eslint-disable react/jsx-max-props-per-line */
 
-import { SignalCellularAlt as SignalCellularAltIcon, SignalCellularAlt1Bar as SignalCellularAlt1BarIcon, SignalCellularAlt2Bar as SignalCellularAlt2BarIcon, Api } from '@mui/icons-material';
-import { Grid, Popover, Typography } from '@mui/material';
+import { SignalCellularAlt as SignalCellularAltIcon, SignalCellularAlt1Bar as SignalCellularAlt1BarIcon, SignalCellularAlt2Bar as SignalCellularAlt2BarIcon } from '@mui/icons-material';
+import { Grid, Popover, Typography, useTheme } from '@mui/material';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { ApiPromise } from '@polkadot/api';
@@ -22,6 +22,7 @@ interface Props {
 type EndpointsDelay = { name: string, delay: number | undefined, value: string }[];
 
 function NodeSwitch({ address, api, genesisHash }: Props): React.ReactElement {
+  const theme = useTheme();
   const account = useAccount(address);
   const endpointOptions = useEndpoints(genesisHash || account?.genesisHash);
   const endpointUrl = useEndpoint2(address);
@@ -31,17 +32,25 @@ function NodeSwitch({ address, api, genesisHash }: Props): React.ReactElement {
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
   const [endpointsDelay, setEndpointsDelay] = useState<EndpointsDelay>();
 
+  const colors = {
+    green: '#1F7720',
+    orange: '#FF5722',
+    red: '#C70000',
+    gray: theme.palette.mode === 'light' ? '#E8E0E5' : '#4B4B4B'
+  };
+
   const sanitizedCurrentEndpointName = useMemo(() => {
     const currentEndpointName = endpointOptions.find((endpoint) => endpoint.value === endpointUrl)?.text;
 
     return currentEndpointName?.replace(/^via\s/, '');
   }, [endpointOptions, endpointUrl]);
 
-  const statusColor = useCallback((ms: number) => ms <= 100
-    ? '#1F7720'
-    : ms <= 300
-      ? '#FF5722'
-      : '#C70000', []);
+  const statusColor = useCallback((ms: number) =>
+    ms <= 100
+      ? colors.green
+      : ms <= 300
+        ? colors.orange
+        : colors.red, []);
 
   const updateNodesList = useCallback(() => {
     setEndpointsDelay((prevEndpoints) => {
@@ -91,14 +100,14 @@ function NodeSwitch({ address, api, genesisHash }: Props): React.ReactElement {
     });
   }, [address, chainName]);
 
-  useEffect(() => {
-    endpointsDelay?.sort((a, b) => {
-      if (a.name === sanitizedCurrentEndpointName) return -1;
-      if (b.name === sanitizedCurrentEndpointName) return 1;
+  // useEffect(() => {
+  //   endpointsDelay?.sort((a, b) => {
+  //     if (a.name === sanitizedCurrentEndpointName) return -1;
+  //     if (b.name === sanitizedCurrentEndpointName) return 1;
 
-      return 0;
-    });
-  }, [endpointsDelay, sanitizedCurrentEndpointName]);
+  //     return 0;
+  //   });
+  // }, [endpointsDelay, sanitizedCurrentEndpointName]);
 
   // Function to calculate node delay and update state
   const calculateAndSetDelay = useCallback(() => {
@@ -116,7 +125,7 @@ function NodeSwitch({ address, api, genesisHash }: Props): React.ReactElement {
 
   // Set up the interval when the component mounts
   useEffect(() => {
-    const intervalId = setInterval(calculateAndSetDelay, 5000);
+    const intervalId = setInterval(calculateAndSetDelay, 120000);
 
     // Clean up the interval when the component unmounts
     return () => {
@@ -131,32 +140,32 @@ function NodeSwitch({ address, api, genesisHash }: Props): React.ReactElement {
           (ms <= 100
             ? (
               <SignalCellularAltIcon
-                sx={{ bottom: '2px', color: '#1F7720', fontSize: '35px', left: '2px', position: 'absolute' }}
+                sx={{ bottom: '2px', color: colors.green, fontSize: '35px', left: '2px', position: 'absolute' }}
               />
             )
             : ms <= 300
               ? (
                 <SignalCellularAlt2BarIcon
-                  sx={{ bottom: '2px', color: '#FF5722', fontSize: '35px', left: '2px', position: 'absolute' }}
+                  sx={{ bottom: '2px', color: colors.orange, fontSize: '35px', left: '2px', position: 'absolute' }}
                 />
               )
               : (
                 <SignalCellularAlt1BarIcon
-                  sx={{ bottom: '2px', color: '#C70000', fontSize: '35px', left: '2px', position: 'absolute' }}
+                  sx={{ bottom: '2px', color: colors.red, fontSize: '35px', left: '2px', position: 'absolute' }}
                 />
               ))}
       </>
     );
   };
 
-  const NodeStatusAndDelay = ({ endpointDelay }: { endpointDelay: number | undefined }) => (
-    <Grid container item sx={{ width: 'fit-content' }}>
-      {<Typography color={endpointDelay ? statusColor(endpointDelay) : 'inherit'} fontSize='16px' fontWeight={400} pr='10px'>
-        {endpointDelay ? `${endpointDelay} ms` : ''}
+  const NodeStatusAndDelay = ({ endpointDelay, isSelected = false }: { endpointDelay: number | undefined, isSelected?: boolean }) => (
+    <Grid alignItems='center' container item sx={{ width: 'fit-content' }}>
+      {<Typography color={endpointDelay ? statusColor(endpointDelay) : 'inherit'} fontSize='16px' fontWeight={isSelected ? 500 : 400} pr='10px'>
+        {endpointDelay ? `${endpointDelay} ms` : isSelected ? '- - - ms' : ''}
       </Typography>}
-      <Grid container item position='relative' width='40px'>
+      <Grid container height='35px' item position='relative' width='40px'>
         <SignalCellularAltIcon
-          sx={{ bottom: '2px', color: '#E8E0E5', fontSize: '35px', left: '2px', position: 'absolute' }}
+          sx={{ bottom: '2px', color: colors.gray, fontSize: '35px', left: '2px', position: 'absolute' }}
         />
         <NodeStatusIcon ms={endpointDelay} />
       </Grid>
@@ -164,23 +173,30 @@ function NodeSwitch({ address, api, genesisHash }: Props): React.ReactElement {
   );
 
   const NodesList = () => (
-    <Grid container direction='column' item sx={{ display: 'block', p: '8px', width: '250px' }}>
+    <Grid container direction='column' item sx={{ border: '1px solid', borderColor: 'secondary.light', borderRadius: '4px', display: 'block', minWidth: '275px', p: '6px 8px', width: 'fit-content' }}>
       {endpointsDelay && endpointsDelay.length > 0 &&
-        endpointsDelay.map((endpoint, index) => (
-          // eslint-disable-next-line react/jsx-no-bind
-          <Grid container item justifyContent='space-between' key={index} onClick={() => _onChangeEndpoint(endpoint.value)} py='5px' sx={{ cursor: 'pointer', width: '100%' }}>
-            <Typography fontSize='16px' fontWeight={400}>
-              {endpoint.name}
-            </Typography>
-            <NodeStatusAndDelay endpointDelay={endpoint.delay} />
-          </Grid>
-        ))}
+        endpointsDelay.map((endpoint, index) => {
+          const selectedEndpoint = endpoint.name === sanitizedCurrentEndpointName;
+
+          return (
+            // eslint-disable-next-line react/jsx-no-bind
+            <Grid alignItems='center' container item justifyContent='space-between' key={index} onClick={() => _onChangeEndpoint(endpoint.value)} py='5px' sx={{ ':hover': { bgcolor: 'rgba(186, 40, 130, 0.1)' }, bgcolor: selectedEndpoint ? 'rgba(186, 40, 130, 0.2)' : 'transparent', borderRadius: '5px', cursor: 'pointer', my: '3px', px: '5px', width: '100%' }}>
+              <Typography fontSize='16px' fontWeight={selectedEndpoint ? 500 : 400}>
+                {endpoint.name}
+              </Typography>
+              <NodeStatusAndDelay endpointDelay={endpoint.delay} isSelected={selectedEndpoint} />
+            </Grid>
+          );
+        })}
     </Grid>
   );
 
   const handleClick = useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
+    calculateAndSetDelay();
     setAnchorEl(event.currentTarget);
-  }, []);
+  }, [calculateAndSetDelay]);
+
+  console.log('delay:', currentDelay)
 
   const handleClose = useCallback(() => {
     setAnchorEl(null);
@@ -193,7 +209,7 @@ function NodeSwitch({ address, api, genesisHash }: Props): React.ReactElement {
     <>
       <Grid aria-describedby={id} component='button' container item onClick={handleClick} sx={{ bgcolor: 'transparent', border: '1px solid', borderColor: 'secondary.light', borderRadius: '5px', cursor: 'pointer', height: '42px', position: 'relative', width: '42px', zIndex: 10 }}>
         <SignalCellularAltIcon
-          sx={{ bottom: '2px', color: '#E8E0E5', fontSize: '35px', left: '2px', position: 'absolute' }}
+          sx={{ bottom: '2px', color: colors.gray, fontSize: '35px', left: '2px', position: 'absolute' }}
         />
         <NodeStatusIcon ms={currentDelay} />
       </Grid>
@@ -206,6 +222,7 @@ function NodeSwitch({ address, api, genesisHash }: Props): React.ReactElement {
         id={id}
         onClose={handleClose}
         open={open}
+        sx={{ mt: '5px' }}
         transformOrigin={{
           horizontal: 'right',
           vertical: 'top'
