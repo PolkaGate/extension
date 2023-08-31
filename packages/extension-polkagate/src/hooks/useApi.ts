cbfd758cbcb0807f5c3267ef6638dea67d9725ba
Nick <ekbatanifard@gmail.com>
2023-09-01 00:51:24 +0400

    make silent

--- packages/extension-polkagate/src/hooks/useApi.ts
+++ packages/extension-polkagate/src/hooks/useApi.ts
@@ -31,12 +31,12 @@ export default function useApi(address: AccountId | string | undefined, stateApi
     }
 
     if (!endpoint || savedApi?.isRequested === true) {
-      console.log('API is already requested, waiting...');
+      // console.log('API is already requested, waiting...');
 
       return;
     }
 
-    console.log('Initializing API connection...');
+    // console.log('Initializing API connection...');
 
     const wsProvider = new WsProvider(endpoint);
 
@@ -79,7 +79,7 @@ export default function useApi(address: AccountId | string | undefined, stateApi
       const savedApi = apisContext?.apis[chain?.genesisHash ?? '']?.find((sApi) => sApi.endpoint === endpoint);
 
       if (savedApi?.api?.isConnected) {
-        console.log('API connection is ready, updating state.');
+        // console.log('API connection is ready, updating state.');
 
         setApi(savedApi.api);
         clearInterval(pollingInterval);
