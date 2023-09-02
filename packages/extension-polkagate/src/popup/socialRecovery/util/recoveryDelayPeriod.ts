ab8ef859c6e149d781414fadf7eb3e6440033828
Amir Ekbatanifard <amiref007@gmail.com>
2023-09-02 11:39:51 +0330

    Update recoveryDelayPeriod.ts

--- packages/extension-polkagate/src/popup/socialRecovery/util/recoveryDelayPeriod.ts
+++ packages/extension-polkagate/src/popup/socialRecovery/util/recoveryDelayPeriod.ts
@@ -1,12 +1,12 @@
 // Copyright 2019-2023 @polkadot/extension-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export default function recoveryDelayPeriod(blocks: number): string {
+export default function recoveryDelayPeriod(blocks: number, dateDetailLength): string {
   const units = ['Year', 'Week', 'Day', 'Hour', 'Minute', 'Second'];
   const secondsPerUnit = [31536000, 604800, 86400, 3600, 60, 1];
 
   const parts = units.reduce((acc, unit, index) => {
-    if (acc.length >= 2) {
+    if (acc.length >= dateDetailLength ?? 2) {
       return acc;
     }
 
