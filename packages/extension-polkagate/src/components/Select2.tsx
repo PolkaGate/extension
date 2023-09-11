9065f616f7022a44f55a230bdb2f08cc1790d77f
Nick <ekbatanifard@gmail.com>
2023-09-11 14:43:32 +0300

    use mui typography instead of Label

--- packages/extension-polkagate/src/components/Select2.tsx
+++ packages/extension-polkagate/src/components/Select2.tsx
@@ -12,24 +12,22 @@ import { CHAINS_WITH_BLACK_LOGO } from '@polkadot/extension-polkagate/src/util/c
 import getLogo from '../util/getLogo';
 import { DropdownOption } from '../util/types';
 import { sanitizeChainName } from '../util/utils';
-import Label from './Label';
 
 interface Props {
   defaultValue: string | number | undefined;
   value?: string | number | undefined;
-  onChange?: (value: number | string) => void;
+  onChange?: (v: number | string) => void;
   options: DropdownOption[];
   label: string;
   isDisabled?: boolean;
   showLogo?: boolean;
   showIcons?: boolean;
   _mt?: string | number;
-  helperText?: string;
   disabledItems?: string[] | number[];
   isPageLoading?: boolean | undefined;
 }
 
-function CustomizedSelect({ _mt = 0, defaultValue, disabledItems, helperText, isDisabled = false, isPageLoading, label, onChange, options, showIcons = true, showLogo = false, value }: Props) {
+function CustomizedSelect({ _mt = 0, defaultValue, disabledItems, isDisabled = false, isPageLoading, label, onChange, options, showIcons = true, showLogo = false, value }: Props) {
   const theme = useTheme();
 
   const [showMenu, setShowMenu] = useState<boolean>(false);
@@ -73,118 +71,119 @@ function CustomizedSelect({ _mt = 0, defaultValue, disabledItems, helperText, is
 
   return (
     <FormControl disabled={isDisabled} sx={{ mt: `${_mt}`, width: '100%' }} variant='standard'>
-      <Label helperText={helperText} label={label} style={{ fontSize: '10px' }}>
-        <Select
-          MenuProps={{
-            MenuListProps: {
-              sx: {
-                '> li.Mui-selected': {
-                  bgcolor: 'rgba(186, 40, 130, 0.25)'
-                },
-                '> li:hover': {
-                  bgcolor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)'
-                },
-                bgcolor: 'background.paper'
-              }
-            },
-            PaperProps: {
-              style: showLogo
-                ? {
-                  minWidth: 'auto'
-                }
-                : {},
-              sx: {
-                '&::-webkit-scrollbar': {
-                  display: 'none',
-                  width: 0
-                },
-                border: '1px solid',
-                borderColor: 'secondary.light',
-                borderRadius: '7px',
-                filter: 'drop-shadow(-4px 4px 4px rgba(0, 0, 0, 0.15))',
-                mt: '10px',
-                overflow: 'hidden',
-                overflowY: 'scroll'
-              }
+      <Typography sx={{ fontSize: '10px', paddingLeft: '5px' }}>
+        {label}
+      </Typography>
+      <Select
+        MenuProps={{
+          MenuListProps: {
+            sx: {
+              '> li.Mui-selected': {
+                bgcolor: 'rgba(186, 40, 130, 0.25)'
+              },
+              '> li:hover': {
+                bgcolor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)'
+              },
+              bgcolor: 'background.paper'
             }
-          }}
-          defaultValue={defaultValue}
-          id='selectChain'
-          input={<BootstrapInput />}
-          onChange={_onChange}
-          onClick={toggleMenu}
-          open={!isPageLoading && options?.length !== 1 && showMenu} // do not open select when page is loading , or options has just one item
-          // eslint-disable-next-line react/jsx-no-bind
-          renderValue={(value) => {
-            const textToShow = options.find((option) => value === option.value || value === option.text)?.text?.split(/\s*\(/)[0];
-
-            return (
-              <Grid container height={'30px'} justifyContent='flex-start'>
-                {showIcons && textToShow && textToShow !== 'Allow use on any chain' &&
-                  <Grid alignItems='center' container item width='fit-content'>
-                    {<Avatar src={getLogo(chainName(textToShow))} sx={{ filter: (CHAINS_WITH_BLACK_LOGO.includes(textToShow) && theme.palette.mode === 'dark') ? 'invert(1)' : '', borderRadius: '50%', height: 19.8, width: 19.8 }} variant='square' />}
-                  </Grid>
-                }
-                <Grid alignItems='center' container item justifyContent='flex-start' pl='6px' width='fit-content'>
-                  <Typography fontSize='14px' fontWeight={300}>
-                    {textToShow}
-                  </Typography>
-                </Grid>
-              </Grid>
-            );
-          }}
-          sx={{
-            '> #selectChain': {
+          },
+          PaperProps: {
+            style: showLogo
+              ? {
+                minWidth: 'auto'
+              }
+              : {},
+            sx: {
+              '&::-webkit-scrollbar': {
+                display: 'none',
+                width: 0
+              },
               border: '1px solid',
               borderColor: 'secondary.light',
-              borderRadius: '20px',
-              fontSize: '14px',
-              height: '29px',
-              lineHeight: '30px',
-              p: 0,
-              pl: '5px',
-              textAlign: 'left'
-            },
-            '> .MuiSvgIcon-root': {
-              color: 'secondary.light',
-              fontSize: '30px'
-            },
-            '> .MuiSvgIcon-root.Mui-disabled': {
-              color: 'action.disabledBackground',
-              fontSize: '30px'
-            },
-            bgcolor: isDisabled ? 'primary.contrastText' : 'transparent',
-            width: '100%',
-            '.MuiSelect-icon': {
-              display: options?.length && options.length === 1 ? 'none' : 'block'
+              borderRadius: '7px',
+              filter: 'drop-shadow(-4px 4px 4px rgba(0, 0, 0, 0.15))',
+              mt: '10px',
+              overflow: 'hidden',
+              overflowY: 'scroll'
             }
-          }}
-          value={selectedValue} // Assuming selectedValue is a state variable
-        >
-          {options.map(({ text, value }): React.ReactNode => (
-            <MenuItem
-              disabled={disabledItems?.includes(value) || disabledItems?.includes(text)}
-              key={value}
-              sx={{ fontSize: '14px', fontWeight: 300, letterSpacing: '-0.015em' }}
-              value={value !== undefined ? value : text}
-            >
-              <Grid container height={'30px'} justifyContent='flex-start'>
-                {showIcons && text !== 'Allow use on any chain' &&
-                  <Grid alignItems='center' container item pr='6px' width='fit-content'>
-                    <Avatar src={getLogo(chainName(text))} sx={{ filter: (CHAINS_WITH_BLACK_LOGO.includes(text) && theme.palette.mode === 'dark') ? 'invert(1)' : '', borderRadius: '50%', height: 19.8, width: 19.8 }} variant='square' />
-                  </Grid>
-                }
-                <Grid alignItems='center' container item justifyContent='flex-start' pl='6px' width='fit-content'>
-                  <Typography fontSize='14px' fontWeight={300}>
-                    {text}
-                  </Typography>
-                </Grid>
+          }
+        }}
+        defaultValue={defaultValue}
+        id='selectChain'
+        input={<BootstrapInput />}
+        onChange={_onChange}
+        onClick={toggleMenu}
+        open={!isPageLoading && options?.length !== 1 && showMenu} // do not open select when page is loading , or options has just one item
+        // eslint-disable-next-line react/jsx-no-bind
+        renderValue={(v) => {
+          const textToShow = options.find((option) => v === option.value || v === option.text)?.text?.split(/\s*\(/)[0];
 
+          return (
+            <Grid container height={'30px'} justifyContent='flex-start'>
+              {showIcons && textToShow && textToShow !== 'Allow use on any chain' &&
+                <Grid alignItems='center' container item width='fit-content'>
+                  {<Avatar src={getLogo(chainName(textToShow))} sx={{ filter: (CHAINS_WITH_BLACK_LOGO.includes(textToShow) && theme.palette.mode === 'dark') ? 'invert(1)' : '', borderRadius: '50%', height: 19.8, width: 19.8 }} variant='square' />}
+                </Grid>
+              }
+              <Grid alignItems='center' container item justifyContent='flex-start' pl='6px' width='fit-content'>
+                <Typography fontSize='14px' fontWeight={300}>
+                  {textToShow}
+                </Typography>
               </Grid>
-            </MenuItem>
-          ))}
-        </Select>
-      </Label>
+            </Grid>
+          );
+        }}
+        sx={{
+          '> #selectChain': {
+            border: '1px solid',
+            borderColor: 'secondary.light',
+            borderRadius: '20px',
+            fontSize: '14px',
+            height: '29px',
+            lineHeight: '30px',
+            p: 0,
+            pl: '5px',
+            textAlign: 'left'
+          },
+          '> .MuiSvgIcon-root': {
+            color: 'secondary.light',
+            fontSize: '30px'
+          },
+          '> .MuiSvgIcon-root.Mui-disabled': {
+            color: 'action.disabledBackground',
+            fontSize: '30px'
+          },
+          bgcolor: isDisabled ? 'primary.contrastText' : 'transparent',
+          width: '100%',
+          '.MuiSelect-icon': {
+            display: options?.length && options.length === 1 ? 'none' : 'block'
+          }
+        }}
+        value={selectedValue} // Assuming selectedValue is a state variable
+      >
+        {options.map(({ text, value }): React.ReactNode => (
+          <MenuItem
+            disabled={disabledItems?.includes(value) || disabledItems?.includes(text)}
+            key={value}
+            sx={{ fontSize: '14px', fontWeight: 300, letterSpacing: '-0.015em' }}
+            value={value !== undefined ? value : text}
+          >
+            <Grid container height={'30px'} justifyContent='flex-start'>
+              {showIcons && text !== 'Allow use on any chain' &&
+                <Grid alignItems='center' container item pr='6px' width='fit-content'>
+                  <Avatar src={getLogo(chainName(text))} sx={{ filter: (CHAINS_WITH_BLACK_LOGO.includes(text) && theme.palette.mode === 'dark') ? 'invert(1)' : '', borderRadius: '50%', height: 19.8, width: 19.8 }} variant='square' />
+                </Grid>
+              }
+              <Grid alignItems='center' container item justifyContent='flex-start' pl='6px' width='fit-content'>
+                <Typography fontSize='14px' fontWeight={300}>
+                  {text}
+                </Typography>
+              </Grid>
+
+            </Grid>
+          </MenuItem>
+        ))}
+      </Select>
     </FormControl>
   );
 }
