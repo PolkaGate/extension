21d301f0c609a319bb86be6ade91425d1ba8ecc1
Nick <ekbatanifard@gmail.com>
2023-09-16 16:26:42 +0300

    Update getApi.ts

--- packages/extension-polkagate/src/util/getApi.ts
+++ packages/extension-polkagate/src/util/getApi.ts
@@ -1,25 +1,22 @@
 // Copyright 2019-2023 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
+
 /* eslint-disable header/header */
 
-// import Memoize from 'memoize-one';
-import memoize from 'memoizee';
-
-// var memoize = require("memoizee");
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
 import LCConnector from './api/lightClient-connect';
 
-async function getApi(endpoint: string): Promise<ApiPromise> {
+async function getApi(endpoint: string): Promise<ApiPromise | undefined> {
   if (endpoint.startsWith('wss')) {
     const wsProvider = new WsProvider(endpoint);
 
     return await ApiPromise.create({ provider: wsProvider });
-  } else {
+  } else if (endpoint.startsWith('light')) {
     return await LCConnector(endpoint);
   }
+
+  return undefined;
 }
 
-// export default getApi;
-// export default memoize(getApi);
 export default getApi;
