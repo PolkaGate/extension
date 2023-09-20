c3ba2e9ab766a9c3b9aa9ddb1f2b4744b3f15688
Amir Ekbatanifard <amiref007@gmail.com>
2023-09-20 10:54:31 +0330

    Update useActiveRecoveries.ts

--- packages/extension-polkagate/src/hooks/useActiveRecoveries.ts
+++ packages/extension-polkagate/src/hooks/useActiveRecoveries.ts
@@ -7,7 +7,6 @@ import { ApiPromise } from '@polkadot/api';
 import { BN } from '@polkadot/util';
 
 import { AccountContext } from '../components/contexts';
-import { getFormattedAddress } from '../util/utils';
 
 export type ActiveRecoveryFor = {
   rescuer: string;
@@ -21,16 +20,30 @@ export default function useActiveRecoveries(api: ApiPromise | undefined, searchF
   const { accounts } = useContext(AccountContext);
 
   const [activeRecoveries, setActiveRecoveries] = useState<ActiveRecoveryFor[] | null>();
+  const [fetching, setFetching] = useState<boolean | null>(false);
 
   useEffect(() => {
     if (!api) {
       setActiveRecoveries(undefined);
+      setFetching(false);
 
       return;
     }
 
+    if (fetching !== false) {
+      return;
+    }
+
+    setFetching(true);
+
     api.query.recovery.activeRecoveries.entries().then((actives) => {
       const myActiveRecovery: ActiveRecoveryFor[] = [];
+
+      if (actives.length === 0) {
+        setActiveRecoveries(null);
+
+        return;
+      }
 
       actives.forEach((activeRecovery) => {
         const lostAddress = activeRecovery[0].args[0].toString();
@@ -39,37 +52,26 @@ export default function useActiveRecoveries(api: ApiPromise | undefined, searchF
         const createdBlockBN = activeRecoveryInfo.created;
         const depositedValue = activeRecoveryInfo.deposit;
 
+        const gathered = {
+          createdBlock: createdBlockBN.toNumber(),
+          deposit: depositedValue,
+          lost: lostAddress,
+          rescuer: rescuerAddress,
+          vouchedFriends: activeRecoveryInfo.friends.toHuman() as string[]
+        };
+
         if (searchFor) {
           (searchFor === rescuerAddress || searchFor === lostAddress) &&
-            myActiveRecovery.push({
-              createdBlock: createdBlockBN.toNumber(),
-              deposit: depositedValue,
-              lost: lostAddress,
-              rescuer: rescuerAddress,
-              vouchedFriends: activeRecoveryInfo.friends.toHuman() as string[]
-            });
+            myActiveRecovery.push(gathered);
         } else {
-          accounts.forEach((account) => {
-            const formatted = getFormattedAddress(account.address, undefined, api.registry.chainSS58 ?? 42);
-
-            if ([lostAddress, rescuerAddress].includes(formatted)) {
-              setActiveRecoveries((pervActives) => [...(pervActives ?? []), {
-                createdBlock: createdBlockBN.toNumber(),
-                deposit: depositedValue,
-                lost: lostAddress,
-                rescuer: rescuerAddress,
-                vouchedFriends: activeRecoveryInfo.friends.toHuman() as string[]
-              }]);
-            }
-          });
+          setActiveRecoveries((pervActives) => [...(pervActives ?? []), gathered]);
         }
       });
 
-      searchFor && myActiveRecovery.length > 0
-        ? setActiveRecoveries(myActiveRecovery)
-        : setActiveRecoveries(null);
+      searchFor && myActiveRecovery.length > 0 && setActiveRecoveries(myActiveRecovery);
+      setFetching(null);
     }).catch(console.error);
-  }, [accounts, api, searchFor]);
+  }, [accounts, api, fetching, searchFor]);
 
   return activeRecoveries;
 }
