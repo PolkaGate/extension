db31656e35796049b9dcc3fa4fd92dc4e7553830
Amir Ekbatanifard <amiref007@gmail.com>
2023-10-03 16:51:11 +0330

    Update index.tsx

--- packages/extension-ui/src/Popup/index.tsx
+++ packages/extension-ui/src/Popup/index.tsx
@@ -18,7 +18,7 @@ import { subscribeAccounts, subscribeAuthorizeRequests, subscribeMetadataRequest
 import Account from '../../../extension-polkagate/src/popup/account';
 import AuthList from '../../../extension-polkagate/src/popup/authManagement';
 import Authorize from '../../../extension-polkagate/src/popup/authorize/index';
-import CreateAccount from '../../../extension-polkagate/src/popup/createAccount';
+import CreateAccount from '../../../extension-polkagate/src/popup/createAccountFullScreen';
 import CrowdLoans from '../../../extension-polkagate/src/popup/crowdloans';
 import Derive from '../../../extension-polkagate/src/popup/deriveAccount';
 import Export from '../../../extension-polkagate/src/popup/export/Export';
