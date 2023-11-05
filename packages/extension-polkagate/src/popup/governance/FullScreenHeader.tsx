2c4e499522dfb030fd7bbe42cbe21eea464529fa
Amir Ekbatanifard <amiref007@gmail.com>
2023-11-05 09:29:11 +0330

    Update FullScreenHeader.tsx

--- packages/extension-polkagate/src/popup/governance/FullScreenHeader.tsx
+++ packages/extension-polkagate/src/popup/governance/FullScreenHeader.tsx
@@ -9,7 +9,7 @@ import { useParams } from 'react-router';
 
 import { logoBlack } from '../../assets/logos';
 import { ActionContext } from '../../components';
-import { useApi, useChain } from '../../hooks';
+import { useApi, useChain, useGenesisHashOptions } from '../../hooks';
 import { FullScreenChainSwitch, FullScreenRemoteNode } from '../../partials';
 import { EXTENSION_NAME, GOVERNANCE_CHAINS, IDENTITY_CHAINS, SOCIAL_RECOVERY_CHAINS } from '../../util/constants';
 import AddressDropdown from './components/AddressDropdown';
@@ -17,13 +17,14 @@ import ThemeChanger from './partials/ThemeChanger';
 import { MAX_WIDTH } from './utils/consts';
 
 interface Props {
-  page?: 'governance' | 'manageIdentity' | 'send' | 'socialRecovery';
+  page?: 'governance' | 'manageIdentity' | 'send' | 'socialRecovery' | 'AccountDetails';
   noChainSwitch?: boolean;
   noAccountDropDown?: boolean;
 }
 
 export function FullScreenHeader({ noAccountDropDown = false, noChainSwitch = false, page }: Props): React.ReactElement {
   const { address, postId, topMenu } = useParams<{ address: string, topMenu?: 'referenda' | 'fellowship', postId?: string }>();
+  const allChains = useGenesisHashOptions();
 
   const api = useApi(address);
   const chain = useChain(address);
@@ -37,14 +38,28 @@ export function FullScreenHeader({ noAccountDropDown = false, noChainSwitch = fa
         return IDENTITY_CHAINS;
       case 'socialRecovery':
         return SOCIAL_RECOVERY_CHAINS;
+      case 'AccountDetails':
+        return allChains.filter((chain) => chain.value !== '').map((chainOption) => chainOption.value);
       default:
         return [];
     }
-  }, [page]);
+  }, [allChains, page]);
 
-  const onAccountChange = useCallback((address: string) =>
-    onAction(`/${page}/${address}${topMenu ? `/${topMenu}` : page === 'socialRecovery' ? '/false' : ''}${postId ? `/${postId}` : ''}`)
-    , [onAction, page, postId, topMenu]);
+  const onAccountChange = useCallback((selectedAddress: string) => {
+    switch (page) {
+      case 'governance':
+        return onAction(`/governance/${selectedAddress}/${`${topMenu ?? ''}`}/${postId ?? ''}`);
+      case 'manageIdentity':
+        return onAction(`/manageIdentity/${selectedAddress}`);
+      case 'socialRecovery':
+        return onAction(`/socialRecovery/${selectedAddress}/false`);
+      case 'AccountDetails':
+        return onAction(`/account/${selectedAddress}/`);
+      default:
+        return null;
+    }
+  }, [onAction, page, postId, topMenu]);
+  // onAction(`/${page}/${address}${topMenu ? `/${topMenu}` : page === 'socialRecovery' ? '/false' : ''}${postId ? `/${postId}` : ''}`)
 
   return (
     <Grid alignItems='center' container id='header' justifyContent='space-between' sx={{ bgcolor: '#000000', borderBottom: '1px solid', borderBottomColor: 'secondary.light', color: 'text.secondary', fontSize: '42px', fontWeight: 400, height: '70px', minWidth: '810px', px: '40px' }}>
