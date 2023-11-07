73760fe377baf6032a2fe7819eb16bda5a2770a5
Amir Ekbatanifard <amiref007@gmail.com>
2023-11-07 12:57:09 +0330

    Update AccountInformation.tsx

--- packages/extension-polkagate/src/popup/accountDetailsFullScreen/components/AccountInformation.tsx
+++ packages/extension-polkagate/src/popup/accountDetailsFullScreen/components/AccountInformation.tsx
@@ -6,22 +6,26 @@
 import { faShieldHalved, faSitemap } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { CheckCircleOutline as CheckIcon, InsertLinkRounded as LinkIcon } from '@mui/icons-material';
-import { Divider, Grid, IconButton, Skeleton, useTheme } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { Avatar, Divider, Grid, IconButton, Skeleton, Typography, useTheme } from '@mui/material';
 import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
 
 import { ApiPromise } from '@polkadot/api';
 import { Chain } from '@polkadot/extension-chains/types';
 
-import { ActionContext, ChainLogo, FormatBalance2, FormatPrice, Identicon, Identity, Infotip, ShortAddress2 } from '../../../components';
-import { useAccount, useAccountInfo, useProxies, useTranslation } from '../../../hooks';
+import { ActionContext, ChainLogo, FormatBalance2, FormatPrice, Identicon, Identity, Infotip, ShortAddress2, ShowBalance } from '../../../components';
+import { useAccount, useAccountInfo, useDecimal, useProxies, useToken, useTranslation } from '../../../hooks';
 import { showAccount, windowOpen } from '../../../messaging';
-import { BALANCES_VALIDITY_PERIOD } from '../../../util/constants';
+import { BALANCES_VALIDITY_PERIOD, CHAINS_WITH_BLACK_LOGO } from '../../../util/constants';
+import getLogo from '../../../util/getLogo';
 import { BalancesInfo, Price } from '../../../util/types';
 import { getValue } from '../../account/util';
+import { AssetsOnOtherChains } from '..';
 
 interface AddressDetailsProps {
   address: string | undefined;
   api: ApiPromise | undefined;
+  assetsOnOtherChains: AssetsOnOtherChains[] | undefined;
   chain: Chain | null | undefined;
   formatted: string | undefined;
   chainName: string | undefined;
@@ -30,7 +34,7 @@ interface AddressDetailsProps {
   price: Price | undefined;
 }
 
-export default function AccountInformation ({ address, api, balances, chain, chainName, formatted, isDarkTheme, price }: AddressDetailsProps): React.ReactElement {
+export default function AccountInformation({ address, api, assetsOnOtherChains, balances, chain, chainName, formatted, isDarkTheme, price }: AddressDetailsProps): React.ReactElement {
   const { t } = useTranslation();
   const account = useAccount(address);
   const accountInfo = useAccountInfo(api, formatted);
@@ -41,10 +45,18 @@ export default function AccountInformation ({ address, api, balances, chain, cha
   const [hasID, setHasID] = useState<boolean | undefined>();
   const [recoverable, setRecoverable] = useState<boolean | undefined>();
   const [balanceToShow, setBalanceToShow] = useState<BalancesInfo>();
+  const [showMore, setShowMore] = useState<boolean>(false);
 
   const borderColor = useMemo(() => isDarkTheme ? 'rgba(255, 255, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)', [isDarkTheme]);
   const isBalanceOutdated = useMemo(() => balances && Date.now() - balances.date > BALANCES_VALIDITY_PERIOD, [balances]);
   const isPriceOutdated = useMemo(() => price !== undefined && Date.now() - price.date > BALANCES_VALIDITY_PERIOD, [price]);
+  const otherAssetsToShow = useMemo(() => {
+    if (!assetsOnOtherChains || assetsOnOtherChains.length === 0) {
+      return undefined;
+    } else {
+      return assetsOnOtherChains.filter((asset) => !asset.totalBalance.isZero());
+    }
+  }, [assetsOnOtherChains]);
 
   useEffect((): void => {
     api && api?.query.identity && api?.query.identity.identityOf(address).then((id) => setHasID(!id.isEmpty)).catch(console.error);
@@ -110,6 +122,62 @@ export default function AccountInformation ({ address, api, balances, chain, cha
     </Grid>
   );
 
+  const toggleAssets = useCallback(() => setShowMore(!showMore), [showMore]);
+
+  const BalanceColumn = ({ asset }: { asset: AssetsOnOtherChains }) => (
+    <Grid alignItems='flex-start' container direction='column' item xs>
+      <Grid item sx={{ fontSize: '14px', fontWeight: 600, lineHeight: 1 }}>
+        <ShowBalance
+          api={api}
+          balance={asset.totalBalance}
+          decimal={asset.decimal}
+          decimalPoint={2}
+          token={asset.token}
+        />
+      </Grid>
+      <Grid item sx={{ fontSize: '13px', fontWeight: 400, lineHeight: 1 }}>
+        <FormatPrice
+          amount={asset.totalBalance}
+          decimals={asset.decimal}
+          price={asset.price}
+        />
+      </Grid>
+    </Grid>
+  );
+
+  const OtherAssetBox = ({ asset }: { asset: AssetsOnOtherChains }) => (
+    <Grid alignItems='center' container item justifyContent='center' sx={{ border: '1px solid', borderColor: 'secondary.light', borderRadius: '8px', p: '5px' }} width='fit-content'>
+      <Grid alignItems='center' container item pr='5px' width='fit-content'>
+        <Avatar src={getLogo(asset.chainName)} sx={{ borderRadius: '50%', filter: (CHAINS_WITH_BLACK_LOGO.includes(asset.chainName) && theme.palette.mode === 'dark') ? 'invert(1)' : '', height: 22, width: 22 }} variant='square' />
+      </Grid>
+      <BalanceColumn
+        asset={asset}
+      />
+    </Grid>
+  );
+
+  const OtherAssets = ({ assetsOnOtherChains }: { assetsOnOtherChains: AssetsOnOtherChains[] }) => (
+    <Grid container item sx={{ borderTop: '1px solid', borderTopColor: borderColor, mt: '10px', pt: '15px' }}>
+      <Typography fontSize='14px' fontWeight={400} width='80px'>
+        {t<string>('Assets on other chains')}
+      </Typography>
+      <Grid container gap='15px' item justifyContent='space-between' sx={{ height: showMore ? 'fit-content' : '42px', overflow: 'hidden', px: '5%', transitionDuration: '0.2s', transitionProperty: 'transform' }} xs>
+        {assetsOnOtherChains.map((asset, index) => (
+          <OtherAssetBox
+            asset={asset}
+            key={index}
+          />
+        ))}
+      </Grid>
+      <Grid container item justifyContent='center' onClick={toggleAssets} sx={{ cursor: 'pointer', width: '65px' }}>
+        <Typography fontSize='14px' fontWeight={400} sx={{ borderLeft: '1px solid', borderLeftColor: borderColor, height: 'fit-content', pl: '8px' }}>
+          {t<string>(showMore ? 'Less' : 'More')}
+        </Typography>
+        <ArrowDropDownIcon sx={{ color: 'secondary.light', fontSize: '20px', stroke: '#BA2882', strokeWidth: '2px', transform: showMore ? 'rotate(-180deg)' : 'rotate(0deg)', transitionDuration: '0.2s', transitionProperty: 'transform' }} />
+      </Grid>
+    </Grid>
+  );
+
   const openIdentity = useCallback(() => {
     address && windowOpen(`/manageIdentity/${address}`);
   }, [address]);
@@ -127,96 +195,103 @@ export default function AccountInformation ({ address, api, balances, chain, cha
   }, [account?.isHidden, address]);
 
   return (
-    <Grid alignItems='center' container item sx={{ bgcolor: 'background.paper', border: isDarkTheme ? '1px solid' : 'none', borderColor: 'secondary.light', boxShadow: '2px 3px 4px 0px rgba(0, 0, 0, 0.1)', p: '20px 30px' }}>
-      <Grid container item sx={{ borderRight: '2px solid', borderRightColor: borderColor, pr: '15px', width: 'fit-content' }}>
-        <Grid item pr='5px' width='fit-content'>
-          <Identicon
-            iconTheme={chain?.icon ?? 'polkadot'}
-            prefix={chain?.ss58Format ?? 42}
-            size={60}
-            value={formatted || address}
-          />
+    <Grid alignItems='center' container item sx={{ bgcolor: 'background.paper', border: isDarkTheme ? '1px solid' : 'none', borderColor: 'secondary.light', boxShadow: '2px 3px 4px 0px rgba(0, 0, 0, 0.1)', p: '20px 30px 15px' }}>
+      <Grid container item>
+        <Grid container item sx={{ borderRight: '1px solid', borderRightColor: borderColor, pr: '15px', width: 'fit-content' }}>
+          <Grid item pr='5px' width='fit-content'>
+            <Identicon
+              iconTheme={chain?.icon ?? 'polkadot'}
+              prefix={chain?.ss58Format ?? 42}
+              size={60}
+              value={formatted || address}
+            />
+          </Grid>
+          <Grid container direction='column' display='grid' item justifyContent='center' width='fit-content'>
+            <Grid item onClick={openIdentity} sx={{ cursor: 'pointer', display: hasID ? 'inherit' : 'none', height: '16px', width: '16px' }}>
+              {hasID
+                ? accountInfo?.identity?.displayParent
+                  ? <LinkIcon
+                    sx={{
+                      bgcolor: 'success.main',
+                      borderRadius: '50%',
+                      color: 'white',
+                      fontSize: '16px',
+                      transform: 'rotate(-45deg)'
+                    }}
+                  />
+                  : <CheckIcon
+                    sx={{
+                      bgcolor: 'success.main',
+                      borderRadius: '50%',
+                      color: 'white',
+                      fontSize: '16px'
+                    }}
+                  />
+                : undefined
+              }
+            </Grid>
+            <Grid item sx={{ height: '16px', width: '16px' }}>
+              <Infotip placement='bottom-start' text={t('Is recoverable')}>
+                <IconButton
+                  onClick={openSocialRecovery}
+                  sx={{ height: '16px', width: '16px' }}>
+                  <FontAwesomeIcon
+                    color={recoverable ? theme.palette.success.main : theme.palette.action.disabledBackground}
+                    fontSize='16px'
+                    icon={faShieldHalved}
+                  />
+                </IconButton>
+              </Infotip>
+            </Grid>
+            <Grid item sx={{ height: '16px', width: '16px' }}>
+              <Infotip placement='bottom-end' text={t('Has proxy')}>
+                <IconButton onClick={openManageProxy} sx={{ height: '16px', width: '16px' }}>
+                  <FontAwesomeIcon
+                    color={proxies?.length ? theme.palette.success.main : theme.palette.action.disabledBackground}
+                    fontSize='16px'
+                    icon={faSitemap}
+                  />
+                </IconButton>
+              </Infotip>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid container direction='column' display='grid' item justifyContent='center' width='fit-content'>
-          <Grid item onClick={openIdentity} sx={{ cursor: 'pointer', display: hasID ? 'inherit' : 'none', height: '16px', width: '16px' }}>
-            {hasID
-              ? accountInfo?.identity?.displayParent
-                ? <LinkIcon
-                  sx={{
-                    bgcolor: 'success.main',
-                    borderRadius: '50%',
-                    color: 'white',
-                    fontSize: '16px',
-                    transform: 'rotate(-45deg)'
-                  }}
-                />
-                : <CheckIcon
-                  sx={{
-                    bgcolor: 'success.main',
-                    borderRadius: '50%',
-                    color: 'white',
-                    fontSize: '16px'
-                  }}
-                />
-              : undefined
-            }
+        <Grid container direction='column' item sx={{ borderRight: '1px solid', borderRightColor: borderColor, px: '10px' }} xs={5}>
+          <Grid container item justifyContent='space-between'>
+            <Identity
+              address={address}
+              api={api}
+              chain={chain}
+              noIdenticon
+              style={{ width: 'calc(100% - 40px)' }}
+              subIdOnly
+            />
+            <Grid item width='40px'>
+              <Infotip text={account?.isHidden && t('This account is hidden from websites')}>
+                <IconButton onClick={toggleVisibility} sx={{ height: '20px', ml: '7px', mt: '13px', p: 0, width: '28px' }}>
+                  <vaadin-icon icon={account?.isHidden ? 'vaadin:eye-slash' : 'vaadin:eye'} style={{ color: `${theme.palette.secondary.light}`, height: '20px' }} />
+                </IconButton>
+              </Infotip>
+            </Grid>
           </Grid>
-          <Grid item sx={{ height: '16px', width: '16px' }}>
-            <Infotip placement='bottom-start' text={t('Is recoverable')}>
-              <IconButton
-                onClick={openSocialRecovery}
-                sx={{ height: '16px', width: '16px' }}>
-                <FontAwesomeIcon
-                  color={recoverable ? theme.palette.success.main : theme.palette.action.disabledBackground}
-                  fontSize='16px'
-                  icon={faShieldHalved}
-                />
-              </IconButton>
-            </Infotip>
+          <Grid alignItems='center' container item sx={{ '> div div:last-child': { width: 'auto' } }} xs>
+            <ShortAddress2 address={formatted} charsCount={40} showCopy style={{ fontSize: '10px', fontWeight: 300 }} />
           </Grid>
-          <Grid item sx={{ height: '16px', width: '16px' }}>
-            <Infotip placement='bottom-end' text={t('Has proxy')}>
-              <IconButton onClick={openManageProxy} sx={{ height: '16px', width: '16px' }}>
-                <FontAwesomeIcon
-                  color={proxies?.length ? theme.palette.success.main : theme.palette.action.disabledBackground}
-                  fontSize='16px'
-                  icon={faSitemap}
-                />
-              </IconButton>
-            </Infotip>
+        </Grid>
+        <Grid alignItems='center' container item xs>
+          <Grid item px='10px'>
+            <ChainLogo genesisHash={account?.genesisHash ?? ''} size={42} />
+          </Grid>
+          <Grid item sx={{ fontSize: '28px', ml: '5px' }}>
+            <BalanceRow />
           </Grid>
         </Grid>
       </Grid>
-      <Grid container direction='column' item sx={{ borderRight: '2px solid', borderRightColor: borderColor, px: '10px' }} xs={5}>
-        <Grid container item justifyContent='space-between'>
-          <Identity
-            address={address}
-            api={api}
-            chain={chain}
-            noIdenticon
-            style={{ width: 'calc(100% - 40px)' }}
-            subIdOnly
-          />
-          <Grid item width='40px'>
-            <Infotip text={account?.isHidden && t('This account is hidden from websites')}>
-              <IconButton onClick={toggleVisibility} sx={{ height: '20px', ml: '7px', mt: '13px', p: 0, width: '28px' }}>
-                <vaadin-icon icon={account?.isHidden ? 'vaadin:eye-slash' : 'vaadin:eye'} style={{ color: `${theme.palette.secondary.light}`, height: '20px' }} />
-              </IconButton>
-            </Infotip>
-          </Grid>
-        </Grid>
-        <Grid alignItems='center' container item sx={{ '> div div:last-child': { width: 'auto' } }} xs>
-          <ShortAddress2 address={formatted} charsCount={40} showCopy style={{ fontSize: '10px', fontWeight: 300 }} />
-        </Grid>
-      </Grid>
-      <Grid alignItems='center' container item xs>
-        <Grid item px='10px'>
-          <ChainLogo genesisHash={account?.genesisHash ?? ''} size={42} />
-        </Grid>
-        <Grid item sx={{ fontSize: '28px', ml: '5px' }}>
-          <BalanceRow />
-        </Grid>
-      </Grid>
+      {otherAssetsToShow &&
+        <OtherAssets
+          assetsOnOtherChains={otherAssetsToShow}
+        />
+      }
     </Grid>
   );
 }
