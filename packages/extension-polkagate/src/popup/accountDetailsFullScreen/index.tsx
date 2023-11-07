c6d8401911e8b7285b4f8481a753e464767844c1
Amir Ekbatanifard <amiref007@gmail.com>
2023-11-07 12:57:17 +0330

    Update index.tsx

--- packages/extension-polkagate/src/popup/accountDetailsFullScreen/index.tsx
+++ packages/extension-polkagate/src/popup/accountDetailsFullScreen/index.tsx
@@ -8,6 +8,8 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router';
 import { useHistory } from 'react-router-dom';
 
+import { BN, BN_ONE, BN_ZERO } from '@polkadot/util';
+
 import { useAccount, useApi, useBalances, useChain, useChainName, useDecimal, useFormatted, useFullscreen, usePrice, useToken, useTranslation } from '../../hooks';
 import { windowOpen } from '../../messaging';
 import { STAKING_CHAINS } from '../../util/constants';
@@ -17,6 +19,8 @@ import AccountSetting from './components/AccountSetting';
 import CommonTasks from './components/CommonTasks';
 import DisplayBalance from './components/DisplayBalance';
 import LockedBalanceDisplay from './components/LockedBalanceDisplay';
+
+export type AssetsOnOtherChains = { totalBalance: BN, chainName: string, decimal: number, price: number, token: string };
 
 export default function AccountDetails(): React.ReactElement {
   useFullscreen();
@@ -43,10 +47,35 @@ export default function AccountDetails(): React.ReactElement {
   const decimal = useDecimal(address);
 
   const [assetId, setAssetId] = useState<number>();
-
+  const [assetsOnOtherChains, setAssetsOnOtherChains] = useState<AssetsOnOtherChains[]>();
+console.log('assetsOnOtherChains:', assetsOnOtherChains)
   const isDarkTheme = useMemo(() => theme.palette.mode === 'dark', [theme.palette]);
   const indexBgColor = useMemo(() => theme.palette.mode === 'light' ? '#DFDFDF' : theme.palette.background.paper, [theme.palette]);
   const contentBgColor = useMemo(() => theme.palette.mode === 'light' ? '#F1F1F1' : theme.palette.background.default, [theme.palette]);
+
+  const fetchAssetsOnOtherChains = useCallback((accountAddress: string) => {
+    const worker: Worker = new Worker(new URL('../../util/workers/getAssetsOnOtherChains.js', import.meta.url));
+
+    worker.postMessage({ accountAddress });
+
+    worker.onerror = (err) => {
+      console.log(err);
+    };
+
+    worker.onmessage = (e: MessageEvent<string>) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+
+      const fetchedBalances = JSON.parse(e.data) as { balances: number, chain: string, decimal: number, price: number, token: string }[];
+
+      setAssetsOnOtherChains(fetchedBalances.map((asset) => ({ chainName: asset.chain, decimal: Number(asset.decimal), price: asset.price, token: asset.token, totalBalance: new BN(asset.balances) })));
+
+      worker.terminate();
+    };
+  }, []);
+
+  useEffect(() => {
+    address && fetchAssetsOnOtherChains(address);
+  }, [address, fetchAssetsOnOtherChains]);
 
   const goToSend = useCallback(() => {
     address && windowOpen(`/send/${address}/${assetId}`).catch(console.error);
@@ -77,6 +106,7 @@ export default function AccountDetails(): React.ReactElement {
               <AccountInformation
                 address={address}
                 api={api}
+                assetsOnOtherChains={assetsOnOtherChains}
                 balances={balance}
                 chain={chain}
                 chainName={chainName}
