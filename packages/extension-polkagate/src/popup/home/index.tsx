ac026d45c24b8c7055a87f00da06391a69f72d26
Nick <ekbatanifard@gmail.com>
2023-11-15 01:36:01 +0300

    refactor and fix image removal on theme change issue

--- packages/extension-polkagate/src/popup/home/index.tsx
+++ packages/extension-polkagate/src/popup/home/index.tsx
@@ -8,36 +8,29 @@ import '@vaadin/icons';
 
 import { ArrowForwardIosRounded as ArrowForwardIosRoundedIcon } from '@mui/icons-material';
 import { Container, Grid, IconButton, Typography, useTheme } from '@mui/material';
-import React, { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
 
 import { AccountsStore } from '@polkadot/extension-base/stores';
 import keyring from '@polkadot/ui-keyring';
 import { cryptoWaitReady } from '@polkadot/util-crypto';
 
-import { AccountContext, Infotip2, Warning } from '../../components';
+import { AccountContext, Warning } from '../../components';
 import { useMerkleScience, useTranslation } from '../../hooks';
 import { tieAccount, windowOpen } from '../../messaging';
 import HeaderBrand from '../../partials/HeaderBrand';
 import { NEW_VERSION_ALERT, TEST_NETS } from '../../util/constants';
 import AddAccount from '../welcome/AddAccount';
 import AccountsTree from './AccountsTree';
+import AiBackgroundImage from './AiBackgroundImage';
 import Alert from './Alert';
 import YouHave from './YouHave';
 
-const imagePath = `https://raw.githubusercontent.com/PolkaGate/backgrounds/main/${process.env.BG_THEME || 'general'}/`;
-
-type BgImage = {
-  dark: string;
-  light: string;
-}
 
 export default function Home(): React.ReactElement {
   const { t } = useTranslation();
   const { accounts, hierarchy } = useContext(AccountContext);
   const theme = useTheme();
-  const imgRef = useRef(0);
 
-  // usePrices(); // update prices for all tokens saved in chainNames
   useMerkleScience(undefined, undefined, true); // to download the data file
 
   const [hideNumbers, setHideNumbers] = useState<boolean>();
@@ -45,27 +38,6 @@ export default function Home(): React.ReactElement {
   const [quickActionOpen, setQuickActionOpen] = useState<string | boolean>();
   const [hasActiveRecovery, setHasActiveRecovery] = useState<string | null | undefined>(); // if exists, include the account address
   const [bgImage, setBgImage] = useState<string | undefined>();
-  const [imageLoadError, setImageLoadError] = useState(false);
-
-  const clearBackground = useCallback((): void => {
-    setBgImage(undefined);
-    setImageLoadError(true);
-    imgRef.current = 0;
-    chrome.storage.local.remove('backgroundImage').catch(console.error);
-  }, []);
-
-  const testImgUrl = useCallback((url: string) => {
-    const testImg = new Image();
-
-    const handleImageError = () => {
-      console.log('error handled');
-      clearBackground();
-    };
-
-    testImg.src = url;
-    testImg.onerror = handleImageError;
-    testImg.onload = () => setBgImage(url);
-  }, [clearBackground]);
 
   useEffect(() => {
     const isTestnetDisabled = window.localStorage.getItem('testnet_enabled') !== 'true';
@@ -95,15 +67,6 @@ export default function Home(): React.ReactElement {
     }).catch(() => null);
   }, []);
 
-  useEffect(() => {
-    chrome.storage.local.get('backgroundImage', (res) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      const imgUrl = res?.backgroundImage?.[theme.palette.mode] as string;
-
-      testImgUrl(imgUrl);
-    });
-  }, [testImgUrl, theme.palette.mode]);
-
   const sortedAccount = useMemo(() =>
     hierarchy.sort((a, b) => {
       const x = a.name.toLowerCase();
@@ -118,49 +81,14 @@ export default function Home(): React.ReactElement {
       }
 
       return 0;
-    })
-    , [hierarchy]);
+    }), [hierarchy]);
 
-  const onCreate = useCallback(
-    (): void => {
-      windowOpen('/account/create').catch(console.error);
-    }, []
-  );
-
-  const setBackground = useCallback((): void => {
-    setImageLoadError(false);
-
-    const imageUrl = imagePath + theme.palette.mode + `/${imgRef.current}.jpeg`;
-
-    testImgUrl(imageUrl);
-    chrome.storage.local.get('backgroundImage', (res) => {
-      const bgImage = (res?.backgroundImage || { dark: '', light: '' }) as BgImage;
-
-      bgImage[theme.palette.mode] = imageUrl;
-      chrome.storage.local.set({ backgroundImage: bgImage }).catch(console.error);
-    });
-
-    imgRef.current = imgRef.current + 1;
-  }, [testImgUrl, theme.palette.mode]);
+  const onCreate = useCallback((): void => {
+    windowOpen('/account/create').catch(console.error);
+  }, []);
 
   const AddNewAccount = () => (
-    <Grid alignItems='center' container onClick={onCreate} sx={{
-      backgroundColor: 'background.paper',
-      borderColor: 'secondary.main',
-      borderRadius: '10px',
-      borderStyle: 'solid',
-      borderWidth: '0.5px',
-      bottom: '20px',
-      cursor: 'pointer',
-      my: '10px',
-      pl: '22px',
-      position: 'absolute',
-      pr: '7px',
-      py: '13.5px',
-      width: 'inherit',
-      zIndex: 1
-    }}
-    >
+    <Grid alignItems='center' container onClick={onCreate} sx={{ backgroundColor: 'background.paper', borderColor: 'secondary.main', borderRadius: '10px', borderStyle: 'solid', borderWidth: '0.5px', bottom: '20px', cursor: 'pointer', my: '10px', pl: '22px', position: 'absolute', pr: '7px', py: '13.5px', width: 'inherit', zIndex: 1 }}    >
       <Grid item xs={1.5}>
         <vaadin-icon icon='vaadin:plus-circle' style={{ height: '36px', color: `${theme.palette.secondary.light}`, width: '36px' }} />
       </Grid>
@@ -171,35 +99,8 @@ export default function Home(): React.ReactElement {
       </Grid>
       <Grid item xs={1}>
         <IconButton sx={{ p: 0 }}>
-          <ArrowForwardIosRoundedIcon
-            sx={{
-              color: 'secondary.light',
-              fontSize: '24px',
-              stroke: `${theme.palette.secondary.light}`,
-              strokeWidth: 1.5
-            }}
-          />
+          <ArrowForwardIosRoundedIcon sx={{ color: 'secondary.light', fontSize: '24px', stroke: `${theme.palette.secondary.light}`, strokeWidth: 1.5 }} />
         </IconButton>
-      </Grid>
-    </Grid>
-  );
-
-  const AiBackgroundLink = () => (
-    <Grid container justifyContent='space-between' sx={{ backgroundColor: 'background.default', bottom: '3px', color: theme.palette.text.primary, position: 'absolute', zIndex: 6, p: '0 10px 0' }}>
-      <Grid item onClick={clearBackground} xs={1.5}>
-        {bgImage && !imageLoadError && <Typography sx={{ cursor: 'pointer', fontSize: '11px', userSelect: 'none' }}>
-          {t('Clear')}
-        </Typography>
-        }
-      </Grid>
-      <Grid alignItems='baseline' container item justifyContent='flex-end' xs>
-        <Grid item onClick={setBackground}>
-          <Infotip2 showInfoMark text={t('Click to set an AI-generated background.')}>
-            <Typography sx={{ cursor: 'pointer', fontSize: '11px', pl: '5px', userSelect: 'none' }}>
-              {t('AI Background')}
-            </Typography>
-          </Infotip2>
-        </Grid>
       </Grid>
     </Grid>
   );
@@ -270,7 +171,10 @@ export default function Home(): React.ReactElement {
               <AddNewAccount />
             }
           </Container>
-          <AiBackgroundLink />
+          <AiBackgroundImage
+            bgImage={bgImage}
+            setBgImage={setBgImage}
+          />
         </Grid>
       }
     </>
