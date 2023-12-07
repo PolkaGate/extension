72158a7c877eb27db74cc9c18ce874f35ebecc9b
Nick <ekbatanifard@gmail.com>
2023-12-07 23:54:13 +0330

    enable remove password

--- packages/extension-polkagate/src/popup/passwordManagemant/index.tsx
+++ packages/extension-polkagate/src/popup/passwordManagemant/index.tsx
@@ -3,23 +3,23 @@
 
 /* eslint-disable react/jsx-max-props-per-line */
 
+import { Check as CheckIcon } from '@mui/icons-material';
 import { Grid, Typography, useTheme } from '@mui/material';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 
 import { blake2AsHex } from '@polkadot/util-crypto';
-import { CheckCircleOutline as CheckIcon, InsertLinkRounded as LinkIcon } from '@mui/icons-material';
 
-import { ActionContext, PButton, Password, Warning } from '../../components';
+import { ActionContext, Password, PButton, TwoButtons, Warning } from '../../components';
 import { getStorage, LoginInfo, PasswordSettingAlert, setStorage } from '../../components/Loading';
 import { useTranslation } from '../../hooks';
 import { HeaderBrand } from '../../partials';
 import Passwords2 from '../createAccountFullScreen/components/Passwords2';
-import ValidatedInput2 from '../createAccountFullScreen/components/ValidatedInput2';
 
 const STEPS = {
   NO_PASSWORD: 0,
   ALREADY_SET_PASSWORD: 1,
   NEW_PASSWORD_SET: 2,
+  PASSWORD_REMOVED: 3,
   ERROR: 5
 }
 
@@ -35,10 +35,10 @@ export default function LoginPassword(): React.ReactElement {
   const [error, setError] = useState<string>();
 
   useEffect(() => {
-    getStorage('loginInfo').then((info) => {
+    getStorage('loginInfo').then((info: LoginInfo) => {
       setLoginInfo(info);
       setStep(info.status === 'set' ? STEPS.ALREADY_SET_PASSWORD : STEPS.NO_PASSWORD);
-    }).catch(console.err);
+    }).catch(console.error);
   }, []);
 
   const onPassChange = useCallback((pass: string | null): void => {
@@ -53,6 +53,7 @@ export default function LoginPassword(): React.ReactElement {
 
   const onSetPassword = useCallback(async () => {
     if (step === STEPS.ALREADY_SET_PASSWORD) {
+      /** check if current password is correct */
       const hashedCurrentPassword = blake2AsHex(currentPassword, 256);
 
       if (loginInfo?.hashedPassword !== hashedCurrentPassword) {
@@ -62,11 +63,18 @@ export default function LoginPassword(): React.ReactElement {
       }
     }
 
-    const hashedPassword = blake2AsHex(newPassword, 256);
+    if (newPassword) {
+      /** set a new password */
+      const hashedPassword = blake2AsHex(newPassword, 256);
+      const isConfirmed = await setStorage('loginInfo', { hashedPassword, lastLogin: Date.now(), status: 'set' });
 
-    const isConfirmed = await setStorage('loginInfo', { hashedPassword, lastLogin: Date.now(), status: 'set' });
+      setStep(isConfirmed ? STEPS.NEW_PASSWORD_SET : STEPS.ERROR);
+    } else {
+      /** remove password */
+      const isConfirmed = await setStorage('loginInfo', { status: 'no' });
 
-    setStep(isConfirmed ? STEPS.NEW_PASSWORD_SET : STEPS.ERROR);
+      setStep(isConfirmed ? STEPS.PASSWORD_REMOVED : STEPS.ERROR);
+    }
   }, [currentPassword, loginInfo?.hashedPassword, newPassword, step, t]);
 
   const onBackClick = useCallback(() => {
@@ -91,7 +99,7 @@ export default function LoginPassword(): React.ReactElement {
           </Warning>
         </Grid>
       }
-      {STEPS.NO_PASSWORD === step &&
+      {step === STEPS.NO_PASSWORD &&
         <>
           {!error &&
             <Grid container sx={{ height: '120px', top: '30px' }}>
@@ -109,6 +117,7 @@ export default function LoginPassword(): React.ReactElement {
               isFocussed
               label={t<string>('Password')}
               onChange={onPassChange}
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
               onEnter={onSetPassword}
             />
             <PButton
@@ -132,19 +141,20 @@ export default function LoginPassword(): React.ReactElement {
       {STEPS.ALREADY_SET_PASSWORD === step &&
         <>
           {!error &&
-            <Grid container sx={{ height: '120px', top: '30px' }}>
+            <Grid alignContent='center' container sx={{ height: '120px', top: '30px' }}>
               <Warning
                 fontWeight={300}
+                marginRight={18}
                 theme={theme}
               >
                 <Grid item>
-                  <b>{t<string>(' You are about to change your password. ')}</b>
-                  {t<string>('This will affect how you access your accounts and funds. Please make sure you remember your new password and store it securely.')}
+                  <b>{t<string>('You are about to modify your password. ')}</b>
+                  {t<string>('You can set a new password or leave the new password field and its repeat blank to remove the password.')}
                 </Grid>
               </Warning>
             </Grid>
           }
-          <Grid container justifyContent='center' sx={{ display: 'block', px: '10%', pt: '110px' }}>
+          <Grid container sx={{ bottom: '85px', display: 'block', position: 'absolute', px: '10%' }}>
             <Password
               isFocused
               label={t('Current password')}
@@ -157,26 +167,17 @@ export default function LoginPassword(): React.ReactElement {
               onChange={onPassChange}
               onEnter={onSetPassword}
             />
-            <PButton
-              _ml={0}
-              _mt='20px'
-              _onClick={onBackClick}
-              _variant='outlined'
-              _width={45}
-              text={t('Cancel')}
-            />
-            <PButton
-              _ml={10}
-              _mt='20px'
-              _onClick={onSetPassword}
-              _width={45}
-              disabled={!newPassword || !currentPassword}
-              text={t('Set')}
-            />
           </Grid>
+          <TwoButtons
+            disabled={!currentPassword}
+            onPrimaryClick={onSetPassword}
+            onSecondaryClick={onBackClick}
+            primaryBtnText={t<string>('Set')}
+            secondaryBtnText={t<string>('Cancel')}
+          />
         </>
       }
-      {STEPS.NEW_PASSWORD_SET === step &&
+      {[STEPS.NEW_PASSWORD_SET, STEPS.PASSWORD_REMOVED].includes(step) &&
         <>
           <Grid container justifyContent='center' sx={{ mt: '50px' }}>
             <CheckIcon
@@ -190,7 +191,10 @@ export default function LoginPassword(): React.ReactElement {
             />
             <Grid container justifyContent='center' pt='10px'>
               <Typography variant='body1'>
-                {t('Password has been set successfully!')}
+                {step === STEPS.NEW_PASSWORD_SET
+                  ? t('Password has been set successfully!')
+                  : t('Password has been REMOVED successfully!')
+                }
               </Typography>
             </Grid>
           </Grid>
