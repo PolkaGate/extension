80266772cb1bd9633a3b6a783c05bcc73ba4b59a
Amir Ekbatanifard <amiref007@gmail.com>
2023-12-09 11:22:10 +0330

    Update index.tsx

--- packages/extension-polkagate/src/popup/account/index.tsx
+++ packages/extension-polkagate/src/popup/account/index.tsx
@@ -20,7 +20,7 @@ import { useHistory, useLocation } from 'react-router-dom';
 
 import { stakingClose } from '../../assets/icons';
 import { ActionContext, Assets, Chain, HorizontalMenuItem, Identity, Motion } from '../../components';
-import { useApi, useBalances, useChain, useChainInfo, useChainName, useFormatted, useGenesisHashOptions, useMyAccountIdentity, useTranslation } from '../../hooks';
+import { useApi, useBalances, useChain, useChainName, useFormatted, useGenesisHashOptions, useMyAccountIdentity, useTranslation } from '../../hooks';
 import { tieAccount, windowOpen } from '../../messaging';
 import { HeaderBrand } from '../../partials';
 import { CROWDLOANS_CHAINS, GOVERNANCE_CHAINS, STAKING_CHAINS } from '../../util/constants';
