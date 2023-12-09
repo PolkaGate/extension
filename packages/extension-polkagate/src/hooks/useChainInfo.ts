23029b619ca87f8fb5f0e3ae3718e518402ac791
Amir Ekbatanifard <amiref007@gmail.com>
2023-12-09 11:21:34 +0330

    Update useChainInfo.ts

--- packages/extension-polkagate/src/hooks/useChainInfo.ts
+++ packages/extension-polkagate/src/hooks/useChainInfo.ts
@@ -1,20 +1,25 @@
 // Copyright 2019-2023 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 import { useCallback, useEffect, useState } from 'react';
 
 import { AccountId } from '@polkadot/types/interfaces/runtime';
 
+import { ChainInformation } from '../util/types';
 import { sanitizeChainName } from '../util/utils';
 import { useChainName } from './';
 
 const DATABASE_NAME = 'PolkaGateDB';
 const OBJECT_STORE_NAME = 'ChainsInformation';
 
-export default function useChainInfo(address: AccountId | string | undefined) {
+export default function useChainInfo (address: AccountId | string | undefined): ChainInformation | null | undefined {
   const chainName = useChainName(address);
 
-  const [chainInfo, setChainInfo] = useState();
+  const [chainInfo, setChainInfo] = useState<ChainInformation | null | undefined>();
 
   const sanitizedChainName = sanitizeChainName(chainName);
 
@@ -30,7 +35,7 @@ export default function useChainInfo(address: AccountId | string | undefined) {
       const getRequest = objectStore.get(sanitizedChainName);
 
       getRequest.onsuccess = (event) => {
-        const result = event.target.result;
+        const result = event.target.result as ChainInformation;
 
         if (result) {
           setChainInfo(result);
@@ -43,7 +48,7 @@ export default function useChainInfo(address: AccountId | string | undefined) {
         db.close();
       };
 
-      getRequest.onerror = (event) => {
+      getRequest.onerror = () => {
         console.error('Error retrieving version');
         setChainInfo(null);
 
