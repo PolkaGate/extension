db7d99f6a44070ef424ac17c576209a855109f00
Amir Ekbatanifard <amiref007@gmail.com>
2023-12-09 12:27:02 +0330

    make JSON instead of js

--- packages/extension-config/src/index.js
+++ packages/extension-config/src/index.js
@@ -8,34 +8,40 @@ import { fileURLToPath } from 'url';
 // Get the directory of the current module using import.meta.url
 const currentDir = fileURLToPath(new URL('.', import.meta.url));
 
-const logosDir = path.join(currentDir, 'icons/logos/');
+const logosDir = path.join(currentDir, 'icons/logos');
 const tokensDir = path.join(currentDir, 'icons/tokens');
 
 const buildDir = path.join(currentDir, 'build');
-const indexJsPath = path.join(buildDir, 'index.js');
+const indexJsPath = path.join(buildDir, 'icons.json');
+
+const isTheLastOne = (length, index) => index === length;
 
 // Read SVG files from src/icons/
 const logosSvgFiles = fs.readdirSync(logosDir).filter((file) => file.endsWith('.svg'));
 const tokensSvgFiles = fs.readdirSync(tokensDir).filter((file) => file.endsWith('.svg'));
 
 // Convert each SVG to base64 and generate export statements
-const logosExportStatements = logosSvgFiles.map((file) => {
+const logosExportStatements = logosSvgFiles.map((file, index) => {
   const svgPath = path.join(logosDir, file);
   const svgContent = fs.readFileSync(svgPath, 'utf8');
   const base64 = Buffer.from(svgContent).toString('base64');
 
-  return `export const ${file.replace('.svg', '')} = 'data:image/svg+xml;base64,${base64}';`;
+  // return `export const ${file.replace('.svg', '')} = 'data:image/svg+xml;base64,${base64}';`;
+  return `"${file.replace('.svg', '')}": "data:image/svg+xml;base64,${base64}"${isTheLastOne((logosSvgFiles.length - 1) + (tokensSvgFiles.length - 1), index) ? '' : ','}`;
 });
 
-const tokensExportStatements = tokensSvgFiles.map((file) => {
+const tokensExportStatements = tokensSvgFiles.map((file, index) => {
   const svgPath = path.join(tokensDir, file);
   const svgContent = fs.readFileSync(svgPath, 'utf8');
   const base64 = Buffer.from(svgContent).toString('base64');
 
-  return `export const ${file.replace('.svg', '')} = 'data:image/svg+xml;base64,${base64}';`;
+  // return `export const ${file.replace('.svg', '')} = 'data:image/svg+xml;base64,${base64}';`;
+  return `"${file.replace('.svg', '')}": "data:image/svg+xml;base64,${base64}"${isTheLastOne(tokensSvgFiles.length - 1, index) ? '' : ','}`;
 });
 
-// Write index.js with export statements
-fs.writeFileSync(indexJsPath, [...logosExportStatements, ...tokensExportStatements].join('\n'));
+const data = ['{', ...logosExportStatements, ...tokensExportStatements, '}'].join('\n');
 
-console.log('Icons converted and index.js created.');
+// Write index.js with export statements
+fs.writeFileSync(indexJsPath, data);
+
+console.log('Icons converted and icons.json created.');
