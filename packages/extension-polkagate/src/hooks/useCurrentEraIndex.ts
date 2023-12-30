2bd3b1ab3e4d0e3c780ca04f04d363eed041b8ac
Nick <ekbatanifard@gmail.com>
2023-12-30 20:57:36 +0330

    Update useCurrentEraIndex.ts

--- packages/extension-polkagate/src/hooks/useCurrentEraIndex.ts
+++ packages/extension-polkagate/src/hooks/useCurrentEraIndex.ts
@@ -8,7 +8,7 @@ import { AccountId } from '@polkadot/types/interfaces/runtime';
 import { useApi } from '.';
 
 /** This hook is going to be used for users account existing in the extension */
-export default function useCurrentEraIndex(address: AccountId | string): number | undefined {
+export default function useCurrentEraIndex(address: AccountId | string| undefined): number | undefined {
   const [index, setIndex] = useState<number>();
   const api = useApi(address);
 
