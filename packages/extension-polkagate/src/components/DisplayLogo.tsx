e565c08295e11c97a59c23d31df45d7c74310971
Amir Ekbatanifard <amiref007@gmail.com>
2023-12-31 16:24:21 +0330

    Update DisplayLogo.tsx

--- packages/extension-polkagate/src/components/DisplayLogo.tsx
+++ packages/extension-polkagate/src/components/DisplayLogo.tsx
@@ -8,7 +8,7 @@ import React from 'react';
 
 import { selectableNetworks } from '@polkadot/networks';
 
-import { assetHub, USDC, USDT } from '../assets/icons';
+import { assetHub, lcDOT, LDOT, USDC, USDT } from '../assets/icons';
 import { convertToCamelCase } from '../popup/governance/utils/util';
 import allChains from '../util/chains';
 import { CHAINS_WITH_BLACK_LOGO } from '../util/constants';
@@ -27,6 +27,7 @@ interface Props {
 
 const SUPPORTED_TOKENS = ['DOT', 'KSM', 'ACA'];
 const SUPPORTED_ETH_TOKENS = ['USDt', 'USDC'];
+const SUPPORTED_ACALA_TOKENS = ['LDOT', 'lcDOT'];
 
 function DisplayLogo({ assetSize, assetToken, baseTokenSize, chainName, genesisHash, size = 25 }: Props): React.ReactElement<Props> {
   const theme = useTheme();
@@ -37,25 +38,37 @@ function DisplayLogo({ assetSize, assetToken, baseTokenSize, chainName, genesisH
       ? USDT as string
       : USDC as string
     : undefined;
+  const foundAcalaAsset = assetToken && SUPPORTED_ACALA_TOKENS.includes(assetToken)
+    ? assetToken === 'LDOT'
+      ? LDOT as string
+      : lcDOT as string
+    : undefined;
   const _chainName = sanitizeChainName(foundChainName || chainName);
   const logo = getLogo(_chainName);
   const assetLogo = getLogo(foundAsset);
 
   return (
     <>
-      {logo && assetToken &&
+      {logo && assetToken && !foundAcalaAsset &&
         <AssetIcon
-          asset={assetLogo ?? foundETHAsset ?? assetHub}
+          asset={assetLogo ?? foundETHAsset ?? foundAcalaAsset ?? assetHub}
           assetSize={assetSize}
           baseLogo={logo}
           baseLogoSize={baseTokenSize}
+        />
+      }
+      {logo && assetToken && foundAcalaAsset &&
+        <Avatar
+          src={foundAcalaAsset}
+          sx={{ borderRadius: '50%', filter: (CHAINS_WITH_BLACK_LOGO.includes(_chainName ?? '') && theme.palette.mode === 'dark') ? 'invert(1)' : '', height: size, pr: 0, width: size }}
+          variant='square'
         />
       }
       {logo && !assetToken &&
         (logo.startsWith('data:')
           ? <Avatar
             src={logo}
-            sx={{ borderRadius: '50%', filter: (CHAINS_WITH_BLACK_LOGO.includes(_chainName ?? '') && theme.palette.mode === 'dark') ? 'invert(1)' : '', height: size, width: size }}
+            sx={{ borderRadius: '50%', filter: (CHAINS_WITH_BLACK_LOGO.includes(_chainName ?? '') && theme.palette.mode === 'dark') ? 'invert(1)' : '', height: size, pr: 0, width: size }}
             variant='square'
           />
           : <FontAwesomeIcon
