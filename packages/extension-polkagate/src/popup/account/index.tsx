12052e76abf1c8086d4fdc74606810abf2b33b07
Amir Ekbatanifard <amiref007@gmail.com>
2023-12-31 14:46:17 +0330

    Update index.tsx

--- packages/extension-polkagate/src/popup/account/index.tsx
+++ packages/extension-polkagate/src/popup/account/index.tsx
@@ -164,7 +164,7 @@ export default function AccountDetails(): React.ReactElement {
     <Motion>
       <HeaderBrand
         _centerItem={
-          <Identity address={address} api={api} chain={chain} formatted={formatted} identiconSize={40} showSocial={false} style={{ fontSize: '32px', height: '40px', lineHeight: 'initial', maxWidth: '75%' }} subIdOnly />
+          <Identity address={address} api={api} chain={chain} formatted={formatted} identiconSize={40} showSocial={false} style={{ fontSize: '32px', height: '40px', lineHeight: 'initial', maxWidth: '65%' }} subIdOnly />
         }
         address={address}
         fullScreen
