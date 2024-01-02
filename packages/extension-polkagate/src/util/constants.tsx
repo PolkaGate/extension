9b7c1e730a152c106f550ba3c2ffbebbe25f81ec
Amir Ekbatanifard <amiref007@gmail.com>
2024-01-02 10:53:04 +0330

    Add 'Governance' proxy type to westend in constant.tsx

--- packages/extension-polkagate/src/util/constants.tsx
+++ packages/extension-polkagate/src/util/constants.tsx
@@ -134,7 +134,7 @@ export const CONFIRMING_STATE = ['fail', 'success', 'confirming'];
 
 const PROXY_TYPE_POLKADOT = ['Any', 'NonTransfer', 'Staking', 'Governance', 'IdentityJudgement', 'CancelProxy', 'Auction', 'NominationPools'];
 const PROXY_TYPE_KUSAMA = ['Any', 'NonTransfer', 'Staking', 'Society', 'Governance', 'IdentityJudgement', 'CancelProxy', 'Auction', 'NominationPools'];
-const PROXY_TYPE_WESTEND = ['Any', 'NonTransfer', 'Staking', 'SudoBalances', 'IdentityJudgement', 'CancelProxy', 'Auction', 'NominationPools'];
+const PROXY_TYPE_WESTEND = ['Any', 'NonTransfer', 'Staking', 'Governance', 'SudoBalances', 'IdentityJudgement', 'CancelProxy', 'Auction', 'NominationPools'];
 
 export const CHAIN_PROXY_TYPES = { Kusama: PROXY_TYPE_KUSAMA, Polkadot: PROXY_TYPE_POLKADOT, Westend: PROXY_TYPE_WESTEND };
 
