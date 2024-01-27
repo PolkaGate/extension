dbeaaf91b0a6affd1b87379290f325a442e8c666
Amir Ekbatanifard <amiref007@gmail.com>
2024-01-27 12:32:58 +0330

    update

--- packages/extension-polkagate/src/components/FullScreenIcon.tsx
+++ packages/extension-polkagate/src/components/FullScreenIcon.tsx
@@ -1,10 +1,12 @@
 // Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable react/jsx-max-props-per-line */
+
 import { faExpand } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Grid, IconButton, useTheme } from '@mui/material';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import { useTranslation } from '../hooks';
 import { windowOpen } from '../messaging';
@@ -20,6 +22,16 @@ export default function FullScreenIcon ({ url }: Props): React.ReactElement {
 
   const [scale, setScale] = useState<string>('1');
 
+  const iconColor = useMemo(() => {
+    if (url === '/') {
+      return theme.palette.mode === 'dark'
+        ? theme.palette.text.primary
+        : theme.palette.secondary.light;
+    } else {
+      return theme.palette.secondary.light;
+    }
+  }, [theme.palette.mode, theme.palette.secondary.light, theme.palette.text.primary, url]);
+
   const onWindowOpen = useCallback(() => {
     windowOpen(url).catch(console.error);
   }, [url]);
@@ -33,7 +45,7 @@ export default function FullScreenIcon ({ url }: Props): React.ReactElement {
   }, []);
 
   return (
-    <Grid>
+    <Grid container item width='fit-content'>
       <Infotip2
         text={t('Fullscreen')}
       >
@@ -44,7 +56,7 @@ export default function FullScreenIcon ({ url }: Props): React.ReactElement {
           sx={{ height: '35px', ml: '-5px', p: 0, width: '35px' }}
         >
           <FontAwesomeIcon
-            color={theme.palette.secondary.light}
+            color={iconColor}
             icon={faExpand}
             style={{ height: '25px', transform: `scale(${scale})`, transitionDuration: '150ms' }}
           />
