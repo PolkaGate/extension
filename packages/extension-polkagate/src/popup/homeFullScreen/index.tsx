fb731a092abf177452a467c783099b10ea08067f
Amir Ekbatanifard <amiref007@gmail.com>
2024-02-11 12:17:11 +0330

    Home page full screen

--- packages/extension-polkagate/src/popup/homeFullScreen/index.tsx
+++ packages/extension-polkagate/src/popup/homeFullScreen/index.tsx
@@ -4,15 +4,18 @@
 /* eslint-disable react/jsx-max-props-per-line */
 
 import { Grid, useTheme } from '@mui/material';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 
+import { AccountContext } from '../../components';
 import { useFullscreen } from '../../hooks';
 import { FullScreenHeader } from '../governance/FullScreenHeader';
 import HeaderComponents from './components/HeaderComponents';
+import AccountItem from './partials/AccountItem';
 
 export default function HomePageFullScreen(): React.ReactElement {
   useFullscreen();
   const theme = useTheme();
+  const { accounts, hierarchy } = useContext(AccountContext);
 
   const [hideNumbers, setHideNumbers] = useState<boolean>();
 
@@ -23,6 +26,7 @@ export default function HomePageFullScreen(): React.ReactElement {
   }, [setHideNumbers]);
 
   const indexBgColor = useMemo(() => theme.palette.mode === 'light' ? '#DFDFDF' : theme.palette.background.paper, [theme.palette]);
+  const contentBgColor = useMemo(() => theme.palette.mode === 'light' ? '#F1F1F1' : theme.palette.background.default, [theme.palette]);
 
   return (
     <Grid bgcolor={indexBgColor} container item justifyContent='center'>
@@ -36,6 +40,16 @@ export default function HomePageFullScreen(): React.ReactElement {
         noAccountDropDown
         noChainSwitch
       />
+      <Grid container item justifyContent='space-around' sx={{ bgcolor: contentBgColor, height: 'calc(100vh - 70px)', maxWidth: '1282px', py: '40px', overflow: 'scroll' }}>
+        <Grid container direction='column' item rowGap='20px' width='fit-content'>
+          {hierarchy.map((acc, index) => (
+            <AccountItem
+              account={acc}
+              key={index}
+            />
+          ))}
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
