1326ec1f660d42524f8b299093fd7d1490ac419f
Amir Ekbatanifard <amiref007@gmail.com>
2024-02-29 18:40:48 +0330

    Update getPrices2.ts

--- packages/extension-polkagate/src/util/api/getPrices2.ts
+++ packages/extension-polkagate/src/util/api/getPrices2.ts
@@ -3,38 +3,45 @@
 
 import request from 'umi-request';
 
-import { OutputPrices, PricesType } from '../types';
+import { Prices2, PricesType } from '../types';
 
-export default async function getPrices2 (priceIds: string[], currency = 'usd'): Promise<OutputPrices | null> {
+async function updateStorage (newPrice: Prices2) {
+  const load = await chrome.storage.local.get('prices2');
+
+  const savedPrices = load && load.prices2 ? JSON.parse(load.prices2 as string) as Prices2[] : [];
+
+  const index = savedPrices.findIndex((priceItem) => priceItem.currencyCode === newPrice.currencyCode);
+
+  index === -1
+    ? savedPrices.push(newPrice)
+    : savedPrices[index] = newPrice;
+
+  await chrome.storage.local.set({ prices2: JSON.stringify(savedPrices) }).catch(console.error);
+}
+
+export default async function getPrices2 (priceIds: string[], currency = 'usd') {
   try {
     console.log(' getting prices for:', priceIds);
 
-    const prices = await getReq(`https://api.coingecko.com/api/v3/simple/price?ids=${priceIds}&vs_currencies=${currency}`, {});
+    const prices = await getReq(`https://api.coingecko.com/api/v3/simple/price?ids=${priceIds}&vs_currencies=${currency}&include_24hr_change=true`, {});
 
-    // if (chainNames.includes('pendulum')) {
-    //   const pendulumPrice = await getReq(`https://min-api.cryptocompare.com/data/price?fsym=PEN&tsyms=USD`, {});
-
-    //   if (pendulumPrice?.USD) {
-    //     prices.pendulum = { usd: pendulumPrice.USD };
-    //   }
-    // }
-
-    // prices.westend = { usd: 0 };
     const outputObjectPrices: PricesType = {};
 
     for (const [key, value] of Object.entries(prices)) {
-      outputObjectPrices[key] = value[currency];
+      outputObjectPrices[key] = { price: value[currency], change: value[`${currency}_24h_change`] };
     }
 
-    return { currencyCode: currency ,date: Date.now(), prices: outputObjectPrices };
+    const price = { currencyCode: currency, date: Date.now(), prices: outputObjectPrices };
+
+    await updateStorage(price);
+
+    // return price;
   } catch (e) {
     console.log('error while fetching prices:', e);
-
-    return null;
   }
 }
 
-function getReq(api: string, data: Record<string, unknown> = {}, option?: Record<string, unknown>): Promise<Record<string, unknown>> {
+function getReq (api: string, data: Record<string, unknown> = {}, option?: Record<string, unknown>): Promise<Record<string, unknown>> {
   return request.get(api, {
     data,
     ...option
