39ac2ffa885d75ad9288777762000f1d00ded576
Amir Ekbatanifard <amiref007@gmail.com>
2024-03-03 11:44:39 +0330

    Add Popups

--- packages/extension-polkagate/src/popup/homeFullScreen/partials/AccountInformation.tsx
+++ packages/extension-polkagate/src/popup/homeFullScreen/partials/AccountInformation.tsx
@@ -19,11 +19,15 @@ import { ActionContext, Identicon, Identity, Infotip, ShortAddress2 } from '../.
 import { nFormatter } from '../../../components/FormatPrice';
 import { useAccount, useAccountInfo, useCurrency, useTranslation } from '../../../hooks';
 import { showAccount, tieAccount } from '../../../messaging';
-import { ACALA_GENESIS_HASH, ASSET_HUBS, IDENTITY_CHAINS, KUSAMA_GENESIS_HASH, POLKADOT_GENESIS_HASH, SOCIAL_RECOVERY_CHAINS, WESTEND_GENESIS_HASH } from '../../../util/constants';
+import { ACALA_GENESIS_HASH, ASSET_HUBS, IDENTITY_CHAINS, KUSAMA_GENESIS_HASH, POLKADOT_GENESIS_HASH, PROXY_CHAINS, SOCIAL_RECOVERY_CHAINS, WESTEND_GENESIS_HASH } from '../../../util/constants';
 import { AccountAssets, BalancesInfo, Proxy } from '../../../util/types';
 import { amountToHuman } from '../../../util/utils';
 import AOC from '../../accountDetailsFullScreen/components/AOC';
 import FullScreenAccountMenu from './FullScreenAccountMenu';
+import ForgetAccountModal from '../../forgetAccount/ForgetAccountModal';
+import RenameModal from '../../rename/RenameModal';
+import ExportAccountModal from '../../export/ExportAccountModal';
+import DeriveAccountModal from '../../newAccount/deriveAccount/modal/DeriveAccountModal';
 
 interface AddressDetailsProps {
   accountAssets: AccountAssets[] | null | undefined;
@@ -46,6 +50,13 @@ export type DisplayLogoAOC = {
 
 type AccountButtonType = { text: string, onClick: () => void, icon: React.ReactNode };
 
+export const POPUPS_NUMBER = {
+  DERIVE_ACCOUNT: 4,
+  EXPORT_ACCOUNT: 3,
+  FORGET_ACCOUNT: 1,
+  RENAME: 2
+};
+
 export default function AccountInformation({ accountAssets, address, api, assetId, balances, chain, chainName, formatted, hideNumbers, isChild, setAssetId }: AddressDetailsProps): React.ReactElement {
   const { t } = useTranslation();
   const currency = useCurrency();
@@ -58,6 +69,7 @@ export default function AccountInformation({ accountAssets, address, api, assetI
   const [isRecoverable, setIsRecoverable] = useState<boolean | undefined>();
   const [hasProxy, setHasProxy] = useState<boolean | undefined>();
   const [balanceToShow, setBalanceToShow] = useState<BalancesInfo>();
+  const [displayPopup, setDisplayPopup] = useState<number>();
 
   const calculatePrice = useCallback((amount: BN, decimal: number, price: number) => parseFloat(amountToHuman(amount, decimal)) * price, []);
 
@@ -171,11 +183,15 @@ export default function AccountInformation({ accountAssets, address, api, assetI
       setIsRecoverable(false);
     }
 
-    api.query.proxy.proxies(formatted).then((p) => {
-      const fetchedProxies = JSON.parse(JSON.stringify(p[0])) as unknown as Proxy[];
+    if (api.query?.proxy && PROXY_CHAINS.includes(account.genesisHash)) {
+      api.query.proxy.proxies(formatted).then((p) => {
+        const fetchedProxies = JSON.parse(JSON.stringify(p[0])) as unknown as Proxy[];
 
-      setHasProxy(fetchedProxies.length > 0);
-    }).catch(console.error);
+        setHasProxy(fetchedProxies.length > 0);
+      }).catch(console.error);
+    } else {
+      setHasProxy(false);
+    }
   }, [api, address, formatted, account?.genesisHash]);
 
   useEffect(() => {
@@ -247,103 +263,132 @@ export default function AccountInformation({ accountAssets, address, api, assetI
   }, [onAction, address]);
 
   return (
-    <Grid alignItems='center' container item sx={{ bgcolor: 'background.paper', border: isChild ? '1px dashed' : theme.palette.mode === 'dark' ? '1px solid' : 'none', borderColor: 'secondary.light', borderRadius: '5px', p: '20px 10px 15px 20px' }}>
-      <Grid container item>
-        <Grid container item sx={{ borderRight: '1px solid', borderRightColor: borderColor, pr: '8px', width: 'fit-content' }}>
-          <Grid container item pr='7px' sx={{ '> div': { height: 'fit-content' }, m: 'auto', width: 'fit-content' }}>
-            <Identicon
-              iconTheme={chain?.icon ?? 'polkadot'}
-              prefix={chain?.ss58Format ?? 42}
-              size={70}
-              value={formatted || address}
-            />
+    <>
+      <Grid alignItems='center' container item sx={{ bgcolor: 'background.paper', border: isChild ? '1px dashed' : theme.palette.mode === 'dark' ? '1px solid' : 'none', borderColor: 'secondary.light', borderRadius: '5px', p: '20px 10px 15px 30px' }}>
+        <Grid container item>
+          <Grid container item sx={{ borderRight: '1px solid', borderRightColor: borderColor, pr: '8px', width: 'fit-content' }}>
+            <Grid container item pr='7px' sx={{ '> div': { height: 'fit-content' }, m: 'auto', width: 'fit-content' }}>
+              <Identicon
+                iconTheme={chain?.icon ?? 'polkadot'}
+                prefix={chain?.ss58Format ?? 42}
+                size={70}
+                value={formatted || address}
+              />
+            </Grid>
+            <Grid alignItems='center' container direction='column' display='grid' item justifyContent='center' justifyItems='center' width='fit-content'>
+              <Grid item onClick={openIdentity} sx={{ border: '1px solid', borderColor: 'success.main', borderRadius: '5px', cursor: 'pointer', display: hasID ? 'inherit' : 'none', height: '24px', m: 'auto', p: '2px', width: 'fit-content' }}>
+                {hasID
+                  ? accountInfo?.identity?.displayParent
+                    ? <LinkIcon sx={{ bgcolor: 'success.main', border: '1px solid', borderRadius: '50%', color: 'white', fontSize: '18px', transform: 'rotate(-45deg)' }} />
+                    : <CheckIcon sx={{ bgcolor: 'success.main', border: '1px solid', borderRadius: '50%', color: 'white', fontSize: '18px' }} />
+                  : undefined
+                }
+              </Grid>
+              <Grid height='24px' item width='24px'>
+                <Infotip placement='right' text={t(recoverableToolTipTxt)}>
+                  <IconButton
+                    onClick={openSocialRecovery}
+                    sx={{ height: '24px', width: '24px' }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faShieldHalved}
+                      style={{ border: '1px solid', borderRadius: '5px', color: isRecoverable ? theme.palette.success.main : theme.palette.action.disabledBackground, fontSize: '16px', padding: '3px' }}
+                    />
+                  </IconButton>
+                </Infotip>
+              </Grid>
+              <Grid height='24px' item width='fit-content'>
+                <Infotip placement='right' text={t(proxyTooltipTxt)}>
+                  <IconButton onClick={openManageProxy} sx={{ height: '16px', width: '16px' }}>
+                    <FontAwesomeIcon
+                      icon={faSitemap}
+                      style={{ border: '1px solid', borderRadius: '5px', color: hasProxy ? theme.palette.success.main : theme.palette.action.disabledBackground, fontSize: '16px', padding: '2px' }}
+                    />
+                  </IconButton>
+                </Infotip>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid alignItems='center' container direction='column' display='grid' item justifyContent='center' justifyItems='center' width='fit-content'>
-            <Grid item onClick={openIdentity} sx={{ border: '1px solid', borderColor: 'success.main', borderRadius: '5px', cursor: 'pointer', display: hasID ? 'inherit' : 'none', height: '24px', m: 'auto', p: '2px', width: 'fit-content' }}>
-              {hasID
-                ? accountInfo?.identity?.displayParent
-                  ? <LinkIcon sx={{ bgcolor: 'success.main', border: '1px solid', borderRadius: '50%', color: 'white', fontSize: '18px', transform: 'rotate(-45deg)' }} />
-                  : <CheckIcon sx={{ bgcolor: 'success.main', border: '1px solid', borderRadius: '50%', color: 'white', fontSize: '18px' }} />
-                : undefined
-              }
+          <Grid container direction='column' item sx={{ borderRight: '1px solid', borderRightColor: borderColor, px: '7px' }} xs={5.5}>
+            <Grid container item justifyContent='space-between'>
+              <Identity
+                address={address}
+                api={api}
+                chain={chain}
+                noIdenticon
+                onClick={goToDetails}
+                style={{ width: 'calc(100% - 40px)' }}
+                subIdOnly
+              />
+              <Grid item width='40px'>
+                <Infotip text={account?.isHidden && t('This account is hidden from websites')}>
+                  <IconButton onClick={toggleVisibility} sx={{ height: '20px', ml: '7px', mt: '13px', p: 0, width: '28px' }}>
+                    <vaadin-icon icon={account?.isHidden ? 'vaadin:eye-slash' : 'vaadin:eye'} style={{ color: `${theme.palette.secondary.light}`, height: '20px' }} />
+                  </IconButton>
+                </Infotip>
+              </Grid>
             </Grid>
-            <Grid height='24px' item width='24px'>
-              <Infotip placement='right' text={t(recoverableToolTipTxt)}>
-                <IconButton
-                  onClick={openSocialRecovery}
-                  sx={{ height: '24px', width: '24px' }}
-                >
-                  <FontAwesomeIcon
-                    icon={faShieldHalved}
-                    style={{ border: '1px solid', borderRadius: '5px', color: isRecoverable ? theme.palette.success.main : theme.palette.action.disabledBackground, fontSize: '16px', padding: '3px' }}
-                  />
-                </IconButton>
-              </Infotip>
-            </Grid>
-            <Grid height='24px' item width='fit-content'>
-              <Infotip placement='right' text={t(proxyTooltipTxt)}>
-                <IconButton onClick={openManageProxy} sx={{ height: '16px', width: '16px' }}>
-                  <FontAwesomeIcon
-                    icon={faSitemap}
-                    style={{ border: '1px solid', borderRadius: '5px', color: hasProxy ? theme.palette.success.main : theme.palette.action.disabledBackground, fontSize: '16px', padding: '2px' }}
-                  />
-                </IconButton>
-              </Infotip>
+            <Grid alignItems='center' container item sx={{ '> div div:last-child': { width: 'auto' } }} xs>
+              <ShortAddress2 address={formatted || address} charsCount={40} showCopy style={{ fontSize: '10px', fontWeight: 300 }} />
             </Grid>
           </Grid>
+          <AssetsBox />
         </Grid>
-        <Grid container direction='column' item sx={{ borderRight: '1px solid', borderRightColor: borderColor, px: '7px' }} xs={5.5}>
-          <Grid container item justifyContent='space-between'>
-            <Identity
+        <Grid container item justifyContent='flex-end' minHeight='50px'>
+          <Divider sx={{ bgcolor: borderColor, height: '1px', mr: '5px', my: '15px', width: '100%' }} />
+          <Grid container item xs>
+            {(otherAssetsToShow === undefined || (otherAssetsToShow && otherAssetsToShow?.length > 0)) &&
+              <AOC
+                account={account}
+                accountAssets={otherAssetsToShow}
+                api={api}
+                assetId={assetId}
+                balanceToShow={balanceToShow}
+                borderColor={borderColor}
+                displayLogoAOC={displayLogoAOC}
+                mode='Home'
+                onclick={assetBoxClicked}
+              />
+            }
+          </Grid>
+          <Grid alignItems='center' container item width='fit-content'>
+            <Divider orientation='vertical' sx={{ bgcolor: borderColor, height: '34px', ml: 0, mr: '10px', my: 'auto', width: '1px' }} />
+            <FullScreenAccountMenu
               address={address}
-              api={api}
-              chain={chain}
-              noIdenticon
-              onClick={goToDetails}
-              style={{ width: 'calc(100% - 40px)' }}
-              subIdOnly
+              baseButton={
+                <AccountButton icon={<MoreVertIcon style={{ color: textAndIconColor, fontSize: '32px' }} />} onClick={openSettings} text={t<string>('Settings')} />
+              }
+              setDisplayPopup={setDisplayPopup}
             />
-            <Grid item width='40px'>
-              <Infotip text={account?.isHidden && t('This account is hidden from websites')}>
-                <IconButton onClick={toggleVisibility} sx={{ height: '20px', ml: '7px', mt: '13px', p: 0, width: '28px' }}>
-                  <vaadin-icon icon={account?.isHidden ? 'vaadin:eye-slash' : 'vaadin:eye'} style={{ color: `${theme.palette.secondary.light}`, height: '20px' }} />
-                </IconButton>
-              </Infotip>
-            </Grid>
-          </Grid>
-          <Grid alignItems='center' container item sx={{ '> div div:last-child': { width: 'auto' } }} xs>
-            <ShortAddress2 address={formatted || address} charsCount={40} showCopy style={{ fontSize: '10px', fontWeight: 300 }} />
+            <Divider orientation='vertical' sx={{ bgcolor: borderColor, height: '34px', ml: '5px', mr: '15px', my: 'auto', width: '1px' }} />
+            <AccountButton icon={<ArrowForwardIosIcon style={{ color: textAndIconColor, fontSize: '28px' }} />} onClick={goToDetails} text={t<string>('Details')} />
           </Grid>
         </Grid>
-        <AssetsBox />
       </Grid>
-      <Grid container item justifyContent='flex-end' minHeight='50px'>
-        <Divider sx={{ bgcolor: borderColor, height: '1px', mr: '5%', my: '15px', width: '95%' }} />
-        <Grid container item xs>
-          {(otherAssetsToShow === undefined || (otherAssetsToShow && otherAssetsToShow?.length > 0)) &&
-            <AOC
-              account={account}
-              accountAssets={otherAssetsToShow}
-              api={api}
-              assetId={assetId}
-              balanceToShow={balanceToShow}
-              borderColor={borderColor}
-              displayLogoAOC={displayLogoAOC}
-              mode='Home'
-              onclick={assetBoxClicked}
-            />
-          }
-        </Grid>
-        <Grid alignItems='center' container item width='fit-content'>
-          <Divider orientation='vertical' sx={{ bgcolor: borderColor, height: '34px', ml: 0, mr: '10px', my: 'auto', width: '1px' }} />
-          <FullScreenAccountMenu address={address} baseButton={
-            <AccountButton icon={<MoreVertIcon style={{ color: textAndIconColor, fontSize: '32px' }} />} onClick={openSettings} text={t<string>('Settings')} />
-          }
-          />
-          <Divider orientation='vertical' sx={{ bgcolor: borderColor, height: '34px', ml: '5px', mr: '15px', my: 'auto', width: '1px' }} />
-          <AccountButton icon={<ArrowForwardIosIcon style={{ color: textAndIconColor, fontSize: '28px' }} />} onClick={goToDetails} text={t<string>('Details')} />
-        </Grid>
-      </Grid>
-    </Grid>
+      {displayPopup === POPUPS_NUMBER.FORGET_ACCOUNT && account &&
+        <ForgetAccountModal
+          account={account}
+          setDisplayPopup={setDisplayPopup}
+        />
+      }
+      {displayPopup === POPUPS_NUMBER.DERIVE_ACCOUNT && address &&
+        <DeriveAccountModal
+          parentAddress={address}
+          setDisplayPopup={setDisplayPopup}
+        />
+      }
+      {displayPopup === POPUPS_NUMBER.RENAME && address &&
+        <RenameModal
+          address={address}
+          setDisplayPopup={setDisplayPopup}
+        />
+      }
+      {displayPopup === POPUPS_NUMBER.EXPORT_ACCOUNT && address &&
+        <ExportAccountModal
+          address={address}
+          setDisplayPopup={setDisplayPopup}
+        />
+      }
+    </>
   );
 }
