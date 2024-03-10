bc9f75cc55939d7a31f4d04671a59c79c0cbe715
Amir Ekbatanifard <amiref007@gmail.com>
2024-03-10 10:52:16 +0330

    Distinct colors

--- packages/extension-polkagate/src/popup/homeFullScreen/partials/TotalBalancePieChart.tsx
+++ packages/extension-polkagate/src/popup/homeFullScreen/partials/TotalBalancePieChart.tsx
@@ -177,7 +177,7 @@ function TotalBalancePieChart ({ hideNumbers }: Props): React.ReactElement {
                 </Collapse>
                 <Divider sx={{ bgcolor: borderColor, height: '2px', mt: '10px', width: '100%' }} />
                 <Grid alignItems='center' container item onClick={toggleAssets} sx={{ cursor: 'pointer', p: '5px', width: 'fit-content' }}>
-                  <Typography fontSize='16px' fontWeight={400}>
+                  <Typography color='primary.main' fontSize='16px' fontWeight={400}>
                     {t<string>(showMore ? 'Less tokens' : 'More tokens')}
                   </Typography>
                   <ArrowDropDownIcon sx={{ color: 'secondary.light', fontSize: '20px', stroke: '#BA2882', strokeWidth: '2px', transform: showMore ? 'rotate(-180deg)' : 'rotate(0deg)', transitionDuration: '0.2s', transitionProperty: 'transform' }} />
