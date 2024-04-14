01d672df60e6693c57b2402451d1f27639b33b13
Nick <ekbatanifard@gmail.com>
2024-04-14 18:50:56 +0330

    Update Confirmation.tsx

--- packages/extension-polkagate/src/fullscreen/stake/solo/configurePayee/Confirmation.tsx
+++ packages/extension-polkagate/src/fullscreen/stake/solo/configurePayee/Confirmation.tsx
@@ -18,7 +18,7 @@ interface Props {
   handleDone: () => void;
 }
 
-export default function Confirmation ({ handleDone, txInfo }: Props): React.ReactElement {
+export default function Confirmation({ handleDone, txInfo }: Props): React.ReactElement {
   const { t } = useTranslation();
   const { chainName, formatted, token } = useInfo(txInfo.from.address);
 
@@ -51,7 +51,7 @@ export default function Confirmation ({ handleDone, txInfo }: Props): React.Reac
           <Typography lineHeight='40px'>{value}</Typography>
         </Grid>
         {showDivider &&
-         <Div />
+          <Div />
         }
       </Grid>
     );
@@ -80,6 +80,9 @@ export default function Confirmation ({ handleDone, txInfo }: Props): React.Reac
         <Grid fontSize='16px' fontWeight={400} item lineHeight='22px' pl='5px'>
           <ShortAddress address={txInfo.from.address} inParentheses style={{ fontSize: '16px' }} />
         </Grid>
+        {!txInfo.throughProxy &&
+          <Div />
+        }
       </Grid>
       {txInfo.throughProxy &&
         <Grid container m='auto' maxWidth='92%'>
@@ -88,33 +91,33 @@ export default function Confirmation ({ handleDone, txInfo }: Props): React.Reac
         </Grid>
       }
       {txInfo?.payee &&
-      <>
-        <Grid alignItems='end' container justifyContent='center' sx={{ m: 'auto', pt: '5px', width: '90%' }}>
-          <Typography fontSize='16px' fontWeight={400} lineHeight='23px'>
-            {t<string>('Rewards destination')}:
-          </Typography>
-          {maybePayeeAddress &&
-            <Typography fontSize='16px' fontWeight={400} lineHeight='23px' maxWidth='34%' overflow='hidden' pl='5px' textOverflow='ellipsis' whiteSpace='nowrap'>
-              {destinationAccountName || t('Unknown')}
+        <>
+          <Grid alignItems='end' container justifyContent='center' sx={{ m: 'auto', pt: '5px', width: '90%' }}>
+            <Typography fontSize='16px' fontWeight={400} lineHeight='23px'>
+              {t<string>('Rewards destination')}:
             </Typography>
-          }
-          <Grid fontSize='16px' fontWeight={400} item lineHeight='22px' pl='5px'>
-            {maybePayeeAddress
-              ? <ShortAddress address={maybePayeeAddress} inParentheses style={{ fontSize: '16px' }} />
-              : <>{t('Add to staked amount')} </>
+            {maybePayeeAddress &&
+              <Typography fontSize='16px' fontWeight={400} lineHeight='23px' maxWidth='34%' overflow='hidden' pl='5px' textOverflow='ellipsis' whiteSpace='nowrap'>
+                {destinationAccountName || t('Unknown')}
+              </Typography>
             }
+            <Grid fontSize='16px' fontWeight={400} item lineHeight='22px' pl='5px'>
+              {maybePayeeAddress
+                ? <ShortAddress address={maybePayeeAddress} inParentheses style={{ fontSize: '16px' }} />
+                : <>{t('Add to staked amount')} </>
+              }
+            </Grid>
           </Grid>
-        </Grid>
-        <Div />
-      </>
+          <Div />
+        </>
       }
       {txInfo?.amount &&
-      <>
-        <DisplayInfo
-          caption={t('Amount:')}
-          value={`${txInfo.amount} ${token}`}
-        />
-      </>
+        <>
+          <DisplayInfo
+            caption={t('Amount:')}
+            value={`${txInfo.amount} ${token}`}
+          />
+        </>
       }
       <DisplayInfo
         caption={t('Fee:')}
