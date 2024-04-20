9218a786408e64a876430fab2f18720bc5323085
Nick <ekbatanifard@gmail.com>
2024-04-20 12:52:40 +0330

    Update RewardsChart.tsx

--- packages/extension-polkagate/src/fullscreen/stake/solo/partials/RewardsChart.tsx
+++ packages/extension-polkagate/src/fullscreen/stake/solo/partials/RewardsChart.tsx
@@ -409,7 +409,7 @@ export default function RewardsChart ({ address, rewardDestinationAddress }: Pro
                                     {d.era}
                                   </Grid>
                                   <Grid item width='40%'>
-                                    {amountToHuman(d.amount, decimal, 9)} {` ${token}`}
+                                    {amountToHuman(d.amount, decimal, 4)} {` ${token}`}
                                   </Grid>
                                 </Grid>
                               </AccordionSummary>
