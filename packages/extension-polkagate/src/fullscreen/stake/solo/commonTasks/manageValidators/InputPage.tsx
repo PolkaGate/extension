a72b252fa606489828d385c0611fe214fd632d6f
Nick <ekbatanifard@gmail.com>
2024-05-11 16:20:47 +0330

    Update InputPage.tsx

--- packages/extension-polkagate/src/fullscreen/stake/solo/commonTasks/manageValidators/InputPage.tsx
+++ packages/extension-polkagate/src/fullscreen/stake/solo/commonTasks/manageValidators/InputPage.tsx
@@ -24,7 +24,7 @@ interface Props {
   inputs: Inputs | undefined;
 }
 
-function arraysAreEqual(arr1: string[], arr2: string[]): boolean {
+function arraysAreEqual (arr1: string[], arr2: string[]): boolean {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -41,7 +41,7 @@ function arraysAreEqual(arr1: string[], arr2: string[]): boolean {
   return true;
 }
 
-export default function InputPage({ address, inputs, setInputs, setStep }: Props): React.ReactElement {
+export default function InputPage ({ address, inputs, setInputs, setStep }: Props): React.ReactElement {
   const { t } = useTranslation();
 
   const stakingConsts = useStakingConsts(address);
