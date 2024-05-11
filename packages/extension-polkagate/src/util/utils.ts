3b511d53a9153a6bbb376b983d464773713efc28
Amir Ekbatanifard <amiref007@gmail.com>
2024-05-11 12:55:08 +0330

    Create truncString32Bytes

--- packages/extension-polkagate/src/util/utils.ts
+++ packages/extension-polkagate/src/util/utils.ts
@@ -376,3 +376,20 @@ export const isUrl = (input: string | undefined) => {
 export const pgBoxShadow = (theme: Theme): string => theme.palette.mode === 'dark' ? '0px 4px 4px rgba(255, 255, 255, 0.25)' : '2px 3px 4px 0px rgba(0, 0, 0, 0.10)';
 
 export const noop = () => null;
+
+export const truncString32Bytes = (input: string | null | undefined): string | null | undefined => {
+  if (!input) {
+    return input;
+  }
+
+  const encoder = new TextEncoder();
+  let byteLength = encoder.encode(input).length;
+  let inputVal = input;
+
+  while (byteLength > 32) {
+    inputVal = inputVal.substring(0, inputVal.length - 1);
+    byteLength = encoder.encode(inputVal).length;
+  }
+
+  return inputVal;
+};
