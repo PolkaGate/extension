7f6154308b8f1eda5443c7849b11ca524e368924
Nick <ekbatanifard@gmail.com>
2024-05-15 10:52:21 +0330

    remove collapse

--- packages/extension-polkagate/src/fullscreen/onboarding/index.tsx
+++ packages/extension-polkagate/src/fullscreen/onboarding/index.tsx
@@ -86,12 +86,12 @@ function Onboarding (): React.ReactElement {
             </Grid>
             <Grid item>
               <Typography fontSize='30px' fontWeight={700} py='20px' width='100%'>
-                {t<string>('Welcome!')}
+                {t('Welcome!')}
               </Typography>
             </Grid>
           </Grid>
           <Typography fontSize='16px' fontWeight={500} pb='15px' pt='30px' width='100%'>
-            {t<string>('Thank you for choosing Polkagate, the gateway to the Polkadot ecosystem! 🌟')}
+            {t('Thank you for choosing Polkagate, the gateway to the Polkadot ecosystem! 🌟')}
           </Typography>
           <Typography fontSize='16px' fontWeight={400} width='100%'>
             {t('Currently, you do not have any accounts. Begin by creating your first account or importing existing accounts to get started.')}
@@ -103,59 +103,49 @@ function Onboarding (): React.ReactElement {
               _onClick={onCreate}
               _variant={'contained'}
               startIcon={<vaadin-icon icon='vaadin:plus-circle' style={{ height: '18px', color: `${theme.palette.text.main}` }} />}
-              text={t<string>('Create a new account')}
+              text={t('Create a new account')}
             />
             <Divider sx={{ fontSize: '20px', fontWeight: 400, my: '25px', width: '88%' }}>
               {t('Or')}
             </Divider>
-            <Collapse in={!showMore}>
-              <Grid alignItems='flex-end' container item justifyContent='center' onClick={toggleMore} pb='25px'>
-                <Typography sx={{ color: 'secondary.light', cursor: 'pointer', textDecoration: 'underline', userSelect: 'none' }}>
-                  {t('I already have account(s) and wish to import my existing account(s)')}
-                </Typography>
-                <ArrowForwardIosIcon sx={{ color: 'secondary.light', cursor: 'pointer', fontSize: 17, ml: '7px', stroke: '#BA2882', strokeWidth: '2px', transform: showMore ? 'rotate(-90deg)' : 'rotate(90deg)', transitionDuration: '0.3s', transitionProperty: 'transform' }} />
-              </Grid>
-            </Collapse>
-            <Collapse in={showMore}>
-              <Grid container item justifyContent='center' mb='25px'>
-                <PButton
-                  _ml={0}
-                  _mt='0'
-                  _onClick={onRestoreFromJson}
-                  _variant={'outlined'}
-                  text={t<string>('Restore from JSON file')}
-                />
-                <PButton
-                  _ml={0}
-                  _mt='15px'
-                  _onClick={onImport}
-                  _variant={'outlined'}
-                  text={t<string>('Import from recovery phrase')}
-                />
-                <PButton
-                  _ml={0}
-                  _mt='15px'
-                  _onClick={onAddWatchOnly}
-                  _variant={'outlined'}
-                  text={t<string>('Add watch-only account')}
-                />
-                <PButton
-                  _ml={0}
-                  _mt='15px'
-                  _onClick={onAttachQR}
-                  _variant={'outlined'}
-                  text={t<string>('Attach QR-signer')}
-                />
-                <PButton
-                  _ml={0}
-                  _mt='15px'
-                  _onClick={onImportLedger}
-                  _variant={'outlined'}
-                  text={t<string>('Attach ledger device')}
-                />
+            <Grid container item justifyContent='center' mb='25px'>
+              <PButton
+                _ml={0}
+                _mt='0'
+                _onClick={onRestoreFromJson}
+                _variant={'outlined'}
+                text={t('Restore from JSON file')}
+              />
+              <PButton
+                _ml={0}
+                _mt='15px'
+                _onClick={onImport}
+                _variant={'outlined'}
+                text={t('Import from recovery phrase')}
+              />
+              <PButton
+                _ml={0}
+                _mt='15px'
+                _onClick={onAddWatchOnly}
+                _variant={'outlined'}
+                text={t('Add watch-only account')}
+              />
+              <PButton
+                _ml={0}
+                _mt='15px'
+                _onClick={onAttachQR}
+                _variant={'outlined'}
+                text={t('Attach QR-signer')}
+              />
+              <PButton
+                _ml={0}
+                _mt='15px'
+                _onClick={onImportLedger}
+                _variant={'outlined'}
+                text={t('Attach ledger device')}
+              />
 
-              </Grid>
-            </Collapse>
+            </Grid>
             <Grid container justifyContent='center'>
               {/* eslint-disable-next-line react/jsx-no-bind */}
               <Typography onClick={() => setShowPrivacyAndSecurity(true)} sx={{ bottom: 0, cursor: 'pointer', fontSize: '12px', position: 'absolute', textAlign: 'center', textDecoration: 'underline' }}>
