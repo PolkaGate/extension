af5dfec04337dd2e0004845824686aae4b097aa0
Amir Ekbatanifard <amiref007@gmail.com>
2024-05-19 13:30:17 +0330

    Update ProxiedTable.tsx

--- packages/extension-polkagate/src/popup/import/importProxied/ProxiedTable.tsx
+++ packages/extension-polkagate/src/popup/import/importProxied/ProxiedTable.tsx
@@ -6,7 +6,7 @@
 import { faSquare, faSquareCheck } from '@fortawesome/free-regular-svg-icons';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Checkbox, FormControlLabel, Grid, Skeleton, SxProps, Theme, Typography, useTheme } from '@mui/material';
+import { Checkbox, FormControlLabel, Grid, Skeleton, SxProps, Theme, Typography, useTheme } from '@mui/material';
 import React, { useCallback, useMemo } from 'react';
 
 import { ApiPromise } from '@polkadot/api';
@@ -33,6 +33,7 @@ export default function ProxiedTable ({ api, chain, label, maxHeight = '120px', 
   const theme = useTheme();
 
   const isDarkMode = useMemo(() => theme.palette.mode === 'dark', [theme.palette.mode]);
+  const allSelected = useMemo(() => selectedProxied.length === proxiedAccounts?.length, [proxiedAccounts?.length, selectedProxied.length]);
 
   // const isAvailable = useCallback((proxy: Proxy): NameAddress | undefined =>
   //   accounts?.find((a) => a.address === getSubstrateAddress(proxy.delegate) && (proxyTypeFilter ? proxyTypeFilter.includes(proxy.proxyType) : true))
@@ -70,25 +71,6 @@ export default function ProxiedTable ({ api, chain, label, maxHeight = '120px', 
     proxiedAccounts && setSelectedProxied(selectAll ? proxiedAccounts : []);
   }, [proxiedAccounts, setSelectedProxied]);
 
-  const SelectDeselectButton = ({ allSelected, disable }: { allSelected: boolean, disable?: boolean }) => (
-    <Button
-      disabled={disable}
-      endIcon={
-        <FontAwesomeIcon
-          color={theme.palette.secondary.light}
-          fontSize='20px'
-          icon={allSelected ? faSquare : faSquareCheck}
-        />
-      }
-      onClick={onSelect(!allSelected)}
-      style={{ color: theme.palette.text.primary, textDecoration: 'underline' }}
-      sx={{ '&:hover': { bgcolor: 'unset' }, '&:onclick': { bgcolor: 'unset' }, '> span': { mr: 0 }, fontSize: '14px', fontWeight: 400, p: 0, textTransform: 'none', width: 'fit-content' }}
-      variant='text'
-    >
-      {allSelected ? t('Deselect') : t('Select all')}
-    </Button>
-  );
-
   // const fade = (toCheck: ProxyItem) => {
   //   if (mode === 'Delete') {
   //     return (toCheck.status === 'remove');
@@ -103,19 +85,21 @@ export default function ProxiedTable ({ api, chain, label, maxHeight = '120px', 
 
   return (
     <Grid container item sx={{ ...style }}>
-      <Label label={label} style={{ fontWeight: isExtensionMode ? 300 : 400, position: 'relative', width: '100%' }}>
+      <Label label={label} style={{ fontWeight: 400, position: 'relative', width: '100%' }}>
         <Grid container direction='column' item sx={{ '> div:not(:last-child:not(:only-child))': { borderBottom: '1px solid', borderBottomColor: 'secondary.light' }, bgcolor: 'background.paper', border: isExtensionMode || isDarkMode ? '1px solid' : 'none', borderColor: 'secondary.light', borderRadius: isExtensionMode ? '5px' : '0px', boxShadow: !isExtensionMode && !isDarkMode ? 'rgba(0, 0, 0, 0.1) 2px 3px 4px 0px' : 'none', display: 'block', maxHeight, minHeight, overflowY: 'scroll', textAlign: 'center' }}>
-          <Grid container item sx={{ '> div:not(:last-child)': { borderRight: '1px solid', borderRightColor: 'secondary.light' }, textAlign: 'center' }}>
-            <Grid container item xs={isExtensionMode ? 10 : 11}>
-              <Typography fontSize={isExtensionMode ? '12px' : '14px'} fontWeight={isExtensionMode ? 300 : 400} lineHeight={isExtensionMode ? '25px' : '35px'} pl='20px'>
-                {t('Account')}
-              </Typography>
+          <Grid container item sx={{ textAlign: 'center' }}>
+            <Grid alignItems='center' container item width='fit-content'>
+              <Grid alignItems='center' container item onClick={onSelect(!allSelected)} sx={{ cursor: !proxiedAccounts ? 'default' : 'pointer', pl: isExtensionMode ? '18px' : '20px', width: 'fit-content' }}>
+                <FontAwesomeIcon
+                  color={theme.palette.secondary.light}
+                  fontSize='21px'
+                  icon={allSelected ? faSquareCheck : faSquare}
+                />
+              </Grid>
             </Grid>
-            <Grid container item xs={isExtensionMode ? 2 : 1}>
-              <Typography fontSize={isExtensionMode ? '12px' : '14px'} fontWeight={isExtensionMode ? 300 : 400} lineHeight={isExtensionMode ? '25px' : '35px'} sx={{ textAlign: 'center', width: '100%' }}>
-                {t('Select')}
-              </Typography>
-            </Grid>
+            <Typography fontSize={'14px'} fontWeight={isExtensionMode ? 300 : 400} lineHeight={isExtensionMode ? '25px' : '35px'} pl={isExtensionMode ? '30px' : '35px'}>
+              {t('Proxied Account(s)')}
+            </Typography>
           </Grid>
           {proxiedAccounts === undefined &&
             <Grid alignItems='center' container justifyContent='center'>
@@ -131,12 +115,12 @@ export default function ProxiedTable ({ api, chain, label, maxHeight = '120px', 
             </Grid>
           }
           {proxiedAccounts && proxiedAccounts.length > 0 && proxiedAccounts.map((proxiedAccount, index) =>
-            <Grid container item key={index} sx={{ '> div:not(:last-child)': { borderRight: '1px solid', borderRightColor: 'secondary.light' }, height: isExtensionMode ? '41px' : '50px', textAlign: 'center' }}>
-              <Grid alignItems='center' container item justifyContent='left' pl='10px' xs={isExtensionMode ? 10 : 11}>
-                <Identity api={api} chain={chain} formatted={proxiedAccount} identiconSize={25} showShortAddress showSocial={false} style={{ fontSize: isExtensionMode ? '12px' : '16px', maxWidth: '100%' }} subIdOnly />
-              </Grid>
+            <Grid container item key={index} sx={{ height: isExtensionMode ? '41px' : '50px', textAlign: 'center' }}>
               <Grid alignItems='center' container height='100%' item justifyContent='center' xs={isExtensionMode ? 2 : 1}>
                 <Select index={index} proxied={proxiedAccount} />
+              </Grid>
+              <Grid alignItems='center' container item justifyContent='left' pl='10px' xs={isExtensionMode ? 10 : 11}>
+                <Identity api={api} chain={chain} formatted={proxiedAccount} identiconSize={25} showShortAddress showSocial={false} style={{ fontSize: isExtensionMode ? '12px' : '16px', maxWidth: '100%' }} subIdOnly />
               </Grid>
             </Grid>
           )}
@@ -149,7 +133,6 @@ export default function ProxiedTable ({ api, chain, label, maxHeight = '120px', 
           </Typography>
           : <Skeleton animation='wave' height='30px' width='100px' />
         }
-        <SelectDeselectButton allSelected={selectedProxied.length === proxiedAccounts?.length} disable={!proxiedAccounts} />
       </Grid>
     </Grid>
   );
