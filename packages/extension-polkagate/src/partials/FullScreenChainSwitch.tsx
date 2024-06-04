bbbe4473d32f70c87d426178ae1fe8a36e9f1240
Nick <ekbatanifard@gmail.com>
2024-06-04 14:26:39 +0330

    closes  #1067

--- packages/extension-polkagate/src/partials/FullScreenChainSwitch.tsx
+++ packages/extension-polkagate/src/partials/FullScreenChainSwitch.tsx
@@ -6,7 +6,7 @@
 import { Avatar, Grid, Popover, Typography, useTheme } from '@mui/material';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
-import { useChainName, useGenesisHashOptions, useIsTestnetEnabled } from '../hooks';
+import { useGenesisHashOptions, useInfo, useIsTestnetEnabled } from '../hooks';
 import { tieAccount } from '../messaging';
 import { CHAINS_WITH_BLACK_LOGO, TEST_NETS } from '../util/constants';
 import getLogo from '../util/getLogo';
@@ -21,18 +21,24 @@ interface Props {
 function FullScreenChainSwitch ({ address, chains }: Props): React.ReactElement<Props> {
   const theme = useTheme();
   const options = useGenesisHashOptions();
-  const currentChainNameFromAccount = useChainName(address);
+  const { chainName: chainNameFromAccount } = useInfo(address);
   const isTestnetEnabled = useIsTestnetEnabled();
 
-  const [currentSelectedChainName, setCurrentSelectedChainName] = useState<string>();
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const [selectedChainName, setSelectedChainName] = useState<string>();
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [flip, setFlip] = useState(false);
 
   const isTestnetDisabled = useCallback((genesisHash: string) => !isTestnetEnabled && TEST_NETS.includes(genesisHash), [isTestnetEnabled]);
   const selectableNetworks = useMemo(() => !chains.length ? options.filter(({ text }) => text !== 'Allow use on any chain') : options.filter((o) => chains.includes(o.value)), [chains, options]);
 
   useEffect(() => {
-    setCurrentSelectedChainName(currentChainNameFromAccount);
-  }, [currentChainNameFromAccount]);
+    setSelectedChainName(chainNameFromAccount);
+  }, [chainNameFromAccount]);
+
+  useEffect(() => {
+    setFlip(true);
+    setTimeout(() => setFlip(false), 1000);
+  }, [selectedChainName]);
 
   const selectNetwork = useCallback((net: DropdownOption) => {
     setAnchorEl(null);
@@ -46,7 +52,7 @@ function FullScreenChainSwitch ({ address, chains }: Props): React.ReactElement<
     }
 
     tieAccount(address, net.value as string).catch(console.error);
-    setCurrentSelectedChainName(net.text);
+    setSelectedChainName(net.text);
   }, [address, isTestnetDisabled]);
 
   const handleClose = useCallback(() => {
@@ -57,17 +63,16 @@ function FullScreenChainSwitch ({ address, chains }: Props): React.ReactElement<
     setAnchorEl(event.currentTarget);
   }, []);
 
-  const chainName = useCallback((text: string) => sanitizeChainName(text)?.toLowerCase(), []);
+  const sanitizedLowercase = useCallback((text: string) => sanitizeChainName(text)?.toLowerCase(), []);
 
   const NetworkList = () => (
     <Grid container item sx={{ maxHeight: '550px', overflow: 'hidden', overflowY: 'scroll', width: '250px' }}>
       {selectableNetworks && selectableNetworks.length > 0 &&
         selectableNetworks.map((network, index) => {
-          const selectedNetwork = chainName(network.text) === currentSelectedChainName?.toLocaleLowerCase();
+          const selectedNetwork = sanitizedLowercase(network.text) === selectedChainName?.toLocaleLowerCase();
 
           return (
-            // eslint-disable-next-line react/jsx-no-bind
-            <Grid container justifyContent='space-between' key={index} onClick={() => selectNetwork(network)} sx={{ ':hover': { bgcolor: theme.palette.mode === 'light' ? 'rgba(24, 7, 16, 0.1)' : 'rgba(255, 255, 255, 0.1)' }, bgcolor: selectedNetwork ? 'rgba(186, 40, 130, 0.2)' : 'transparent', cursor: isTestnetDisabled(network.value) ? 'not-allowed' : 'pointer', height: '45px', px: '15px', opacity: isTestnetDisabled(network.value) ? 0.3 : 1 }}>
+            <Grid container justifyContent='space-between' key={index} onClick={() => selectNetwork(network)} sx={{ ':hover': { bgcolor: theme.palette.mode === 'light' ? 'rgba(24, 7, 16, 0.1)' : 'rgba(255, 255, 255, 0.1)' }, bgcolor: selectedNetwork ? 'rgba(186, 40, 130, 0.2)' : 'transparent', cursor: isTestnetDisabled(network.value as string) ? 'not-allowed' : 'pointer', height: '45px', opacity: isTestnetDisabled(network.value as string) ? 0.3 : 1, px: '15px' }}>
               <Grid alignItems='center' container item width='fit-content'>
                 <Typography fontSize='16px' fontWeight={selectedNetwork ? 500 : 400}>
                   {network.text}
@@ -76,7 +81,7 @@ function FullScreenChainSwitch ({ address, chains }: Props): React.ReactElement<
               {network.text !== 'Allow use on any chain' &&
                 <Grid alignItems='center' container item pl='15px' width='fit-content'>
                   <Avatar
-                    src={getLogo(chainName(network.text))}
+                    src={getLogo(sanitizedLowercase(network.text))}
                     sx={{ borderRadius: '50%', filter: (CHAINS_WITH_BLACK_LOGO.includes(network.text) && theme.palette.mode === 'dark') ? 'invert(1)' : '', height: 29, width: 29 }}
                     variant='square'
                   />
@@ -95,13 +100,15 @@ function FullScreenChainSwitch ({ address, chains }: Props): React.ReactElement<
     <Grid container item>
       <Grid aria-describedby={id} component='button' container item onClick={handleClick} sx={{ bgcolor: 'transparent', border: '1px solid', borderColor: 'secondary.main', borderRadius: '50%', cursor: 'pointer', height: '40px', p: 0, width: '40px' }}>
         <Avatar
-          src={getLogo(currentSelectedChainName)}
+          src={getLogo(selectedChainName)}
           sx={{
             bgcolor: 'transparent',
             borderRadius: '50%',
-            filter: CHAINS_WITH_BLACK_LOGO.includes(currentSelectedChainName ?? '') ? 'invert(1)' : '',
+            filter: CHAINS_WITH_BLACK_LOGO.includes(selectedChainName ?? '') ? 'invert(1)' : '',
             height: '34px',
             m: 'auto',
+            transform: flip ? 'rotateY(180deg)' : 'rotateY(0deg)',
+            transition: 'transform 1s',
             width: '34px'
           }}
         />
