2db1f531dd88464bf4520ba10144f1b258f91f9c
Nick <ekbatanifard@gmail.com>
2024-06-04 17:45:18 +0330

    use people chain

--- packages/extension-polkagate/src/hooks/useAccountsInfo.ts
+++ packages/extension-polkagate/src/hooks/useAccountsInfo.ts
@@ -2,35 +2,47 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { DeriveAccountInfo } from '@polkadot/api-derive/types';
+import type { PalletIdentityRegistration } from '@polkadot/types/lookup';
 
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { ApiPromise } from '@polkadot/api';
 import { Chain } from '@polkadot/extension-chains/types';
 import { hexToString } from '@polkadot/util';
 import { encodeAddress } from '@polkadot/util-crypto';
 
-export default function useAccountsInfo(api: ApiPromise | undefined, chain: Chain | null | undefined): DeriveAccountInfo[] | undefined {
+import { PEOPLE_CHAINS } from '../util/constants';
+import { useApiWithChain2, usePeopleChain } from '.';
+
+export default function useAccountsInfo (api: ApiPromise | undefined, chain: Chain | null | undefined): DeriveAccountInfo[] | undefined {
   const [infos, setInfos] = useState<DeriveAccountInfo[] | undefined>();
   const [fetching, setFetching] = useState<boolean>(false);
+
+  const { peopleChain } = usePeopleChain(undefined, chain?.genesisHash);
+  const peopleChainApi = useApiWithChain2(peopleChain);
+  const isPeopleChainEnabled = PEOPLE_CHAINS.includes(chain?.name || '');
+
+  const _api = useMemo(() => isPeopleChainEnabled ? peopleChainApi : api, [api, isPeopleChainEnabled, peopleChainApi]);
 
   const getAccountsInfo = useCallback(() => {
     try {
       setFetching(true);
-      api && api.query.identity.identityOf.entries().then((ids) => {
+      _api && _api.query.identity.identityOf.entries().then((ids) => {
         console.log(`${ids?.length} accountsInfo fetched from ${chain?.name ?? ''}`);
 
-        const fetchedAccountsInfo = ids.map(([key, option]) => {
+        const fetchedAccountsInfo = ids.map(([key, value]) => {
+          const { info, judgements } = value.unwrap()[0] as PalletIdentityRegistration;
+
           return {
             accountId: encodeAddress('0x' + key.toString().slice(82), chain?.ss58Format),
             identity: {
-              judgements: option.unwrap().judgements,
-              display: hexToString(option.unwrap().info.display.asRaw.toHex()),
-              email: hexToString(option.unwrap().info.email.asRaw.toHex()),
-              legal: hexToString(option.unwrap().info.legal.asRaw.toHex()),
-              riot: hexToString(option.unwrap().info.riot.asRaw.toHex()),
-              twitter: hexToString(option.unwrap().info.twitter.asRaw.toHex()),
-              web: hexToString(option.unwrap().info.web.asRaw.toHex()),
+              display: hexToString(info.display.asRaw.toHex()),
+              email: hexToString(info.email.asRaw.toHex()),
+              judgements,
+              legal: hexToString(info.legal.asRaw.toHex()),
+              riot: hexToString((info.riot || info.matrix).asRaw.toHex()),
+              twitter: hexToString(info.twitter.asRaw.toHex()),
+              web: hexToString(info.web.asRaw.toHex())
             }
           };
         });
@@ -53,10 +65,10 @@ export default function useAccountsInfo(api: ApiPromise | undefined, chain: Chai
       setInfos(undefined);
       setFetching(false);
     }
-  }, [api, chain]);
+  }, [_api, chain]);
 
   useEffect(() => {
-    if (!api || !chain || !chain.genesisHash || !chain.name || !chain.ss58Format || fetching || api.genesisHash.toHex() !== chain.genesisHash) {
+    if (!_api || !chain || !chain.genesisHash || !chain.name || !chain.ss58Format || fetching) {
       return;
     }
 
@@ -71,7 +83,7 @@ export default function useAccountsInfo(api: ApiPromise | undefined, chain: Chai
     });
 
     getAccountsInfo();
-  }, [api, chain, fetching, getAccountsInfo]);
+  }, [_api, chain, fetching, getAccountsInfo]);
 
   return infos;
 }
