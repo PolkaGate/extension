0d4bd26db51b0c64255a968b6618796fb671ba49
Nick <ekbatanifard@gmail.com>
2024-06-17 16:35:32 +0330

    remove  @ts-nocheck

--- packages/extension-polkagate/src/util/chains.ts
+++ packages/extension-polkagate/src/util/chains.ts
@@ -1,6 +1,5 @@
 // Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-// @ts-nocheck
 
 import type { MetadataDefBase } from '@polkadot/extension-inject/types';
 
