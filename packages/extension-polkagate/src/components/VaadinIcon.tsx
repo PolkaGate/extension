265d0d00e240ca3073754cc9825baec117b48193
Amir Ekbatanifard <amiref007@gmail.com>
2024-06-18 10:23:47 +0330

    Create VaadinIcon component to use instead of vaadin-icon

---
+++ packages/extension-polkagate/src/components/VaadinIcon.tsx
@@ -0,0 +1,16 @@
+// Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+// @ts-nocheck
+
+import React from 'react';
+import '@vaadin/icons';
+
+interface VaadinIconProps extends React.HTMLAttributes<HTMLElement> {
+  icon: string;
+}
+
+const VaadinIcon: React.FC<VaadinIconProps> = ({ icon, ...props }) => {
+  return <vaadin-icon icon={icon} {...props}></vaadin-icon>;
+};
+
+export default VaadinIcon;
