e7a96b795cdf4a798da138b71fc51f751e3003ef
Nick <ekbatanifard@gmail.com>
2024-06-19 19:10:22 +0330

    add profile text as inputs
    + add Ledger

--- packages/extension-polkagate/src/hooks/useProfileAccounts.tsx
+++ packages/extension-polkagate/src/hooks/useProfileAccounts.tsx
@@ -1,18 +1,22 @@
 // Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { useEffect, useLayoutEffect, useState } from 'react';
 import type { AccountsOrder } from '../fullscreen/homeFullScreen';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { getStorage, watchStorage } from '../components/Loading';
 import { useTranslation } from '.';
 
-export default function useProfileAccounts(initialAccountList: AccountsOrder[] | undefined) {
+export default function useProfileAccounts(initialAccountList: AccountsOrder[] | undefined, profile?: string) {
   const { t } = useTranslation();
 
-  const [profile, setProfile] = useState<string>();
+  const [_profile, setProfile] = useState<string>();
   const [profileAccounts, setProfileAccounts] = useState<AccountsOrder[] | undefined>();
 
   useEffect(() => {
+    if (profile) {
+      return setProfile(profile);
+    }
+
     getStorage('profile').then((res) => {
       setProfile(res as string || t('All'));
     }).catch(console.error);
@@ -25,12 +29,15 @@ export default function useProfileAccounts(initialAccountList: AccountsOrder[] |
       return;
     }
 
-    switch (profile) {
+    switch (_profile) {
       case t('All'):
         return setProfileAccounts(initialAccountList);
       case t('Local'):
         const localAccounts = initialAccountList.filter(({ account: { isExternal } }) => !isExternal);
         return setProfileAccounts(localAccounts);
+      case t('Ledger'):
+        const ledgerAccounts = initialAccountList.filter(({ account: { isHardware } }) => isHardware);
+        return setProfileAccounts(ledgerAccounts);
       case t('Watch Only'):
         const watchOnlyAccounts = initialAccountList.filter(({ account: { isExternal, isQR, isHardware } }) => isExternal && !isQR && !isHardware);
         return setProfileAccounts(watchOnlyAccounts);
@@ -38,10 +45,10 @@ export default function useProfileAccounts(initialAccountList: AccountsOrder[] |
         const qrAttachedAccounts = initialAccountList.filter(({ account: { isQR } }) => isQR);
         return setProfileAccounts(qrAttachedAccounts);
       default:
-        const useDefinedProfile = initialAccountList.filter(({ account }) => account?.profile && account.profile === profile);
+        const useDefinedProfile = initialAccountList.filter(({ account }) => account?.profile && account.profile === _profile);
         return setProfileAccounts(useDefinedProfile);
     }
-  }, [profile, initialAccountList]);
+  }, [_profile, initialAccountList]);
 
   return profileAccounts && (profileAccounts?.length ? profileAccounts : initialAccountList);
 }
