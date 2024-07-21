616a18b1fcd80c4533d041835f061576970ba6cf
Nick <ekbatanifard@gmail.com>
2024-07-21 23:19:19 +0330

    Update news.ts

--- packages/extension-polkagate/src/popup/home/news.ts
+++ packages/extension-polkagate/src/popup/home/news.ts
@@ -8,15 +8,16 @@ export interface News {
 
 export const news: News[] = [
   {
-    "version": "6.3.3",
+    "version": "0.6.3",
     "notes": [
       "Support for Ledger Polkadot Generic App: The new Generic Ledger App and Migration App are now supported.",
       "Fixed People Chain Price Issue: Token prices on the People Chain are now displayed correctly.",
+      "Resolve token unlock issues with Polkadot Vault: Address the problem of unlocking tokens locked in referenda using Polkadot Vault.",
       "Fixed Various UI Issues: Resolved the blinking issue on the Pool Info Page and corrected text misalignment on the Staking Reward Page."
     ]
   },
   {
-    "version": "6.0.1",
+    "version": "0.6.1",
     "notes": [
       "Paseo Support: The Paseo testnet and its asset hub are now accessible through the wallet.",
       "Multiple Profile Accounts: An account can now be added to multiple profiles, allowing for better organization of accounts.",
