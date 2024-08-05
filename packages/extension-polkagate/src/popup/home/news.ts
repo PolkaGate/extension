4c33d21bcdc3d94ea9b43ecb8534eb86de40b16c
Nick <ekbatanifard@gmail.com>
2024-08-05 14:09:34 +0330

    Update news.ts

--- packages/extension-polkagate/src/popup/home/news.ts
+++ packages/extension-polkagate/src/popup/home/news.ts
@@ -10,8 +10,9 @@ export interface News {
 
 export const news: News[] = [
   {
-    version: '0.7.0',
+    version: '0.7.2',
     notes: [
+      'Granular access to dApps: Select specific accounts when authorizing websites to access your accounts.',
       'Support for Stablecoin Requests in Treasury: Accurately displaying requests for USDT on the Asset Hub through Governance mechanisms.',
       'Hide Numbers on Login Page: Add an option to hide numbers on the login page before logging in.',
       'Nomination Pools are evolving: New alerts for dual stakers (solo and pool) to stay on one. Unstake solo funds soon for automatic pool migration, enabling pool and governance participation without manual changes.'
