cefbb6f635e1eb7e0423940bf30061ad0af3f356
Nick <ekbatanifard@gmail.com>
2024-08-25 14:34:06 +0330

    decrease padding

--- packages/extension-polkagate/src/fullscreen/stake/pool/commonTasks/manageValidators/index.tsx
+++ packages/extension-polkagate/src/fullscreen/stake/pool/commonTasks/manageValidators/index.tsx
@@ -50,7 +50,7 @@ export default function ManageValidators (): React.ReactElement {
   return (
     <Grid bgcolor='backgroundFL.primary' container item justifyContent='center'>
       <FullScreenHeader page='stake' unableToChangeAccount />
-      <Grid alignItems='center' container item justifyContent='center' sx={{ bgcolor: 'backgroundFL.secondary', display: 'block', height: 'calc(100vh - 70px)', maxWidth: FULLSCREEN_WIDTH, overflow: 'scroll', px: '5%' }}>
+      <Grid alignItems='center' container item justifyContent='center' sx={{ bgcolor: 'backgroundFL.secondary', display: 'block', height: 'calc(100vh - 70px)', maxWidth: FULLSCREEN_WIDTH, overflow: 'scroll', px: '4%' }}>
         <Bread />
         <Title
           height='100px'
