5b3daa73472936f5d7ae14a3905defecab617d33
Nick <ekbatanifard@gmail.com>
2024-09-09 18:48:12 +0330

    fix: handle unexpected error during address encoding/decoding

--- packages/extension-polkagate/src/hooks/useFormatted2.ts
+++ packages/extension-polkagate/src/hooks/useFormatted2.ts
@@ -1,18 +1,17 @@
 // Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-// @ts-nocheck
+
+import type { Chain } from '@polkadot/extension-chains/types';
+import type { AccountId } from '@polkadot/types/interfaces/runtime';
 
 import { useMemo } from 'react';
 
-import type { Chain } from '@polkadot/extension-chains/types';
-
-import type { AccountId } from '@polkadot/types/interfaces/runtime';
 import { decodeAddress, encodeAddress } from '@polkadot/util-crypto';
 
 import { useChain } from './';
 
 /** We use chain here to fetch formatted address */
-export default function useFormatted2(address?: AccountId | string, formatted?: AccountId | string, chain: Chain | null | undefined): AccountId | string | undefined {
+export default function useFormatted2 (address: AccountId | string |undefined, formatted: AccountId | string|undefined, chain: Chain | null | undefined): AccountId | string | undefined {
   const _chain = useChain(address, chain);
 
   const encodedAddress = useMemo(() => {
@@ -26,10 +25,14 @@ export default function useFormatted2(address?: AccountId | string, formatted?: 
 
     const prefix: number = _chain.ss58Format;
 
-    if (address && prefix !== undefined) {
-      const publicKey = decodeAddress(address);
+    try {
+      if (address && prefix !== undefined) {
+        const publicKey = decodeAddress(address);
 
-      return encodeAddress(publicKey, prefix);
+        return encodeAddress(publicKey, prefix);
+      }
+    } catch (error) {
+      console.error('error in useFormatted 2:', error);
     }
 
     return undefined;
