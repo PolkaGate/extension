eacb3ae309993fa0d7c3d302ae0d85c6f0823fc8
Nick <ekbatanifard@gmail.com>
2024-09-09 18:29:05 +0330

    chore: remove ts no check

--- packages/extension-polkagate/src/hooks/useAccountName.ts
+++ packages/extension-polkagate/src/hooks/useAccountName.ts
@@ -1,15 +1,14 @@
 // Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-// @ts-nocheck
-
-import { useEffect, useState } from 'react';
 
 import type { AccountId } from '@polkadot/types/interfaces/runtime';
+
+import { useEffect, useState } from 'react';
 
 import { getSubstrateAddress } from '../util/utils';
 import { useAccount } from '.';
 
-export default function useAccountName(address: string | AccountId | undefined): string | undefined {
+export default function useAccountName (address: string | AccountId | undefined): string | undefined {
   const [name, setName] = useState<string | undefined>();
   const substrateAddress = getSubstrateAddress(address);
 
