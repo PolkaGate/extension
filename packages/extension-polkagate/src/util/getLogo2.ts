fed12cce6aefb0d259c8766d95a7cee4984aacee
Nick <ekbatanifard@gmail.com>
2024-09-15 14:08:56 +0330

    fix: show skeleton instead of any chain alert before fetching the chain

--- packages/extension-polkagate/src/util/getLogo2.ts
+++ packages/extension-polkagate/src/util/getLogo2.ts
@@ -1,10 +1,11 @@
 // Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { Chain } from '../../../extension-chains/src/types';
+
 import { createWsEndpoints, externalLinks } from '@polkagate/apps-config';
 import { createAssets } from '@polkagate/apps-config/assets';
 
-import type { Chain } from '../../../extension-chains/src/types';
 import { toCamelCase } from '../fullscreen/governance/utils/util';
 import getNetworkMap from './getNetworkMap';
 import { sanitizeChainName } from './utils';
@@ -17,7 +18,7 @@ export interface LogoInfo {
   subLogo?: string;
 }
 
-export default function getLogo2(info: string | undefined | Chain, token?: string): LogoInfo | undefined {
+export default function getLogo2 (info: string | undefined | null | Chain, token?: string): LogoInfo | undefined {
   let chainNameFromGenesisHash;
 
   if (token) {
