62f9fa3151dfe7703cefb91aa13467e302d0967c
Nick <ekbatanifard@gmail.com>
2024-09-23 12:09:15 +0330

    chore: rename function

--- packages/extension-polkagate/src/hooks/useApi.ts
+++ packages/extension-polkagate/src/hooks/useApi.ts
@@ -195,8 +195,8 @@ export default function useApi (address: AccountId | string | undefined, stateAp
     apisContext.setIt({ ...apisContext.apis });
   }, [apisContext]);
 
-  // Handles connection request to manual endpoint
-  const handleManualApiRequest = useCallback((manualEndpoint: string, genesisHash: string) => {
+  // Handles connection request to a manual endpoint
+  const handleApiWithChain = useCallback((manualEndpoint: string, genesisHash: string) => {
     // Check if there is a saved API that is already connected
     const savedApi = apisContext?.apis[genesisHash]?.find((sApi) => sApi.endpoint === manualEndpoint);
 
@@ -220,7 +220,7 @@ export default function useApi (address: AccountId | string | undefined, stateAp
   useEffect(() => {
     // if _endpoint & _genesisHash are available means useApiWithChain2 is trying to create a new connection!
     if (_endpoint && _genesisHash) {
-      handleManualApiRequest(_endpoint, _genesisHash);
+      handleApiWithChain(_endpoint, _genesisHash);
 
       return;
     }
@@ -293,7 +293,7 @@ export default function useApi (address: AccountId | string | undefined, stateAp
 
     // @ts-expect-error to bypass access to private prop
     // eslint-disable-next-line react-hooks/exhaustive-deps, @typescript-eslint/no-unsafe-member-access
-  }, [address, apisContext?.apis?.[chainGenesisHash]?.length, _endpoint, _genesisHash, addApiRequest, chainGenesisHash, checkForNewOne, connectToEndpoint, endpoint, handleAutoMode, handleManualApiRequest, handleNewApi, state?.api?._options?.provider?.endpoint, state.isLoading]);
+  }, [address, apisContext?.apis?.[chainGenesisHash]?.length, _endpoint, _genesisHash, addApiRequest, chainGenesisHash, checkForNewOne, connectToEndpoint, endpoint, handleAutoMode, handleApiWithChain, handleNewApi, state?.api?._options?.provider?.endpoint, state.isLoading]);
 
   return state.api;
 }
