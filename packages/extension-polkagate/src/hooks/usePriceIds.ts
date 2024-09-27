3df180ce94f9749943883b87fa1306697d387a60
Nick <ekbatanifard@gmail.com>
2024-09-27 17:14:42 +0330

    fix: get price for user added chains

--- packages/extension-polkagate/src/hooks/usePriceIds.ts
+++ packages/extension-polkagate/src/hooks/usePriceIds.ts
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { createAssets } from '@polkagate/apps-config/assets';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
+import { getStorage } from '../components/Loading';
 import { TEST_NETS } from '../util/constants';
 import getChainName from '../util/getChainName';
 import useSelectedChains from './useSelectedChains';
@@ -16,16 +17,27 @@ const assetsChains = createAssets();
  */
 export default function usePriceIds (): string[] | undefined | null {
   const selectedChains = useSelectedChains();
+  const [useAddedPriceIds, setUserAddedPriceIds] = useState<string[]>([]);
+
+  useEffect(() => {
+    getStorage('userAddedEndpoint').then((info) => {
+      if (info) {
+        const maybePriceIds: string[] | undefined = Object.entries(info).map(([_, { priceId }]) => priceId as string).filter(Boolean);
+
+        maybePriceIds?.length && setUserAddedPriceIds(maybePriceIds);
+      }
+    }).catch(console.error);
+  }, []);
 
   return useMemo(() => {
     const nonTestNetSelectedChains = selectedChains?.filter((genesisHash) => !TEST_NETS.includes(genesisHash));
-    let selectedChainsChainName = nonTestNetSelectedChains?.map((genesisHash) => getChainName(genesisHash));
+    let selectedChainsChainName = nonTestNetSelectedChains?.map((genesisHash) => getChainName(genesisHash)).filter(Boolean);
 
     const assetsInfoOfMultiAssetSelectedChains = selectedChainsChainName?.map((chainName) => chainName && assetsChains[chainName]?.map((asset) => asset?.priceId))?.flat().filter((item) => !!item);
 
     selectedChainsChainName = selectedChainsChainName?.map((item) => item?.replace('AssetHub', '')); // TODO: needs double check
-    const nonDuplicatedPriceIds = new Set([...(selectedChainsChainName || []), ...(assetsInfoOfMultiAssetSelectedChains || [])]);
+    const nonDuplicatedPriceIds = new Set([...(selectedChainsChainName || []), ...(assetsInfoOfMultiAssetSelectedChains || []), ...useAddedPriceIds]);
 
     return nonDuplicatedPriceIds.size ? [...nonDuplicatedPriceIds] as string[] : null;
-  }, [selectedChains]);
+  }, [selectedChains, useAddedPriceIds]);
 }
