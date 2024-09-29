0325dcaba6513fbcb6bcb6c64121ffd2dbe34549
Nick <ekbatanifard@gmail.com>
2024-09-29 12:31:44 +0330

    fix: increase the decimal points length when currency is a crypto such as BTC or ETH

--- packages/extension-polkagate/src/components/FormatPrice.tsx
+++ packages/extension-polkagate/src/components/FormatPrice.tsx
@@ -29,13 +29,13 @@ interface Props {
 
 export function nFormatter (num: number, decimalPoint: number) {
   const lookup = [
-    { value: 1, symbol: '' },
-    { value: 1e3, symbol: 'k' },
-    { value: 1e6, symbol: 'M' },
-    { value: 1e9, symbol: 'G' },
-    { value: 1e12, symbol: 'T' },
-    { value: 1e15, symbol: 'P' },
-    { value: 1e18, symbol: 'E' }
+    { symbol: '', value: 1 },
+    { symbol: 'k', value: 1e3 },
+    { symbol: 'M', value: 1e6 },
+    { symbol: 'G', value: 1e9 },
+    { symbol: 'T', value: 1e12 },
+    { symbol: 'P', value: 1e15 },
+    { symbol: 'E', value: 1e18 }
   ];
 
   const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
@@ -49,6 +49,8 @@ export function nFormatter (num: number, decimalPoint: number) {
 
   return item ? (num / item.value).toFixed(decimalPoint).replace(rx, '$1') + item.symbol : '0';
 }
+
+const DECIMAL_POINTS_FOR_CRYPTO_AS_CURRENCY = 4;
 
 function FormatPrice ({ amount, decimalPoint = 2, decimals, fontSize, fontWeight, height, lineHeight = 1, mt = '0px', num, price, sign, skeletonHeight = 15, textAlign = 'left', textColor, width = '90px' }: Props): React.ReactElement<Props> {
   const currency = useCurrency();
@@ -65,6 +67,14 @@ function FormatPrice ({ amount, decimalPoint = 2, decimals, fontSize, fontWeight
     return undefined;
   }, [amount, decimals, num, price]);
 
+  const _decimalPoint = useMemo(() => {
+    if (currency?.code && ['ETH', 'BTC'].includes(currency.code)) {
+      return DECIMAL_POINTS_FOR_CRYPTO_AS_CURRENCY;
+    }
+
+    return decimalPoint;
+  }, [currency?.code, decimalPoint]);
+
   return (
     <Grid
       item
@@ -79,7 +89,7 @@ function FormatPrice ({ amount, decimalPoint = 2, decimals, fontSize, fontWeight
           lineHeight={lineHeight}
           sx={{ color: textColor }}
         >
-          {sign || currency?.sign || ''}{nFormatter(total as number, decimalPoint)}
+          {sign || currency?.sign || ''}{nFormatter(total as number, _decimalPoint)}
         </Typography>
         : <Skeleton
           animation='wave'
