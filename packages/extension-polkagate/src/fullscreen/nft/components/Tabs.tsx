a3ed1f20688187c3b4bf648101e5a13bf404d385
Nick <ekbatanifard@gmail.com>
2024-10-13 18:09:26 +0330

    style: add tab divider

--- packages/extension-polkagate/src/fullscreen/nft/components/Tabs.tsx
+++ packages/extension-polkagate/src/fullscreen/nft/components/Tabs.tsx
@@ -5,16 +5,18 @@
 
 import type { FilterSectionProps } from '../utils/types';
 
-import { Tab, Tabs as MUITabs } from '@mui/material';
+import { Tab, Tabs as MUITabs, Divider } from '@mui/material';
 import React, { useCallback, useEffect, useMemo } from 'react';
 
 enum TabsNumber {
   NFT,
   UNIQUE,
-  ALL
+  ALL,
+  DIVIDER
 }
 
 const tabStyle = { ':is(button.MuiButtonBase-root.MuiTab-root.Mui-selected)': { fontWeight: 500 }, color: 'primary.main', fontSize: '16px', fontWeight: 400 };
+const TabDivider = () => <Tab disabled icon={<Divider orientation='vertical' sx={{ backgroundColor: 'divider', height: '19px', mx: '5px', my: 'auto' }} />} label='' sx={{ minWidth: '1px', p: '0', width: '1px' }} value={TabsNumber.DIVIDER} />
 
 function Tabs ({ items, setItemsToShow }: FilterSectionProps): React.ReactElement {
   const [tab, setTab] = React.useState(TabsNumber.ALL);
@@ -58,7 +60,9 @@ function Tabs ({ items, setItemsToShow }: FilterSectionProps): React.ReactElemen
   return (
     <MUITabs centered onChange={handleChange} sx={{ mt: '20px' }} value={tab}>
       <Tab disabled={!items} label={`All ${items ? `(${items.length})` : ''}`} sx={tabStyle} value={TabsNumber.ALL} />
+      <TabDivider />
       <Tab disabled={!items} label={`NFTs ${nfts ? `(${nfts.length})` : ''}`} sx={tabStyle} value={TabsNumber.NFT} />
+      <TabDivider />
       <Tab disabled={!items} label={`Uniques ${uniques ? `(${uniques.length})` : ''}`} sx={tabStyle} value={TabsNumber.UNIQUE} />
     </MUITabs>
   );
