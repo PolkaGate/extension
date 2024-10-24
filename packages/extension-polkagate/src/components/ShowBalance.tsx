41e8970bdf7c6d32f09107fad39e898768030e4c
Nick <ekbatanifard@gmail.com>
2024-10-25 00:03:26 +0330

    refactor: adjust types

--- packages/extension-polkagate/src/components/ShowBalance.tsx
+++ packages/extension-polkagate/src/components/ShowBalance.tsx
@@ -1,26 +1,24 @@
 // Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-// @ts-nocheck
 
 /* eslint-disable react/jsx-max-props-per-line */
 
 /**
  * @description  this component is used to show an account's balance
  * */
+import type { ApiPromise } from '@polkadot/api';
 import type { Balance } from '@polkadot/types/interfaces';
 import type { BN } from '@polkadot/util';
 
 import { Grid, Skeleton } from '@mui/material';
 import React from 'react';
 
-import { ApiPromise } from '@polkadot/api';
-
 import { FLOATING_POINT_DIGIT } from '../util/constants';
 import FormatBalance from './FormatBalance';
 import { FormatBalance2 } from '.';
 
 export interface Props {
-  balance: Balance | string | BN | number | null | undefined;
+  balance: Balance | string | BN | null | undefined;
   api?: ApiPromise | undefined;
   decimalPoint?: number;
   height?: number;
@@ -30,7 +28,7 @@ export interface Props {
   withCurrency?: boolean;
 }
 
-export default function ShowBalance({ api, balance, decimalPoint, withCurrency = true, height = 20, skeletonWidth = 90, decimal = undefined, token = undefined }: Props): React.ReactElement<Props> {
+export default function ShowBalance ({ api, balance, decimal = undefined, decimalPoint, height = 20, skeletonWidth = 90, token = undefined, withCurrency = true }: Props): React.ReactElement<Props> {
   return (
     <Grid alignItems='center' container justifyContent='center' width='fit-content'>
       {
