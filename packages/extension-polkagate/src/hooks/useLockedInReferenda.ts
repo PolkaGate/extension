a21ab06867649f11b07e53ba61194868f7fcb695
Amir Ekbatanifard <amiref007@gmail.com>
2024-10-27 11:23:47 +0330

    fix: hasDescription type issue

--- packages/extension-polkagate/src/hooks/useLockedInReferenda.ts
+++ packages/extension-polkagate/src/hooks/useLockedInReferenda.ts
@@ -20,26 +20,25 @@ interface Lock {
 interface OutputType {
   classToUnlock: Lock[] | undefined;
   delegatedBalance: BN | null | undefined;
-  hasDescription: string | true | null | undefined;
+  hasDescription: boolean;
   isDisable: boolean;
   lockedInRef: BN | undefined
   timeToUnlock: string | null | undefined;
   totalLocked: BN | null | undefined;
   unlockableAmount: BN | undefined;
-
 }
 
 export default function useLockedInReferenda (address: string | undefined, refreshNeeded: boolean | undefined): OutputType {
   const delegatedBalance = useHasDelegated(address, refreshNeeded);
   const referendaLocks = useAccountLocks(address, 'referenda', 'convictionVoting', false, refreshNeeded);
   const currentBlock = useCurrentBlockNumber(address);
-  const { lockedInRef, timeToUnlock, totalLocked, unlockableAmount } = useTimeToUnlock(address, referendaLocks, refreshNeeded);
+  const { lockedInRef, timeToUnlock, totalLocked, unlockableAmount } = useTimeToUnlock(address, delegatedBalance, referendaLocks, refreshNeeded);
 
   const classToUnlock = currentBlock ? referendaLocks?.filter((ref) => ref.endBlock.ltn(currentBlock) && ref.classId.lt(BN_MAX_INTEGER)) : undefined;
   const isDisable = useMemo(() => !unlockableAmount || unlockableAmount.isZero() || !classToUnlock || !totalLocked, [classToUnlock, totalLocked, unlockableAmount]);
 
   const hasDescription = useMemo(() =>
-    (unlockableAmount && !unlockableAmount.isZero()) || (delegatedBalance && !delegatedBalance.isZero()) || timeToUnlock
+    Boolean((unlockableAmount && !unlockableAmount.isZero()) || (delegatedBalance && !delegatedBalance.isZero()) || timeToUnlock)
   , [delegatedBalance, timeToUnlock, unlockableAmount]);
 
   return {
