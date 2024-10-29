35ce74695ac22152d22b49d6ff2bec1e57c1ce57
Nick <ekbatanifard@gmail.com>
2024-10-29 09:45:07 +0330

    chore: remove  ts no check

--- packages/extension-polkagate/src/components/ColorContext.ts
+++ packages/extension-polkagate/src/components/ColorContext.ts
@@ -1,7 +1,5 @@
 // Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-// @ts-nocheck
-/* eslint-disable header/header */
 
 import React from 'react';
 
