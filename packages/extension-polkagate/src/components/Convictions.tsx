d302dc360a78800a5de87561bddf57c8ef9e71f5
Nick <ekbatanifard@gmail.com>
2024-11-05 11:32:59 +0330

    feat: use Slider for Conviction settings

--- packages/extension-polkagate/src/components/Convictions.tsx
+++ packages/extension-polkagate/src/components/Convictions.tsx
@@ -1,14 +1,12 @@
 // Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
-// @ts-nocheck
 
 /* eslint-disable react/jsx-max-props-per-line */
 
-import { Grid, type SxProps, type Theme } from '@mui/material';
-import React, { useCallback } from 'react';
+import { Grid, Slider, type SxProps, type Theme, Typography, useTheme } from '@mui/material';
+import React, { useCallback, useEffect, useMemo } from 'react';
 
 import { useBlockInterval, useConvictionOptions, useTranslation } from '../hooks';
-import { Select } from '.';
 
 interface Props {
   address: string | undefined;
@@ -18,26 +16,90 @@ interface Props {
   style?: SxProps<Theme> | undefined;
 }
 
-export default function Convictions({ address, children, conviction, setConviction, style }: Props): React.ReactElement {
-  const { t } = useTranslation();
-  const blockTime = useBlockInterval(address);
-  const convictionOptions = useConvictionOptions(address, blockTime, t);
+export const DEFAULT_CONVICTION = 1;
 
-  const onChangeConviction = useCallback((conviction: number): void => {
-    setConviction(conviction);
+export default function Convictions ({ address, children, conviction, setConviction, style }: Props): React.ReactElement {
+  const { t } = useTranslation();
+  const theme = useTheme();
+
+  const blockTime = useBlockInterval(address);
+  const convictionOptions = useConvictionOptions(address, blockTime);
+
+  const onChangeConviction = useCallback((_event: Event, newValue: number | number[], _activeThumb: number): void => {
+    setConviction(newValue as number);
   }, [setConviction]);
 
+  const { max, min } = useMemo(() => {
+    const min = (convictionOptions?.[0]?.value || 0) as number;
+    const max = (convictionOptions?.[convictionOptions.length - 1]?.value || min) as number;
+
+    return { max, min };
+  }, [convictionOptions]);
+
+  const info = useMemo((): string => {
+    const newText = convictionOptions?.find(({ value }) => value === (conviction || DEFAULT_CONVICTION))?.text;
+    const match = newText?.match(/\(([^)]+)\)/);
+    const result = match ? match[1] : '0 days';
+
+    return 'Tokens will be locked for ' + result;
+  }, [conviction, convictionOptions]);
+
+  const marks = useMemo(() =>
+    convictionOptions?.map(({ value }) => ({ label: `${value} X`, value: value as number }))
+  , [convictionOptions]);
+
+  const valuetext = useCallback((value: number) => {
+    return `${value} X`;
+  }, []);
+
+  useEffect(() => {
+    // Select all mark labels and apply styles based on slider value
+    const markLabels = document.querySelectorAll('.MuiSlider-markLabel');
+
+    convictionOptions && markLabels.forEach((label, index) => {
+      const markValue = convictionOptions[index]?.value as number | undefined;
+
+      if (markValue && conviction && markValue > conviction) {
+        (label as HTMLElement).style.color = theme.palette.text.disabled; // Untraversed color
+      } else {
+        (label as HTMLElement).style.color = theme.palette.text.primary;
+      }
+    });
+  }, [conviction, convictionOptions, theme]);
+
   return (
-    <Grid alignContent='flex-start' alignItems='flex-start' container justifyContent='center' sx={{ '> div div div': { fontSize: '16px', fontWeight: 400 }, position: 'relative', ...style }}>
-      <Select
-        defaultValue={convictionOptions?.[0]?.value}
-        isDisabled={!convictionOptions}
-        label={t<string>('Vote Multiplier')}
+    <Grid alignContent='flex-start' alignItems='flex-start' container justifyContent='flex-start' sx={{ '> div div div': { fontSize: '16px', fontWeight: 400 }, position: 'relative', ...style }}>
+      <Typography sx={{ fontSize: '16px' }}>
+        {t('Vote Multiplier')}
+      </Typography>
+      <Slider
+        aria-label='Vote Convictions'
+        defaultValue={conviction || DEFAULT_CONVICTION}
+        disabled={!convictionOptions}
+        getAriaValueText={valuetext}
+        marks={marks}
+        max={max}
+        min={min}
         onChange={onChangeConviction}
-        options={convictionOptions || []}
-        value={conviction || convictionOptions?.[0]?.value}
+        size='small'
+        step={null}
+        sx={{
+          '& .MuiSlider-rail': {
+            color: 'action.focus' // Non-selected track color
+          },
+          '& .MuiSlider-thumb': {
+            color: 'success.light' // Thumb color
+          },
+          '& .MuiSlider-track': {
+            color: 'secondary.light' // Selected track color
+          }
+        }}
+        valueLabelDisplay='auto'
       />
       {children}
+      <Typography sx={{ fontSize: '14px', mt: '5px' }}>
+        {t(info)}
+      </Typography>
     </Grid>
   );
 }
