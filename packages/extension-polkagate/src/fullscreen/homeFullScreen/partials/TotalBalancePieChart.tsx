d2ca5910174fc73802211d01e7bfb75ebc3f343a
Nick <ekbatanifard@gmail.com>
2024-11-12 18:25:15 +0330

    style: minor adjustments

--- packages/extension-polkagate/src/fullscreen/homeFullScreen/partials/TotalBalancePieChart.tsx
+++ packages/extension-polkagate/src/fullscreen/homeFullScreen/partials/TotalBalancePieChart.tsx
@@ -211,7 +211,7 @@ function TotalBalancePieChart ({ hideNumbers, setGroupedAssets }: Props): React.
   return (
     <Grid alignItems='flex-start' container direction='column' item justifyContent='flex-start' sx={{ bgcolor: 'background.paper', borderRadius: '5px', boxShadow: '2px 3px 4px 0px rgba(0, 0, 0, 0.1)', height: 'fit-content', p: '15px 25px 10px', width: '430px' }}>
       <Grid alignItems='flex-start' container item justifyContent='flex-start'>
-        <Typography sx={{ fontSize: '23px', fontVariant: 'small-caps', fontWeight: 400 }}>
+        <Typography sx={{ fontSize: '22px', fontVariant: 'small-caps', fontWeight: 400 }}>
           {t('My Portfolio')}
         </Typography>
         <Grid alignItems='center' container item justifyContent = 'space-between' sx={{ my: '13px' }}>
