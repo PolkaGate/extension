618ba61c296f784a4180feba79d5f975690cf645
Amir Ekbatanifard <amiref007@gmail.com>
2024-11-24 13:19:32 +0330

    fix: type issue in ShowValue

--- packages/extension-polkagate/src/components/ShowValue.tsx
+++ packages/extension-polkagate/src/components/ShowValue.tsx
@@ -4,23 +4,22 @@
 /**
  * @description  this component is used to show an account balance in some pages like contributeToCrowdloan
  * */
-import type { BN } from '@polkadot/util';
 
 import { Skeleton } from '@mui/material';
 import React from 'react';
 
 export interface Props {
-  value: number | string | BN | null | undefined;
+  value: number | string | null | undefined;
   height?: number;
   unit?: string;
   width?: string;
 }
 
-export default function ShowValue ({ height = 20, unit, value, width = '90px' }: Props): React.ReactElement<Props> {
+export default function ShowValue ({ height = 20, unit = '', value, width = '90px' }: Props): React.ReactElement<Props> {
   return (
     <>
       {value !== undefined
-        ? `${value} ${unit}`
+        ? <>{value}{' '}{unit}</>
         : <Skeleton
           animation='wave'
           height={height}
