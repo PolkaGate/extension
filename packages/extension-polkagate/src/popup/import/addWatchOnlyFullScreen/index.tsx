1fa77981877308b758cb15a72b8a3524ce050f09
Amir Ekbatanifard <amiref007@gmail.com>
2024-11-25 16:36:50 +0330

    feat: get chain using the address (#1671)

--- packages/extension-polkagate/src/popup/import/addWatchOnlyFullScreen/index.tsx
+++ packages/extension-polkagate/src/popup/import/addWatchOnlyFullScreen/index.tsx
@@ -25,6 +25,7 @@ import { useApiWithChain, useFullscreen, useTranslation } from '../../../hooks';
 import { createAccountExternal, getMetadata } from '../../../messaging';
 import { Name } from '../../../partials';
 import getLogo from '../../../util/getLogo';
+import { addressToChain, getSubstrateAddress } from '../../../util/utils';
 
 export interface AccountInfo {
   address: string;
@@ -48,6 +49,12 @@ export default function AddWatchOnlyFullScreen (): React.ReactElement {
 
   const disabledItems = useMemo(() => (['Allow use on any chain']), []);
 
+  const getChain = useCallback((genesisHash: string) => {
+    getMetadata(genesisHash, true).then(setChain).catch((error): void => {
+      console.error(error);
+    });
+  }, []);
+
   useEffect(() => {
     // eslint-disable-next-line no-void
     void cryptoWaitReady().then(() => {
@@ -60,23 +67,36 @@ export default function AddWatchOnlyFullScreen (): React.ReactElement {
   }, [realAddress, chain]);
 
   useEffect(() => {
-    realAddress && api?.query['proxy']?.['proxies'](realAddress).then((proxies) => {
-      // @ts-expect-error will be resolved later
-      const fetchedProxyItems = (JSON.parse(JSON.stringify(proxies[0])))?.map((p: Proxy) => ({ proxy: p, status: 'current' })) as ProxyItem[];
+    if (realAddress) {
+      const addressChain = addressToChain(realAddress);
 
-      setProxies(fetchedProxyItems);
-    });
-  }, [api, chain, realAddress]);
+      addressChain?.genesisHash && getChain(addressChain.genesisHash);
+
+      api?.query['proxy']?.['proxies'](realAddress)
+        .then((proxies) => {
+          // @ts-expect-error will be resolved later
+          const fetchedProxyItems = (JSON.parse(JSON.stringify(proxies[0])) as Proxy[])?.map((p: Proxy) => ({ proxy: p, status: 'current' })) as ProxyItem[];
+
+          setProxies(fetchedProxyItems);
+        })
+        .catch(console.error);
+    }
+  }, [api, getChain, realAddress]);
 
   const onAdd = useCallback(() => {
     if (name && realAddress && chain?.genesisHash) {
       setIsBusy(true);
 
+      const substrateAddress = getSubstrateAddress(realAddress);
+
       createAccountExternal(name, realAddress, chain.genesisHash as HexString)
         .then(() => {
           setStorage('profile', PROFILE_TAGS.WATCH_ONLY).catch(console.error);
-          openOrFocusTab('/', true);
         })
+        .finally(() => substrateAddress
+          ? openOrFocusTab(`/accountfs/${substrateAddress}/0`, true)
+          : openOrFocusTab('/', true)
+        )
         .catch((error: Error) => {
           setIsBusy(false);
           console.error(error);
@@ -90,10 +110,8 @@ export default function AddWatchOnlyFullScreen (): React.ReactElement {
 
   const onChangeGenesis = useCallback((genesisHash?: string | null): void => {
     setProxies(undefined);
-    genesisHash && getMetadata(genesisHash, true).then(setChain).catch((error): void => {
-      console.error(error);
-    });
-  }, []);
+    genesisHash && getChain(genesisHash);
+  }, [getChain]);
 
   return (
     <Grid bgcolor='backgroundFL.primary' container item justifyContent='center'>
@@ -108,7 +126,7 @@ export default function AddWatchOnlyFullScreen (): React.ReactElement {
             logo={
               <VaadinIcon icon='vaadin:tag' style={{ color: `${theme.palette.text.primary}`, height: '25px', width: '25px' }} />
             }
-            text= {t('Add Watch-Only Account')}
+            text={t('Add Watch-Only Account')}
           />
           <Typography fontSize='16px' fontWeight={400} width='100%'>
             {t('Enter the watch-only address. It can also serve as a proxied account, but without transaction signing. A proxy account in the extension is needed for signing.')}
