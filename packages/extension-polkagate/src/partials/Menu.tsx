205d4d69bc352a01c8c696a244a97892ded00407
Nick <ekbatanifard@gmail.com>
2024-11-30 16:14:06 +0330

    style: adjust TLF buttons

--- packages/extension-polkagate/src/partials/Menu.tsx
+++ packages/extension-polkagate/src/partials/Menu.tsx
@@ -18,8 +18,8 @@ import { TEST_NETS } from '../util/constants';
 import ImportAccSubMenu from './ImportAccSubMenu';
 import NewAccountSubMenu from './NewAccountSubMenu';
 import SettingSubMenu from './SettingSubMenu';
-import VersionSocial from './VersionSocial';
 import TLFActions from './TLFActions';
+import VersionSocial from './VersionSocial';
 
 interface Props {
   setShowMenu: React.Dispatch<React.SetStateAction<boolean>>;
@@ -191,7 +191,7 @@ function Menu ({ isMenuOpen, setShowMenu }: Props): React.ReactElement<Props> {
           <VersionSocial fontSize='11px' />
         </Grid>
         <IconButton onClick={onCloseMenu} sx={{ left: '3%', p: 0, position: 'absolute', top: '1%' }}>
-          <CloseIcon sx={{ color: 'white', fontSize: 35 }} />
+          <CloseIcon sx={{ color: 'secondary.light', fontSize: 35 }} />
         </IconButton>
       </Grid>
     </Dialog>
