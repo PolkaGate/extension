4d9e51cc1600ace0bcb950daac4b47beeb01e787
Amir Ekbatanifard <amiref007@gmail.com>
2024-12-25 17:07:51 +0330

    feat: add handWave.gif

---
+++ packages/extension-polkagate/src/assets/gif/index.ts
@@ -0,0 +1,6 @@
+// Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// @ts-nocheck
+
+export { default as handWave } from './handWave.gif';
