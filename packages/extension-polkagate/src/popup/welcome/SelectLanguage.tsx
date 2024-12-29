c1953d41f820d1bc5d3e2eb9eb0adb35eb87b080
Amir Ekbatanifard <amiref007@gmail.com>
2024-12-29 12:19:44 +0330

    feat: create SelectLanguage.tsx

---
+++ packages/extension-polkagate/src/popup/welcome/SelectLanguage.tsx
@@ -0,0 +1,152 @@
+// Copyright 2019-2024 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/* eslint-disable react/jsx-max-props-per-line */
+
+import type { DropdownOption } from '../../util/types';
+
+import { Box, Grid, styled, Typography } from '@mui/material';
+import * as flags from 'country-flag-icons/string/3x2';
+import { Translate } from 'iconsax-react';
+import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+
+import uiSetting from '@polkadot/ui-settings';
+
+import { ExtensionPopup, GradientButton, SettingsContext } from '../../components';
+import { useTranslation } from '../../hooks';
+import { GradientDivider } from '../../style';
+import getLanguageOptions from '../../util/getLanguageOptions';
+import { Popups } from '.';
+
+interface Props {
+  setPopup: React.Dispatch<React.SetStateAction<Popups>>;
+  openMenu: boolean;
+}
+
+interface LanguageOptionProps {
+  languageOptions: DropdownOption[];
+  selectedLanguage: string | undefined;
+  handleLanguageSelect: (lang: string) => () => void;
+}
+
+const COUNTRY_CODES = {
+  EN: 'GB', // English - United Kingdom
+  ES: 'ES', // Spanish - Spain
+  FR: 'FR', // French - France
+  HI: 'IN', // Hindi - India
+  RU: 'RU', // Russian - Russia
+  ZH: 'CN' // Chinese - China
+} as const;
+
+type CountryCodeKey = keyof typeof COUNTRY_CODES;
+
+const ListItem = styled(Grid)(() => ({
+  '&.selected': {
+    backgroundColor: '#6743944D',
+    paddingLeft: '20px'
+  },
+  '&:hover': {
+    backgroundColor: '#6743944D'
+  },
+  alignItems: 'center',
+  borderRadius: '12px',
+  columnGap: '10px',
+  cursor: 'pointer',
+  height: '50px',
+  justifyContent: 'flex-start',
+  padding: '10px',
+  transition: 'padding-left 0.3s ease, background-color 0.3s ease'
+}));
+
+const LanguageOptions = React.memo(function LanguageOptions ({ handleLanguageSelect, languageOptions, selectedLanguage }: LanguageOptionProps): React.ReactElement {
+  const flag = useCallback((value: string) => {
+    const key = value.toUpperCase() as CountryCodeKey;
+    const languageCode = COUNTRY_CODES[key] ?? '';
+
+    const svg = (flags as Record<string, string>)[languageCode];
+
+    if (svg) {
+      return `data:image/svg+xml;base64,${btoa(svg)}`;
+    }
+
+    return '';
+  }, []);
+
+  return (
+    <Grid container item justifyContent='center' sx={{ maxHeight: '380px', overflow: 'scroll' }}>
+      {languageOptions.map(({ text, value }, index) => (
+        <>
+          <ListItem className={selectedLanguage === value ? 'selected' : ''} container item key={value} onClick={handleLanguageSelect(value as string)}>
+            <Box
+              component='img'
+              src={flag(value as string)}
+              sx={{ borderRadius: '999px', height: '18px', width: '18px' }}
+            />
+            <Typography color='text.primary' fontFamily='Inter' fontSize='14px' fontWeight={600}>
+              {text}
+            </Typography>
+          </ListItem>
+          {index !== languageOptions.length - 1 &&
+            <GradientDivider style={{ my: '5px' }} />
+          }
+        </>
+      ))}
+    </Grid>
+  );
+});
+
+function SelectLanguage ({ openMenu, setPopup }: Props): React.ReactElement {
+  const { t } = useTranslation();
+  const settings = useContext(SettingsContext);
+
+  const [selectedLanguage, setSelectedLanguage] = useState<string | undefined>();
+
+  const languageOptions = useMemo(() => getLanguageOptions(), []);
+
+  useEffect(() => {
+    if (selectedLanguage === undefined) {
+      setSelectedLanguage(settings.i18nLang !== 'default' ? settings.i18nLang : languageOptions[0].value as string);
+    }
+  }, [languageOptions, selectedLanguage, settings.i18nLang]);
+
+  const handleLanguageSelect = useCallback((lang: string) => () => {
+    setSelectedLanguage(lang);
+  }, []);
+
+  const handleClose = useCallback(() => setPopup(Popups.NONE), [setPopup]);
+
+  const applyLanguageChange = useCallback(() => {
+    selectedLanguage && uiSetting.set({ i18nLang: selectedLanguage });
+    handleClose();
+  }, [selectedLanguage, handleClose]);
+
+  return (
+    <ExtensionPopup
+      TitleIcon={Translate}
+      handleClose={handleClose}
+      openMenu={openMenu}
+      title={t('Select your language')}
+    >
+      <Grid container item justifyContent='center' sx={{ position: 'relative', py: '5px', zIndex: 1 }}>
+        <LanguageOptions
+          handleLanguageSelect={handleLanguageSelect}
+          languageOptions={languageOptions}
+          selectedLanguage={selectedLanguage}
+        />
+        <GradientButton
+          contentPlacement='center'
+          disabled={settings.i18nLang === selectedLanguage}
+          onClick={applyLanguageChange}
+          style={{
+            height: '44px',
+            marginTop: '20px',
+            width: '345px'
+          }}
+          text={t('Apply')}
+        />
+      </Grid>
+    </ExtensionPopup>
+  );
+}
+
+export default SelectLanguage;
