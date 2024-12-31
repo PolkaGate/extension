ff0e03f20278611006f4ef9f3fe676c7f7926a6d
Amir Ekbatanifard <amiref007@gmail.com>
2024-12-31 11:14:27 +0330

    style: add hover effect

--- packages/extension-polkagate/src/partials/WelcomeHeader.tsx
+++ packages/extension-polkagate/src/partials/WelcomeHeader.tsx
@@ -3,14 +3,14 @@
 
 /* eslint-disable react/jsx-max-props-per-line */
 
-import { Box, Container, Grid, Typography } from '@mui/material';
+import { Box, Container, Grid, type SxProps, type Theme, Typography } from '@mui/material';
 import { ArrowDown2, ShieldTick } from 'iconsax-react';
 import React, { useCallback, useState } from 'react';
 
 import { logoTransparent } from '../assets/logos';
 import { EXTENSION_NAME } from '../util/constants';
-import SelectLanguage from './SelectLanguage';
 import PrivacyPolicy from './PrivacyPolicy';
+import SelectLanguage from './SelectLanguage';
 
 export enum WelcomeHeaderPopups {
   NONE,
@@ -20,20 +20,51 @@ export enum WelcomeHeaderPopups {
 
 function WelcomeHeader (): React.ReactElement {
   const [popup, setPopup] = useState<WelcomeHeaderPopups>(WelcomeHeaderPopups.NONE);
+  const [hovered, setHovered] = useState<WelcomeHeaderPopups>(WelcomeHeaderPopups.NONE);
 
-  const openLanguage = useCallback(() => {
-    setPopup(WelcomeHeaderPopups.LANGUAGE);
+  const shieldHoveredStyle = {
+    '&::after': {
+      background: 'linear-gradient(262.56deg, #6E00B1 0%, #DC45A0 45%, #6E00B1 100%)',
+      borderRadius: '10px',
+      content: '""',
+      inset: 0,
+      opacity: hovered === WelcomeHeaderPopups.PRIVACY ? 1 : 0,
+      position: 'absolute',
+      transition: 'all 250ms ease-out'
+    },
+    background: '#BFA1FF26',
+    borderRadius: '10px',
+    inset: 0,
+    position: 'absolute',
+    transition: 'all 250ms ease-out'
+  } as SxProps<Theme>;
+
+  const openPopup = useCallback((popup: WelcomeHeaderPopups) => () => {
+    setPopup(popup);
   }, [setPopup]);
 
-  const openPrivacy = useCallback(() => {
-    setPopup(WelcomeHeaderPopups.PRIVACY);
-  }, [setPopup]);
+  const onHoveredPopup = useCallback((popup?: WelcomeHeaderPopups) => () => {
+    setHovered(popup ?? WelcomeHeaderPopups.NONE);
+  }, [setHovered]);
 
   return (
     <>
       <Container disableGutters sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between', p: '5px 15px', position: 'relative', zIndex: 1 }}>
-        <Grid alignItems='center' container item onClick={openPrivacy} sx={{ bgcolor: '#BFA1FF26', borderRadius: '10px', cursor: 'pointer', p: '3px', width: 'fit-content' }}>
-          <ShieldTick color='#AA83DC' size='24' variant='Bulk' />
+        <Grid
+          container
+          item
+          onClick={openPopup(WelcomeHeaderPopups.PRIVACY)}
+          onMouseEnter={onHoveredPopup(WelcomeHeaderPopups.PRIVACY)}
+          onMouseLeave={onHoveredPopup()}
+          sx={{ alignItems: 'center', borderRadius: '10px', cursor: 'pointer', p: '3px', position: 'relative', width: 'fit-content' }}
+        >
+          <ShieldTick
+            color={hovered === WelcomeHeaderPopups.PRIVACY ? '#EAEBF1' : '#AA83DC'}
+            size='24'
+            style={{ transition: 'all 250ms ease-out', zIndex: 10 }}
+            variant={hovered === WelcomeHeaderPopups.PRIVACY ? 'Bold' : 'Bulk'}
+          />
+          <Grid sx={shieldHoveredStyle}></Grid>
         </Grid>
         <Grid alignItems='center' container item width='fit-content'>
           <Box
@@ -45,11 +76,22 @@ function WelcomeHeader (): React.ReactElement {
             {EXTENSION_NAME}
           </Typography>
         </Grid>
-        <Grid alignItems='center' container item onClick={openLanguage} sx={{ bgcolor: '#BFA1FF26', borderRadius: '10px', cursor: 'pointer', p: '5px', width: 'fit-content' }}>
-          <Typography color='#AA83DC' fontFamily='Inter' fontSize='13px' fontWeight={500}>
+        <Grid
+          container
+          item
+          onClick={openPopup(WelcomeHeaderPopups.LANGUAGE)}
+          onMouseEnter={onHoveredPopup(WelcomeHeaderPopups.LANGUAGE)}
+          onMouseLeave={onHoveredPopup()}
+          sx={{ alignItems: 'center', bgcolor: hovered === WelcomeHeaderPopups.LANGUAGE ? '#674394' : '#BFA1FF26', borderRadius: '10px', cursor: 'pointer', p: '5px', transition: 'all 250ms ease-out', width: 'fit-content' }}
+        >
+          <Typography color={hovered === WelcomeHeaderPopups.LANGUAGE ? '#EAEBF1' : '#AA83DC'} fontFamily='Inter' fontSize='13px' fontWeight={500} sx={{ transition: 'all 250ms ease-out' }}>
             {'EN'}
           </Typography>
-          <ArrowDown2 color='#AA83DC80' size='15' variant='Bold' />
+          <ArrowDown2
+            size='15'
+            style={{ color: hovered === WelcomeHeaderPopups.LANGUAGE ? '#EAEBF1' : '#AA83DC80', transition: 'all 250ms ease-out' }}
+            variant='Bold'
+          />
         </Grid>
       </Container>
       <SelectLanguage
