4abd8ca126e65ff2c02c13b7e6ba2d7d938d4381
Amir Ekbatanifard <amiref007@gmail.com>
2025-01-05 10:40:53 +0330

    feat: add Box.svg

--- packages/extension-polkagate/src/assets/icons/index.ts
+++ packages/extension-polkagate/src/assets/icons/index.ts
@@ -6,6 +6,7 @@
 export { default as auctionBlack } from './auctionBlack.svg';
 export { default as auctionRed } from './auctionRed.svg';
 export { default as auctionWhite } from './auctionWhite.svg';
+export { default as Box } from './Box.svg';
 export { default as checkBox } from './checkBox.svg';
 export { default as checkedBox } from './checkedBox.svg';
 export { default as controllerSettingBlack } from './controllerSettingBlack.svg';
