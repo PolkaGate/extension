878cbc47207016ff549677abf5402f784a326c1a
Amir Ekbatanifard <amiref007@gmail.com>
2025-01-05 12:54:34 +0330

    style: redesign AskToSetPassword.tsx

--- packages/extension-polkagate/src/popup/passwordManagement/AskToSetPassword.tsx
+++ packages/extension-polkagate/src/popup/passwordManagement/AskToSetPassword.tsx
@@ -3,14 +3,18 @@
 
 /* eslint-disable react/jsx-max-props-per-line */
 
-import { Grid, Typography } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import React, { useCallback } from 'react';
 
-import { PButton } from '../../components';
+import { Box as BoxIcon } from '../../assets/icons';
+import { ActionButton, GradientBox, GradientButton, NeonButton } from '../../components';
 import { setStorage } from '../../components/Loading';
 import { useExtensionLockContext } from '../../context/ExtensionLockContext';
 import { useTranslation } from '../../hooks';
+import { Version } from '../../partials';
+import { RedGradient } from '../../style';
 import { STEPS } from './constants';
+import Header from './Header';
 
 interface Props {
   setStep: (value: React.SetStateAction<number | undefined>) => void;
@@ -36,31 +40,55 @@ function AskToSetPassword ({ setStep }: Props): React.ReactElement {
   }, [setStep]);
 
   return (
-    <Grid alignItems='center' container direction='column' justifyContent='center'>
-      <Typography fontSize={16} pb='25px'>
-        {t('Would you like to set a login password?')}
-      </Typography>
-      <PButton
-        _ml={0}
-        _mt='10px'
-        _onClick={onYesToSetPassword}
-        text={t('Yes')}
-      />
-      <PButton
-        _ml={0}
-        _mt='10px'
-        _onClick={onMayBeLater}
-        _variant='outlined'
-        text={t('Later')}
-      />
-      <PButton
-        _ml={0}
-        _mt='10px'
-        _onClick={onNoPassword}
-        _variant='text'
-        text={t('No')}
-      />
-    </Grid>
+    <Container disableGutters sx={{ position: 'relative' }}>
+      <Header />
+      <GradientBox noGradient style={{ m: 'auto', mt: '8px', width: '359px' }}>
+        <RedGradient style={{ right: '-8%', top: '20px' }} />
+        <Grid container item justifyContent='center' sx={{ p: '18px 32px 32px' }}>
+          <Box
+            component='img'
+            src={BoxIcon as string}
+            sx={{ height: '145px', mt: '20px', width: '140px' }}
+          />
+          <Typography fontFamily='OdibeeSans' fontSize='29px' fontWeight={400} sx={{ mb: '6px', mt: '25px', width: '100%' }} textAlign='center' textTransform='uppercase'>
+            {t('welcome')}!
+          </Typography>
+          <Typography fontFamily='Inter' fontSize='13px' fontWeight={500} sx={{ color: 'text.secondary', width: '100%' }} textAlign='center'>
+            {t('Would you like to set a login password?')}
+          </Typography>
+          <GradientButton
+            onClick={onYesToSetPassword}
+            style={{
+              height: '48px',
+              marginTop: '24px',
+              width: '325px'
+            }}
+            text={t('Yes')}
+          />
+          <ActionButton
+            contentPlacement='center'
+            onClick={onMayBeLater}
+            style={{
+              height: '46px',
+              marginTop: '15px',
+              width: '325px'
+            }}
+            text={{ firstPart: t('Later') }}
+          />
+          <NeonButton
+            contentPlacement='center'
+            onClick={onNoPassword}
+            style={{
+              height: '44px',
+              marginTop: '15px',
+              width: '325px'
+            }}
+            text={t('No')}
+          />
+        </Grid>
+      </GradientBox>
+      <Version />
+    </Container>
   );
 }
 
