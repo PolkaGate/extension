4970682c7e6ca2ff0a8740360fcbb5b5454548e8
Amir Ekbatanifard <amiref007@gmail.com>
2025-01-13 16:45:06 +0330

    feat: create UserDashboardHeader.tsx

---
+++ packages/extension-polkagate/src/partials/UserDashboardHeader.tsx
@@ -0,0 +1,21 @@
+// Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/* eslint-disable react/jsx-max-props-per-line */
+
+import { Container } from '@mui/material';
+import React from 'react';
+
+import { HomeButton } from '../components';
+import FullscreenModeButton from './FullscreenModeButton';
+
+function UserDashboardHeader () {
+  return (
+    <Container disableGutters sx={{ display: 'flex', justifyContent: 'space-between', p: '10px 15px' }}>
+      <HomeButton type='active' />
+      <FullscreenModeButton />
+    </Container>
+  );
+}
+
+export default UserDashboardHeader;
