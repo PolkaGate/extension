b39c5a6ef2e32ec18d9f46822f1045746214a85b
Amir Ekbatanifard <amiref007@gmail.com>
2025-01-13 16:38:35 +0330

    feat: add BackgroundLogo.png

--- packages/extension-polkagate/src/assets/logos/index.ts
+++ packages/extension-polkagate/src/assets/logos/index.ts
@@ -2,9 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //@ts-nocheck
+export { default as BackgroundLogo } from './BackgroundLogo.png';
 export { default as logoMotionDark } from './logoMotionDark.gif';
 export { default as logoMotionLight } from './logoMotionLight.gif';
 export { default as logoBlack } from './pg-b.svg';
 export { default as logoPink } from './pg-p.svg';
 export { default as logoTransparent } from './pg-t.svg';
+export { default as logoBlackBirdTransparent } from './pg-t-b.svg';
+export { default as logoWhiteTransparent } from './pg-t-w.svg';
 export { default as logoWhite } from './pg-w.svg';
