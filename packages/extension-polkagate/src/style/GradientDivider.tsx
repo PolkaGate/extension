b2021ec22644ed34fa4e634d01131a4dfd6becdb
Amir Ekbatanifard <amiref007@gmail.com>
2025-01-15 18:55:20 +0330

    style: update GradientDivider.tsx

--- packages/extension-polkagate/src/style/GradientDivider.tsx
+++ packages/extension-polkagate/src/style/GradientDivider.tsx
@@ -13,7 +13,10 @@ interface Props {
 
 export default function GradientDivider ({ orientation = 'horizontal', style }: Props): React.ReactElement<Props> {
   const DividerStyle = {
-    background: 'linear-gradient(90deg, rgba(210, 185, 241, 0.03) 0%, rgba(210, 185, 241, 0.15) 50.06%, rgba(210, 185, 241, 0.03) 100%)',
+    background: orientation === 'horizontal'
+      ? 'linear-gradient(90deg, rgba(210, 185, 241, 0.03) 0%, rgba(210, 185, 241, 0.15) 50.06%, rgba(210, 185, 241, 0.03) 100%)'
+      : 'linear-gradient(102deg, rgba(16, 16, 25, 0.1) 0%, rgba(238, 71, 151, 0.5) 50.06%, rgba(16, 16, 25, 0) 100%)',
+    border: 'none',
     height: orientation === 'vertical' ? '100%' : '1px',
     width: orientation === 'vertical' ? '1px' : '100%'
   };
