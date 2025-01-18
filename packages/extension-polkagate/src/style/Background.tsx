7e74bc8e48d9062225aa93521aa18f76518dec99
Amir Ekbatanifard <amiref007@gmail.com>
2025-01-18 14:22:24 +0330

    refactor: update some styles

--- packages/extension-polkagate/src/style/Background.tsx
+++ packages/extension-polkagate/src/style/Background.tsx
@@ -8,62 +8,67 @@ import React from 'react';
 
 import { BackgroundLogo } from '../assets/logos';
 
-function Background (): React.ReactElement {
-  const BallStyle: React.CSSProperties = {
-    borderRadius: '50%',
-    height: '128px',
-    opacity: 1,
-    position: 'absolute',
-    width: '128px'
-  };
+const BallStyle: React.CSSProperties = {
+  borderRadius: '50%',
+  height: '128px',
+  opacity: 1,
+  position: 'absolute',
+  width: '128px'
+};
 
-  const redBallStyle: React.CSSProperties = {
-    ...BallStyle,
-    backgroundColor: '#b30a0a',
-    filter: 'blur(90px)', // Glow effect
-    left: '35%',
-    top: '-60px'
-  };
+const redBallStyle: React.CSSProperties = {
+  ...BallStyle,
+  backgroundColor: '#b30a0a',
+  filter: 'blur(90px)', // Glow effect
+  left: '35%',
+  top: '-60px'
+};
 
-  const blueBallStyle: React.CSSProperties = {
-    ...BallStyle,
-    backgroundColor: '#5B00B6',
-    filter: 'blur(100px)', // Glow effect
-    left: '-35px',
-    top: '-45px'
-  };
+const blueBallStyle: React.CSSProperties = {
+  ...BallStyle,
+  backgroundColor: '#5B00B6',
+  filter: 'blur(100px)', // Glow effect
+  left: '-35px',
+  top: '-45px'
+};
 
-  const backgroundImageStyle: SxProps<Theme> = {
-    filter: 'blur(8px)',
-    height: '300px',
-    mixBlendMode: 'color-dodge',
-    position: 'absolute',
-    px: '15px',
-    rotate: '15deg',
-    top: '-30px',
-    width: '375px'
-  };
+const backgroundImageStyle: SxProps<Theme> = {
+  filter: 'blur(8px)',
+  height: '300px',
+  mixBlendMode: 'color-dodge',
+  position: 'absolute',
+  px: '15px',
+  rotate: '15deg',
+  top: '-30px',
+  width: '375px'
+};
 
-  const fadeOutStyle = {
-    background: 'linear-gradient(180deg, transparent 13.79%, #05091C 100%)',
-    height: '220px',
-    inset: 0,
-    position: 'absolute',
-    width: '375px'
-  } as React.CSSProperties;
+const fadeOutStyle = {
+  background: 'linear-gradient(180deg, transparent 13.79%, #05091C 100%)',
+  height: '220px',
+  inset: 0,
+  position: 'absolute',
+  width: '375px'
+} as React.CSSProperties;
 
-  // const SmootherStyle = {
-  //   background: 'linear-gradient(0deg, #7671A3 0%, #080610 100%)',
-  //   height: '200px',
-  //   inset: 0,
-  //   maskMode: 'alpha',
-  //   opacity: 0.05,
-  //   position: 'absolute',
-  //   width: '375px'
-  // } as React.CSSProperties;
+// const SmootherStyle = {
+//   background: 'linear-gradient(0deg, #7671A3 0%, #080610 100%)',
+//   height: '200px',
+//   inset: 0,
+//   maskMode: 'alpha',
+//   opacity: 0.05,
+//   position: 'absolute',
+//   width: '375px'
+// } as React.CSSProperties;
 
+interface Props {
+  style?: React.CSSProperties;
+  id?: string;
+}
+
+function Background ({ id, style }: Props): React.ReactNode {
   return (
-    <Box sx={{ height: '220px', inset: 0, position: 'absolute' }}>
+    <Box id={id} sx={{ height: '220px', inset: 0, position: 'absolute', ...style }}>
       <Container disableGutters sx={{ height: '220px', overflow: 'hidden', position: 'relative', width: '100%' }}>
         <div style={redBallStyle} />
         <div style={blueBallStyle} />
