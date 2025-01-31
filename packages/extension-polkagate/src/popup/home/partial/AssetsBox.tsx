61c6f981ea11b3b6226b4acce35aa817e68c137c
Amir Ekbatanifard <amiref007@gmail.com>
2025-01-31 11:06:05 +0330

    style: add glow ball

--- packages/extension-polkagate/src/popup/home/partial/AssetsBox.tsx
+++ packages/extension-polkagate/src/popup/home/partial/AssetsBox.tsx
@@ -3,9 +3,12 @@
 
 /* eslint-disable react/jsx-max-props-per-line */
 
-import { Container, Grid } from '@mui/material';
-import React, { useState } from 'react';
+import { Box, Container, Grid, styled, Typography } from '@mui/material';
+import React, { useCallback, useState } from 'react';
 
+import { SafeBox } from '../../../assets/icons';
+import { useAccountAssets, useSelectedAccount, useTranslation } from '../../../hooks';
+import AssetLoading from './AssetLoading';
 import AssetTabs from './AssetTabs';
 import ChainsAssetsBox from './ChainsAssetsBox';
 import NFTBox from './NFTBox';
@@ -17,20 +20,89 @@ export enum TAB {
   NFTS = 'nfts'
 }
 
+const GlowBall = styled('div')({
+  background: '#CC429D',
+  borderRadius: '50%',
+  filter: 'blur(25px)',
+  height: '32px',
+  left: 0,
+  opacity: 1,
+  pointerEvents: 'none', // Ensures the glow doesn't interfere with interactions
+  position: 'absolute',
+  top: 0,
+  width: '32px'
+});
+
+const AssetNull = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Container disableGutters sx={{ alignItems: 'center', display: 'grid', justifyItems: 'center', py: '33px', width: '100%' }}>
+      <Box
+        component='img'
+        src={SafeBox as string}
+        sx={{ width: '135px' }}
+      />
+      <Typography fontFamily='Inter' fontSize='14px' fontWeight={600} pt='10px'>
+        {t("You don't have any tokens yet")}
+      </Typography>
+    </Container>
+  );
+};
+
 function AssetsBox (): React.ReactElement {
+  const account = useSelectedAccount();
+  const accountAssets = useAccountAssets(account?.address);
+
   const [tab, setTab] = useState<TAB>(TAB.CHAINS);
+
+  const isLoading = accountAssets === undefined;
+  const nothingToShow = accountAssets === null;
+
+  const renderContent = useCallback(() => {
+    if (TAB.NFTS === tab) {
+      return <NFTBox />;
+    }
+
+    if (isLoading) {
+      return <AssetLoading />;
+    }
+
+    if (nothingToShow) {
+      return <AssetNull />;
+    }
+
+    if ([TAB.CHAINS, TAB.TOKENS].includes(tab)) {
+      return (
+        <Grid
+          container
+          item
+          sx={{
+            borderRadius: '14px',
+            display: 'grid',
+            maxHeight: '290px',
+            overflowY: 'scroll',
+            position: 'relative',
+            rowGap: tab === TAB.TOKENS ? '10px' : '4px',
+            transition: 'all 250ms ease-out',
+            zIndex: 1
+          }}
+        >
+          {tab === TAB.CHAINS && <ChainsAssetsBox />}
+          {tab === TAB.TOKENS && <TokensAssetsBox />}
+        </Grid>
+      );
+    }
+
+    return null;
+  }, [isLoading, nothingToShow, tab]);
 
   return (
     <>
       <AssetTabs setTab={setTab} />
-      <Container disableGutters sx={{ bgcolor: '#1B133C', borderRadius: '18px', mx: '15px', p: '4px', width: '100%' }}>
-        {[TAB.CHAINS, TAB.TOKENS].includes(tab) &&
-          <Grid container item sx={{ borderRadius: '14px', display: 'grid', maxHeight: '290px', overflowY: 'scroll', rowGap: tab === TAB.TOKENS ? '10px' : '4px', transition: 'all 250ms ease-out' }}>
-            {tab === TAB.CHAINS && <ChainsAssetsBox />}
-            {tab === TAB.TOKENS && <TokensAssetsBox />}
-          </Grid>
-        }
-        {TAB.NFTS === tab && <NFTBox />}
+      <Container disableGutters sx={{ bgcolor: '#1B133C', borderRadius: '18px', mx: '15px', overflow: 'hidden', p: '4px', position: 'relative', width: '100%' }}>
+        {renderContent()}
+        <GlowBall />
       </Container>
     </>
   );
