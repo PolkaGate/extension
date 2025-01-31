2d9f15c9f2b83b76fda07543aff00df187d88d72
Amir Ekbatanifard <amiref007@gmail.com>
2025-01-31 11:06:05 +0330

    style: add glow ball

--- packages/extension-polkagate/src/popup/home/partial/AssetsBox.tsx
+++ packages/extension-polkagate/src/popup/home/partial/AssetsBox.tsx
@@ -92,6 +92,47 @@ function AssetsBox (): React.ReactElement {
     return null;
   }, [isLoading, nothingToShow, tab]);
 
+  const isLoading = accountAssets === undefined;
+  const nothingToShow = accountAssets === null;
+
+  const renderContent = useCallback(() => {
+    if (TAB.NFTS === tab) {
+      return <NFTBox />;
+    }
+
+    if (isLoading) {
+      return <AssetLoading />;
+    }
+
+    if (nothingToShow) {
+      return <AssetNull />;
+    }
+
+    if ([TAB.CHAINS, TAB.TOKENS].includes(tab)) {
+      return (
+        <Grid
+          container
+          item
+          sx={{
+            borderRadius: '14px',
+            display: 'grid',
+            maxHeight: '290px',
+            overflowY: 'scroll',
+            position: 'relative',
+            rowGap: tab === TAB.TOKENS ? '10px' : '4px',
+            transition: 'all 250ms ease-out',
+            zIndex: 1
+          }}
+        >
+          {tab === TAB.CHAINS && <ChainsAssetsBox />}
+          {tab === TAB.TOKENS && <TokensAssetsBox />}
+        </Grid>
+      );
+    }
+
+    return null;
+  }, [isLoading, nothingToShow, tab]);
+
   return (
     <>
       <AssetTabs setTab={setTab} tab={tab} />
