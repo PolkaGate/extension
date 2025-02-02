02cf039f2fa9c297042892406634844e3694b0c9
Amir Ekbatanifard <amiref007@gmail.com>
2025-02-02 11:12:21 +0330

    fix: divider color and refactor some small components

--- packages/extension-polkagate/src/partials/HomeMenu.tsx
+++ packages/extension-polkagate/src/partials/HomeMenu.tsx
@@ -5,7 +5,7 @@
 
 import type { Icon } from 'iconsax-react';
 
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, styled } from '@mui/material';
 import { BuyCrypto, Clock, Logout, MedalStar, ScanBarcode, Setting } from 'iconsax-react';
 import React, { useCallback, useRef, useState } from 'react';
 
@@ -13,14 +13,27 @@ import Tooltip from '../components/Tooltip';
 import { useTranslation } from '../components/translate';
 import { GradientDivider } from '../style';
 
-const backgroundStyle: React.CSSProperties = {
+const Background = styled('div')(() => ({
   backdropFilter: 'blur(20px)',
   borderRadius: '16px',
   boxShadow: '0px 0px 25px 20px #4E2B7280 inset',
   inset: 0,
   position: 'absolute',
   zIndex: -1
-};
+}));
+
+const SelectedItemBackground = styled('div')(({ hovered }: { hovered: boolean }) => ({
+  background: '#FF4FB9',
+  filter: 'blur(14px)',
+  height: '15px',
+  opacity: hovered ? 1 : 0,
+  position: 'absolute',
+  right: 0,
+  top: '50%',
+  transform: 'translate(-50%, -50%)',
+  transition: 'all 250ms ease-out',
+  width: '15px'
+}));
 
 interface MenuItemProps {
   ButtonIcon: Icon;
@@ -31,19 +44,6 @@ interface MenuItemProps {
 function MenuItem ({ ButtonIcon, buttonName, withBorder = true }: MenuItemProps) {
   const [hovered, setHovered] = useState<boolean>(false);
 
-  const selectedItemBackgroundStyle: React.CSSProperties = {
-    background: '#FF4FB9',
-    filter: 'blur(14px)',
-    height: '15px',
-    opacity: hovered ? 1 : 0,
-    position: 'absolute',
-    right: 0,
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-    transition: 'all 250ms ease-out',
-    width: '15px'
-  };
-
   const refContainer = useRef(null);
 
   const toggleHovered = useCallback(() => setHovered((isHovered) => !isHovered), []);
@@ -52,9 +52,9 @@ function MenuItem ({ ButtonIcon, buttonName, withBorder = true }: MenuItemProps)
     <>
       <Grid container item onMouseEnter={toggleHovered} onMouseLeave={toggleHovered} ref={refContainer} sx={{ cursor: 'pointer', p: '3px', position: 'relative', width: 'fit-content' }}>
         <ButtonIcon color={hovered ? '#FF4FB9' : '#AA83DC'} size='24' variant='Bulk' />
-        <div style={selectedItemBackgroundStyle} />
+        <SelectedItemBackground hovered={hovered} />
       </Grid>
-      {withBorder && <GradientDivider orientation='vertical' style={{ height: '24px' }} />}
+      {withBorder && <GradientDivider orientation='vertical' style={{ height: '24px', opacity: 0.35 }} />}
       <Tooltip
         content={buttonName}
         placement='top'
@@ -70,14 +70,14 @@ function HomeMenu (): React.ReactElement {
 
   return (
     <Container disableGutters sx={{ bottom: '15px', mx: '15px', position: 'fixed', width: 'calc(100% - 30px)' }}>
-      <Grid sx={{ display: 'flex', justifyContent: 'space-between', p: '12px 17px', position: 'relative' }}>
+      <Grid alignItems='center' sx={{ display: 'flex', justifyContent: 'space-between', p: '12px 17px', position: 'relative' }}>
         <MenuItem ButtonIcon={Logout} buttonName={t('Send')} />
         <MenuItem ButtonIcon={ScanBarcode} buttonName={t('Receive')} />
         <MenuItem ButtonIcon={BuyCrypto} buttonName={t('Staking')} />
         <MenuItem ButtonIcon={MedalStar} buttonName={t('Governance')} />
         <MenuItem ButtonIcon={Setting} buttonName={t('Settings')} />
         <MenuItem ButtonIcon={Clock} buttonName={t('History')} withBorder={false} />
-        <div style={backgroundStyle} />
+        <Background />
       </Grid>
     </Container>
   );
