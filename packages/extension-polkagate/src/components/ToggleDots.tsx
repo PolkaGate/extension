5ba17622b40c21e3b035e717c41795cf1e9d5050
Amir Ekbatanifard <amiref007@gmail.com>
2025-02-06 10:49:00 +0330

    feat: add ToggleDots.tsx component

---
+++ packages/extension-polkagate/src/components/ToggleDots.tsx
@@ -0,0 +1,41 @@
+// Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/* eslint-disable react/jsx-max-props-per-line */
+
+import React from 'react';
+
+interface ToggleDotsProps {
+  active: boolean;
+  size?: number;
+  activeBallColor?: string;
+  inActiveBallColor?: string;
+}
+
+export default function ToggleDots ({ active, activeBallColor = '#FF4FB9', inActiveBallColor = '#674394CC', size = 10 }: ToggleDotsProps) {
+  const dotStyle = {
+    borderRadius: '999px',
+    height: size,
+    transition: 'transform 250ms ease',
+    width: size
+  };
+
+  return (
+    <div style={{ alignItems: 'center', display: 'flex', gap: 4 }}>
+      <div
+        style={{
+          ...dotStyle,
+          backgroundColor: active ? activeBallColor : inActiveBallColor,
+          transform: active ? 'scale(1.2)' : 'scale(0.8)'
+        }}
+      />
+      <div
+        style={{
+          ...dotStyle,
+          backgroundColor: active ? inActiveBallColor : activeBallColor,
+          transform: active ? 'scale(0.8)' : 'scale(1.2)'
+        }}
+      />
+    </div>
+  );
+}
