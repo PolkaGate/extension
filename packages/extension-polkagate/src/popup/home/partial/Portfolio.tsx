37962cd55d9d31c0c8686e7ee8bddd68bc1e746c
Amir Ekbatanifard <amiref007@gmail.com>
2025-02-06 10:50:52 +0330

    refactor: update Portfolio.tsx using GlowBox

--- packages/extension-polkagate/src/popup/home/partial/Portfolio.tsx
+++ packages/extension-polkagate/src/popup/home/partial/Portfolio.tsx
@@ -3,53 +3,17 @@
 
 /* eslint-disable react/jsx-max-props-per-line */
 
-import { Container, Grid, Skeleton, styled, type SxProps, type Theme, Typography, useTheme } from '@mui/material';
+import { Grid, Skeleton, Typography, useTheme } from '@mui/material';
 import { Eye, EyeSlash } from 'iconsax-react';
 import React, { useCallback, useMemo } from 'react';
 
 import { FormatPrice } from '../../../components';
 import { useAccountAssets, useIsHideNumbers, usePrices, useSelectedAccount, useTranslation } from '../../../hooks';
 import { calcPrice } from '../../../hooks/useYouHave';
-import { GradientBorder, GradientDivider } from '../../../style';
+import { GlowBox } from '../../../style';
 import AccountVisibilityToggler from './AccountVisibilityToggler';
 import Currency from './Currency';
 import DailyChange from './DailyChange';
-
-const PortfolioContainerStyle: SxProps<Theme> = {
-  border: '2px solid transparent',
-  borderRadius: '24px',
-  height: '132px',
-  overflow: 'hidden',
-  position: 'relative',
-  width: '359px'
-};
-
-const GlowBall = styled('div')({
-  backgroundColor: '#FF59EE',
-  borderRadius: '50%',
-  filter: 'blur(60px)', // Glow effect
-  height: '128px',
-  left: '35%',
-  position: 'absolute',
-  top: '-75px',
-  width: '100px'
-});
-
-const FadeOut = styled('div')({
-  background: 'linear-gradient(180deg, transparent 13.79%, #05091C 100%)',
-  height: '160px',
-  inset: 0,
-  position: 'absolute',
-  width: '375px'
-});
-
-const Fade = styled('div')({
-  backgroundColor: 'rgba(255, 255, 255, 0.02)',
-  height: '220px',
-  inset: 0,
-  position: 'absolute',
-  width: '375px'
-});
 
 function Portfolio (): React.ReactElement {
   const { t } = useTranslation();
@@ -78,8 +42,8 @@ function Portfolio (): React.ReactElement {
   }, [accountAssets, priceOf]);
 
   return (
-    <Container disableGutters sx={PortfolioContainerStyle}>
-      <Grid alignItems='center' container item justifyContent='space-between' sx={{ p: '15px 20px', position: 'relative', zIndex: 1 }}>
+    <GlowBox>
+      <Grid alignItems='center' container item justifyContent='space-between' sx={{ p: '15px 20px 0' }}>
         <Grid alignItems='center' container item sx={{ columnGap: '5px', width: 'fit-content' }}>
           <Typography color='text.secondary' variant='B-2'>
             {t('Account Portfolio')}
@@ -114,14 +78,7 @@ function Portfolio (): React.ReactElement {
         <DailyChange />
         <AccountVisibilityToggler />
       </Grid>
-      {/* Styles */}
-      <GradientBorder />
-      <GradientDivider orientation='vertical' style={{ bottom: 0, height: '65%', left: 0, m: 'auto', position: 'absolute', top: 0, width: '2px' }} />
-      <GradientDivider orientation='vertical' style={{ bottom: 0, height: '65%', m: 'auto', position: 'absolute', right: 0, top: 0, width: '2px' }} />
-      <GlowBall />
-      <Fade />
-      <FadeOut />
-    </Container>
+    </GlowBox>
   );
 }
 
