92cd6753fbf77328aa1cfa414372018d255099fd
Amir Ekbatanifard <amiref007@gmail.com>
2025-02-09 13:02:45 +0330

    fix: shared Keyframe Name issue in ScrollingTextBox.tsx

--- packages/extension-polkagate/src/components/ScrollingTextBox.tsx
+++ packages/extension-polkagate/src/components/ScrollingTextBox.tsx
@@ -4,8 +4,8 @@
 /* eslint-disable sort-keys */
 /* eslint-disable react/jsx-max-props-per-line */
 
-import { Box, type SxProps, type Theme, Typography } from '@mui/material';
-import React, { useEffect, useRef, useState } from 'react';
+import { Box, styled, type SxProps, type Theme, Typography } from '@mui/material';
+import React, { memo, useEffect, useMemo, useRef, useState } from 'react';
 
 interface ScrollingTextBoxProps {
   text: string;
@@ -14,10 +14,38 @@ interface ScrollingTextBoxProps {
   textStyle?: SxProps<Theme>;
 }
 
+const BoxContainer = styled(Box)(({ maximumWidth, shouldScroll }: { shouldScroll: boolean; maximumWidth: number }) => ({
+  '&::after': {
+    background: shouldScroll ? 'linear-gradient(90deg, transparent 0%, #591467 100%)' : undefined,
+    opacity: 0.3,
+    right: 0
+  },
+  '&::before': {
+    background: shouldScroll ? 'linear-gradient(90deg, #20123E 0%, transparent 100%)' : undefined,
+    left: 0,
+    opacity: 0.3
+  },
+  '&::before, &::after': {
+    content: '""',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+    width: '13px',
+    zIndex: 1
+  },
+  borderRadius: '8px',
+  maxWidth: `${maximumWidth}px`,
+  overflow: 'hidden',
+  position: 'relative',
+  width: 'fit-content'
+}));
+
 function ScrollingTextBox ({ style, text, textStyle, width }: ScrollingTextBoxProps): React.ReactElement {
   const textRef = useRef<HTMLDivElement>(null);
   const [shouldScroll, setShouldScroll] = useState(false);
   const [textWidth, setTextWidth] = useState(0);
+
+  const uniqueKeyframeName = useMemo(() => `scrollText-${Math.random().toString(36).substring(2, 11)}`, []);
 
   useEffect(() => {
     if (textRef.current) {
@@ -28,39 +56,13 @@ function ScrollingTextBox ({ style, text, textStyle, width }: ScrollingTextBoxPr
     }
   }, [text]);
 
-  const animationDuration = Math.max(10, textWidth / 50); // Adjusts scrolling speed
+  const animationDuration = useMemo(() => Math.max(10, textWidth / 50), [textWidth]); // Adjusts scrolling speed
 
-  const containerStyle: SxProps<Theme> = {
-    '&::after': {
-      background: shouldScroll ? 'linear-gradient(90deg, transparent 0%, #591467 100%)' : undefined,
-      opacity: 0.3,
-      right: 0
-    },
-    '&::before': {
-      background: shouldScroll ? 'linear-gradient(90deg, #20123E 0%, transparent 100%)' : undefined,
-      left: 0,
-      opacity: 0.3
-    },
-    '&::before, &::after': {
-      content: '""',
-      height: '100%',
-      position: 'absolute',
-      top: 0,
-      width: '13px',
-      zIndex: 1
-    },
-    borderRadius: '8px',
-    maxWidth: `${width}px`,
-    overflow: 'hidden',
-    position: 'relative',
-    width: 'fit-content'
-  };
-
-  const textboxStyle: SxProps<Theme> = {
+  const textboxStyle: SxProps<Theme> = useMemo(() => ({
     '&:hover': {
       animationPlayState: 'paused'
     },
-    '@keyframes scrollText': {
+    [`@keyframes ${uniqueKeyframeName}`]: {
       '0%': { transform: 'translateX(0)' },
       '25%': { transform: `translateX(${width - textWidth}px)` },
       '50%': { transform: `translateX(${width - textWidth}px)` },
@@ -68,18 +70,18 @@ function ScrollingTextBox ({ style, text, textStyle, width }: ScrollingTextBoxPr
       '100%': { transform: 'translateX(0)' }
     },
     animation: shouldScroll
-      ? `scrollText ${animationDuration}s linear infinite`
+      ? `${uniqueKeyframeName} ${animationDuration}s linear infinite`
       : 'none',
     whiteSpace: 'nowrap'
-  };
+  }), [animationDuration, shouldScroll, textWidth, uniqueKeyframeName, width]);
 
   return (
-    <Box sx={{ ...containerStyle, ...style }}>
+    <BoxContainer maximumWidth={width} shouldScroll={shouldScroll} sx={style}>
       <Typography ref={textRef} sx={{ ...textboxStyle, ...textStyle }}>
         {text}
       </Typography>
-    </Box>
+    </BoxContainer>
   );
 }
 
-export default ScrollingTextBox;
+export default memo(ScrollingTextBox);
