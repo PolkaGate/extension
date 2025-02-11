ca224484b857283cea6c44043ea7933d640c7cc8
Nick <ekbatanifard@gmail.com>
2025-02-11 14:50:38 +0330

    style: adjust switch style

--- packages/extension-polkagate/src/popup/settings/extensionSettings/components/Switch.tsx
+++ packages/extension-polkagate/src/popup/settings/extensionSettings/components/Switch.tsx
@@ -17,7 +17,7 @@ interface Props extends SwitchProps {
 
 const PSwitch = styled(
   ({ checked, columnGap, label, onChange, ...props }: Props) => (
-    <Stack columnGap={columnGap} direction='row'>
+    <Stack component='label' columnGap={columnGap} direction='row'>
       <Switch
         checked={checked}
         disableRipple
@@ -35,18 +35,26 @@ const PSwitch = styled(
   background: checked
     ? 'linear-gradient(#2D1E4A, #2D1E4A) padding-box, linear-gradient(262.56deg, #6E00B1 0%, #DC45A0 45%, #6E00B1 100%) border-box'
     : '#2D1E4A',
-
   backgroundClip: checked ? 'padding-box, border-box' : 'unset',
   backgroundOrigin: 'border-box',
   borderRadius: '109.71px',
+  cursor: 'pointer',
   height: '24px',
   padding: 0,
   width: '36px',
+  '& .MuiSwitch-root': {
+    height: '100%',
+    width: '100%'
+  },
   '& .MuiSwitch-switchBase': {
     alignSelf: 'anchor-center',
+    left: 0,
     margin: 2,
     padding: 0,
-    transitionDuration: '300ms',
+    transition: theme.transitions.create(['transform', 'background-color', 'box-shadow'], {
+      duration: 300,
+      easing: 'cubic-bezier(0.4, 0, 0.2, 1)'
+    }),
     '&.Mui-checked': {
       background: '#EAEBF1',
       padding: 2,
@@ -56,6 +64,10 @@ const PSwitch = styled(
       },
       '& + .MuiSwitch-track': {
         backgroundColor: '#EAEBF1',
+        transition: theme.transitions.create('background-color', {
+          duration: 300,
+          easing: 'ease-in-out'
+        }),
         ...theme.applyStyles('dark', {
           backgroundColor: '#2D1E4A'
         })
@@ -81,7 +93,11 @@ const PSwitch = styled(
     background: '#674394',
     boxSizing: 'border-box',
     height: 10.29,
-    width: 10.29
+    width: 10.29,
+    transition: theme.transitions.create(['background-color', 'transform'], {
+      duration: 300,
+      easing: 'cubic-bezier(0.4, 0, 0.2, 1)'
+    })
   },
   '& .MuiSwitch-track': {
     transition: theme.transitions.create(['background-color'], {
