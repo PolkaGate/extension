4b4c28857e9162d9b44388c1e82e8ffda0b33878
Amir Ekbatanifard <amiref007@gmail.com>
2025-02-19 11:31:58 +0330

    refactor: add new change log page

--- packages/extension-polkagate/src/partials/Version2.tsx
+++ packages/extension-polkagate/src/partials/Version2.tsx
@@ -8,6 +8,7 @@ import React, { type CSSProperties, useCallback, useState } from 'react';
 
 import Sparkles from '../components/SVG/Sparkles';
 import { useManifest, useTranslation } from '../hooks';
+import ChangeLog from '../popup/home/ChangeLog';
 import { GradientBorder } from '../style';
 
 interface Props {
@@ -18,23 +19,34 @@ interface Props {
 export default function Version ({ showLabel = true, style = { columnGap: '5px', paddingBottom: '24px', paddingTop: '24px' } }: Props): React.ReactElement {
   const { t } = useTranslation();
   const version = useManifest()?.version;
+
   const [hovered, setHovered] = useState<boolean>(false);
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+
   const toggleHovered = useCallback(() => setHovered((isHovered) => !isHovered), []);
+  const toggleOpenPopup = useCallback(() => setOpenMenu(true), []);
 
   return (
-    <Grid alignItems='center' container item justifyContent='center' sx={{ ...style }}>
-      {showLabel &&
+    <>
+      <Grid alignItems='center' container item justifyContent='center' sx={{ ...style }}>
+        {showLabel &&
+          <Typography color='#BEAAD880' variant='B-1'>
+            {t('Version')}
+          </Typography>}
         <Typography color='#BEAAD880' variant='B-1'>
-          {t('Version')}
-        </Typography>}
-      <Typography color='#BEAAD880' variant='B-1'>
-        {version}
-      </Typography>
-      <GradientBorder style={{ height: '1px', opacity: 0.5, position: 'static', rotate: '90deg', width: '14px' }} />
-      <Sparkles color={hovered ? '#AA83DC' : '#FF4FB9'} height={12} width={12} />
-      <Typography color={hovered ? '#AA83DC' : '#BEAAD8'} onMouseEnter={toggleHovered} onMouseLeave={toggleHovered} sx={{ cursor: 'pointer', textDecoration: hovered ? 'underline' : 'none' }} variant='B-1'>
-        {t('What’s new page')}
-      </Typography>
-    </Grid>
+          {version}
+        </Typography>
+        <GradientBorder style={{ height: '1px', opacity: 0.5, position: 'static', rotate: '90deg', width: '14px' }} />
+        <Sparkles color={hovered ? '#AA83DC' : '#FF4FB9'} height={12} width={12} />
+        <Typography color={hovered ? '#AA83DC' : '#BEAAD8'} onClick={toggleOpenPopup} onMouseEnter={toggleHovered} onMouseLeave={toggleHovered} sx={{ cursor: 'pointer', textDecoration: hovered ? 'underline' : 'none' }} variant='B-1'>
+          {t('What’s new page')}
+        </Typography>
+      </Grid>
+      {openMenu &&
+        <ChangeLog
+          openMenu={openMenu}
+          setShowAlert={setOpenMenu}
+        />}
+    </>
   );
 }
