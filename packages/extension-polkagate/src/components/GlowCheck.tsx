2a40324647e88b677951fff6c70c8024f91d8541
Amir Ekbatanifard <amiref007@gmail.com>
2025-03-04 16:03:10 +0330

    feat: create GlowCheck.tsx component

---
+++ packages/extension-polkagate/src/components/GlowCheck.tsx
@@ -0,0 +1,24 @@
+// Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+/* eslint-disable react/jsx-max-props-per-line */
+
+import CheckIcon from '@mui/icons-material/Check';
+import { Fade } from '@mui/material';
+import React from 'react';
+
+interface Props {
+  show: boolean;
+  timeout?: number;
+  size?: string;
+}
+
+function GlowCheck ({ show = false, size = '20px', timeout = 300 }: Props) {
+  return (
+    <Fade in={show} timeout={timeout}>
+      <CheckIcon sx={{ background: 'linear-gradient(262.56deg, #6E00B1 0%, #DC45A0 45%, #6E00B1 100%)', borderRadius: '999px', fontSize: size, p: '3px' }} />
+    </Fade>
+  );
+}
+
+export default GlowCheck;
