7c28e4b760484f2646b9bc275d8dcfcb4e98c420
Nick <ekbatanifard@gmail.com>
2025-03-15 17:11:14 +0330

    style: add hover effect

--- packages/extension-polkagate/src/popup/home/partial/NFTBox.tsx
+++ packages/extension-polkagate/src/popup/home/partial/NFTBox.tsx
@@ -1,14 +1,12 @@
 // Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-/* eslint-disable react/jsx-max-props-per-line */
-
 import type { ApiPromise } from '@polkadot/api';
 import type { Chain } from '@polkadot/extension-chains/types';
 import type { ItemInformation } from '../../../fullscreen/nft/utils/types';
 
-import { Avatar, Box, Container, Grid, Typography, useTheme } from '@mui/material';
-import { ArrowRight2 } from 'iconsax-react';
+import { Box, Container, Grid, Typography, useTheme } from '@mui/material';
+import { ArrowCircleRight, ArrowRight2 } from 'iconsax-react';
 import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
 
 import { logoBlackBirdTransparent } from '../../../assets/logos';
@@ -81,36 +79,71 @@ function NFTItem ({ apis, item }: NftItemProps) {
   const theme = useTheme();
   const isDark = useIsDark();
   const api = apis[item.chainName];
+  const [isHovered, setHovered] = useState(false);
 
-  const bgcolor = isDark ? '#1B133C' : '#FFF';
+  const bgcolor = isDark ? isHovered ? '#2D1E4A' : '#1B133C' : '#FFF';
   const bgcolor2 = isDark ? '#05091C' : '#EFEEF7';
   const itemIdColor = isDark ? '#EAEBF1' : '#291443';
   const itemNameColor = isDark ? '#BEAAD8' : '#745D8B';
 
+  const handleMouseEnter = useCallback(() => setHovered(true), []);
+  const handleMouseLeave = useCallback(() => setHovered(false), []);
+
   return (
-    <Grid container item sx={{ bgcolor, borderRadius: '18px', p: '4px', width: '152px' }}>
+    <Grid container item onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} sx={{ bgcolor, borderRadius: '18px', p: '4px', width: '152px' }}>
       <Grid container direction='column' item sx={{ bgcolor: bgcolor2, borderRadius: '14px' }}>
-        <Avatar
-          draggable={false}
-          src={item.image ?? ''}
-          sx={{
-            borderRadius: '14px',
-            height: '144px',
-            img: {
-              objectFit: 'contain',
-              objectPosition: 'center'
-            },
-            pointerEvents: 'none',
-            width: '100%'
-          }}
-          variant='square'
-        />
-        <Grid container direction='column' item sx={{ p: '6px 12px' }}>
+        <Grid container item sx={{
+          borderRadius: '14px',
+          height: isHovered ? '135px' : '144px',
+          overflow: 'hidden',
+          position: 'relative',
+          transition: 'height 0.3s ease-in-out',
+          width: '144px'
+        }}
+        >
+          <Box
+            sx={{
+              '&:hover': {
+                transform: 'translate(-50%, -50%) scale(1.1)'
+              },
+              backgroundImage: `url(${item.image ?? ''})`,
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+              borderRadius: '14px',
+              height: '100%',
+              left: '50%',
+              position: 'absolute',
+              top: '50%',
+              transform: 'translate(-50%, -50%)',
+              transition: 'transform 0.3s ease-in-out',
+              width: '100%'
+            }}
+          />
+          <ArrowCircleRight
+            color='#FF4FB9'
+            id='arrow'
+            size='40'
+            style={{
+              left: '50%',
+              opacity: isHovered ? 1 : 0, // Control visibility based on state
+              position: 'absolute',
+              top: '50%',
+              transform: 'translate(-50%, -50%)',
+              transition: 'opacity 0.3s ease-in-out'
+            }}
+            variant='Bold'
+          />
+        </Grid>
+        <Grid container direction='column' item sx={{ m: '6px 12px' }}>
           <Grid container item width='fit-content'>
-            <span style={{ color: isDark ? '#BEAAD880' : '#745D8B', ...theme.typography['B-2'] }}>#</span>
-            <span style={{ color: itemIdColor, ...theme.typography['B-2'] }}>{item.itemId}</span>
+            <span style={{ color: isDark ? '#BEAAD880' : '#745D8B', ...theme.typography['B-2'] }}>
+              #
+            </span>
+            <span style={{ color: itemIdColor, ...theme.typography['B-2'] }}>
+              {item.itemId}
+            </span>
           </Grid>
-          <Typography color= {itemNameColor} sx={{ maxWidth: '110px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }} variant='B-2'>
+          <Typography color={itemNameColor} sx={{ maxWidth: '110px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }} variant='B-2'>
             {toTitleCase(item.name)}
           </Typography>
         </Grid>
@@ -123,7 +156,7 @@ function NFTItem ({ apis, item }: NftItemProps) {
   );
 }
 
-function NFTBox() {
+function NFTBox () {
   const { t } = useTranslation();
   const account = useSelectedAccount();
   const isDark = useIsDark();
@@ -193,7 +226,7 @@ function NFTBox() {
     <>
       {nfts
         ? <>
-          <Container disableGutters sx={{ bgcolor: isDark ? '#05091C' : '#F5F4FF', borderRadius: '14px', display: 'flex', justifyContent: 'space-evenly', py: '10px', width: '100%' }}>
+          <Container disableGutters sx={{ bgcolor: isDark ? '#05091C' : '#F5F4FF', borderRadius: '14px', display: 'flex', height: '259px', justifyContent: 'space-evenly', py: '10px', width: '100%' }}>
             {itemsToShow?.map((item, index) => (
               <NFTItem
                 apis={apis}
@@ -203,7 +236,7 @@ function NFTBox() {
             ))}
           </Container>
           <Grid alignItems='center' columnGap='5px' container item justifyContent='center' onClick={openNft} sx={{ cursor: 'pointer', p: '8px 0 4px' }}>
-            <Typography color= {isDark ? '#BEAAD8' : '#745D8B'} variant='B-2'>
+            <Typography color={isDark ? '#BEAAD8' : '#745D8B'} variant='B-2'>
               {t('See all')}
             </Typography>
             <ArrowRight2 color='#BEAAD880' size='14' />
