5bab50af8ac84d42a8323556c683d29ef51de7ee
Amir Ekbatanifard <amiref007@gmail.com>
2025-05-05 15:44:36 +0330

    feat: create new page WaitScreen2.tsx

--- packages/extension-polkagate/src/partials/index.ts
+++ packages/extension-polkagate/src/partials/index.ts
@@ -21,4 +21,5 @@ export { default as UserDashboardHeader } from './UserDashboardHeader';
 export { default as Version } from './Version';
 export { default as Version2 } from './Version2';
 export { default as WaitScreen } from './WaitScreen';
+export { default as WaitScreen2 } from './WaitScreen2';
 export { default as WelcomeHeader } from './WelcomeHeader';
