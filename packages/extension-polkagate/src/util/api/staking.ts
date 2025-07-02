d0bad02a5ae53aa0deeed961bf6079b982f09c2b
Amir Ekbatanifard <amiref007@gmail.com>
2025-07-02 10:07:03 +0330

    refactor: separate restaking functionality

--- packages/extension-polkagate/src/util/api/staking.ts
+++ packages/extension-polkagate/src/util/api/staking.ts
@@ -310,3 +310,78 @@ export const useUnstakingSolo = (
     unstakingValue
   };
 };
+
+export const useRestakeSolo = (
+  address: string | undefined,
+  genesisHash: string | undefined
+) => {
+  const { t } = useTranslation();
+
+  const stakingInfo = useSoloStakingInfo(address, genesisHash);
+  const { api, decimal } = useChainInfo(genesisHash);
+  const formatted = useFormatted3(address, genesisHash);
+
+  const rebond = api?.tx['staking']['rebond'];
+
+  const [rebondValue, setRebondValue] = useState<BN | null | undefined>();
+
+  const estimatedFee = useEstimatedFee2(genesisHash ?? '', formatted, rebond, [rebondValue]);
+
+  const staked = useMemo(() => stakingInfo.stakingAccount?.stakingLedger.active, [stakingInfo.stakingAccount?.stakingLedger.active]);
+  const unlockingAmount = useMemo(() => stakingInfo.sessionInfo?.unlockingAmount, [stakingInfo.sessionInfo?.unlockingAmount]);
+  const errorMessage = useMemo(() => {
+    if (!unlockingAmount || unlockingAmount.isZero() || !rebondValue || rebondValue.isZero() || !api) {
+      return undefined;
+    }
+
+    if (rebondValue.gt(unlockingAmount)) {
+      return t('It is more than unstaking amount.');
+    }
+
+    return undefined;
+  }, [api, rebondValue, t, unlockingAmount]);
+  const transactionInformation = useMemo(() => {
+    return [{
+      content: rebondValue,
+      title: t('Amount'),
+      withLogo: true
+    },
+    {
+      content: estimatedFee,
+      title: t('Fee')
+    },
+    {
+      content: rebondValue && staked ? (staked as unknown as BN).add(rebondValue) : undefined,
+      title: t('Total stake after'),
+      withLogo: true
+    }
+    ];
+  }, [estimatedFee, rebondValue, staked, t]);
+  const tx = useMemo(() => rebond?.(rebondValue), [rebond, rebondValue]);
+
+  const onInputChange = useCallback((value: string | null | undefined) => {
+    const valueAsBN = value ? amountToMachine(value, decimal) : null;
+
+    setRebondValue(valueAsBN);
+  }, [decimal]);
+
+  const onMaxValue = useMemo(() => {
+    if (!unlockingAmount || !decimal) {
+      return '0';
+    }
+
+    return unlockingAmount.toString();
+  }, [decimal, unlockingAmount]);
+
+  return {
+    errorMessage,
+    estimatedFee,
+    onInputChange,
+    onMaxValue,
+    rebondValue,
+    setRebondValue,
+    transactionInformation,
+    tx,
+    unlockingAmount
+  };
+};
