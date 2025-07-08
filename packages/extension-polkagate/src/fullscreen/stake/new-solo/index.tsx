9d5194ab761c83d77ef502845f4adaa8c184093b
Amir Ekbatanifard <amiref007@gmail.com>
2025-07-08 14:47:25 +0330

    feat: create and use rewards tab in staking full-screen

--- packages/extension-polkagate/src/fullscreen/stake/new-solo/index.tsx
+++ packages/extension-polkagate/src/fullscreen/stake/new-solo/index.tsx
@@ -2,25 +2,39 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Stack } from '@mui/material';
-import React, { useMemo } from 'react';
+import { Discover, MagicStar, Wallet } from 'iconsax-react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { useParams } from 'react-router';
 
 import { type BN } from '@polkadot/util';
 
-import { useAccountAssets, usePrices, useSelectedAccount, useSoloStakingInfo } from '../../../hooks';
+import { useAccountAssets, useChainInfo, usePrices, useSelectedAccount, useSoloStakingInfo, useStakingRewards3, useTranslation } from '../../../hooks';
 import HomeLayout from '../../components/layout';
 import StakingIcon from '../partials/StakingIcon';
 import StakingPortfolioAndTiles from '../partials/StakingPortfolioAndTiles';
+import StakingTabs, { type StakingTabsHeaderItems } from '../partials/StakingTabs';
 import { useStakingPopups } from '../util/utils';
 import PopUpHandler from './PopUpHandler';
+import Rewards from './rewards';
+
+enum SOLO_TAB {
+  STAKING_POSITIONS,
+  REWARDS,
+  VALIDATORS
+}
 
 export default function SoloFullScreen (): React.ReactElement {
+  const { t } = useTranslation();
   const { genesisHash } = useParams<{ genesisHash: string }>();
+  const { token } = useChainInfo(genesisHash, true);
   const selectedAccount = useSelectedAccount();
   const stakingInfo = useSoloStakingInfo(selectedAccount?.address, genesisHash);
   const accountAssets = useAccountAssets(selectedAccount?.address);
   const pricesInCurrency = usePrices();
   const { popupCloser, popupOpener, stakingPopup } = useStakingPopups();
+  const rewardInfo = useStakingRewards3(selectedAccount?.address, genesisHash, 'solo', true);
+
+  const [tab, setTab] = useState<SOLO_TAB>(SOLO_TAB.STAKING_POSITIONS);
 
   const asset = useMemo(() =>
     accountAssets?.find(({ assetId, genesisHash: accountGenesisHash }) => accountGenesisHash === genesisHash && String(assetId) === '0')
@@ -33,6 +47,29 @@ export default function SoloFullScreen (): React.ReactElement {
   const toBeReleased = useMemo(() => stakingInfo.sessionInfo?.toBeReleased, [stakingInfo.sessionInfo?.toBeReleased]);
   const unlockingAmount = useMemo(() => stakingInfo.sessionInfo?.unlockingAmount, [stakingInfo.sessionInfo?.unlockingAmount]);
   const rewards = useMemo(() => stakingInfo.rewards, [stakingInfo.rewards]);
+
+  const tabSetter = useCallback((selectedTab: SOLO_TAB) => () => setTab(selectedTab), []);
+
+  const tabItems: StakingTabsHeaderItems[] = useMemo(() => ([
+    {
+      Icon: Wallet,
+      isSelected: tab === SOLO_TAB.STAKING_POSITIONS,
+      onClick: tabSetter(SOLO_TAB.STAKING_POSITIONS),
+      title: t('Your staking positions')
+    },
+    {
+      Icon: MagicStar,
+      isSelected: tab === SOLO_TAB.REWARDS,
+      onClick: tabSetter(SOLO_TAB.REWARDS),
+      title: t('Rewards')
+    },
+    {
+      Icon: Discover,
+      isSelected: tab === SOLO_TAB.VALIDATORS,
+      onClick: tabSetter(SOLO_TAB.VALIDATORS),
+      title: t('Validators')
+    }
+  ]), [t, tab, tabSetter]);
 
   return (
     <>
@@ -50,6 +87,17 @@ export default function SoloFullScreen (): React.ReactElement {
             tokenPrice={tokenPrice}
             type='solo'
             unlockingAmount={unlockingAmount}
+          />
+          <StakingTabs
+            content={
+              <Rewards
+                genesisHash={genesisHash}
+                popupOpener={popupOpener}
+                rewardInfo={rewardInfo}
+                token={token}
+              />
+            }
+            items={tabItems}
           />
         </Stack>
       </HomeLayout>
---
+++ packages/extension-polkagate/src/fullscreen/stake/new-solo/rewards/index.tsx
@@ -0,0 +1,125 @@
+// Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { Chart, ChartArea } from 'chart.js';
+import type { UseStakingRewards } from '../../../../hooks/useStakingRewards3';
+
+import { ChevronLeft, ChevronRight } from '@mui/icons-material';
+import { Box, Container, Grid, Skeleton, Stack, Typography } from '@mui/material';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+
+import { AssetLogo } from '../../../../components';
+import getLogo2 from '../../../../util/getLogo2';
+import { type PopupOpener, StakingPopUps } from '../../util/utils';
+import RewardConfigureButton from '../components/RewardConfigureButton';
+
+interface WindowChangerProps {
+  onNextPeriod: () => void;
+  onPreviousPeriod: () => void;
+  dateInterval: string | undefined;
+}
+
+const WindowChanger = ({ dateInterval, onNextPeriod, onPreviousPeriod }: WindowChangerProps) => {
+  const chevronStyle = {
+    ':hover': { color: '#EAEBF1' },
+    backdropFilter: 'blur(20px)',
+    borderRadius: '10px',
+    boxShadow: '0px 0px 24px 8px #4E2B7259 inset',
+    color: '#AA83DC',
+    cursor: 'pointer',
+    fontSize: '24px'
+  };
+
+  return (
+    <Container disableGutters sx={{ alignItems: 'center', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', m: 0, minWidth: '190px', width: 'fit-content' }}>
+      <ChevronLeft onClick={onPreviousPeriod} sx={chevronStyle} />
+      {dateInterval
+        ? (
+          <Typography color='text.highlight' variant='H-2'>
+            {dateInterval}
+          </Typography>)
+        : (
+          <Skeleton
+            animation='wave'
+            height='30px'
+            sx={{ borderRadius: '50px', fontWeight: 'bold', transform: 'none', width: '90px' }}
+            variant='text'
+          />
+        )
+      }
+      <ChevronRight onClick={onNextPeriod} sx={chevronStyle} />
+    </Container>
+  );
+};
+
+interface RewardSettingProps {
+  genesisHash: string | undefined;
+  token: string | undefined;
+  popupOpener: PopupOpener;
+}
+
+const RewardSetting = ({ genesisHash, popupOpener, token }: RewardSettingProps) => {
+  const logoInfo = useMemo(() => getLogo2(genesisHash, token), [genesisHash, token]);
+
+  return (
+    <Container disableGutters sx={{ alignItems: 'center', display: 'flex', flexDirection: 'row', gap: '4px', m: 0, width: 'fit-content' }}>
+      <Container disableGutters sx={{ alignItems: 'center', bgcolor: '#05091C', borderRadius: '10px', display: 'flex', flexDirection: 'row', gap: '6px', p: '6px', pr: '10px', width: 'fit-content' }}>
+        <AssetLogo assetSize='24px' baseTokenSize='0' genesisHash={genesisHash} logo={logoInfo?.logo} subLogo={undefined} />
+        <Typography color='text.primary' textTransform='uppercase' variant='B-2'>
+          {token}
+        </Typography>
+      </Container>
+      <RewardConfigureButton onClick={popupOpener(StakingPopUps.REWARD_DESTINATION_CONFIG)} />
+    </Container>
+  );
+};
+
+interface ChartHeaderProps extends RewardSettingProps {
+  rewardInfo: UseStakingRewards;
+}
+
+const ChartHeader = ({ genesisHash, popupOpener, rewardInfo, token }: ChartHeaderProps) => {
+  return (
+    <Container disableGutters sx={{ display: 'flex', flexDirection: 'row', gap: '20px', justifyContent: 'space-between', mb: '20px' }}>
+      <WindowChanger
+        dateInterval={rewardInfo.dateInterval}
+        onNextPeriod={rewardInfo.onNextPeriod}
+        onPreviousPeriod={rewardInfo.onPreviousPeriod}
+      />
+      <RewardSetting
+        genesisHash={genesisHash}
+        popupOpener={popupOpener}
+        token={token}
+      />
+    </Container>
+  );
+};
+
+interface RewardChartProps {
+  rewardInfo: UseStakingRewards;
+}
+
+const RewardChart = ({ rewardInfo }: RewardChartProps) => {
+  return (
+    <Box sx={{ height: '253px', width: '533px' }}>
+      <Bar data={rewardInfo.chartData} id='myCanvas' options={{ ...rewardInfo.chartOptions, maintainAspectRatio: false }} style={{ backgroundColor: '#05091C', borderRadius: '14px', padding: '4px' }} />
+    </Box>
+  );
+};
+
+export default function Rewards ({ genesisHash, popupOpener, rewardInfo, token }: ChartHeaderProps) {
+  return (
+    <Container disableGutters sx={{ display: 'flex', flexDirection: 'row', gap: '18px', p: '18px', pr: 0 }}>
+      <Stack direction='column' sx={{ bgcolor: '#1B133C', borderRadius: '18px', width: '533px' }}>
+        <ChartHeader
+          genesisHash={genesisHash}
+          popupOpener={popupOpener}
+          rewardInfo={rewardInfo}
+          token={token}
+        />
+        <RewardChart rewardInfo={rewardInfo} />
+      </Stack>
+    </Container>
+  );
+}
