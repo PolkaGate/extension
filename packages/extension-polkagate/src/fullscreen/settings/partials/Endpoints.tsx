e835765cf6f4da53ac2fec52528f81857ab76dd7
Nick <ekbatanifard@gmail.com>
2025-07-13 11:51:55 +0330

    style: handle fade on scroll isuue

--- packages/extension-polkagate/src/fullscreen/settings/partials/Endpoints.tsx
+++ packages/extension-polkagate/src/fullscreen/settings/partials/Endpoints.tsx
@@ -26,6 +26,45 @@ interface Props {
   onClose: () => void;
   onEnableChain: (value: string, checked: boolean) => void;
 }
+interface EndpointRowProps {
+  isFirst: boolean;
+  isLast: boolean;
+  checked: boolean;
+  name: string;
+  value: string;
+  delay: number | null | undefined;
+  onChangeEndpoint: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+function EndpointRow ({ checked, delay, isFirst, isLast, name, onChangeEndpoint, value }: EndpointRowProps): React.ReactElement {
+  const { t } = useTranslation();
+
+  return (
+    <Grid alignItems='start' container direction='column' item key={value} py='5px' sx={{ bgcolor: '#05091C', borderRadius: isFirst ? '14px 14px 0 0' : isLast ? '0 0 14px 14px' : 0, flexWrap: 'nowrap', height: isFirst ? '100px' : '73px', mt: '2px', px: '10px' }}>
+      {
+        isFirst &&
+        <Typography color='#7956A5' fontFamily='Inter' fontSize='11px' fontWeight={600} sx={{ p: '8px' }}>
+          {t('NODES')}
+        </Typography>
+      }
+      <Stack alignItems='center' columnGap='10px' direction='row'>
+        <Radio
+          checked={checked}
+          columnGap='5px'
+          label={name}
+          onChange={onChangeEndpoint}
+          value={value}
+        />
+        <DotIndicator delay={delay} />
+      </Stack>
+      <Grid item sx={{ mt: '-5px', pl: '10px' }}>
+        <Typography color='#674394' variant='B-5'>
+          {value}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
 
 function Endpoints ({ genesisHash, isEnabled, onClose, onEnableChain, open }: Props): React.ReactElement {
   const { t } = useTranslation();
@@ -143,50 +182,39 @@ function Endpoints ({ genesisHash, isEnabled, onClose, onEnableChain, open }: Pr
       title={displayName}
     >
       <Stack direction='column'>
-        <Grid container height='420px' item ref={refContainer} sx={{ bgcolor: '#1B133C', borderRadius: '14px', display: 'block', overflowY: 'scroll', position: 'relative' }}>
-          <MySwitch
-            checked={mayBeEnabled}
-            columnGap='8px'
-            label={t('Enable Network')}
-            onChange={onEnableNetwork}
-            style={{ alignItems: 'center', backgroundColor: '#05091C', borderRadius: '18px', height: '52px', justifyContent: 'flex-start', padding: '0 15px', width: '100%' }}
-            value={mayBeEnabled}
-          />
-          <MySwitch
-            checked={isAutoMode}
-            columnGap='8px'
-            label={t('Auto Node Selection')}
-            onChange={onChangeEndpoint}
-            style={{ alignItems: 'center', backgroundColor: '#05091C', borderRadius: '18px', height: '52px', justifyContent: 'flex-start', margin: '8px 0', padding: '0 15px', width: '100%' }}
-            value={AUTO_MODE.value}
-          />
-          {filteredEndpoints?.map(({ delay, name, value }, index) => (
-            <Grid alignItems='start' container direction='column' item key={value} py='5px' sx={{ bgcolor: '#05091C', borderRadius: index === 0 ? '14px 14px 0 0' : index === filteredEndpoints.length - 1 ? '0 0 14px 14px' : 0, flexWrap: 'nowrap', height: index === 0 ? '100px' : '73px', mt: '2px', px: '10px' }}>
-              {
-                index === 0 &&
-                <Typography color='#7956A5' fontFamily='Inter' fontSize='11px' fontWeight={600} sx={{ p: '8px' }}>
-                  {t('NODES')}
-                </Typography>
-              }
-              <Stack alignItems='center' columnGap='10px' direction='row'>
-                <Radio
-                  checked={maybeNewEndpoint === value}
-                  columnGap='5px'
-                  label={name}
-                  onChange={onChangeEndpoint}
-                  value={value}
-                />
-                <DotIndicator delay={delay} />
-              </Stack>
-              <Grid item sx={{ mt: '-5px', pl: '10px' }}>
-                <Typography color='#674394' variant='B-5'>
-                  {value}
-                </Typography>
-              </Grid>
-            </Grid>
-          ))}
-          <FadeOnScroll containerRef={refContainer} height='50px' ratio={0.3} style={{ borderRadius: '14px', justifySelf: 'center', position: 'sticky', width: '100%' }} />
-        </Grid>
+        <Stack direction='column' sx={{ position: 'relative', width: '100%' }}>
+          <Grid container height='420px' item ref={refContainer} sx={{ bgcolor: '#1B133C', borderRadius: '14px', display: 'block', overflowY: 'scroll', position: 'relative' }}>
+            <MySwitch
+              checked={mayBeEnabled}
+              columnGap='8px'
+              label={t('Enable Network')}
+              onChange={onEnableNetwork}
+              style={{ alignItems: 'center', backgroundColor: '#05091C', borderRadius: '18px', height: '52px', justifyContent: 'flex-start', padding: '0 15px', width: '100%' }}
+              value={mayBeEnabled}
+            />
+            <MySwitch
+              checked={isAutoMode}
+              columnGap='8px'
+              label={t('Auto Node Selection')}
+              onChange={onChangeEndpoint}
+              style={{ alignItems: 'center', backgroundColor: '#05091C', borderRadius: '18px', height: '52px', justifyContent: 'flex-start', margin: '8px 0', padding: '0 15px', width: '100%' }}
+              value={AUTO_MODE.value}
+            />
+            {filteredEndpoints?.map(({ delay, name, value }, index) => (
+              <EndpointRow
+                checked={maybeNewEndpoint === value}
+                delay={delay}
+                isFirst={index === 0}
+                isLast={index === filteredEndpoints.length - 1}
+                key={index}
+                name={name}
+                onChangeEndpoint={onChangeEndpoint}
+                value ={value}
+              />
+            ))}
+          </Grid>
+          <FadeOnScroll containerRef={refContainer} height='50px' ratio={0.3} style={{ borderRadius: '14px', justifySelf: 'center', width: '100%' }} />
+        </Stack>
         <DecisionButtons
           cancelButton
           direction='vertical'
