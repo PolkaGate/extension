741afd9dd21707b076e2a3eb89406e643122363d
Nick <ekbatanifard@gmail.com>
2025-07-22 01:17:01 +0330

    style: adjust add style

--- packages/extension-polkagate/src/fullscreen/home/AccountsAdd.tsx
+++ packages/extension-polkagate/src/fullscreen/home/AccountsAdd.tsx
@@ -1,51 +1,55 @@
 // Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { Add } from '@mui/icons-material';
 import { Box, Stack, Typography } from '@mui/material';
-import { Add } from 'iconsax-react';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useTranslation } from '../../hooks';
 
-function AccountsAdd(): React.ReactElement {
+function AccountsAdd (): React.ReactElement {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const [hovered, setHovered] = useState(false);
 
   const onClick = useCallback(() => navigate('account/create'), [navigate]);
+  const toggleHover = useCallback(() => setHovered((pre) => !pre), []);
 
   return (
-    <Stack alignItems='center' direction='row' justifyContent='flex-start' sx={{ cursor: 'pointer', ml: '10px' }}>
+    <Stack alignItems='center' direction='row' justifyContent='flex-start' sx={{ ml: '10px' }}>
       <Typography color='#EAEBF1' sx={{ userSelect: 'none' }} textTransform='uppercase' variant='H-2'>
         {t('Accounts')}
       </Typography>
       <Box
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
         onClick={onClick}
+        onMouseEnter={(toggleHover)}
+        onMouseLeave={toggleHover}
         sx={{
-          background: 'linear-gradient(262.56deg, #6E00B1 0%, #DC45A0 45%, #6E00B1 100%)',
+          background: hovered ? '#6E00B1' : 'linear-gradient(262.56deg, #6E00B1 0%, #DC45A0 45%, #6E00B1 100%)',
           borderRadius: '50%',
+          cursor: 'pointer',
           display: 'inline-flex',
           marginLeft: '10px',
-          p: '2px'
+          p: '2px',
+          transition: 'all 250ms ease-out'
         }}
       >
         <Box
           sx={{
-            '&:hover': {
-              background: 'linear-gradient(262.56deg, #6E00B1 0%, #DC45A0 45%, #6E00B1 100%)',
-              transition: 'all 250ms ease-out'
-            },
             alignItems: 'center',
-            backgroundColor: '#1E1E1E',
+            background: hovered ? '#6E00B1' : 'linear-gradient(262.56deg, #6E00B1 0%, #DC45A0 45%, #6E00B1 100%)',
             borderRadius: '50%',
             color: '#FFFFFF',
             display: 'flex',
             height: '23px',
             justifyContent: 'center',
+            transition: 'all 250ms ease-out',
             width: '23px'
           }}
         >
-          <Add color='#FFFFFF' size='20' variant='Linear' />
+          <Add sx={{ color: '#FFFFFF', fontSize: 19, stroke: '#FFFFFF', strokeWidth: 1.1 }} />
         </Box>
       </Box>
     </Stack>
