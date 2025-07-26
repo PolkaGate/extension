1e59b5cb8fb2442b09a0603c51fad0a031a95a9c
Nick <ekbatanifard@gmail.com>
2025-07-26 15:21:31 +0330

    chore: remove comment

--- packages/extension/webpack.shared.cjs
+++ packages/extension/webpack.shared.cjs
@@ -3,7 +3,7 @@
 
 const path = require('path');
 
-require('dotenv').config({ path: path.resolve(__dirname, '../../', '.env') }); // ✅ Add this
+require('dotenv').config({ path: path.resolve(__dirname, '../../', '.env') });
 
 const webpack = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
