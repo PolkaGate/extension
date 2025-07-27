9df1a7b9b347bc035cd3ffb5053d691869444cfa
Nick <ekbatanifard@gmail.com>
2025-07-27 14:03:24 +0330

    chore: remove unused

--- packages/extension-ui/src/Popup/routes/importRoutes.ts
+++ packages/extension-ui/src/Popup/routes/importRoutes.ts
@@ -4,9 +4,6 @@
 import type { RouteConfig } from './RouteDefinitions';
 
 import AddWatchOnlyFullScreen from '@polkadot/extension-polkagate/src/popup/import/addWatchOnlyFullScreen';
-import AttachQR from '@polkadot/extension-polkagate/src/popup/import/attachQR';
-import AttachQrFullScreen from '@polkadot/extension-polkagate/src/popup/import/attachQrFullScreen';
-import ImportProxied from '@polkadot/extension-polkagate/src/popup/import/importProxied';
 import ImportProxiedFullScreen from '@polkadot/extension-polkagate/src/popup/import/importProxiedFullScreen';
 
 export const IMPORT_ROUTES: RouteConfig[] = [
@@ -14,21 +11,6 @@ export const IMPORT_ROUTES: RouteConfig[] = [
     Component: AddWatchOnlyFullScreen,
     path: '/import/add-watch-only-full-screen',
     trigger: 'import-add-watch-only-full-screen'
-  },
-  {
-    Component: AttachQR,
-    path: '/import/attach-qr',
-    trigger: 'attach-qr'
-  },
-  {
-    Component: AttachQrFullScreen,
-    path: '/import/attach-qr-full-screen',
-    trigger: 'attach-qr-full-screen'
-  },
-  {
-    Component: ImportProxied,
-    path: '/import/proxied',
-    trigger: 'import-proxied'
   },
   {
     Component: ImportProxiedFullScreen,
