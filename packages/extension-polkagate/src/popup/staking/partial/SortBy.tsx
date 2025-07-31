c45fd8ca6c16084475380a361fc71e1f18d66ef2
Nick <ekbatanifard@gmail.com>
2025-07-31 12:48:55 +0330

    style: correct sort by in fullscreen

--- packages/extension-polkagate/src/popup/staking/partial/SortBy.tsx
+++ packages/extension-polkagate/src/popup/staking/partial/SortBy.tsx
@@ -1,15 +1,13 @@
 // Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-/* eslint-disable react/jsx-first-prop-new-line */
-
 import CheckIcon from '@mui/icons-material/Check';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { ClickAwayListener, Container, Grid, Popover, styled, type SxProps, type Theme, Typography, useTheme } from '@mui/material';
 import { ArrowDown2 } from 'iconsax-react';
 import React, { useCallback, useRef } from 'react';
 
-import { useIsExtensionPopup, useIsHovered, useTranslation } from '../../../hooks';
+import { useIsBlueish, useIsExtensionPopup, useIsHovered, useTranslation } from '../../../hooks';
 
 // This code is used in both extension and fullscreen modes, so the UI design varies between the two.
 
@@ -38,6 +36,7 @@ interface TabProps {
 
 function Tab ({ label, setSortBy, sortBy }: TabProps): React.ReactElement {
   const { t } = useTranslation();
+  const isBlueish = useIsBlueish();
   const refContainer = useRef(null);
   const hovered = useIsHovered(refContainer);
 
@@ -47,12 +46,16 @@ function Tab ({ label, setSortBy, sortBy }: TabProps): React.ReactElement {
     setSortBy(label);
   }, [label, setSortBy]);
 
+  const textColor = isBlueish ? 'text.highlight' : 'text.primary';
+  const hoveredTextColor = isBlueish ? 'text.primary' : 'text.secondary';
+  const color = isBlueish ? '#3988FF' : '#FF4FB9';
+
   return (
-    <Container disableGutters onClick={onClick} ref={refContainer} sx={{ bgcolor: hovered ? '#222540A6' : 'transparent', borderRadius: '10px', cursor: 'pointer', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', p: '10px', width: '100%' }}>
-      <Typography color={isSelected ? '#3988FF' : hovered ? 'text.primary' : 'text.highlight'} variant='B-2'>
+    <Container disableGutters onClick={onClick} ref={refContainer} sx={{ alignItems: 'center', bgcolor: hovered ? '#222540A6' : 'transparent', borderRadius: '10px', cursor: 'pointer', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', p: '10px', width: '100%' }}>
+      <Typography color={isSelected ? color : hovered ? hoveredTextColor : textColor } variant='B-2'>
         {t(label)}
       </Typography>
-      <CheckIcon sx={{ bgcolor: '#3988FF', borderRadius: '999px', color: '#fff', fontSize: '16px', fontWeight: 900, height: '20px', transition: 'all 100ms ease-out', visibility: isSelected ? 'visible' : 'hidden', width: '20px' }} />
+      <CheckIcon sx={{ background: isBlueish ? color : 'linear-gradient(262.56deg, #6E00B1 0%, #DC45A0 45%, #6E00B1 100%)', borderRadius: '999px', color: '#fff', fontSize: '16px', fontWeight: 900, height: '18px', transition: 'all 100ms ease-out', visibility: isSelected ? 'visible' : 'hidden', width: '18px' }} />
     </Container>
   );
 }
@@ -68,6 +71,7 @@ interface DropContentProps {
 function DropContent ({ containerRef, open, setSortBy, sortBy, sortOptions }: DropContentProps) {
   const id = open ? 'dropContent-popover' : undefined;
   const anchorEl = open ? containerRef.current : null;
+  const isExtension = useIsExtensionPopup();
 
   return (
     <Popover
@@ -83,12 +87,13 @@ function DropContent ({ containerRef, open, setSortBy, sortBy, sortOptions }: Dr
           sx: {
             background: 'none',
             backgroundImage: 'none',
-            borderRadius: '12px'
+            borderRadius: '12px',
+            marginTop: isExtension ? 0 : '8px'
           }
         }
       }}
       transformOrigin={{
-        horizontal: 'right',
+        horizontal: isExtension ? 'right' : 'left',
         vertical: 'top'
       }}
     >
