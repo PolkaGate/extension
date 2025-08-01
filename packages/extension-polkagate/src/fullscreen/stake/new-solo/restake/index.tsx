21f732cffbf1a38b5c83582dc0cd0455f1d6f64d
Nick <ekbatanifard@gmail.com>
2025-08-01 13:40:17 +0330

    refactor: utilize StakeUnstake

--- packages/extension-polkagate/src/fullscreen/stake/new-solo/restake/index.tsx
+++ packages/extension-polkagate/src/fullscreen/stake/new-solo/restake/index.tsx
@@ -1,16 +1,10 @@
 // Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { Stack } from '@mui/material';
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 
-import { GradientButton } from '../../../../components/index';
-import { useChainInfo, useRestakeSolo, useTranslation } from '../../../../hooks';
-import FeeValue from '../../../../popup/staking/partial/FeeValue';
-import StakeAmountInput from '../../../../popup/staking/partial/StakeAmountInput';
-import TokenStakeStatus from '../../../../popup/staking/partial/TokenStakeStatus';
-import StakingPopup from '../../partials/StakingPopup';
-import { FULLSCREEN_STAKING_TX_FLOW, type FullScreenTransactionFlow } from '../../util/utils';
+import { useRestakeSolo, useTranslation } from '../../../../hooks';
+import StakeUnstake from '../unstake/StakeUnstake';
 
 interface Props {
   address: string | undefined;
@@ -20,7 +14,6 @@ interface Props {
 
 function Restake ({ address, genesisHash, onClose }: Props) {
   const { t } = useTranslation();
-  const { api, decimal, token } = useChainInfo(genesisHash);
 
   const { errorMessage,
     estimatedFee,
@@ -32,57 +25,23 @@ function Restake ({ address, genesisHash, onClose }: Props) {
     tx,
     unlockingAmount } = useRestakeSolo(address, genesisHash);
 
-  const [flowStep, setFlowStep] = useState<FullScreenTransactionFlow>(FULLSCREEN_STAKING_TX_FLOW.NONE);
-
-  const onNext = useCallback(() => setFlowStep(FULLSCREEN_STAKING_TX_FLOW.REVIEW), []);
-
   return (
-    <StakingPopup
+    <StakeUnstake
       address={address}
-      flowStep={flowStep}
+      amountLabel={t('Unstaking')}
+      balance={unlockingAmount}
+      errorMessage={errorMessage}
+      estimatedFee={estimatedFee}
       genesisHash={genesisHash}
       onClose={onClose}
-      setFlowStep={setFlowStep}
+      onInputChange={onInputChange}
+      onMaxValue={onMaxValue}
       setValue={setRebondValue}
       title={t('Restake')}
-      transaction={tx}
       transactionInformation={transactionInformation}
-    >
-      <Stack direction='column' justifyContent='space-between' sx={{ mt: '16px', position: 'relative', px: '15px', zIndex: 1 }}>
-        <TokenStakeStatus
-          amount={unlockingAmount}
-          decimal={decimal}
-          genesisHash={genesisHash}
-          style={{ mt: '8px' }}
-          text={t('Unstaking')}
-          token={token}
-        />
-        <StakeAmountInput
-          buttonsArray={[{
-            buttonName: t('Max'),
-            value: onMaxValue
-          }]}
-          decimal={decimal}
-          errorMessage={errorMessage}
-          focused
-          onInputChange={onInputChange}
-          style={{ mb: '18px', mt: '18px' }}
-          title={t('Amount') + ` (${token?.toUpperCase() ?? '--'})`}
-          titleInColor={` (${token?.toUpperCase() ?? '--'})`}
-        />
-        <FeeValue
-          decimal={decimal}
-          feeValue={estimatedFee}
-          token={token}
-        />
-        <GradientButton
-          disabled={!rebondValue || rebondValue.isZero() || !!errorMessage || !api}
-          onClick={onNext}
-          style={{ marginTop: '215px' }}
-          text={t('Next')}
-        />
-      </Stack>
-    </StakingPopup>
+      tx={tx}
+      value={rebondValue}
+    />
   );
 }
 
