ac62c9dac257a6533298db75cde4c7f07d0184df
Nick <ekbatanifard@gmail.com>
2025-08-02 14:09:53 +0330

    refactor: use new component in pools

--- packages/extension-polkagate/src/fullscreen/stake/new-pool/bondExtra/index.tsx
+++ packages/extension-polkagate/src/fullscreen/stake/new-pool/bondExtra/index.tsx
@@ -1,16 +1,10 @@
 // Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { Stack } from '@mui/material';
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 
-import { GradientButton } from '../../../../components';
-import { useBondExtraPool, useChainInfo, useTranslation } from '../../../../hooks';
-import FeeValue from '../../../../popup/staking/partial/FeeValue';
-import StakeAmountInput from '../../../../popup/staking/partial/StakeAmountInput';
-import TokenStakeStatus from '../../../../popup/staking/partial/TokenStakeStatus';
-import StakingPopup from '../../partials/StakingPopup';
-import { FULLSCREEN_STAKING_TX_FLOW, type FullScreenTransactionFlow } from '../../util/utils';
+import { useBondExtraPool, useTranslation } from '../../../../hooks';
+import StakeUnstake from '../../new-solo/unstake/StakeUnstake';
 
 interface Props {
   address: string | undefined;
@@ -20,9 +14,6 @@ interface Props {
 
 export default function BondExtra ({ address, genesisHash, onClose }: Props): React.ReactElement {
   const { t } = useTranslation();
-  const { api, decimal, token } = useChainInfo(genesisHash);
-
-  const [flowStep, setFlowStep] = useState<FullScreenTransactionFlow>(FULLSCREEN_STAKING_TX_FLOW.NONE);
 
   const { availableBalanceToStake,
     bondAmount,
@@ -34,54 +25,22 @@ export default function BondExtra ({ address, genesisHash, onClose }: Props): Re
     transactionInformation,
     tx } = useBondExtraPool(address, genesisHash);
 
-  const onNext = useCallback(() => setFlowStep?.(FULLSCREEN_STAKING_TX_FLOW.REVIEW), [setFlowStep]);
-
   return (
-    <StakingPopup
+    <StakeUnstake
       address={address}
-      flowStep={flowStep}
+      amountLabel={t('Available to stake')}
+      balance={availableBalanceToStake}
+      errorMessage={errorMessage}
+      estimatedFee={estimatedFee}
       genesisHash={genesisHash}
       onClose={onClose}
-      setFlowStep={setFlowStep}
+      onInputChange={onInputChange}
+      onMaxValue={onMaxValue}
       setValue={setBondAmount}
       title={t('Stake more')}
-      transaction={tx}
       transactionInformation={transactionInformation}
-    >
-      <Stack direction='column' justifyContent='space-between' sx={{ mt: '16px', position: 'relative', px: '15px', zIndex: 1 }}>
-        <TokenStakeStatus
-          amount={availableBalanceToStake}
-          decimal={decimal}
-          genesisHash={genesisHash}
-          style={{ mt: '8px' }}
-          text={t('Available to stake')}
-          token={token}
-        />
-        <StakeAmountInput
-          buttonsArray={[{
-            buttonName: t('Max'),
-            value: onMaxValue
-          }]}
-          decimal={decimal}
-          errorMessage={errorMessage}
-          focused
-          onInputChange={onInputChange}
-          style={{ mb: '18px', mt: '18px' }}
-          title={t('Amount') + ` (${token?.toUpperCase() ?? '--'})`}
-          titleInColor={` (${token?.toUpperCase() ?? '--'})`}
-        />
-        <FeeValue
-          decimal={decimal}
-          feeValue={estimatedFee}
-          token={token}
-        />
-        <GradientButton
-          disabled={!bondAmount || bondAmount.isZero() || !!errorMessage || !api}
-          onClick={onNext}
-          style={{ marginTop: '215px' }}
-          text={t('Next')}
-        />
-      </Stack>
-    </StakingPopup>
+      tx={tx}
+      value={bondAmount}
+    />
   );
 }
