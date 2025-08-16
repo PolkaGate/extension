fef175c72381a01393ecc39636fddf3927eb4366
Amir Ekbatanifard <amiref007@gmail.com>
2025-08-16 11:08:09 +0330

    feat: create EasyStakeReviewHeader.tsx

---
+++ packages/extension-polkagate/src/fullscreen/stake/easyStake/partials/EasyStakeReviewHeader.tsx
@@ -0,0 +1,36 @@
+// Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { Stack, Typography, useTheme } from '@mui/material';
+import React, { useMemo } from 'react';
+
+import { useTranslation } from '../../../../hooks';
+import { Amount } from '../../partials/StakingConfirmation';
+
+interface Props {
+  amount: string | undefined;
+  genesisHash: string | undefined;
+  token: string | undefined;
+  isExtension?: boolean;
+}
+
+export default function EasyStakeReviewHeader ({ amount, genesisHash, isExtension, token }: Props) {
+  const { t } = useTranslation();
+  const theme = useTheme();
+
+  const color = useMemo(() => isExtension ? theme.palette.text.highlight : '#AA83DC', [isExtension, theme.palette.text.highlight]);
+
+  return (
+    <Stack sx={{ alignItems: 'center', bgcolor: isExtension ? '#110F2A' : 'transparent', borderRadius: '14px', py: isExtension ? '20px' : 0 }}>
+      <Typography color={color} variant='B-2'>
+        {t('Start Staking')}
+      </Typography>
+      <Amount
+        amount={amount}
+        differentValueColor={color}
+        genesisHash={genesisHash}
+        token={token}
+      />
+    </Stack>
+  );
+}
