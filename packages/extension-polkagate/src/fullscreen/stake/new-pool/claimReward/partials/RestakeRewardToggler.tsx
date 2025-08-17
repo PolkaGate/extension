4c19486e1df5a9ef5614721c385823281d471d80
Nick <ekbatanifard@gmail.com>
2025-08-18 03:21:17 +0330

    style: apply is blueish to restake rewards section in extension mode

--- packages/extension-polkagate/src/fullscreen/stake/new-pool/claimReward/partials/RestakeRewardToggler.tsx
+++ packages/extension-polkagate/src/fullscreen/stake/new-pool/claimReward/partials/RestakeRewardToggler.tsx
@@ -7,7 +7,7 @@ import React, { useCallback } from 'react';
 
 import { noop } from '@polkadot/util';
 
-import { GradientSwitch } from '../../../../../components';
+import { MySwitch } from '../../../../../components';
 import { useIsExtensionPopup, useTranslation } from '../../../../../hooks';
 
 export interface RestakeRewardTogglerProps {
@@ -20,22 +20,23 @@ export default function RestakeRewardToggler ({ restake, setRestake }: RestakeRe
   const theme = useTheme();
   const isExtension = useIsExtensionPopup();
 
-  const bluishColor = isExtension ? theme.palette.text.highlight : '#AA83DC';
+  const enabledColor = isExtension ? theme.palette.text.highlight : '#AA83DC';
+  const disableColor = isExtension ? '#809acb8c' : '#674394';
 
   const toggler = useCallback(() => setRestake((isChecked) => !isChecked), [setRestake]);
 
   return (
-    <Container disableGutters onClick={toggler} sx={{ alignItems: 'center', bgcolor: isExtension ? '#110F2A' : '#05091C', borderRadius: '14px', cursor: 'pointer', display: 'flex', flexDirection: 'row', gap: '12px', m: 0, mt: '8px', p: '14px 18px' }}>
-      <MagicStar color={restake ? bluishColor : '#674394'} size='24' variant='Bold' />
+    <Container disableGutters onClick={toggler} sx={{ alignItems: 'center', bgcolor: isExtension ? '#110F2A' : '#05091C', borderRadius: '14px', cursor: 'pointer', display: 'flex', flexDirection: 'row', gap: '12px', m: 0, mt: '8px', p: isExtension ? '14px 3px' : '14px 18px' }}>
+      <MagicStar color={restake ? enabledColor : disableColor} size='24' variant='Bold' />
       <Stack direction='column' sx={{ alignItems: 'flex-start', ml: 0, mr: 'auto', width: 'fit-content' }}>
-        <Typography color={restake ? 'text.primary' : bluishColor} variant='B-3'>
+        <Typography color={restake ? 'text.primary' : enabledColor} variant='B-3'>
           {t('Restake rewards')}
         </Typography>
-        <Typography color={restake ? bluishColor : '#674394'} variant='B-1'>
+        <Typography color={restake ? enabledColor : disableColor} variant='B-1'>
           {t('Your tokens will return to stake')}
         </Typography>
       </Stack>
-      <GradientSwitch
+      <MySwitch
         checked={restake}
         onChange={noop}
       />
