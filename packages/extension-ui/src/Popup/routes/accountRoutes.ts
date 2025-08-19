20bfb6edf687bd95c4b2c34f8983105bb0840aad
Amir Ekbatanifard <amiref007@gmail.com>
2025-08-19 15:01:58 +0330

    chore: remove unused part1

--- packages/extension-ui/src/Popup/routes/accountRoutes.ts
+++ packages/extension-ui/src/Popup/routes/accountRoutes.ts
@@ -6,14 +6,12 @@ import type { RouteConfig } from './RouteDefinitions';
 import AccountFS from '@polkadot/extension-polkagate/src/fullscreen/accountDetails';
 import HaveWallet from '@polkadot/extension-polkagate/src/fullscreen/haveWallet';
 import Export from '@polkadot/extension-polkagate/src/popup/export/Export';
-import ExportAll from '@polkadot/extension-polkagate/src/popup/export/ExportAll';
 import ImportLedger from '@polkadot/extension-polkagate/src/popup/import/importLedger';
 import ImportRawSeed from '@polkadot/extension-polkagate/src/popup/import/importRawSeedFullScreen';
 import ImportSeed from '@polkadot/extension-polkagate/src/popup/import/importSeedFullScreen';
 import RestoreJson from '@polkadot/extension-polkagate/src/popup/import/restoreJSONFullScreen';
 import CreateAccount from '@polkadot/extension-polkagate/src/popup/newAccount/createAccountFullScreen';
 import Receive from '@polkadot/extension-polkagate/src/popup/receive';
-import Rename from '@polkadot/extension-polkagate/src/popup/rename';
 
 export const ACCOUNT_ROUTES: RouteConfig[] = [
   {
@@ -25,11 +23,6 @@ export const ACCOUNT_ROUTES: RouteConfig[] = [
     Component: HaveWallet,
     path: '/account/have-wallet',
     trigger: 'account-creation'
-  },
-  {
-    Component: ExportAll,
-    path: '/account/export-all',
-    trigger: 'export-all-address'
   },
   {
     Component: ImportLedger,
@@ -60,11 +53,6 @@ export const ACCOUNT_ROUTES: RouteConfig[] = [
     Component: Export,
     path: '/export/:address',
     trigger: 'export-address'
-  },
-  {
-    Component: Rename,
-    path: '/rename/:address',
-    trigger: 'rename'
   },
   {
     Component: Receive,
