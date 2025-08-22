a54c72025ec648749153c57ee4babffc6e17fb63
Nick <ekbatanifard@gmail.com>
2025-08-22 14:52:23 +0400

    chore: ignore ts check

--- packages/extension-polkagate/src/fullscreen/governance/util.tsx
+++ packages/extension-polkagate/src/fullscreen/governance/util.tsx
@@ -1,6 +1,7 @@
 // Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+//@ts-nocheck
 import type { ApiPromise } from '@polkadot/api';
 // @ts-ignore
 import type { PalletConvictionVotingVoteVoting } from '@polkadot/types/lookup';
