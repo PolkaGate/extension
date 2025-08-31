1df78958829c5814858700bcf01005a65f133b02
Nick <ekbatanifard@gmail.com>
2025-08-31 18:38:44 +0400

    feat: add update metadata design

--- packages/extension-polkagate/src/popup/metadata/Request.tsx
+++ packages/extension-polkagate/src/popup/metadata/Request.tsx
@@ -1,13 +1,15 @@
 // Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-
 import type { MetadataDef } from '@polkadot/extension-inject/types';
 
-import { Grid, Typography, useTheme } from '@mui/material';
+import { Box, Grid, Stack, Typography } from '@mui/material';
 import React, { useCallback, useContext } from 'react';
 
-import { ActionContext, TwoButtons, Warning } from '../../components';
+import { toTitleCase } from '@polkadot/extension-polkagate/src/util';
+import { toShortAddress } from '@polkadot/extension-polkagate/src/util/utils';
+
+import { ActionContext, DecisionButtons } from '../../components';
 import { useMetadata, useTranslation } from '../../hooks';
 import { approveMetaRequest, rejectMetaRequest } from '../../messaging';
 
@@ -17,8 +19,23 @@ interface Props {
   url: string;
 }
 
-export default function Request({ metaId, request, url }: Props): React.ReactElement<Props> {
-  const theme = useTheme();
+function ItemValue ({ item, value }: { item: string, value: string }): React.ReactElement<Props> {
+  return (
+    <>
+      <Grid alignItems='center' container item justifyContent='space-between' sx={{ minHeight: '36px', px: '8px' }}>
+        <Typography color= '#AA83DC' variant='B-4'>
+          {toTitleCase(item)}
+        </Typography>
+        <Typography color='#EAEBF1' sx={{ mr: '3px' }} variant='B-4'>
+          {value}
+        </Typography>
+      </Grid>
+      <Box sx={{ background: 'linear-gradient(90deg, rgba(210, 185, 241, 0.03) 0%, rgba(210, 185, 241, 0.15) 50.06%, rgba(210, 185, 241, 0.03) 100%)', height: '1px', width: '345px' }} />
+    </>
+  );
+}
+
+export default function Request ({ metaId, request, url }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
 
   const chain = useMetadata(request.genesisHash, true);
@@ -45,76 +62,38 @@ export default function Request({ metaId, request, url }: Props): React.ReactEle
   const firstSlash = url.indexOf('/');
   const secondSlash = url.indexOf('/', firstSlash + 1);
   const thirdSlash = url.indexOf('/', secondSlash + 1);
-  const final = url.substring(0, thirdSlash);
+  const from = url.substring(0, thirdSlash);
 
   return (
     <>
-      <Grid container fontSize='16px' sx={{ '> div:last-child': { border: 'none' }, bgcolor: 'background.paper', border: '1px solid', borderColor: 'secondary.light', borderRadius: '5px', m: '15px auto', width: '92%' }}>
-        <Grid alignItems='center' container item sx={{ borderBottom: '1px solid', borderBottomColor: 'secondary.light', minHeight: '36px', px: '8px' }}>
-          <Typography fontWeight={300} width='35%'>
-            {t('from')}
+      <Grid container fontSize='16px' sx={{ m: '0 15px auto', width: '92%', zIndex: 100 }}>
+        <Stack direction='row' justifyContent='space-between' sx={{ m: '10px 0 10px', p: '0 15px 0 5px', width: '100%' }}>
+          <Typography color='#7956A5' sx={{ fontWeight: 600, textTransform: 'uppercase' }} variant='B-5'>
+            {t('item')}
           </Typography>
-          <Typography fontWeight={400} textAlign='right' width='65%'>
-            {final}
+          <Typography color='#7956A5' sx={{ fontWeight: 600, textTransform: 'uppercase' }} variant='B-5'>
+            {t('value')}
           </Typography>
-        </Grid>
-        <Grid alignItems='center' container item sx={{ borderBottom: '1px solid', borderBottomColor: 'secondary.light', minHeight: '36px', px: '8px' }}>
-          <Typography fontWeight={300} width='35%'>
-            {t('chain')}
-          </Typography>
-          <Typography fontWeight={400} textAlign='right' width='65%'>
-            {request.chain}
-          </Typography>
-        </Grid>
-        <Grid alignItems='center' container item sx={{ borderBottom: '1px solid', borderBottomColor: 'secondary.light', minHeight: '36px', px: '8px' }}>
-          <Typography fontWeight={300} width='35%'>
-            {t('icon')}
-          </Typography>
-          <Typography fontWeight={400} textAlign='right' width='65%'>
-            {request.icon}
-          </Typography>
-        </Grid>
-        <Grid alignItems='center' container item sx={{ borderBottom: '1px solid', borderBottomColor: 'secondary.light', minHeight: '36px', px: '8px' }}>
-          <Typography fontWeight={300} width='35%'>
-            {t('decimals')}
-          </Typography>
-          <Typography fontWeight={400} textAlign='right' width='65%'>
-            {request.tokenDecimals}
-          </Typography>
-        </Grid>
-        <Grid alignItems='center' container item sx={{ borderBottom: '1px solid', borderBottomColor: 'secondary.light', minHeight: '36px', px: '8px' }}>
-          <Typography fontWeight={300} width='35%'>
-            {t('symbol')}
-          </Typography>
-          <Typography fontWeight={400} textAlign='right' width='65%'>
-            {request.tokenSymbol}
-          </Typography>
-        </Grid>
-        <Grid alignItems='center' container item sx={{ borderBottom: '1px solid', borderBottomColor: 'secondary.light', minHeight: '36px', px: '8px' }}>
-          <Typography fontWeight={300} width='35%'>
-            {t('upgrade')}
-          </Typography>
-          <Typography fontWeight={400} textAlign='right' width='65%'>
-            {chain ? chain.specVersion : t('<unknown>')} -&gt; {request.specVersion}
-          </Typography>
-        </Grid>
+        </Stack>
+        <ItemValue item={t('from')} value={from} />
+        <ItemValue item={t('chain')} value={request.chain} />
+        <ItemValue item={t('genesis hash')} value={toShortAddress(request.genesisHash, 6)} />
+        <ItemValue item={t('icon')} value={request.icon} />
+        <ItemValue item={t('decimals')} value={String(request.tokenDecimals)} />
+        <ItemValue item={t('symbol')} value={request.tokenSymbol} />
+        <ItemValue item={t('upgrade')} value={`${chain ? chain.specVersion : t('<unknown>')} => ${request.specVersion}`} />
       </Grid>
-      <Grid height='80px' m='auto' width='96%'>
-        <Warning
-          fontWeight={400}
-          iconDanger
-          isBelowInput
-          theme={theme}
-        >
-          {t('This approval will add the metadata to your extension instance, allowing future requests to be decoded using this metadata.')}
-        </Warning>
-      </Grid>
-      <TwoButtons
+      <Typography color='#BEAAD8' sx={{ display: 'flex', p: ' 30px 10px 10px', textAlign: 'center', width: '100%' }} variant='B-4'>
+        {t('This approval adds the metadata to your extension so future requests can be decoded with it')}
+      </Typography>
+      <DecisionButtons
+        cancelButton
+        direction='horizontal'
         onPrimaryClick={onApprove}
         onSecondaryClick={onReject}
         primaryBtnText={t('Approve')}
         secondaryBtnText={t('Reject')}
-        variant='text'
+        style={{ marginTop: '25px', width: '92%' }}
       />
     </>
   );
