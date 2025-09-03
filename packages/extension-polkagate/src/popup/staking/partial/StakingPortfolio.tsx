146aca1472fa2fbce020537e01a3d1d08cedd699
Nick <ekbatanifard@gmail.com>
2025-09-03 17:52:28 +0400

    chore: increase staked amount decimal point to 4

--- packages/extension-polkagate/src/popup/staking/partial/StakingPortfolio.tsx
+++ packages/extension-polkagate/src/popup/staking/partial/StakingPortfolio.tsx
@@ -185,6 +185,7 @@ export default function StakingPortfolio ({ buttons = [], disabled, genesisHash,
       <FormatBalance3
         balanceProps={{
           decimal,
+          decimalPoint: 4,
           token,
           value: staked
         }}
