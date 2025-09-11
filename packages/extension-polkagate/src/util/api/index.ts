e4469f6718cca8b200f9c2ce5a24ab6b7a6b75a0
Nick <ekbatanifard@gmail.com>
2025-09-11 17:23:59 +0330

    chore: remove unused

--- packages/extension-polkagate/src/util/api/index.ts
+++ packages/extension-polkagate/src/util/api/index.ts
@@ -1,11 +1,7 @@
 // Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-/* eslint-disable header/header */
-
-export { default as broadcast } from './broadcast';
 export * from './getNominationPoolsClaimedRewards';
 export { default as getPrices } from './getPrices';
 export { default as postData } from './postData';
 export * from './signAndSend';
-export * from './staking';
