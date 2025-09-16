64354fac73070857ddfba39a3b1e5f481bb61707
Amir Ekbatanifard <amiref007@gmail.com>
2025-09-16 13:28:30 +0330

    fix: replace back button with 'Done' in extension mode on receive page

--- packages/extension-polkagate/src/popup/receive/Receive.tsx
+++ packages/extension-polkagate/src/popup/receive/Receive.tsx
@@ -222,7 +222,7 @@ function QrCode ({ address, onBackToAccount, selectedChain, setSelectedChain }: 
           height: '44px',
           width: '320px'
         }}
-        text={t('Back')}
+        text={t('Done')}
       />
     </Grid>
   );
