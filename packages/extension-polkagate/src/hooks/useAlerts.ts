13b72e4f72e1bfc09bd762f51264946df21dfb67
Nick <ekbatanifard@gmail.com>
2025-09-23 12:59:54 +0330

    chore: add sound effect to alerts

--- packages/extension-polkagate/src/hooks/useAlerts.ts
+++ packages/extension-polkagate/src/hooks/useAlerts.ts
@@ -5,12 +5,15 @@ import type { Severity } from '../util/types';
 
 import { Chance } from 'chance';
 import { useCallback, useContext, useMemo } from 'react';
+import useSound from 'use-sound';
 
+import { soft } from '../assets/sounds';
 import { AlertContext } from '../components';
 import { TIME_TO_REMOVE_ALERT } from '../util/constants';
 
 export default function useAlerts () {
   const { alerts, setAlerts } = useContext(AlertContext);
+  const [play] = useSound(soft, { volume: 0.4 });
 
   const random = useMemo(() => new Chance(), []);
 
@@ -21,11 +24,12 @@ export default function useAlerts () {
   const notify = useCallback((text: string, severity?: Severity) => {
     const id = random.string({ length: 10 });
 
+    play();
     setAlerts((prev) => [...prev, { id, severity: severity || 'info', text }]);
     const timeout = setTimeout(() => removeAlert(id), TIME_TO_REMOVE_ALERT);
 
     return () => clearTimeout(timeout);
-  }, [random, removeAlert, setAlerts]);
+  }, [play, random, removeAlert, setAlerts]);
 
   return { alerts, notify, removeAlert };
 }
