c4929d0a7d297a4f250aff83bb94f14e819bb994
Nick <ekbatanifard@gmail.com>
2025-11-08 15:47:56 +0330

    chore: update chain info to display 0 for staking tokens with zero balance

--- packages/extension-polkagate/src/util/getChain.ts
+++ packages/extension-polkagate/src/util/getChain.ts
@@ -1,27 +1,44 @@
 // Copyright 2019-2025 @polkadot/extension-polkagate authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-// @ts-nocheck
-
-import type { Chain } from '@polkadot/extension-chains/types';
+import type { Icon } from '@polkadot/networks/types';
+import type { HexString } from '@polkadot/util/types';
 
 import allChains from './chains';
+
+interface ChainInfo {
+  chain: string;
+  genesisHash: HexString;
+  decimal: number;
+  icon: Icon;
+  name: string;
+  ss58Format: number;
+  token: string;
+}
 
 /**
  * @description get a chain by its genesisHash
  * @param _genesisHash
  * @returns sanitized chain
  */
-export default function getChain (_genesisHash: string | undefined): Chain | undefined {
+export default function getChain (_genesisHash: string | undefined): ChainInfo | undefined {
   if (!_genesisHash) {
     return undefined;
   }
 
   const found = allChains.find(({ genesisHash }) => genesisHash === _genesisHash);
 
-  if (found) {
-    found.name = found.chain;
+  if (!found) {
+    return undefined;
   }
 
-  return found;
+  const { chain, tokenDecimal, tokenSymbol, ...rest } = found;
+
+  return {
+    ...rest,
+    chain,
+    decimal: tokenDecimal,
+    name: chain,
+    token: tokenSymbol
+  };
 }
