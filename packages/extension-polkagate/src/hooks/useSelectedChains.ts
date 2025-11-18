35aed7447396dd3f2ed2aad1d518ec4d9472f728
Nick <ekbatanifard@gmail.com>
2025-11-18 11:58:37 +0330

    chore: remove log

--- packages/extension-polkagate/src/hooks/useSelectedChains.ts
+++ packages/extension-polkagate/src/hooks/useSelectedChains.ts
@@ -12,18 +12,16 @@ import { useIsTestnetEnabled } from '.';
  * @description get the selected chains
  * @returns a list of selected chains genesis hashes
  */
-export default function useSelectedChains(): string[] | undefined {
+export default function useSelectedChains (): string[] | undefined {
   const isTestnetEnabled = useIsTestnetEnabled();
 
   const [selected, setSelected] = useState<string[] | undefined>();
   const defaultSelectedGenesisHashes = useMemo(() => DEFAULT_SELECTED_CHAINS.map(({ value }) => value as string), []);
 
   useEffect(() => {
-    const filteredDefault = !isTestnetEnabled
-      ? defaultSelectedGenesisHashes.filter((genesis) => !TEST_NETS.includes(genesis))
-      : defaultSelectedGenesisHashes;
-
-    console.log('filteredDefault:', filteredDefault);
+    const filteredDefault = isTestnetEnabled
+      ? defaultSelectedGenesisHashes
+      : defaultSelectedGenesisHashes.filter((genesis) => !TEST_NETS.includes(genesis));
 
     const unsubscribe = getAndWatchStorage(STORAGE_KEY.SELECTED_CHAINS, setSelected, false, filteredDefault);
 
