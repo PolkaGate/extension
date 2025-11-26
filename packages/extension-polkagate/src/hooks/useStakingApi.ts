625fdfea07e399c1d961d010dbe77b783053b86e
G. Kami Ekbatanifard <46442452+Nick-1979@users.noreply.github.com>
2025-11-26 10:46:50 +0330

    fix: resolve solo staking settings issue where selecting “Others” disables the next step (#2060)

--- packages/extension-polkagate/src/hooks/useStakingApi.ts
+++ packages/extension-polkagate/src/hooks/useStakingApi.ts
@@ -8,7 +8,7 @@ import type { Forcing } from '@polkadot/types/interfaces';
 import type { PalletNominationPoolsPoolState } from '@polkadot/types/lookup';
 import type { ExpandedRewards } from '../fullscreen/stake/type';
 import type { Content } from '../partials/Review';
-import type { MyPoolInfo, PoolInfo, RewardDestinationType } from '../util/types';
+import type { MyPoolInfo, PoolInfo } from '../util/types';
 
 import { People, UserOctagon } from 'iconsax-react';
 import { useCallback, useEffect, useMemo, useReducer, useState } from 'react';
@@ -831,112 +831,6 @@ export const useBondExtraPool = (
     onInputChange,
     onMaxValue,
     setBondAmount,
-    transactionInformation,
-    tx
-  };
-};
-
-export const useSoloSettings = (
-  address: string | undefined,
-  genesisHash: string | undefined
-) => {
-  const { t } = useTranslation();
-
-  const stakingInfo = useSoloStakingInfo(address, genesisHash);
-  const { api } = useChainInfo(genesisHash);
-  const formatted = useFormatted(address, genesisHash);
-
-  const setPayee = api?.tx['staking']['setPayee'];
-
-  const stashId = stakingInfo.stakingAccount?.stashId.toString() ?? formatted ?? address;
-  const rewardDestinationAddress = stakingInfo.rewardDestinationAddress;
-  const ED = stakingInfo.stakingConsts?.existentialDeposit;
-
-  const [rewardDestinationType, setRewardDestinationType] = useState<RewardDestinationType>(undefined);
-  const [specificAccount, setSpecificAccount] = useState<string | undefined>(undefined);
-
-  const rewardType = useMemo(() => {
-    if (!stakingInfo.stakingAccount) {
-      return undefined;
-    }
-
-    // initialize settings
-    const parsedStakingAccount = stakingInfo.stakingAccount;
-
-    /** in Westend it is null recently if user has not staked yet */
-    if (!parsedStakingAccount.rewardDestination) {
-      return undefined;
-    }
-
-    const destinationType = Object.keys(parsedStakingAccount.rewardDestination)[0];
-
-    if (destinationType.toLowerCase() === 'staked') {
-      return 'Staked';
-    } else {
-      return 'Others';
-    }
-  }, [stakingInfo.stakingAccount]);
-
-  useEffect(() => {
-    if (!stakingInfo.stakingAccount) {
-      return;
-    }
-
-    setRewardDestinationType(rewardType);
-  }, [rewardType, stakingInfo.stakingAccount]);
-
-  const makePayee = useCallback((value: RewardDestinationType, account: string | undefined) => {
-    if (!value) {
-      return;
-    }
-
-    if (value === 'Staked') {
-      return 'Staked';
-    }
-
-    if (account === stashId) {
-      return 'Stash';
-    }
-
-    if (account) {
-      return { Account: account };
-    }
-
-    return undefined;
-  }, [stashId]);
-
-  const rewardDestination = useMemo(() => makePayee(rewardDestinationType, specificAccount ?? rewardDestinationAddress ?? stashId), [makePayee, rewardDestinationAddress, rewardDestinationType, specificAccount, stashId]);
-
-  const estimatedFee = useEstimatedFee(genesisHash, formatted, setPayee, [rewardDestination ?? 'Staked']);
-  const changeToStake = useMemo(() => rewardType === 'Others' && rewardDestinationType === 'Staked', [rewardDestinationType, rewardType]);
-  const nextDisabled = useMemo(() => rewardDestinationType === 'Others' && (rewardDestinationAddress === specificAccount || !specificAccount), [rewardDestinationAddress, rewardDestinationType, specificAccount]);
-
-  const transactionInformation: Content[] = useMemo(() => {
-    return [{
-      content: rewardDestinationType === 'Others' ? (specificAccount ?? rewardDestinationAddress ?? stashId) : 'Staked',
-      title: t('Reward destination')
-    },
-    {
-      content: estimatedFee,
-      itemKey: 'fee',
-      title: t('Fee')
-    }];
-  }, [estimatedFee, rewardDestinationAddress, rewardDestinationType, specificAccount, stashId, t]);
-  const tx = useMemo(() => {
-    return rewardDestination && setPayee
-      ? setPayee(rewardDestination)
-      : undefined;
-  }, [rewardDestination, setPayee]);
-
-  return {
-    ED,
-    changeToStake,
-    nextDisabled,
-    rewardDestinationAddress,
-    rewardDestinationType,
-    setRewardDestinationType,
-    setSpecificAccount,
-    specificAccount,
     transactionInformation,
     tx
   };
