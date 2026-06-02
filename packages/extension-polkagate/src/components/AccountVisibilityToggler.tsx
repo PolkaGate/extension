8f02f6dfb245a6ae5b3c03b169c78241cb36010b
Nick <ekbatanifard@gmail.com>
2026-06-02 13:00:30 +0330

    style: update invisible account red line for light mode

--- packages/extension-polkagate/src/components/AccountVisibilityToggler.tsx
+++ packages/extension-polkagate/src/components/AccountVisibilityToggler.tsx
@@ -23,6 +23,10 @@ function AccountVisibilityToggler({ size = '24', style = {} }: Props): React.Rea
   const hovered = useIsHovered(ref);
   const hoverBg = isDark ? '#674394' : '#E9ECFB';
   const activeBg = isDark ? '#AA83DC26' : '#FFFFFF';
+  const hiddenSlashOpacity = account?.isHidden ? 1 : 0;
+  const hiddenSlashColor = isDark ? '#FF4FB9' : '#E6007A';
+  const hiddenSlashHeight = isDark ? '1.5px' : '2.25px';
+  const hiddenSlashWidth = isDark ? '28px' : '30px';
   const iconColor = !account?.isHidden && hovered
     ? (isDark ? '#EAEBF1' : '#4F4779')
     : theme.palette.accent.icon;
@@ -58,7 +62,8 @@ function AccountVisibilityToggler({ size = '24', style = {} }: Props): React.Rea
     >
       <Grid container item onClick={toggleVisibility} ref={ref} sx={containerStyle}>
         <Radar2 color={iconColor} size={size} />
-        <Divider sx={{ bgcolor: '#FF4FB9', height: '1.5px', opacity: account?.isHidden ? 1 : 0, position: 'absolute', rotate: '-45deg', transition: 'all 150ms ease-out', width: '28px' }} />
+        <Divider sx={{ bgcolor: '#FFFFFF', borderRadius: '999px', height: '4px', opacity: isDark ? 0 : hiddenSlashOpacity, position: 'absolute', rotate: '-45deg', transition: 'all 150ms ease-out', width: '30px' }} />
+        <Divider sx={{ bgcolor: hiddenSlashColor, borderRadius: '999px', height: hiddenSlashHeight, opacity: hiddenSlashOpacity, position: 'absolute', rotate: '-45deg', transition: 'all 150ms ease-out', width: hiddenSlashWidth }} />
       </Grid>
     </MyTooltip>
   );
